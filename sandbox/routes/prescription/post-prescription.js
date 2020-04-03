7ae22f4076ef0173046edc52d045496ab33d0b53
Matt Jarvis <matt.jarvis@gmail.com>
2020-04-03 10:26:37 +0100

    AEA-50: Added validation from the spike.

--- sandbox/routes/prescription/post-prescription.js
+++ sandbox/routes/prescription/post-prescription.js
@@ -1,6 +1,44 @@
 const Boom = require('boom')
 const examples = require('../../services/examples')
 //const fhirHelper = require('../../helpers/fhir-helper')
+const requestValidator = require("../../validators/request-validator")
+
+function verifyBundleEntryContents(request) {
+  if (!requestValidator.verifyBundleEntryContainsMedicationRequest(request)) {
+    throw Boom.badRequest(
+      "Request must contain at least one 'MedicationRequest' entry",
+      {operationOutcomeCode: "value", apiErrorCode: "MISSING_FIELD"}
+    )
+  }
+
+  if (!requestValidator.verifyBundleEntryContainsOnePatient(request)) {
+    throw Boom.badRequest(
+      "Request must contain one 'Patient' entry",
+      {operationOutcomeCode: "value", apiErrorCode: "MISSING_FIELD"}
+    )
+  }
+
+  if (!requestValidator.verifyBundleEntryContainsPractitioner(request)) {
+    throw Boom.badRequest(
+      "Request must contain at least one 'Practitioner' entry",
+      {operationOutcomeCode: "value", apiErrorCode: "MISSING_FIELD"}
+    )
+  }
+
+  if (!requestValidator.verifyBundleEntryContainsOneEncounter(request)) {
+    throw Boom.badRequest(
+      "Request must contain one 'Encounter' entry",
+      {operationOutcomeCode: "value", apiErrorCode: "MISSING_FIELD"}
+    )
+  }
+
+  if (!requestValidator.verifyBundleEntryContainsTwoOrganizations(request)) {
+    throw Boom.badRequest(
+      "Request must contain two 'Organization' entries",
+      {operationOutcomeCode: "value", apiErrorCode: "MISSING_FIELD"}
+    )
+  }
+}
 
 module.exports = [
   /*
@@ -10,16 +48,27 @@ module.exports = [
     method: 'POST',
     path: '/Prescription',
     handler: (request, h) => {
-      //TODO check payload and id present
-      //TODO validate more fields
-      const prescription = JSON.parse(request.payload)
-      if (prescription.id === examples.prescriptionPostSuccessRequest.id) {
+      if (!requestValidator.verifyResourceTypeIsBundle(request)) {
+        throw Boom.badRequest(
+          "ResourceType must be 'Bundle' on request",
+          {operationOutcomeCode: "value", apiErrorCode: "INCORRECT_RESOURCETYPE"}
+        )
+      }
+
+      if (!requestValidator.verifyBundleContainsEntries(request)) {
+        throw Boom.badRequest(
+          "ResourceType Bundle must contain 'entry' field",
+          {operationOutcomeCode: "value", apiErrorCode: "MISSING_FIELD"}
+        )
+      }
+
+      verifyBundleEntryContents(request)
+
+      if (request.payload.id === examples.prescriptionPostSuccessRequest.id) {
         //TODO add meta to the response schema and use fhirHelper
         //return fhirHelper.createFhirResponse(h, examples.prescriptionPostSuccessResponse)
         return h.response(examples.prescriptionPostSuccessResponse)
       } else {
-        console.log(prescription.id)
-        console.log(examples.prescriptionPostSuccessRequest.id)
         throw Boom.badRequest("Unsupported prescription id", {operationOutcomeCode: "value", apiErrorCode: "unsupportedPrescriptionId"})
       }
     }
--- sandbox/validators/request-validator.js
+++ sandbox/validators/request-validator.js
@@ -1,17 +1,29 @@
 module.exports = {
-  validateIfMatchParameter: function (request) {
-    return request.headers["if-match"] || (request.headers["if-match"].startsWith('W/"') && request.headers["if-match"].endsWith('"'))
+  verifyResourceTypeIsBundle: function(request) {
+    return request.payload && request.payload["resourceType"] && request.payload["resourceType"] === "Bundle"
   },
 
-  validateIfMatchHeaderIsCorrectVersion: function (request, examplePatient) {
-    return request.headers["if-match"].slice(3, -1) == examplePatient.meta.versionId
+  verifyBundleContainsEntries: function(request) {
+    return request.payload && request.payload["entry"]
   },
 
-  validateContentTypeHeader: function (request) {
-    return request.headers["content-type"] || request.headers["content-type"].toLowerCase() === "application/json-patch+json"
+  verifyBundleEntryContainsMedicationRequest: function(request) {
+    return request.payload["entry"].filter(entry => entry["resourceType"] === "MedicationRequest").length !== 0
   },
 
-  verifyPatchObjectHasBeenSubmitted: function (request) {
-    return request.payload || request.payload.patches || request.payload.patches.length !== 0
+  verifyBundleEntryContainsOnePatient: function(request) {
+    return request.payload["entry"].filter(entry => entry["resourceType"] === "Patient").length === 1
+  },
+
+  verifyBundleEntryContainsPractitioner: function(request) {
+    return request.payload["entry"].filter(entry => entry["resourceType"] === "Practitioner").length !== 0
+  },
+
+  verifyBundleEntryContainsOneEncounter: function(request) {
+    return request.payload["entry"].filter(entry => entry["resourceType"] === "Encounter").length === 1
+  },
+
+  verifyBundleEntryContainsTwoOrganizations: function(request) {
+    return request.payload["entry"].filter(entry => entry["resourceType"] === "Organization").length === 2
   }
 }
