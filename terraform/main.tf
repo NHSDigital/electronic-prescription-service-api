ae38ad01566988cd73278633d5476cf15f727364
Matt Jarvis <matt.jarvis@gmail.com>
2020-05-27 15:27:04 +0100

    AEA-191: Update terraform files to match the PDS repo.

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,22 +1,25 @@
 provider "apigee" {
-  org          = var.apigee_organization
-  access_token = var.apigee_token
+    org = var.apigee_organization
+    access_token = var.apigee_token
 }
 
 terraform {
-  backend "azurerm" {}
+    backend "azurerm" {}
 
-  required_providers {
-    apigee = "~> 0.0"
-    archive = "~> 1.3"
-  }
+    required_providers {
+        apigee = "~> 0.0"
+        archive = "~> 1.3"
+    }
 }
 
 module "electronic-prescription-service-api" {
-  source             = "github.com/NHSDigital/api-platform-service-module?ref=apm-501-change-proxy-location"
-  name               = "electronic-prescription-service-api"
-  path               = "electronic-prescriptions"
-  apigee_environment = var.apigee_environment
-  proxy_type         = "sandbox"
-  namespace          = var.namespace
+    source = "github.com/NHSDigital/api-platform-service-module"
+    name = "electronic-prescription-service-api"
+    path = "electronic-prescriptions"
+    apigee_environment = var.apigee_environment
+    proxy_type = (var.force_sandbox || length(regexall("sandbox", var.apigee_environment)) > 0) ? "sandbox" : "live"
+    namespace = var.namespace
+    make_api_product = !(length(var.namespace) > 0 || length(regexall("sandbox", var.apigee_environment)) > 0)
+    api_product_display_name = "Electronic Prescription Service"
+    api_product_description = ""
 }
