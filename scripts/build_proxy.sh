26beadeabeb25aa1c6462b0787cc4a49680a70de
Matt Jarvis <61870561+mfjarvis@users.noreply.github.com>
2020-06-01 10:40:19 +0100

    Add stub Prescription Coordinator with translation module (#4)
    * created stub translation library
    
    * updated coordinator endpoints
    
    * AEA-195: Adding coordinator as a hosted target for the live proxy.
    
    * AEA-195: Started implementing an actual translation between FHIR and HL7 V3 messages.
    
    * AEA-195: Inlined references to constructors from the HL7 V3 modules to avoid having a massive list at the start of the file.
    
    * AEA-195: Tidying up.
    
    Co-authored-by: Conner Jowitt <conner.jowitt@infinityworks.com>
    Co-authored-by: Matt Jarvis <matt.jarvis@gmail.com>

--- scripts/build_proxy.sh
+++ scripts/build_proxy.sh
@@ -11,4 +11,6 @@ mkdir -p build/proxies/live
 cp -Rv proxies/sandbox/apiproxy build/proxies/sandbox
 cp -Rv proxies/live/apiproxy build/proxies/live
 mkdir -p build/proxies/sandbox/apiproxy/resources/hosted
+mkdir -p build/proxies/live/apiproxy/resources/hosted
 rsync -av --copy-links --exclude="node_modules" --filter=':- .gitignore' sandbox/ build/proxies/sandbox/apiproxy/resources/hosted
+rsync -av --copy-links --exclude="node_modules" --filter=':- .gitignore' coordinator/ build/proxies/live/apiproxy/resources/hosted
