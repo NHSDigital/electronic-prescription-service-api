a7ab18a2b8f79b17b5758ecb24a5f4ed5e451d2d
Alex McCormick <alex.mccormick1@nhs.net>
2020-06-12 20:41:23 +0100

    AEA-224 Refactor sandbox handlers and validation to add sad path examples

--- sandbox/routes/prescription/post-prescription.js
+++ sandbox/routes/prescription/post-prescription.js
@@ -1,7 +1,6 @@
-const Boom = require('boom')
-const examples = require('../../services/examples')
 //const fhirHelper = require('../../helpers/fhir-helper')
 const requestValidator = require("../../validators/request-validator")
+const responseBuilder = require("../../services/response-builder")
 
 module.exports = [
   /*
@@ -11,14 +10,12 @@ module.exports = [
     method: 'POST',
     path: '/Prescription',
     handler: (request, h) => {
-      requestValidator.verifyPrescriptionBundle(request.payload)
-      if (request.payload.id === examples.prescriptionPostSuccessRequest.id) {
-        //TODO add meta to the response schema and use fhirHelper
-        //return fhirHelper.createFhirResponse(h, examples.prescriptionPostSuccessResponse)
-        return h.response(examples.prescriptionPostSuccessResponse)
-      } else {
-        throw Boom.badRequest("Unsupported prescription id", {operationOutcomeCode: "value", apiErrorCode: "unsupportedPrescriptionId"})
-      }
+      var validation = requestValidator.verifyPrescriptionBundle(request.payload)
+      var response = responseBuilder.create(validation, request.payload.id)
+      var statusCode = requestValidator.getStatusCode(validation)
+      //TODO add meta to the response schema and use fhirHelper
+      //return fhirHelper.createFhirResponse(h, examples.prescriptionPostSuccessResponse)
+      return h.response(response).code(statusCode)
     }
   }
 ]
--- sandbox/routes/prescription/put-prescription.js
+++ sandbox/routes/prescription/put-prescription.js
@@ -1,7 +1,6 @@
-const Boom = require('boom')
-const examples = require('../../services/examples')
 //const fhirHelper = require('../../helpers/fhir-helper')
 const requestValidator = require("../../validators/request-validator")
+const responseBuilder = require("../../services/response-builder")
 
 module.exports = [
   /*
@@ -11,14 +10,12 @@ module.exports = [
     method: 'PUT',
     path: '/Prescription',
     handler: (request, h) => {
-      requestValidator.verifyPrescriptionAndSignatureBundle(request.payload)
-      if (request.payload.id === examples.prescriptionPutSuccessRequest.id) {
-        //TODO add meta to the response schema and use fhirHelper
-        //return fhirHelper.createFhirResponse(h, examples.prescriptionPostSuccessResponse)
-        return h.response(examples.prescriptionPutSuccessResponse)
-      } else {
-        throw Boom.badRequest("Unsupported prescription id", {operationOutcomeCode: "value", apiErrorCode: "unsupportedPrescriptionId"})
-      }
+      var validation = requestValidator.verifyPrescriptionAndSignatureBundle(request.payload)
+      var response = responseBuilder.create(validation, request.payload.id)
+      var statusCode = requestValidator.getStatusCode(validation)
+      //TODO add meta to the response schema and use fhirHelper
+      //return fhirHelper.createFhirResponse(h, examples.prescriptionPostSuccessResponse)
+      return h.response(response).code(statusCode)
     }
   }
 ]
