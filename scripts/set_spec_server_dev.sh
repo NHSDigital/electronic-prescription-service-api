dd58d7d1fa190b6d19b8291607be3fbf31095d35
Alex McCormick <alex.mccormick1@nhs.net>
2020-06-16 11:09:34 +0100

    AEA-250 Rename specification to match Apigee API product

--- scripts/set_spec_server_dev.sh
+++ scripts/set_spec_server_dev.sh
@@ -3,6 +3,6 @@
 
 set -o nounset errexit pipefail
 
-export PATH_TO_SPEC_FILE="${PATH_TO_SPEC_FILE:=build/electronic-prescriptions.json}"
+export PATH_TO_SPEC_FILE="${PATH_TO_SPEC_FILE:=build/electronic-prescription-service-api.json}"
 
 cat <<< """$(jq 'del(.servers[])|.servers[.servers| length] |= . + {"url": "http://localhost:9000", "description": "Local Dev environment."}' $PATH_TO_SPEC_FILE)""" > $PATH_TO_SPEC_FILE
