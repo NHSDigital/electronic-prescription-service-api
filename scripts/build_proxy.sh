57e7d9291782fa88d215a0823fbb6316aeb328bd
Matt Jarvis <matt.jarvis@gmail.com>
2020-06-22 14:50:22 +0100

    AEA-270: Remove hosted target from the live proxy. Update target of the live proxy to the load balancer URL.

--- scripts/build_proxy.sh
+++ scripts/build_proxy.sh
@@ -11,8 +11,4 @@ mkdir -p build/proxies/live
 cp -Rv proxies/sandbox/apiproxy build/proxies/sandbox
 cp -Rv proxies/live/apiproxy build/proxies/live
 mkdir -p build/proxies/sandbox/apiproxy/resources/hosted
-mkdir -p build/proxies/live/apiproxy/resources/hosted
 rsync -av --copy-links --exclude="node_modules" --filter=':- .gitignore' sandbox/ build/proxies/sandbox/apiproxy/resources/hosted
-rsync -av --copy-links --exclude="node_modules" --filter=':- .gitignore' coordinator/dist/ build/proxies/live/apiproxy/resources/hosted
-cp coordinator/package.json build/proxies/live/apiproxy/resources/hosted
-cp coordinator/app.yaml build/proxies/live/apiproxy/resources/hosted
