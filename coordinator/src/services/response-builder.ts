23b20368b7e3fc228c848d6bd2bfa131048ca951
conner-jowitt <55279861+conner-jowitt@users.noreply.github.com>
2020-06-22 12:08:04 +0100

    AEA-267: Update validator functions in coordinator (#20)
    * updated validators, updated endpoints to use new validator functions
    
    * cleaned up verifyPrescriptionAndSignatureBundle function
    
    * removed unused imports and unused function
    
    * cleaned verifyBundleContainsEntries function
    
    * fixed tests
    
    * removed comment
    
    * AEA-267: Changed Organization validation from exactly 2 to at least 2. Removed Encounter validation. Refactored Bundle validation for readability.
    
    Co-authored-by: Matt Jarvis <matt.jarvis@gmail.com>
    Co-authored-by: Matt Jarvis <61870561+mfjarvis@users.noreply.github.com>

---
+++ coordinator/src/services/response-builder.ts
@@ -0,0 +1,48 @@
+import * as translator from "./translation-service";
+import {Bundle, OperationOutcome} from "./fhir-resources";
+import {ValidationError} from "../validators/request-validator";
+
+export function createPrescription(validation: Array<ValidationError>, requestPayload: unknown): OperationOutcome | string {
+    if (validation.length > 0) {
+        return FhirError(validation)
+    }
+    return translator.convertFhirMessageToHl7V3ParentPrescription(requestPayload as Bundle)
+}
+
+export function createSignedInfo(validation: Array<ValidationError>, requestPayload: unknown): OperationOutcome | string {
+    if (validation.length > 0) {
+        return FhirError(validation)
+    }
+    return translator.convertFhirMessageToHl7V3SignedInfo(requestPayload as Bundle)
+}
+
+export function sendMessage(validation: Array<ValidationError>): OperationOutcome | string {
+    if (validation.length > 0) {
+        return FhirError(validation)
+    }
+    return "Message Sent"
+}
+
+function FhirError(validation: Array<ValidationError>): OperationOutcome {
+    /* Reformat errors to FHIR spec
+      * v.operationOutcomeCode: from the [IssueType ValueSet](https://www.hl7.org/fhir/valueset-issue-type.html)
+      * v.apiErrorCode: Our own code defined for each particular error. Refer to OAS.
+    */
+    return {
+        resourceType: "OperationOutcome",
+        issue: validation.map(v => {
+            return {
+                severity: v.severity,
+                code: v.operationOutcomeCode,
+                details: {
+                    coding: [{
+                        system: "https://fhir.nhs.uk/R4/CodeSystem/Spine-ErrorOrWarningCode",
+                        version: 1,
+                        code: v.apiErrorCode,
+                        display: v.message
+                    }]
+                }
+            }
+        })
+    }
+}
