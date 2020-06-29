5f5e89718ed5c07ec6e00f3044f39bc256202765
jalal <jalalhosseiny@gmail.com>
2020-06-29 11:50:50 +0100

    AMB-68 remove /Health endpoint and use /_status instead

--- coordinator/src/routes/index.ts
+++ coordinator/src/routes/index.ts
@@ -1,13 +1,11 @@
 import convertPrescriptionRoutes from './prescription/post-convert-full-message'
 import preparePrescriptionRoutes from './prescription/post-prepare-message'
 import sendPrescriptionRoutes from './prescription/post-send-message'
-import healthRoutes from './health/get-health'
 import statusRoutes from './health/get-status'
 
 export default [
     ...convertPrescriptionRoutes,
     ...preparePrescriptionRoutes,
     ...sendPrescriptionRoutes,
-    ...healthRoutes,
     ...statusRoutes
 ]
--- sandbox/routes/index.js
+++ sandbox/routes/index.js
@@ -1,8 +1,7 @@
 const postPrescription = require('./prescription/prepare');
 const putPrescription = require('./prescription/send');
-const health = require('./health/get-health');
 const status = require('./health/get-status');
 
-const routes = [].concat(postPrescription, putPrescription, health, status);
+const routes = [].concat(postPrescription, putPrescription, status);
 
 module.exports = routes;
