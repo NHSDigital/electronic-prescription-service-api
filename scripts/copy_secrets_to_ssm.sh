a17a8c0db27e91da3930cd7d6cf88b76e62cd8c2
Matt Jarvis <matt.jarvis@gmail.com>
2020-06-30 10:21:12 +0100

    AEA-294: Adding script to the build pipeline to copy secrets from secrets manager to SSM. Adding config to pull these in as environment variables at runtime.

---
+++ scripts/copy_secrets_to_ssm.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -euo pipefail
+
+# Just set this to ptl for the time being. It's hard coded all over the pipeline at the moment anyway.
+ACCOUNT=ptl
+
+clientCertificate=$(
+    aws secretsmanager get-secret-value \
+    --profile build-eps-coordinator \
+    --secret-id "$ACCOUNT/eps-coordinator/client-certificate"
+)
+
+aws ssm put-parameter \
+    --profile build-eps-coordinator \
+    --name "$ACCOUNT/eps-coordinator/client-certificate" \
+    --value "$clientCertificate"
+
+fromAsid=$(
+    aws secretsmanager get-secret-value \
+    --profile build-eps-coordinator \
+    --secret-id "$ACCOUNT/eps-coordinator/from-asid"
+)
+
+aws ssm put-parameter \
+    --profile build-eps-coordinator \
+    --name "$ACCOUNT/eps-coordinator/from-asid" \
+    --value "$fromAsid"
