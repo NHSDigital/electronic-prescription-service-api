81859aa689c0bfe84a6e17bf7bd0d41575e0cced
Matt Jarvis <matt.jarvis@gmail.com>
2020-06-30 14:00:11 +0100

    AEA-294: Add something to the coordinator so we can see whether our secrets are being populated.

--- coordinator/src/routes/health/get-health.ts
+++ coordinator/src/routes/health/get-health.ts
@@ -5,6 +5,10 @@ export default [
         method: 'GET',
         path: '/Health',
         handler: (request: Hapi.Request, h: Hapi.ResponseToolkit): Hapi.ResponseObject => {
+            if (process.env.CLIENT_CERT !== undefined && process.env.CLIENT_CERT.length > 0) {
+                return h.response("Coordinator is alive and we have secrets")
+            }
+
             return h.response("Coordinator is alive")
         }
     }
