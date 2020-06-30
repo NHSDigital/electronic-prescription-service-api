e9871a9356560f4bc80ea1b62fb1909344b5e6d5
Matt Jarvis <matt.jarvis@gmail.com>
2020-06-30 15:42:54 +0100

    AEA-294: Log out environment variable names to diagnose missing secrets.

--- coordinator/src/routes/health/get-health.ts
+++ coordinator/src/routes/health/get-health.ts
@@ -5,11 +5,15 @@ export default [
         method: 'GET',
         path: '/Health',
         handler: (request: Hapi.Request, h: Hapi.ResponseToolkit): Hapi.ResponseObject => {
-            if (process.env.CLIENT_CERT !== undefined && process.env.CLIENT_CERT.length > 0) {
+            if (process.env.CLIENT_CERT !== undefined && process.env.CLIENT_CERT !== "") {
                 return h.response("Coordinator is alive and we have secrets")
             }
 
-            return h.response("Coordinator is alive")
+            return h.response(
+                "Coordinator is alive but we don't have secrets.\n"
+                + "Environment variable names:\n"
+                + Object.getOwnPropertyNames(process.env)
+            )
         }
     }
 ]
