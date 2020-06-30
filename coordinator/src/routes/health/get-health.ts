c23b5f85cdf4daae18cdb20741a7a1f7df75fa5d
Matt Jarvis <matt.jarvis@gmail.com>
2020-06-30 16:16:02 +0100

    AEA-294: Reverting changes to the Health endpoint following successful testing.

--- coordinator/src/routes/health/get-health.ts
+++ coordinator/src/routes/health/get-health.ts
@@ -5,15 +5,7 @@ export default [
         method: 'GET',
         path: '/Health',
         handler: (request: Hapi.Request, h: Hapi.ResponseToolkit): Hapi.ResponseObject => {
-            if (process.env.CLIENT_CERT !== undefined && process.env.CLIENT_CERT !== "") {
-                return h.response("Coordinator is alive and we have secrets")
-            }
-
-            return h.response(
-                "Coordinator is alive but we don't have secrets.\n"
-                + "Environment variable names:\n"
-                + Object.getOwnPropertyNames(process.env)
-            )
+            return h.response("Coordinator is alive")
         }
     }
 ]
