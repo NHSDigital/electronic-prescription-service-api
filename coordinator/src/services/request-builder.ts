9d7964ff27a247105556b793fa6afab48785d06f
Matt Jarvis <matt.jarvis@gmail.com>
2020-07-03 12:19:12 +0100

    AEA-296: Moved most of the EPS config out of secrets manager and directly into SSM. Added a map from interaction id to CPA id.

--- coordinator/src/services/request-builder.ts
+++ coordinator/src/services/request-builder.ts
@@ -4,16 +4,16 @@ import moment from "moment"
 import path from "path"
 import * as uuid from "uuid"
 
-const ebxml_request_template = fs.readFileSync(path.join(__dirname, "../resources/ebxml_request.mustache"), "utf-8")
+const ebxmlRequestTemplate = fs.readFileSync(path.join(__dirname, "../resources/ebxml_request.mustache"), "utf-8")
+const cpaIdMap = new Map<string, string>(JSON.parse(process.env.CPA_ID_MAP))
 
 class EbXmlRequest {
+    timestamp = moment.utc().format()
+    conversation_id = uuid.v4().toUpperCase()
+    message_id = uuid.v4().toUpperCase()
     from_party_id = process.env.FROM_PARTY_KEY
     to_party_id = process.env.TO_PARTY_KEY
-    cpa_id = process.env.CPA_ID
-    conversation_id = uuid.v4().toUpperCase()
     service = "urn:nhs:names:services:mm"
-    message_id = uuid.v4().toUpperCase()
-    timestamp = moment.utc().format()
 
     //These are the parameters for async reliable, as this is the only messaging pattern we need for EPS.
     duplicate_elimination = true
@@ -21,15 +21,17 @@ class EbXmlRequest {
     ack_soap_actor = "urn:oasis:names:tc:ebxml-msg:actor:toPartyMSH"
     sync_reply = true
 
-    hl7_message: string
     action: string
+    cpa_id: string
+    hl7_message: string
 
     constructor(interactionId: string, hl7V3Message: string) {
         this.action = interactionId
+        this.cpa_id = cpaIdMap.get(interactionId)
         this.hl7_message = hl7V3Message
     }
 }
 
 export function addEbXmlWrapper(hl7V3Message: string): string {
-    return Mustache.render(ebxml_request_template, new EbXmlRequest("PORX_IN020101UK31", hl7V3Message))
+    return Mustache.render(ebxmlRequestTemplate, new EbXmlRequest("PORX_IN020101UK31", hl7V3Message))
 }
