9af5217600a5662baee4a247df2667b9609c6cb8
Matt Jarvis <matt.jarvis@gmail.com>
2020-07-06 14:48:41 +0100

    AEA-293: Remove reformatting of unhandled exceptions as it makes error messages less useful, and we already have a catch all error response in the proxy.

--- coordinator/src/services/fhir-resources.ts
+++ coordinator/src/services/fhir-resources.ts
@@ -135,16 +135,15 @@ export class OperationOutcomeIssue {
 }
 
 export class OperationOutcome {
-    resourceType: "OperationOutcome"
+    readonly resourceType = "OperationOutcome"
     issue: Array<OperationOutcomeIssue>
 }
 
 export class Parameters {
-    resourceType: string
+    readonly resourceType = "Parameters"
     parameter: Array<Parameter>
 
     constructor(parameters: Array<Parameter>) {
-        this.resourceType = "Parameters"
         this.parameter = parameters
     }
 }
