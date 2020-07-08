8effe727cf0250904ea45429c04e14a96297ad8d
Matt Jarvis <matt.jarvis@gmail.com>
2020-07-08 15:10:04 +0100

    AEA-293: Set coordinator to sandbox mode in the build pipeline.

--- scripts/set_env_vars.sh
+++ scripts/set_env_vars.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
 set -eu
 
-CLIENT_KEY=$(cat ~/Documents/DELETE_AFTER_USE/private-key.pem)
-CLIENT_CERT=$(cat ~/Documents/DELETE_AFTER_USE/public-key.pem)
-ROOT_CA_CERT=$(cat ~/Documents/DELETE_AFTER_USE/ca-certs-int/root.pem)
-SUB_CA_CERT=$(cat ~/Documents/DELETE_AFTER_USE/ca-certs-int/sub.pem)
+#CLIENT_KEY=$(cat ~/Documents/DELETE_AFTER_USE/private-key.pem)
+#CLIENT_CERT=$(cat ~/Documents/DELETE_AFTER_USE/public-key.pem)
+#ROOT_CA_CERT=$(cat ~/Documents/DELETE_AFTER_USE/ca-certs-int/root.pem)
+#SUB_CA_CERT=$(cat ~/Documents/DELETE_AFTER_USE/ca-certs-int/sub.pem)
+#export CLIENT_KEY
+#export CLIENT_CERT
+#export ROOT_CA_CERT
+#export SUB_CA_CERT
 
-export CLIENT_KEY
-export CLIENT_CERT
-export ROOT_CA_CERT
-export SUB_CA_CERT
-
-#export CLIENT_KEY=
-#export CLIENT_CERT=
-#export ROOT_CA_CERT=
-#export SUB_CA_CERT=
-
-export SANDBOX=0
+export CLIENT_KEY=
+export CLIENT_CERT=
+export ROOT_CA_CERT=
+export SUB_CA_CERT=
+export SANDBOX=1
 export FROM_ASID=200000001285
 export TO_ASID=567456789789
 export CPA_ID_MAP='[["PORX_IN020101UK31","S20001A000100"],["PORX_IN020102UK31","S20000A000086"],["PORX_IN030101UK32","S20001A000126"]]'
