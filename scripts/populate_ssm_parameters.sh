bbaf4e191e7926a75d95fb4c590c481a7dcda9f7
Matt Jarvis <matt.jarvis@gmail.com>
2020-07-08 15:35:59 +0100

    AEA-293: Added leading slashes to SSM parameter names.

--- scripts/populate_ssm_parameters.sh
+++ scripts/populate_ssm_parameters.sh
@@ -40,5 +40,5 @@ copy-secret "ptl/eps/veit07.devspineservices.nhs.uk/certificate" "/ptl/api-deplo
 copy-secret "ptl/veit07.devspineservices.nhs.uk/root-ca/crt" "/ptl/api-deployment/eps-coordinator/veit07.devspineservices.nhs.uk/root-ca-certificate"
 copy-secret "ptl/veit07.devspineservices.nhs.uk/sub-ca/crt" "/ptl/api-deployment/eps-coordinator/veit07.devspineservices.nhs.uk/sub-ca-certificate"
 
-copy-parameter "ptl/platform-common/veit07.devspineservices.nhs.uk/cpa-id-map" "/ptl/api-deployment/eps-coordinator/veit07.devspineservices.nhs.uk/cpa-id-map"
-copy-parameter "ptl/platform-common/veit07.devspineservices.nhs.uk/party-key" "/ptl/api-deployment/eps-coordinator/veit07.devspineservices.nhs.uk/to-party-key"
+copy-parameter "/ptl/platform-common/veit07.devspineservices.nhs.uk/cpa-id-map" "/ptl/api-deployment/eps-coordinator/veit07.devspineservices.nhs.uk/cpa-id-map"
+copy-parameter "/ptl/platform-common/veit07.devspineservices.nhs.uk/party-key" "/ptl/api-deployment/eps-coordinator/veit07.devspineservices.nhs.uk/to-party-key"
