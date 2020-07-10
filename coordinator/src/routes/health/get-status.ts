aa1287dcb5aa3e6e5896f2a8602d1d98c2c721ec
Jalal Hosseini <artronics@users.noreply.github.com>
2020-07-10 09:56:19 +0100

    Replace /Health endpoint with /_status
    AMB-68 Replace /Health endpoint with /_status

--- coordinator/src/routes/health/get-status.ts
+++ coordinator/src/routes/health/get-status.ts
@@ -3,7 +3,7 @@ import Hapi from "@hapi/hapi";
 export default [
     {
         method: 'GET',
-        path: '/Health',
+        path: '/_status',
         handler: (request: Hapi.Request, h: Hapi.ResponseToolkit): Hapi.ResponseObject => {
             return h.response("Coordinator is alive")
         }
