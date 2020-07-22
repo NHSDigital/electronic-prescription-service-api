0a125b8ed6524008b48c95517939a14322996bae
Alex McCormick <alex.mccormick1@nhs.net>
2020-07-22 15:01:39 +0100

    AEA-389 Remove token option for pact publish and verify to simplify process

--- tests/e2e/pact/broker/verify-eps-sandbox.sh
+++ tests/e2e/pact/broker/verify-eps-sandbox.sh
@@ -2,13 +2,6 @@
 
 set -o pipefail
 
-if [ -z "$PACT_BROKER_TOKEN" ]
-then
-    docker run --rm -e PACT_BROKER_BASE_URL=$PACT_BROKER_URL -e PACT_BROKER_USERNAME=$PACT_BROKER_BASIC_AUTH_USERNAME -e PACT_BROKER_PASSWORD=$PACT_BROKER_BASIC_AUTH_PASSWORD pactfoundation/pact-cli:latest verify --provider=nhsd-apim-eps-sandbox --provider-base-url=https://$APIGEE_ENVIRONMENT.api.service.nhs.uk/$SERVICE_BASE_PATH --provider-app-version=$GIT_TAG-$COMMIT_ID --publish-verification-results
-    docker run --rm -e PACT_BROKER_BASE_URL=$PACT_BROKER_URL -e PACT_BROKER_USERNAME=$PACT_BROKER_BASIC_AUTH_USERNAME -e PACT_BROKER_PASSWORD=$PACT_BROKER_BASIC_AUTH_PASSWORD pactfoundation/pact-cli:latest broker create-version-tag --pacticipant=nhsd-apim-eps-sandbox --version=$GIT_TAG-$COMMIT_ID --tag=$APIGEE_ENVIRONMENT-sandbox
-    docker run --rm -e PACT_BROKER_BASE_URL=$PACT_BROKER_URL -e PACT_BROKER_USERNAME=$PACT_BROKER_BASIC_AUTH_USERNAME -e PACT_BROKER_PASSWORD=$PACT_BROKER_BASIC_AUTH_PASSWORD pactfoundation/pact-cli:latest broker can-i-deploy --pacticipant nhsd-apim-eps-sandbox --version $GIT_TAG-$COMMIT_ID --pacticipant nhsd-apim-eps-test-client --version $GIT_TAG-$COMMIT_ID
-else
-    docker run --rm -e PACT_BROKER_BASE_URL=$PACT_BROKER_URL -e PACT_BROKER_TOKEN=$PACT_BROKER_TOKEN pactfoundation/pact-cli:latest verify --provider=nhsd-apim-eps-sandbox --provider-base-url=https://$APIGEE_ENVIRONMENT.api.service.nhs.uk/$SERVICE_BASE_PATH --provider-app-version=$GIT_TAG-$COMMIT_ID --publish-verification-results
-    docker run --rm -e PACT_BROKER_BASE_URL=$PACT_BROKER_URL -e PACT_BROKER_TOKEN=$PACT_BROKER_TOKEN pactfoundation/pact-cli:latest broker create-version-tag --pacticipant=nhsd-apim-eps-sandbox --version=$GIT_TAG-$COMMIT_ID --tag=$APIGEE_ENVIRONMENT-sandbox
-    docker run --rm -e PACT_BROKER_BASE_URL=$PACT_BROKER_URL -e PACT_BROKER_TOKEN=$PACT_BROKER_TOKEN pactfoundation/pact-cli:latest broker can-i-deploy --pacticipant nhsd-apim-eps-sandbox --version $GIT_TAG-$COMMIT_ID --pacticipant nhsd-apim-eps-test-client --version $GIT_TAG-$COMMIT_ID
-fi
+docker run --rm -e PACT_BROKER_BASE_URL=$PACT_BROKER_URL -e PACT_BROKER_USERNAME=$PACT_BROKER_BASIC_AUTH_USERNAME -e PACT_BROKER_PASSWORD=$PACT_BROKER_BASIC_AUTH_PASSWORD pactfoundation/pact-cli:latest verify --provider=nhsd-apim-eps-sandbox --provider-base-url=https://$APIGEE_ENVIRONMENT.api.service.nhs.uk/$SERVICE_BASE_PATH --provider-app-version=$GIT_TAG-$COMMIT_ID --publish-verification-results
+docker run --rm -e PACT_BROKER_BASE_URL=$PACT_BROKER_URL -e PACT_BROKER_USERNAME=$PACT_BROKER_BASIC_AUTH_USERNAME -e PACT_BROKER_PASSWORD=$PACT_BROKER_BASIC_AUTH_PASSWORD pactfoundation/pact-cli:latest broker create-version-tag --pacticipant=nhsd-apim-eps-sandbox --version=$GIT_TAG-$COMMIT_ID --tag=$APIGEE_ENVIRONMENT-sandbox
+docker run --rm -e PACT_BROKER_BASE_URL=$PACT_BROKER_URL -e PACT_BROKER_USERNAME=$PACT_BROKER_BASIC_AUTH_USERNAME -e PACT_BROKER_PASSWORD=$PACT_BROKER_BASIC_AUTH_PASSWORD pactfoundation/pact-cli:latest broker can-i-deploy --pacticipant nhsd-apim-eps-sandbox --version $GIT_TAG-$COMMIT_ID --pacticipant nhsd-apim-eps-test-client --version $GIT_TAG-$COMMIT_ID
