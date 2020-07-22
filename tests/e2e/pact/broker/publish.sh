5e21ac338a4be935f24e282f90acab0d0e69e848
Alex McCormick <alex.mccormick1@nhs.net>
2020-07-22 12:17:27 +0100

    AEA-389 Replace pact-node/ruby-standalone publish pacts with docker pact cli publish

--- tests/e2e/pact/broker/publish.sh
+++ tests/e2e/pact/broker/publish.sh
@@ -4,7 +4,7 @@ set -o pipefail
 
 if [ -z "$PACT_BROKER_TOKEN" ]
 then
-    docker run --rm -v ${PWD}:${PWD} -e PACT_BROKER_BASE_URL=$PACT_BROKER_URL -e PACT_BROKER_TOKEN=$PACT_BROKER_TOKEN pactfoundation/pact-cli:latest publish ${PWD}/pact/pacts --consumer-app-version $BUILD_VERSION
-else
     docker run --rm -v ${PWD}:${PWD} -e PACT_BROKER_BASE_URL=$PACT_BROKER_URL -e PACT_BROKER_USERNAME=$PACT_BROKER_BASIC_AUTH_USERNAME -e PACT_BROKER_PASSWORD=$PACT_BROKER_BASIC_AUTH_PASSWORD pactfoundation/pact-cli:latest publish ${PWD}/pact/pacts --consumer-app-version $BUILD_VERSION
+else
+    docker run --rm -v ${PWD}:${PWD} -e PACT_BROKER_BASE_URL=$PACT_BROKER_URL -e PACT_BROKER_TOKEN=$PACT_BROKER_TOKEN pactfoundation/pact-cli:latest publish ${PWD}/pact/pacts --consumer-app-version $BUILD_VERSION
 fi
