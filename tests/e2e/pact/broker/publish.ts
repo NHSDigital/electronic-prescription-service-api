ab91095e841d34c6b94b798d0e78cd344401c219
Alex McCormick <alex.mccormick1@nhs.net>
2020-07-22 10:28:05 +0100

    AEA-389 Add option to publish pact by token, replace hardcoded publish tags and versions with passed in variables

--- tests/e2e/pact/broker/publish.ts
+++ tests/e2e/pact/broker/publish.ts
@@ -2,32 +2,41 @@ import publisher, { PublisherOptions } from "@pact-foundation/pact-node";
 import * as cp from "child_process";
 import { resolve } from "path";
 
-let revision: string = "";
+let version: string = "";
 let branch: string = "";
-let artifactTag: string = "";
 let opts: PublisherOptions;
 const PACT_BROKER_URL: string = process.env.PACT_BROKER_URL || "";
 const PACT_BROKER_BASIC_AUTH_USERNAME: string = process.env.PACT_BROKER_BASIC_AUTH_USERNAME || "";
 const PACT_BROKER_BASIC_AUTH_PASSWORD: string = process.env.PACT_BROKER_BASIC_AUTH_PASSWORD || "";
+const PACT_BROKER_TOKEN: string = process.env.PACT_BROKER_TOKEN || "";
 
 main();
 
 function getOpts() {
-  const consumerVersion = artifactTag + "-" + revision;
-  opts = {
-    pactFilesOrDirs: [resolve(process.cwd(), "pact/pacts")],
-    pactBroker: PACT_BROKER_URL,
-    pactBrokerUsername: PACT_BROKER_BASIC_AUTH_USERNAME,
-    pactBrokerPassword: PACT_BROKER_BASIC_AUTH_PASSWORD,
-    consumerVersion: consumerVersion,
-    tags: [branch]
-  };
+  if (PACT_BROKER_TOKEN) {
+    opts = {
+      pactFilesOrDirs: [resolve(process.cwd(), "pact/pacts")],
+      pactBroker: PACT_BROKER_URL,
+      pactBrokerToken: PACT_BROKER_TOKEN,
+      consumerVersion: version,
+      tags: [branch]
+    };
+  }
+  else {
+    opts = {
+      pactFilesOrDirs: [resolve(process.cwd(), "pact/pacts")],
+      pactBroker: PACT_BROKER_URL,
+      pactBrokerUsername: PACT_BROKER_BASIC_AUTH_USERNAME,
+      pactBrokerPassword: PACT_BROKER_BASIC_AUTH_PASSWORD,
+      consumerVersion: version,
+      tags: [branch]
+    };
+  }
 }
 
 function main() {
   getBranch();
-  getRevision();
-  getTags();
+  getVersion();
   getOpts();
   performPublish();
 }
@@ -48,41 +57,10 @@ function performPublish() {
     });
 }
 
-function getRevision() {
-  try {
-    return (revision = cp
-      .execSync("git rev-parse --short HEAD")
-      .toString()
-      .trim());
-  } catch (Error) {
-    throw new TypeError(
-      "Couldn't find a git commit hash, is this a git directory?"
-    );
-  }
+function getBranch() {
+  branch = process.env.BUILD_SOURCE_BRANCH || ""
 }
 
-function getBranch() {
-  try {
-    branch = "pr" 
-    // return (branch = cp
-    //   .execSync("git rev-parse --abbrev-ref HEAD")
-    //   .toString()
-    //   .trim());
-  } catch (Error) {
-    throw new TypeError("Couldn't find a git branch, is this a git directory?");
-  }
-}
-function getTags() {
-  try {
-    artifactTag = "v1.0.44-alpha"
-      // cp
-      // .execSync("git describe")
-      // .toString()
-      // .trim();
-  } catch (Error) {
-    const errorMessage = Error.message;
-    if (errorMessage.indexOf("fatal") >= 0) {
-        throw new TypeError("Couldn't find a git tag");
-    }
-  }
+function getVersion() {
+  branch = process.env.BUILD_VERSION || ""
 }
