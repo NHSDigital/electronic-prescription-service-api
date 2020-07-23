db6c51867648554c989e4f37458e1f65cf709375
Alex McCormick <alex.mccormick1@nhs.net>
2020-07-23 09:44:00 +0100

    AEA-389 Add pact broker next to publish pacts to new broker

--- tests/e2e/pact/broker/publish.sh
+++ tests/e2e/pact/broker/publish.sh
@@ -3,3 +3,4 @@
 set -o pipefail
 
 docker run --rm -v ${PWD}:${PWD} -e PACT_BROKER_BASE_URL=$PACT_BROKER_URL -e PACT_BROKER_USERNAME=$PACT_BROKER_BASIC_AUTH_USERNAME -e PACT_BROKER_PASSWORD=$PACT_BROKER_BASIC_AUTH_PASSWORD pactfoundation/pact-cli:latest publish ${PWD}/pact/pacts --consumer-app-version $BUILD_VERSION
+docker run --rm -v ${PWD}:${PWD} -e PACT_BROKER_BASE_URL=$PACT_BROKER_URL_NEXT -e PACT_BROKER_TOKEN=$PACT_BROKER_TOKEN_NEXT pactfoundation/pact-cli:latest publish ${PWD}/pact/pacts --consumer-app-version $BUILD_VERSION
