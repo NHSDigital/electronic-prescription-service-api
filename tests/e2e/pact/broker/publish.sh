9468daa170edac3b54aa20c44835bd4605e81ef8
Alex McCormick <alex.mccormick1@nhs.net>
2020-07-28 17:46:31 +0100

    AEA-419 Return error codes from pact publish, retry publish 3 times

--- tests/e2e/pact/broker/publish.sh
+++ tests/e2e/pact/broker/publish.sh
@@ -2,5 +2,18 @@
 
 set -o pipefail
 
-docker run --rm -v ${PWD}:${PWD} -e PACT_BROKER_BASE_URL=$PACT_BROKER_URL -e PACT_BROKER_USERNAME=$PACT_BROKER_BASIC_AUTH_USERNAME -e PACT_BROKER_PASSWORD=$PACT_BROKER_BASIC_AUTH_PASSWORD pactfoundation/pact-cli:latest publish ${PWD}/pact/pacts --consumer-app-version $BUILD_VERSION
-docker run --rm -v ${PWD}:${PWD} -e PACT_BROKER_BASE_URL=$PACT_BROKER_URL_NEXT -e PACT_BROKER_TOKEN=$PACT_BROKER_TOKEN_NEXT pactfoundation/pact-cli:latest publish ${PWD}/pact/pacts --consumer-app-version $BUILD_VERSION
+exitCode=0
+
+publish() {
+    docker-compose -f ./broker/docker-compose.yml up -d
+    docker wait pact-cli
+    exitCode=$(docker wait pact-cli)
+    return $exitCode
+}
+
+for i in {1..3}; do publish && break || sleep 5; done
+
+docker logs pact-cli
+docker-compose -f ./broker/docker-compose.yml down
+
+exit $exitCode
