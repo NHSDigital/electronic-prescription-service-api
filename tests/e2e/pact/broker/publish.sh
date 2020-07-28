55ce4fd5a99cca258073c37c39d76c9552a8a7ea
Alex McCormick <alex.mccormick1@nhs.net>
2020-07-28 17:49:04 +0100

    AEA-419 Return error codes from pact publish, retry publish 3 times

--- tests/e2e/pact/broker/publish.sh
+++ tests/e2e/pact/broker/publish.sh
@@ -11,7 +11,7 @@ publish() {
     return $exitCode
 }
 
-for i in {1..3}; do publish && break || sleep 5; done
+for i in {1..3}; do publish && break || sleep 1; done
 
 docker logs pact-cli
 docker-compose -f ./broker/docker-compose.yml down
