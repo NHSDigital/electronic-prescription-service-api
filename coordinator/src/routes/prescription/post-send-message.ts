2d16c5fb364aaf725a2c36976c6746830b4baca3
Conner Jowitt <conner.jowitt@infinityworks.com>
2020-07-31 10:07:47 +0100

    changed import style in post-send-message

--- coordinator/src/routes/prescription/post-send-message.ts
+++ coordinator/src/routes/prescription/post-send-message.ts
@@ -3,7 +3,7 @@ import * as translator from "../../services/translation/translation-service";
 import {Bundle} from "../../model/fhir-resources";
 import {sendData} from "../../services/spine-communication";
 import Hapi from "@hapi/hapi";
-import * as common from "../../services/translation/common"
+import {wrapInOperationOutcome} from "../../services/translation/common"
 
 export default [
     /*
@@ -17,7 +17,7 @@ export default [
             async (requestPayload: Bundle, responseToolkit: Hapi.ResponseToolkit) => {
                 const translatedMessage = translator.convertFhirMessageToHl7V3ParentPrescriptionMessage(requestPayload)
                 const spineResponse = await sendData(translatedMessage)
-                return responseToolkit.response(common.wrapInOperationOutcome(spineResponse)).code(spineResponse.statusCode)
+                return responseToolkit.response(wrapInOperationOutcome(spineResponse)).code(spineResponse.statusCode)
             }
         )
     }
