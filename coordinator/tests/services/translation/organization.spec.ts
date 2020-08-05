2f18bde2c21f512c5f5cde9f4ed04304e5c6aec4
Dan Jones <dan.jones@infinityworks.com>
2020-08-05 16:40:02 +0100

    typos in test file

--- coordinator/tests/services/translation/organization.spec.ts
+++ coordinator/tests/services/translation/organization.spec.ts
@@ -8,10 +8,10 @@ import {onlyElement} from "../../../src/services/translation/common"
 test("convertOrganization maps identifier from fhir organization to AgentPerson.representedOrganization", () => {
   const bundle = clone(TestResources.examplePrescription1.fhirMessageUnsigned)
   const fhirOrganizations = getResourcesOfType(bundle, new Organization())
-  const firstFhirOrgnization = fhirOrganizations[0]
-  firstFhirOrgnization.identifier = [{system: "https://fhir.nhs.uk/Id/ods-organization-code", value: "identifier"}]
-  const fhirIdentifier = firstFhirOrgnization.identifier.reduce(onlyElement).value
-  const hl7v3AgentPersonRepresentedOrganization = convertOrganization(bundle, firstFhirOrgnization)
+  const firstFhirOrganization = fhirOrganizations[0]
+  firstFhirOrganization.identifier = [{system: "https://fhir.nhs.uk/Id/ods-organization-code", value: "identifier"}]
+  const fhirIdentifier = firstFhirOrganization.identifier.reduce(onlyElement).value
+  const hl7v3AgentPersonRepresentedOrganization = convertOrganization(bundle, firstFhirOrganization)
   const hl7v3Id = hl7v3AgentPersonRepresentedOrganization.id._attributes.extension
   expect(hl7v3Id).toEqual(fhirIdentifier)
 })
@@ -19,10 +19,10 @@ test("convertOrganization maps identifier from fhir organization to AgentPerson.
 test("convertOrganization maps name from fhir organization to AgentPerson.representedOrganization", () => {
   const bundle = clone(TestResources.examplePrescription1.fhirMessageUnsigned)
   const fhirOrganizations = getResourcesOfType(bundle, new Organization())
-  const firstFhirOrgnization = fhirOrganizations[0]
-  firstFhirOrgnization.name = "name"
-  const fhirName = firstFhirOrgnization.name
-  const hl7v3AgentPersonRepresentedOrganization = convertOrganization(bundle, firstFhirOrgnization)
+  const firstFhirOrganization = fhirOrganizations[0]
+  firstFhirOrganization.name = "name"
+  const fhirName = firstFhirOrganization.name
+  const hl7v3AgentPersonRepresentedOrganization = convertOrganization(bundle, firstFhirOrganization)
   const hl7v3Name = hl7v3AgentPersonRepresentedOrganization.name._text
   expect(hl7v3Name).toEqual(fhirName)
 })
@@ -30,10 +30,10 @@ test("convertOrganization maps name from fhir organization to AgentPerson.repres
 test("convertOrganization maps telecom from fhir organization to AgentPerson.representedOrganization when present", () => {
   const bundle = clone(TestResources.examplePrescription1.fhirMessageUnsigned)
   const fhirOrganizations = getResourcesOfType(bundle, new Organization())
-  const firstFhirOrgnization = fhirOrganizations[0]
-  firstFhirOrgnization.telecom = [{use: "work", value: "tel:01234567890"}]
-  const fhirTelecom = firstFhirOrgnization.telecom.reduce(onlyElement).value
-  const hl7v3AgentPersonRepresentedOrganization = convertOrganization(bundle, firstFhirOrgnization)
+  const firstFhirOrganization = fhirOrganizations[0]
+  firstFhirOrganization.telecom = [{use: "work", value: "tel:01234567890"}]
+  const fhirTelecom = firstFhirOrganization.telecom.reduce(onlyElement).value
+  const hl7v3AgentPersonRepresentedOrganization = convertOrganization(bundle, firstFhirOrganization)
   const hl7v3Telecom = hl7v3AgentPersonRepresentedOrganization.telecom._attributes.value
   expect(hl7v3Telecom).toEqual(fhirTelecom)
 })
@@ -41,10 +41,10 @@ test("convertOrganization maps telecom from fhir organization to AgentPerson.rep
 test("convertOrganization maps address from fhir organization to AgentPerson.representedOrganization when present", () => {
   const bundle = clone(TestResources.examplePrescription1.fhirMessageUnsigned)
   const fhirOrganizations = getResourcesOfType(bundle, new Organization())
-  const firstFhirOrgnization = fhirOrganizations[0]
-  firstFhirOrgnization.address = [{use: "work", line: ["53 Address"], postalCode: "P0STC0D3"}]
-  const fhirAddress = firstFhirOrgnization.address.reduce(onlyElement)
-  const hl7v3AgentPersonRepresentedOrganization = convertOrganization(bundle, firstFhirOrgnization)
+  const firstFhirOrganization = fhirOrganizations[0]
+  firstFhirOrganization.address = [{use: "work", line: ["53 Address"], postalCode: "P0STC0D3"}]
+  const fhirAddress = firstFhirOrganization.address.reduce(onlyElement)
+  const hl7v3AgentPersonRepresentedOrganization = convertOrganization(bundle, firstFhirOrganization)
   const hl7v3Address = hl7v3AgentPersonRepresentedOrganization.addr
   expect(hl7v3Address.streetAddressLine[0]._text).toEqual(fhirAddress.line[0])
   expect(hl7v3Address.postalCode._text).toEqual(fhirAddress.postalCode)
@@ -53,9 +53,9 @@ test("convertOrganization maps address from fhir organization to AgentPerson.rep
 test("convertOrganization does not throw when minimum required fields are provided", () => {
   const bundle = clone(TestResources.examplePrescription1.fhirMessageUnsigned)
   const fhirOrganizations = getResourcesOfType(bundle, new Organization())
-  const firstFhirOrgnization = fhirOrganizations[0]
-  firstFhirOrgnization.address = undefined
-  firstFhirOrgnization.partOf = undefined
-  firstFhirOrgnization.telecom = undefined
-  convertOrganization(bundle, firstFhirOrgnization)
+  const firstFhirOrganization = fhirOrganizations[0]
+  firstFhirOrganization.address = undefined
+  firstFhirOrganization.partOf = undefined
+  firstFhirOrganization.telecom = undefined
+  convertOrganization(bundle, firstFhirOrganization)
 })
