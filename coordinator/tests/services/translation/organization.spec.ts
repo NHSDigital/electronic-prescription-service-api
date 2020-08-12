61fe28c2fd0a48b3763c5e048460604ff9af3a56
dan-jones-iw <55280310+dan-jones-iw@users.noreply.github.com>
2020-08-12 10:40:49 +0100

    AEA-384 Add tests for organization mappings (#103)
    * AEA-384 Add tests for organization mappings
    
    * complete merge
    
    * typos in test file
    
    * refactored tests
    
    * renamed tests
    
    * AEA-384: Added a few additional assertions. Defined expected data first instead of reducing arrays to get it.
    
    * fixed tests to use correct convertOrganizationAndProviderLicense function
    
    Co-authored-by: Alex McCormick <alex.mccormick1@nhs.net>
    Co-authored-by: Conner Jowitt <conner.jowitt@infinityworks.com>
    Co-authored-by: conner-jowitt <55279861+conner-jowitt@users.noreply.github.com>
    Co-authored-by: Matt Jarvis <matt.jarvis@gmail.com>

---
+++ coordinator/tests/services/translation/organization.spec.ts
@@ -0,0 +1,67 @@
+import {clone} from "../../resources/test-helpers"
+import * as TestResources from "../../resources/test-resources"
+import {Bundle, Organization} from "../../../src/model/fhir-resources"
+import {getResourcesOfType} from "../../../src/services/translation/common"
+import {convertOrganizationAndProviderLicense} from "../../../src/services/translation/organization"
+
+describe("convertOrganizationAndProviderLicense", ()=> {
+  let bundle: Bundle
+  let firstFhirOrganization: Organization
+
+  beforeEach(() => {
+    bundle = clone(TestResources.examplePrescription1.fhirMessageUnsigned)
+    firstFhirOrganization = getResourcesOfType(bundle, new Organization())[0]
+  })
+
+  test("maps identifier from fhir organization to AgentPerson.representedOrganization", () => {
+    const expectedValue = "identifier"
+    firstFhirOrganization.identifier = [{system: "https://fhir.nhs.uk/Id/ods-organization-code", value: expectedValue}]
+
+    const hl7v3AgentPersonRepresentedOrganization = convertOrganizationAndProviderLicense(bundle, firstFhirOrganization)
+    const attributes = hl7v3AgentPersonRepresentedOrganization.id._attributes
+
+    expect(attributes.root).toEqual("1.2.826.0.1285.0.1.10")
+    expect(attributes.extension).toEqual(expectedValue)
+  })
+
+  test("maps name from fhir organization to AgentPerson.representedOrganization", () => {
+    const expectedName = "name"
+    firstFhirOrganization.name = expectedName
+
+    const hl7v3AgentPersonRepresentedOrganization = convertOrganizationAndProviderLicense(bundle, firstFhirOrganization)
+
+    expect(hl7v3AgentPersonRepresentedOrganization.name._text).toEqual(expectedName)
+  })
+
+  test("maps telecom from fhir organization to AgentPerson.representedOrganization when present", () => {
+    const expectedTelecomValue = "tel:01234567890"
+    firstFhirOrganization.telecom = [{use: "work", value: expectedTelecomValue}]
+
+    const hl7v3AgentPersonRepresentedOrganization = convertOrganizationAndProviderLicense(bundle, firstFhirOrganization)
+    const attributes = hl7v3AgentPersonRepresentedOrganization.telecom._attributes
+
+    expect(attributes.use).toEqual("WP")
+    expect(attributes.value).toEqual(expectedTelecomValue)
+  })
+
+  test("maps address from fhir organization to AgentPerson.representedOrganization when present", () => {
+    const expectedAddressLine = "53 Address"
+    const expectedPostalCode = "P0STC0D3"
+    firstFhirOrganization.address = [{use: "work", line: [expectedAddressLine], postalCode: expectedPostalCode}]
+
+    const hl7v3AgentPersonRepresentedOrganization = convertOrganizationAndProviderLicense(bundle, firstFhirOrganization)
+    const hl7v3Address = hl7v3AgentPersonRepresentedOrganization.addr
+
+    expect(hl7v3Address._attributes.use).toEqual("WP")
+    expect(hl7v3Address.streetAddressLine[0]._text).toEqual(expectedAddressLine)
+    expect(hl7v3Address.postalCode._text).toEqual(expectedPostalCode)
+  })
+
+  test("does not throw when minimum required fields are provided", () => {
+    firstFhirOrganization.address = undefined
+    firstFhirOrganization.partOf = undefined
+    firstFhirOrganization.telecom = undefined
+
+    expect(() => convertOrganizationAndProviderLicense(bundle, firstFhirOrganization)).not.toThrow()
+  })
+})
