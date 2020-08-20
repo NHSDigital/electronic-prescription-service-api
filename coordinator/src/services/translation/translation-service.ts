a352c38801950db06005b832ccceda0288a793f8
Conner Jowitt <conner.jowitt@infinityworks.com>
2020-08-20 12:32:14 +0100

    refactored parameter creation

--- coordinator/src/services/translation/translation-service.ts
+++ coordinator/src/services/translation/translation-service.ts
@@ -33,21 +33,7 @@ export function convertFhirMessageToSignedInfoMessage(fhirMessage: fhir.Bundle):
   const signedInfo = createSignedInfo(digestValue)
   const xmlString = writeXmlStringCanonicalized(signedInfo)
   const base64Payload = Buffer.from(xmlString).toString("base64")
-  const base64Display = createBase64Display()
-  const parameters = new fhir.Parameters([
-    {
-      name: "payload",
-      valueString: base64Payload
-    },
-    {
-      name: "display",
-      valueString: base64Display
-    },
-    {
-      name: "algorithm",
-      valueString: "RS1"
-    }
-  ])
+  const parameters = createParameters(base64Payload)
   return JSON.stringify(parameters, null, 2)
 }
 
@@ -100,6 +86,14 @@ function createSignedInfo(digestValue: string): XmlJs.ElementCompact {
 function createBase64Display(): string {
   const hardCodedResponse = "####Patient**NHS Number**: 945 374 0586**Name**: PENSON, HEADLEY TED (Mr)**Date of Birth**: 1977-03-27**Address (Home)**:  10 CRECY CLOSE,  DERBY,  DE22 3JU####Author**Name**: CHANDLER, ANDREW**Telecom (Work)**: 01945700223####Organisation**Name**: PARSON DROVE SURGERY**Telecom (Work)**: 01945700223**Address (Work)**:  240 MAIN ROAD,  PARSON DROVE,  WISBECH,  CAMBRIDGESHIRE,  PE13 4JA####Medication Requested|Name|Dose|Quantity|Unit||----|----|--------|----||Microgynon 30 tablets (Bayer Plc)|As Directed|63|tabletetc."
   return Buffer.from(hardCodedResponse).toString("base64")
+}
+
+function createParameters(base64Payload: string) {
+  const parameters: Array<fhir.Parameter> = []
+  parameters.push({name: "payload", valueString: base64Payload})
+  parameters.push({name: "display", valueString: createBase64Display()})
+  parameters.push({name: "algorithm", valueString: "RS1"})
+  return new fhir.Parameters(parameters)
 }
 
 class AlgorithmIdentifier implements XmlJs.ElementCompact {
