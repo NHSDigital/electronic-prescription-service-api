b9e5a8a18ddf5fc99aa8e2ea254791fbae7f8763
Conner Jowitt <conner.jowitt@infinityworks.com>
2020-08-22 14:51:49 +0100

    review comment changes, made tests use toContain instead of toEqual, refactored beforEach method

--- coordinator/tests/services/translation/prescription.spec.ts
+++ coordinator/tests/services/translation/prescription.spec.ts
@@ -4,6 +4,7 @@ import {getResourcesOfType} from "../../../src/services/translation/common"
 import {Bundle, CommunicationRequest, MedicationRequest} from "../../../src/model/fhir-resources"
 import {convertBundleToPrescription, convertCourseOfTherapyType} from "../../../src/services/translation/prescription"
 import * as translator from "../../../src/services/translation/translation-service"
+import {LineItemPertinentInformation1} from "../../../src/model/hl7-v3-prescriptions";
 
 describe("convertCourseOfTherapyType", () => {
   const cases = [
@@ -30,24 +31,28 @@ describe("PertinentInformation2", () => {
   let fhirCommunicationRequests: Array<CommunicationRequest>
 
   beforeEach(() => {
-    bundle = clone(TestResources.examplePrescription1.fhirMessageUnsigned)
-    fhirCommunicationRequests = createCommunicationRequestIfNone(bundle)
+    bundle = getBundleWithEmptyCommunicationRequestAndAtLeast2MedicationRequests()
+    fhirCommunicationRequests = getResourcesOfType(bundle, new CommunicationRequest())
   })
 
-  function createCommunicationRequestIfNone(bundle: Bundle){
-    fhirCommunicationRequests = getResourcesOfType(bundle, new CommunicationRequest())
-    if (fhirCommunicationRequests.length == 0) {
-      addCommunicationRequestToBundle(bundle)
-      fhirCommunicationRequests = getResourcesOfType(bundle, new CommunicationRequest())
-    }
-    return fhirCommunicationRequests
+  function getBundleWithEmptyCommunicationRequestAndAtLeast2MedicationRequests() {
+    const result = clone(TestResources.examplePrescription1.fhirMessageUnsigned)
+    result.entry = result.entry.filter((entry) => entry.resource.resourceType !== "CommunicationRequest")
+    addEmptyCommunicationRequestToBundle(result)
+    ensureAtLeast2MedicationRequests(result)
+    return result
   }
 
-  function addCommunicationRequestToBundle(bundle: Bundle) {
+  function ensureAtLeast2MedicationRequests(bundle: Bundle) {
+    const fhirMedicationRequests = getResourcesOfType(bundle, new MedicationRequest())
+    if (fhirMedicationRequests.length == 1)
+      bundle.entry.push({resource: fhirMedicationRequests[0]})
+  }
+
+  function addEmptyCommunicationRequestToBundle(bundle: Bundle) {
     const communicationRequest = new CommunicationRequest()
     communicationRequest.payload = []
-    const communicationRequestEntry = {resource: communicationRequest}
-    bundle.entry.push(communicationRequestEntry)
+    bundle.entry.push({resource: communicationRequest})
   }
 
   test("PatientInfo comes from communicationRequest and displays correctly", () => {
@@ -58,31 +63,40 @@ describe("PertinentInformation2", () => {
 
     const firstPertinentInformation2 = pertinentInformation2Array[0]
     const additionalInstructions = firstPertinentInformation2.pertinentLineItem.pertinentInformation1.pertinentAdditionalInstructions.value
-    expect(additionalInstructions).toBe(`<patientInfo>${contentString}</patientInfo>`)
+    const expected = `<patientInfo>${contentString}</patientInfo>`
+    expect(additionalInstructions).toContain(expected)
   })
 
   test("multiple PatientInfos display correctly", () => {
     const contentString1 = "examplePatientInfo1"
     const contentString2 = "secondExamplePatientInfo"
-    fhirCommunicationRequests[0].payload.push({contentString: contentString1})
-    fhirCommunicationRequests[0].payload.push({contentString: contentString2})
+    fhirCommunicationRequests[0].payload.push({contentString: contentString1}, {contentString: contentString2})
 
     const pertinentInformation2Array = convertBundleToPrescription(bundle).pertinentInformation2
 
     const firstPertinentInformation2 = pertinentInformation2Array[0]
     const additionalInstructions = firstPertinentInformation2.pertinentLineItem.pertinentInformation1.pertinentAdditionalInstructions.value
-    expect(additionalInstructions).toBe(`<patientInfo>${contentString1}</patientInfo><patientInfo>${contentString2}</patientInfo>`)
+    expect(additionalInstructions).toContain(`<patientInfo>${contentString1}</patientInfo><patientInfo>${contentString2}</patientInfo>`)
   })
 
   test("PatientInfo display on first LineItem only", () => {
-    const contentString1 = "examplePatientInfo1"
-    fhirCommunicationRequests[0].payload.push({contentString: contentString1})
+    const contentString = "examplePatientInfo1"
+    const expected = `<patientInfo>${contentString}</patientInfo>`
+    fhirCommunicationRequests[0].payload.push({contentString: contentString})
 
     const pertinentInformation2Array = convertBundleToPrescription(bundle).pertinentInformation2
       .map((pertinentInformation2) => pertinentInformation2.pertinentLineItem.pertinentInformation1)
 
-    expect(pertinentInformation2Array.shift()).not.toBe(undefined)
-    pertinentInformation2Array.forEach((pertinentInformation1) => expect(pertinentInformation1).toBe(undefined))
+    const firstPertinentInformation1 = pertinentInformation2Array.shift()
+    expect(firstPertinentInformation1.pertinentAdditionalInstructions.value).toContain(expected)
+
+    pertinentInformation2Array.forEach((pertinentInformation1) => checkValueDoesNotContainExpected(pertinentInformation1))
+
+    function checkValueDoesNotContainExpected(pertinentInformation1: LineItemPertinentInformation1) {
+      const actual = pertinentInformation1?.pertinentAdditionalInstructions?.value
+      if (actual)
+        expect(actual).not.toContain(expected)
+    }
   })
 
   test("additionalInfo XML escaped after final conversion", () => {
@@ -90,7 +104,7 @@ describe("PertinentInformation2", () => {
     fhirCommunicationRequests[0].payload.push({contentString: contentString1})
 
     const result = translator.convertFhirMessageToHl7V3ParentPrescriptionMessage(bundle)
-    expect(result.includes(`&lt;patientInfo&gt;${contentString1}&lt;/patientInfo&gt;`)).toBe(true)
-    expect(result.includes(`<patientInfo>${contentString1}</patientInfo>`)).toBe(false)
+    expect(result).toContain(`&lt;patientInfo&gt;${contentString1}&lt;/patientInfo&gt;`)
+    expect(result).not.toContain(`<patientInfo>${contentString1}</patientInfo>`)
   })
 })
