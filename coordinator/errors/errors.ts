38a706cd7d1d7321ed5f492f7d7a99a9106d5a45
Conner Jowitt <conner.jowitt@infinityworks.com>
2020-09-08 10:23:20 +0100

    refactored errors slightly

--- coordinator/errors/errors.ts
+++ coordinator/errors/errors.ts
@@ -7,15 +7,9 @@ export interface ValidationError {
 
 export class EntryNumberError implements ValidationError{
   message: string
-  operationOutcomeCode: "value"
-  severity: "error"
-  apiErrorCode: "MISSING_FIELD"
-
-  constructor() {
-    this.operationOutcomeCode = "value"
-    this.severity = "error"
-    this.apiErrorCode = "MISSING_FIELD"
-  }
+  operationOutcomeCode: "value" = "value"
+  severity: "error" = "error"
+  apiErrorCode = "MISSING_FIELD"
 }
 
 export class ContainsExactlyError extends EntryNumberError {
@@ -55,43 +49,34 @@ export class MissingIdError extends EntryNumberError {
 }
 
 export class MedicationRequestValueError implements ValidationError {
-  operationOutcomeCode: "value"
-  severity: "error"
+  operationOutcomeCode: "value" = "value"
+  severity: "error" = "error"
   message: string
-  apiErrorCode: "INVALID_VALUE"
+  apiErrorCode = "INVALID_VALUE"
 
   constructor(fieldName: string, uniqueFieldValues: Array<string>) {
-    this.operationOutcomeCode = "value"
-    this.apiErrorCode = "INVALID_VALUE"
-    this.severity = "error"
     this.message = `Expected all MedicationRequests to have the same value for ${fieldName}. Received ${[...uniqueFieldValues]}.`
   }
 }
 
 export class NoEntryInBundleError implements ValidationError {
-  operationOutcomeCode: "value"
-  severity: "fatal"
+  operationOutcomeCode: "value" = "value"
+  severity: "fatal" = "fatal"
   apiErrorCode: "MISSING_FIELD"
   message: string
 
   constructor() {
-    this.operationOutcomeCode = "value"
-    this.severity = "fatal"
-    this.apiErrorCode = "MISSING_FIELD"
     this.message = "ResourceType Bundle must contain 'entry' field"
   }
 }
 
 export class RequestNotBundleError implements ValidationError {
-  operationOutcomeCode: "value"
-  severity: "fatal"
-  apiErrorCode: "INCORRECT_RESOURCETYPE"
+  operationOutcomeCode: "value" = "value"
+  severity: "fatal" = "fatal"
+  apiErrorCode = "INCORRECT_RESOURCETYPE"
   message: string
 
   constructor() {
-    this.operationOutcomeCode = "value"
-    this.severity = "fatal"
-    this.apiErrorCode = "INCORRECT_RESOURCETYPE"
     this.message = "ResourceType must be 'Bundle' on request"
   }
 }
