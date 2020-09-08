3837a635b31c0ea2f2e0a486d61243ea54229865
Conner Jowitt <conner.jowitt@infinityworks.com>
2020-09-08 11:22:14 +0100

    refactored error classes

--- coordinator/errors/errors.ts
+++ coordinator/errors/errors.ts
@@ -5,78 +5,71 @@ export interface ValidationError {
     severity: "error" | "fatal"
 }
 
-export class EntryNumberError implements ValidationError{
+class ValueError implements ValidationError {
   message: string
-  operationOutcomeCode: "value" = "value"
-  severity: "error" = "error"
+  operationOutcomeCode = "value" as const
   apiErrorCode = "MISSING_FIELD"
+  severity =  "error" as const
 }
 
-export class ContainsExactlyError extends EntryNumberError {
-  message: string
-
+export class ContainsExactlyError extends ValueError {
   constructor(number: number, resourceType: string) {
     super()
     this.message = `Bundle must contain exactly ${number} resource(s) of type ${resourceType}`
   }
 }
 
-export class ContainsBetweenError extends EntryNumberError {
-  message: string
-
+export class ContainsBetweenError extends ValueError {
   constructor(min: number, max: number, resourceType: string) {
     super()
     this.message = `Bundle must contain between ${min} and ${max} resource(s) of type ${resourceType}`
   }
 }
 
-export class ContainsAtLeastError extends EntryNumberError {
-  message: string
-
+export class ContainsAtLeastError extends ValueError {
   constructor(number: number, resourceType: string) {
     super()
     this.message = `Bundle must contain at least ${number} resource(s) of type ${resourceType}`
   }
 }
 
-export class MissingIdError extends EntryNumberError {
-  message: string
-
+export class MissingIdError extends ValueError {
   constructor() {
     super()
     this.message = "ResourceType Bundle must contain 'id' field"
   }
 }
 
-export class MedicationRequestValueError implements ValidationError {
-  operationOutcomeCode: "value" = "value"
-  severity: "error" = "error"
-  message: string
+export class MedicationRequestValueError extends ValueError {
   apiErrorCode = "INVALID_VALUE"
 
   constructor(fieldName: string, uniqueFieldValues: Array<string>) {
+    super()
     this.message = `Expected all MedicationRequests to have the same value for ${fieldName}. Received ${[...uniqueFieldValues]}.`
   }
 }
 
-export class NoEntryInBundleError implements ValidationError {
-  operationOutcomeCode: "value" = "value"
-  severity: "fatal" = "fatal"
-  apiErrorCode: "MISSING_FIELD"
+class FatalError implements  ValidationError {
+  operationOutcomeCode = "value" as const
+  severity = "fatal" as const
   message: string
+  apiErrorCode: string
+}
+
+export class NoEntryInBundleError extends FatalError{
+  apiErrorCode: "MISSING_FIELD"
 
   constructor() {
+    super()
     this.message = "ResourceType Bundle must contain 'entry' field"
   }
 }
 
-export class RequestNotBundleError implements ValidationError {
-  operationOutcomeCode: "value" = "value"
-  severity: "fatal" = "fatal"
+export class RequestNotBundleError extends FatalError{
   apiErrorCode = "INCORRECT_RESOURCETYPE"
-  message: string
 
   constructor() {
+    super()
     this.message = "ResourceType must be 'Bundle' on request"
   }
 }
