2813b6f2b1b07c1459ad052fb60d41064ab0da68
Alex McCormick <alex.mccormick@infinityworks.com>
2020-09-22 16:27:21 +0100

    AEA-579 Rename routes from post-send-message to post-process-message

--- coordinator/src/routes/index.ts
+++ coordinator/src/routes/index.ts
@@ -1,6 +1,6 @@
 import convertPrescriptionRoutes from "./prescription/post-convert-full-message"
 import preparePrescriptionRoutes from "./prescription/post-prepare-message"
-import sendPrescriptionRoutes from "./prescription/post-send-message"
+import sendPrescriptionRoutes from "./prescription/post-process-message"
 import statusRoutes from "./health/get-status"
 import pollingRoutes from "./prescription/polling"
 
