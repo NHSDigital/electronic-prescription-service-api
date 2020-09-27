ce5930e159cd1b2ba45a4f2aa182af5d57508bdf
Alex McCormick <alex.mccormick@infinityworks.com>
2020-09-27 20:27:01 +0100

    AEA-594 Extract test specs from test resources

--- tests/e2e/pact/resources/test-resources.ts
+++ tests/e2e/pact/resources/test-resources.ts
@@ -1,77 +1,7 @@
 /* eslint-disable */
-import * as XmlJs from "xml-js"
-import { ElementCompact } from "xml-js"
-import * as fs from "fs"
-import * as path from "path"
-import { Bundle, Parameters } from "./fhir-resources"
-import * as LosslessJson from "lossless-json"
-import * as uuid from "uuid"
 import { ConvertSpec } from "./test-convert-specs"
 import { ProcessSpec } from "./test-process-specs"
-
-class ExamplePrescription {
-  description: string
-  fhirMessageUnsigned: Bundle
-  fhirMessageSigned: Bundle
-  fhirMessageCancel: Bundle
-  fhirMessageDigest: Parameters
-  hl7V3Message: ElementCompact
-  hl7V3MessageCancel: ElementCompact
-
-  hl7V3SignatureFragments?: ElementCompact
-  hl7V3FragmentsCanonicalized?: string
-
-  constructor(description: string, location: string) {
-    const fhirMessageUnsignedStr = fs.readFileSync(path.join(__dirname, location, "PrepareRequest-FhirMessageUnsigned.json"), "utf-8")
-    const fhirMessageSignedStr = fs.readFileSync(path.join(__dirname, location, "SendRequest-FhirMessageSigned.json"), "utf-8")
-    const fhirMessageDigestStr = fs.readFileSync(path.join(__dirname, location, "PrepareResponse-FhirMessageDigest.json"), "utf-8")
-    const hl7V3MessageStr = fs.readFileSync(path.join(__dirname, location, "ConvertResponse-Hl7V3Message.xml"), "utf-8")
-
-    this.description = description
-    this.fhirMessageUnsigned = LosslessJson.parse(fhirMessageUnsignedStr)
-    this.fhirMessageSigned = LosslessJson.parse(fhirMessageSignedStr)
-    this.fhirMessageDigest = LosslessJson.parse(fhirMessageDigestStr)
-    this.hl7V3Message = XmlJs.xml2js(hl7V3MessageStr, { compact: true })
-
-    this.fhirMessageUnsigned.identifier.value = uuid.v4()
-    this.fhirMessageSigned.identifier.value = uuid.v4()
-
-    const fhirMessageCancelPath = path.join(__dirname, location, "CancelRequest-FhirMessage.json")
-    if (fs.existsSync(fhirMessageCancelPath)) {
-      const fhirMessageCancelStr = fs.readFileSync(fhirMessageCancelPath, "utf-8")
-      this.fhirMessageCancel = LosslessJson.parse(fhirMessageCancelStr)
-    }
-
-    const hl7V3MessageCancelPath = path.join(__dirname, location, "CancelResponse-Hl7V3Message.xml")
-    if (fs.existsSync(hl7V3MessageCancelPath)) {
-      const hl7V3MessageCancelStr = fs.readFileSync(hl7V3MessageCancelPath, "utf-8")
-      this.hl7V3MessageCancel = XmlJs.xml2js(hl7V3MessageCancelStr, { compact: true })
-    }
-
-  }
-}
-
-const examplePrescription1 = new ExamplePrescription("repeat dispensing", "parent-prescription-1")
-
-const hl7V3SignatureFragments1Str = fs.readFileSync(path.join(__dirname, "./parent-prescription-1/PrepareIntermediate-Hl7V3SignatureFragments.xml"), "utf8")
-const hl7V3SignatureFragments1 = XmlJs.xml2js(hl7V3SignatureFragments1Str, { compact: true }) as ElementCompact
-examplePrescription1.hl7V3SignatureFragments = hl7V3SignatureFragments1
-
-const hl7V3SignatureFragmentsCanonicalized1 = fs.readFileSync(path.join(__dirname, "./parent-prescription-1/PrepareIntermediate-Hl7V3SignatureFragmentsCanonicalized.txt"), "utf8")
-examplePrescription1.hl7V3FragmentsCanonicalized = hl7V3SignatureFragmentsCanonicalized1.replace("\n", "")
-
-const examplePrescription2 = new ExamplePrescription("acute, nominated pharmacy", "parent-prescription-2")
-
-const examplePrescription3 = new ExamplePrescription("homecare", "parent-prescription-3")
-
-//export const examplePrescription4 = new ExamplePrescription("homecare repeat dispensing", "parent-prescription-4")
-
-const specification = [
-  examplePrescription1,
-  examplePrescription2,
-  examplePrescription3
-  //examplePrescription4
-]
+import { specificationSpecs } from "./test-specification-specs"
 
 const sendSpec1 = new ProcessSpec(
   "./parent-prescription",
@@ -93,7 +23,7 @@ const sendSpecs = [
 ]
 
 export const prepareCases = [
-  ...specification.map(example => [example.description, example.fhirMessageUnsigned, example.fhirMessageDigest])
+  ...specificationSpecs.map(example => [example.description, example.fhirMessageUnsigned, example.fhirMessageDigest])
 ]
 
 export const convertCases = [
@@ -104,7 +34,7 @@ export const convertCases = [
 ]
 
 export const sendCases = [
-  ...specification.map(example => [example.description, example.fhirMessageSigned]),
-  ...specification.filter(example => example.fhirMessageCancel).map(example => [`cancel ${example.description}`, example.fhirMessageCancel]),
+  ...specificationSpecs.map(example => [example.description, example.fhirMessageSigned]),
+  ...specificationSpecs.filter(example => example.fhirMessageCancel).map(example => [`cancel ${example.description}`, example.fhirMessageCancel]),
   ...sendSpecs.map(spec => [spec.description, spec.request])
 ]
