eeb51c251a7f13abfa33dc5b60d14a5fc6379398
Alex McCormick <alex.mccormick@infinityworks.com>
2020-09-28 08:34:07 +0100

    AEA-594 Remove body matching from send pact tests

--- tests/e2e/pact/broker/verify.ts
+++ tests/e2e/pact/broker/verify.ts
@@ -16,6 +16,7 @@ async function verify(provider: string): Promise<any> {
     ],
     provider: provider,
     providerVersion: process.env.BUILD_VERSION,
+    providerVersionTags: [process.env.BUILD_VERSION],
     providerBaseUrl: `https://${process.env.APIGEE_ENVIRONMENT}.api.service.nhs.uk/${process.env.SERVICE_BASE_PATH}`,
     logLevel: "info",
     customProviderHeaders: [
