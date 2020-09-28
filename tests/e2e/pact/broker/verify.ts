1b5d92ab24236de3ef889623806cb9d328aa6744
Alex McCormick <alex.mccormick@infinityworks.com>
2020-09-28 06:52:40 +0100

    AEA-594 Fix request filter middleware for pact verification

--- tests/e2e/pact/broker/verify.ts
+++ tests/e2e/pact/broker/verify.ts
@@ -5,28 +5,27 @@ import { Verifier } from "@pact-foundation/pact"
 async function verify(provider: string): Promise<any> { 
   const verifier =  new Verifier({
     publishVerificationResult: true,
-    providerVersion: process.env.BUILD_VERSION,
-    consumerVersionTags: process.env.BUILD_VERSION,
-    providerVersionTags: process.env.BUILD_VERSION,
+    pactBrokerUrl: process.env.PACT_BROKER_URL,
     pactBrokerUsername: process.env.PACT_BROKER_BASIC_AUTH_USERNAME,
     pactBrokerPassword: process.env.PACT_BROKER_BASIC_AUTH_PASSWORD,
+    consumerVersionSelectors: [
+      {
+        pacticipant: process.env.PACT_CONSUMER,
+        version: process.env.BUILD_VERSION
+      }
+    ],
     provider: provider,
-    changeOrigin: true,
-    validateSSL: false,
+    providerVersion: process.env.BUILD_VERSION,
     providerBaseUrl: `https://${process.env.APIGEE_ENVIRONMENT}.api.service.nhs.uk/${process.env.SERVICE_BASE_PATH}`,
     logLevel: "info",
-    requestFilter: (req) => {
+    requestFilter: (req, res, next) => {
       req.headers["x-smoke-test"] = "1"
       if (process.env.APIGEE_ACCESS_TOKEN)
       {
         req.headers["Authorization"] = `Bearer: ${process.env.APIGEE_ACCESS_TOKEN}`
       }
-      return req
-    },
-    pactUrls: [
-      //`${process.env.PACT_BROKER_URL}/pacts/provider/${process.env.PACT_PROVIDER}/consumer/${process.env.PACT_CONSUMER}/version/${process.env.BUILD_VERSION}`,
-      path.resolve(process.cwd(), `pact/pacts/${process.env.PACT_CONSUMER}-${process.env.PACT_PROVIDER}.json`)
-    ]
+      next()
+    }
   })
   
   return await verifier.verifyProvider()
