f45bd0499c51ba089768f77b80dca94279070a13
Alex McCormick <alex.mccormick@infinityworks.com>
2020-09-29 23:41:50 +0100

    AEA-664 Fix formatting in convert-example fetcher

--- tests/e2e/pact/services/convert-example-fetcher.ts
+++ tests/e2e/pact/services/convert-example-fetcher.ts
@@ -1,6 +1,6 @@
 import path from "path"
-import {ConvertCase} from "../models/cases/convert-case"
-import {allExamplePaths, rootPath} from "./example-fetcher"
+import { ConvertCase } from "../models/cases/convert-case"
+import { allExamplePaths, rootPath } from "./example-fetcher"
 
 const convertResponsePaths = allExamplePaths.filter(examplePath => {
 	const filename = path.basename(examplePath)
@@ -11,10 +11,10 @@ const convertRequestPaths: Array<string> = allExamplePaths.filter(examplePath =>
 	const filename = path.basename(examplePath)
 	const isRequestFile = filename.endsWith(".json") && filename.split("-")[1] === "Request"
 	if (isRequestFile) {
-			const requestEndpoint = filename.split("-")[0]
-			const convertResponseFilenames = convertResponsePaths.map(convertResponsePath => path.basename(convertResponsePath))
-			const hasConvertResponse = convertResponseFilenames.some(convertResponseFilename => convertResponseFilename.startsWith(`Convert-Response-${requestEndpoint}`))
-			return hasConvertResponse
+		const requestEndpoint = filename.split("-")[0]
+		const convertResponseFilenames = convertResponsePaths.map(convertResponsePath => path.basename(convertResponsePath))
+		const hasConvertResponse = convertResponseFilenames.some(convertResponseFilename => convertResponseFilename.startsWith(`Convert-Response-${requestEndpoint}`))
+		return hasConvertResponse
 	}
 	return false
 })
@@ -22,30 +22,30 @@ const convertRequestPaths: Array<string> = allExamplePaths.filter(examplePath =>
 const conventionBasedConvertExamples: ConvertCase[] = convertResponsePaths.map(convertResponsePath => new ConvertCase(
 	path.parse(path.relative(path.join(__dirname, rootPath), convertResponsePath)).dir.replace(/\./g, "").replace(/\//g, " ") + " " + path.parse(convertResponsePath).name.split("-")[2].toLowerCase() + " " + path.parse(convertResponsePath).name.split("-")[3] + "_OK example no. " + path.parse(convertResponsePath).name.split("-")[4],
 	convertRequestPaths.find(convertRequestPath =>
-			path.basename(convertRequestPath).split("-")[0] === path.basename(convertResponsePath).split("-")[2]
-			&& path.parse(convertRequestPath).name.split("-")[3] === path.parse(convertResponsePath).name.split("-")[4]
+		path.basename(convertRequestPath).split("-")[0] === path.basename(convertResponsePath).split("-")[2]
+		&& path.parse(convertRequestPath).name.split("-")[3] === path.parse(convertResponsePath).name.split("-")[4]
 	),
 	convertResponsePath
 ))
 
-const convertUnsignedSpecificationSuccessExamples = [1,2,3,4].map(i => new ConvertCase(
-  `parent-prescription-${i} specification example unsigned`,
+const convertUnsignedSpecificationSuccessExamples = [1, 2, 3, 4].map(i => new ConvertCase(
+	`parent-prescription-${i} specification example unsigned`,
 	path.join(__dirname, rootPath, `/../parent-prescription-${i}/PrepareRequest-FhirMessageUnsigned.json`),
-  path.join(__dirname, rootPath, `/../parent-prescription-${i}/ConvertResponse-UnsignedHl7V3Message.xml`)))
+	path.join(__dirname, rootPath, `/../parent-prescription-${i}/ConvertResponse-UnsignedHl7V3Message.xml`)))
 
-const convertSignedSpecificationSuccessExamples = [1,2,3,4].map(i => new ConvertCase(
+const convertSignedSpecificationSuccessExamples = [1, 2, 3, 4].map(i => new ConvertCase(
 	`parent-prescription-${i} specification example signed`,
 	path.join(__dirname, rootPath, `/../parent-prescription-${i}/SendRequest-FhirMessageSigned.json`),
 	path.join(__dirname, rootPath, `/../parent-prescription-${i}/ConvertResponse-SignedHl7V3Message.xml`)))
 
 const convertCancelSpecificationSuccessExamples = [3].map(i => new ConvertCase(
 	`parent-prescription-${i} specification example cancel`,
-  path.join(__dirname, rootPath, `/../parent-prescription-${i}/CancelRequest-FhirMessage.json`),
+	path.join(__dirname, rootPath, `/../parent-prescription-${i}/CancelRequest-FhirMessage.json`),
 	path.join(__dirname, rootPath, `/../parent-prescription-${i}/CancelResponse-Hl7V3Message.xml`)))
 
 export const convertExamples = [
-  ...convertUnsignedSpecificationSuccessExamples,
-  ...convertSignedSpecificationSuccessExamples,
+	...convertUnsignedSpecificationSuccessExamples,
+	...convertSignedSpecificationSuccessExamples,
 	...convertCancelSpecificationSuccessExamples,
 	...conventionBasedConvertExamples
 ]
