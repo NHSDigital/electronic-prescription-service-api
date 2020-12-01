4259ec3afdf5e8ce9dac0c2c896fad877c865a04
Harry Coulter <48717159+harryhmc@users.noreply.github.com>
2020-12-01 09:47:01 +0000

    Update verify-v3.ts

--- tests/e2e/pact/broker/verify-v3.ts
+++ tests/e2e/pact/broker/verify-v3.ts
@@ -16,7 +16,7 @@ async function verify(): Promise<any> {
     logLevel: isLocal? "debug" : "info",
     requestFilter: (req) => {
       req.headers["x-smoke-test"] = "1"
-      req.headers["Authorization"] = child.execSync(`docker run --rm artronics/nhsd-login-docker:latest "${process.env.IDP_URL}"`)
+      req.headers["Authorization"] = `Bearer ${child.execSync(`docker run --rm artronics/nhsd-login-docker:latest "${process.env.IDP_URL}"`)}`
       return req
     },
     pactUrls: isLocal 
