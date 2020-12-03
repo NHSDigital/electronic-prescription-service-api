b0bc3ef4402afbc41bd86be3dfd70e6da2366288
Conner Jowitt <conner.jowitt@infinityworks.com>
2020-12-03 13:49:29 +0000

    return fhir and hl7 when translating responses

--- coordinator/src/routes/util.ts
+++ coordinator/src/routes/util.ts
@@ -26,7 +26,7 @@ export function handleResponse<T>(
       .header("Content-Type", "application/fhir+json; fhirVersion=4.0")
   } else {
     const translatedSpineResponse = translateToFhir(spineResponse)
-    return responseToolkit.response(translatedSpineResponse.body)
+    return responseToolkit.response({fhir: translatedSpineResponse.body, hl7: spineResponse.body.toString()})
       .code(translatedSpineResponse.statusCode)
       .header("Content-Type", "application/fhir+json; fhirVersion=4.0")
   }
