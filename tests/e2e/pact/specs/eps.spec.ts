310825dbd2b42fe9465bacbb4419dcbbcaa01cb4
harryhmc <coulterharry@gmail.com>
2020-12-07 15:51:32 +0000

    AEA-789 review changes

--- tests/e2e/pact/specs/eps.spec.ts
+++ tests/e2e/pact/specs/eps.spec.ts
@@ -59,102 +59,105 @@ jestpact.pactWith(
         const bundleStr = LosslessJson.stringify(message)
         const bundle = JSON.parse(bundleStr) as Bundle
 
-        // if (process.env.APIGEE_ENVIRONMENT && !process.env.APIGEE_ENVIRONMENT.includes("sandbox")) {
+        /**
+         * Don't think we need any of this
+         */
+        /*if (process.env.APIGEE_ENVIRONMENT && !process.env.APIGEE_ENVIRONMENT.includes("sandbox")) {
 
-        //   // upload payload and display from matching prepare response to signing service, get token
+          // upload payload and display from matching prepare response to signing service, get token
 
-        //   const signatureRequest = {
-        //     "algorithm": prepareResponse.parameter[3].valueString,
-        //     "payload": prepareResponse.parameter[0].valueString,
-        //     "display": prepareResponse.parameter[2].valueString
-        //   }
+          const signatureRequest = {
+            "algorithm": prepareResponse.parameter[3].valueString,
+            "payload": prepareResponse.parameter[0].valueString,
+            "display": prepareResponse.parameter[2].valueString
+          }
 
-        //   const signatureRequestOptions: RequestInit = {
-        //     method: 'POST',
-        //     headers: [
-        //       ["Authorization", `Bearer ${process.env.APIGEE_ACCESS_TOKEN}`],
-        //       ["Content-Type", "application/json"]
-        //     ],
-        //     body: JSON.stringify(signatureRequest)
-        //   }
+          const signatureRequestOptions: RequestInit = {
+            method: 'POST',
+            headers: [
+              ["Authorization", `Bearer ${process.env.APIGEE_ACCESS_TOKEN}`],
+              ["Content-Type", "application/json"]
+            ],
+            body: JSON.stringify(signatureRequest)
+          }
 
-        //   const signingServiceUrl = `https://${process.env.APIGEE_ENVIRONMENT}.api.service.nhs.uk/signing-service`
+          const signingServiceUrl = `https://${process.env.APIGEE_ENVIRONMENT}.api.service.nhs.uk/signing-service`
 
-        //   const signatureResponse = await fetch(`${signingServiceUrl}/api/v1/SignatureRequest`, signatureRequestOptions)
-        //   const signatureResponseJson = await signatureResponse.json()
-        //   const token = signatureResponseJson.token
+          const signatureResponse = await fetch(`${signingServiceUrl}/api/v1/SignatureRequest`, signatureRequestOptions)
+          const signatureResponseJson = await signatureResponse.json()
+          const token = signatureResponseJson.token
 
-        //   // get uploaded payload from signing service using token and ignore (required to pass validation in signing service)
+          // get uploaded payload from signing service using token and ignore (required to pass validation in signing service)
 
-        //   const signatureRequestTokenOptions: RequestInit = {
-        //     method: 'GET',
-        //     headers: [
-        //       ["Authorization", `Bearer ${process.env.APIGEE_ACCESS_TOKEN}`],
-        //       ["Content-Type", "application/json"]
-        //     ]
-        //   }
+          const signatureRequestTokenOptions: RequestInit = {
+            method: 'GET',
+            headers: [
+              ["Authorization", `Bearer ${process.env.APIGEE_ACCESS_TOKEN}`],
+              ["Content-Type", "application/json"]
+            ]
+          }
 
-        //   await fetch(`${signingServiceUrl}/api/v1/SignatureRequest/${token}`, signatureRequestTokenOptions)
+          await fetch(`${signingServiceUrl}/api/v1/SignatureRequest/${token}`, signatureRequestTokenOptions)
 
-        //   const convertResponseInteraction = convertResponse.PORX_IN020101SM31 ?? convertResponse.PORX_IN020101SM32
+          const convertResponseInteraction = convertResponse.PORX_IN020101SM31 ?? convertResponse.PORX_IN020101SM32
 
-        //   if (!convertResponseInteraction) {
-        //     return // todo: investigate why convert responses are missing for certain examples
-        //   }
+          if (!convertResponseInteraction) {
+            return // todo: investigate why convert responses are missing for certain examples
+          }
 
-        //   // upload signature and certificate from matching pre-signed convert response example to signing service using token
+          // upload signature and certificate from matching pre-signed convert response example to signing service using token
 
-        //   const signaturePostResponseOptions: RequestInit = {
-        //     method: 'POST',
-        //     headers: [
-        //       ["Authorization", `Bearer ${process.env.APIGEE_ACCESS_TOKEN}`],
-        //       ["Content-Type", "application/json"]
-        //     ],
-        //     body: JSON.stringify({
-        //       "signature": convertResponseInteraction.ControlActEvent.subject.ParentPrescription.pertinentInformation1.pertinentPrescription.author.signatureText.Signature.SignatureValue._text,
-        //       "certificate": convertResponseInteraction.ControlActEvent.subject.ParentPrescription.pertinentInformation1.pertinentPrescription.author.signatureText.Signature.KeyInfo.X509Data.X509Certificate._text
-        //     })
-        //   }
+          const signaturePostResponseOptions: RequestInit = {
+            method: 'POST',
+            headers: [
+              ["Authorization", `Bearer ${process.env.APIGEE_ACCESS_TOKEN}`],
+              ["Content-Type", "application/json"]
+            ],
+            body: JSON.stringify({
+              "signature": convertResponseInteraction.ControlActEvent.subject.ParentPrescription.pertinentInformation1.pertinentPrescription.author.signatureText.Signature.SignatureValue._text,
+              "certificate": convertResponseInteraction.ControlActEvent.subject.ParentPrescription.pertinentInformation1.pertinentPrescription.author.signatureText.Signature.KeyInfo.X509Data.X509Certificate._text
+            })
+          }
 
-        //   await fetch(`${signingServiceUrl}/api/v1/SignatureResponse/${token}`, signaturePostResponseOptions)
+          await fetch(`${signingServiceUrl}/api/v1/SignatureResponse/${token}`, signaturePostResponseOptions)
 
-        //   // get uploaded signature and certificate from signing service using token
+          // get uploaded signature and certificate from signing service using token
 
-        //   const signatureGetResponseOptions: RequestInit = {
-        //     method: 'GET',
-        //     headers: [
-        //       ["Authorization", `Bearer ${process.env.APIGEE_ACCESS_TOKEN}`],
-        //       ["Content-Type", "application/json"]
-        //     ]
-        //   }
+          const signatureGetResponseOptions: RequestInit = {
+            method: 'GET',
+            headers: [
+              ["Authorization", `Bearer ${process.env.APIGEE_ACCESS_TOKEN}`],
+              ["Content-Type", "application/json"]
+            ]
+          }
 
-        //   const signatureGetResponse = await fetch(`${signingServiceUrl}/api/v1/SignatureResponse/${token}`, signatureGetResponseOptions)
-        //   const signatureGetResponseJson = await signatureGetResponse.json()
+          const signatureGetResponse = await fetch(`${signingServiceUrl}/api/v1/SignatureResponse/${token}`, signatureGetResponseOptions)
+          const signatureGetResponseJson = await signatureGetResponse.json()
 
-        //   // get values to replace in provenance xmldsig from signing service signature response
+          // get values to replace in provenance xmldsig from signing service signature response
 
-        //   const digest = Buffer.from(prepareResponse.parameter[1].valueString, "base64").toString() // todo: can get this from signing-service ??
-        //   const regex = /(?<=<DigestValue>).*(?=<\/DigestValue>)/g
-        //   const digestValue = digest.match(regex)[0]
+          const digest = Buffer.from(prepareResponse.parameter[1].valueString, "base64").toString() // todo: can get this from signing-service ??
+          const regex = /(?<=<DigestValue>).*(?=<\/DigestValue>)/g
+          const digestValue = digest.match(regex)[0]
 
-        //   const signature = signatureGetResponseJson.signature
-        //   const certificate = signatureGetResponseJson.certificate
+          const signature = signatureGetResponseJson.signature
+          const certificate = signatureGetResponseJson.certificate
 
-        //   // update provenance xmldsig values from signing service
+          // update provenance xmldsig values from signing service
 
-        //   bundle.entry.forEach(entry => {
-        //     if (entry.resource.resourceType === "Provenance") {
-        //       const provenance = entry.resource as Provenance
-        //       let xmlDSig = Buffer.from(provenance.signature[0].data, "base64").toString()
-        //       const provenanceDSig = xmlDSig.repeat(1)
-        //       xmlDSig = xmlDSig.replace(/(?<=<DigestValue>).*(?=<\/DigestValue>)/g, digestValue)
-        //       xmlDSig = xmlDSig.replace(/(?<=<SignatureValue>).*(?=<\/SignatureValue>)/g, signature)
-        //       xmlDSig = xmlDSig.replace(/(?<=<X509Certificate>).*(?=<\/X509Certificate>)/g, certificate)
-        //       expect(provenanceDSig).toBe(xmlDSig)
-        //       provenance.signature[0].data = Buffer.from(xmlDSig).toString("base64")
-        //     }
-        //   })
-        // }
+          bundle.entry.forEach(entry => {
+            if (entry.resource.resourceType === "Provenance") {
+              const provenance = entry.resource as Provenance
+              let xmlDSig = Buffer.from(provenance.signature[0].data, "base64").toString()
+              const provenanceDSig = xmlDSig.repeat(1)
+              xmlDSig = xmlDSig.replace(/(?<=<DigestValue>).*(?=<\/DigestValue>)/g, digestValue)
+              xmlDSig = xmlDSig.replace(/(?<=<SignatureValue>).*(?=<\/SignatureValue>)/g, signature)
+              xmlDSig = xmlDSig.replace(/(?<=<X509Certificate>).*(?=<\/X509Certificate>)/g, certificate)
+              expect(provenanceDSig).toBe(xmlDSig)
+              provenance.signature[0].data = Buffer.from(xmlDSig).toString("base64")
+            }
+          })
+        }*/
 
         const interaction: InteractionObject = {
           state: null,
