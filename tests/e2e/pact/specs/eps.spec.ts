3b243085d59befad46b9458864fe094e13201124
Alex McCormick <alex.mccormick@infinityworks.com>
2020-12-11 04:41:17 +0000

    Add reference to matchers for eps spec

--- tests/e2e/pact/specs/eps.spec.ts
+++ tests/e2e/pact/specs/eps.spec.ts
@@ -1,4 +1,4 @@
-import { InteractionObject } from "@pact-foundation/pact"
+import { InteractionObject, Matchers } from "@pact-foundation/pact"
 import * as jestpact from "jest-pact"
 import supertest from "supertest"
 import * as TestResources from "../resources/test-resources"
