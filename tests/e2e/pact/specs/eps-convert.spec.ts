baab0fa28054e6096ba7ee21ac08654e87409f17
harryhmc <coulterharry@gmail.com>
2020-12-15 08:44:50 +0000

    AEA-909 refactor convert spec

--- tests/e2e/pact/specs/eps-convert.spec.ts
+++ tests/e2e/pact/specs/eps-convert.spec.ts
@@ -19,9 +19,33 @@ jestpact.pactWith(
       return supertest(url)
     }
 
+    const apiPath = "/$convert"
+
     describe("convert e2e tests", () => {
-      const successfulCases = TestResources.convertCases.filter(convertCase => convertCase[4] === "200")
-      const errorCases = TestResources.convertCases.filter(convertCase => convertCase[4] !== "200")
+      const successfulCases = TestResources.convertCases.filter(convertCase => convertCase[4] === "200-OK")
+      const errorCases = TestResources.convertCases.filter(convertCase => convertCase[4] !== "200-OK")
+
+
+      const createInteraction = (desc: string, requestJson: any, response: string, responseMatcher: string, statusCode: string): InteractionObject => ({
+        state: null,
+        uponReceiving: `a request to convert ${desc} message`,
+        withRequest: {
+          headers: {
+            "Content-Type": "application/fhir+json; fhirVersion=4.0"
+          },
+          method: "POST",
+          path: apiPath,
+          body: requestJson
+        },
+        willRespondWith: {
+          headers: {
+            "Content-Type": "text/plain; charset=utf-8"
+          },
+          body: Matchers.term({ generate: response, matcher: responseMatcher }),
+          status: parseInt(statusCode)
+        }
+      })
+
       test.each(successfulCases)("should be able to convert %s message to HL7V3", async (desc: string, request: Bundle, response: string, responseMatcher: string) => {
         const regex = new RegExp(responseMatcher)
         const isMatch = regex.test(response)
@@ -30,26 +54,7 @@ jestpact.pactWith(
         const requestStr = LosslessJson.stringify(request)
         const requestJson = JSON.parse(requestStr)
 
-        const apiPath = "/$convert"
-        const interaction: InteractionObject = {
-          state: null,
-          uponReceiving: `a request to convert ${desc} message`,
-          withRequest: {
-            headers: {
-              "Content-Type": "application/fhir+json; fhirVersion=4.0"
-            },
-            method: "POST",
-            path: "/$convert",
-            body: requestJson
-          },
-          willRespondWith: {
-            headers: {
-              "Content-Type": "text/plain; charset=utf-8"
-            },
-            body: Matchers.term({ generate: response, matcher: responseMatcher }),
-            status: 200
-          }
-        }
+        const interaction = createInteraction(desc, apiPath, requestJson, response, responseMatcher)
         await provider.addInteraction(interaction)
         await client()
           .post(apiPath)
@@ -66,26 +71,7 @@ jestpact.pactWith(
         const requestStr = LosslessJson.stringify(request)
         const requestJson = JSON.parse(requestStr)
 
-        const apiPath = "/$convert"
-        const interaction: InteractionObject = {
-          state: null,
-          uponReceiving: `a request to convert ${desc} message`,
-          withRequest: {
-            headers: {
-              "Content-Type": "application/fhir+json; fhirVersion=4.0"
-            },
-            method: "POST",
-            path: "/$convert",
-            body: requestJson
-          },
-          willRespondWith: {
-            headers: {
-              "Content-Type": "text/plain; charset=utf-8"
-            },
-            body: Matchers.term({ generate: response, matcher: responseMatcher }),
-            status: parseInt(statusCode)
-          }
-        }
+        const interaction = createInteraction(desc, apiPath, requestJson, response, responseMatcher)
         await provider.addInteraction(interaction)
         await client()
           .post(apiPath)
