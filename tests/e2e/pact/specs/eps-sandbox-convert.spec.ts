064ae2e7b5edbf94b01e700efdf2397408ea18d8
harryhmc <coulterharry@gmail.com>
2020-12-15 15:26:26 +0000

    AEA-909 sandbox convert fix

--- tests/e2e/pact/specs/eps-sandbox-convert.spec.ts
+++ tests/e2e/pact/specs/eps-sandbox-convert.spec.ts
@@ -20,8 +20,10 @@ jestpact.pactWith(
     }
 
     describe("convert sandbox e2e tests", () => {
-
-      test.each(TestResources.convertCases)("should be able to convert %s message to HL7V3", async (desc: string, request: Bundle, response: string, responseMatcher: string) => {
+      const successfulCases = TestResources.convertCases.filter(convertCase => convertCase[4] === "200-OK")
+      const errorCases = TestResources.convertCases.filter(convertCase => convertCase[4] !== "200-OK")
+      const apiPath = "/$convert"
+      test.each(successfulCases)("should be able to convert %s message to HL7V3", async (desc: string, request: Bundle, response: string, responseMatcher: string) => {
         const regex = new RegExp(responseMatcher)
         const isMatch = regex.test(response)
         expect(isMatch).toBe(true)
@@ -29,7 +31,6 @@ jestpact.pactWith(
         const requestStr = LosslessJson.stringify(request)
         const requestJson = JSON.parse(requestStr)
 
-        const apiPath = "/$convert"
         const interaction: InteractionObject = {
           state: null,
           uponReceiving: `a request to convert ${desc} message`,
@@ -55,6 +56,41 @@ jestpact.pactWith(
           .set('Content-Type', 'application/fhir+json; fhirVersion=4.0')
           .send(requestJson)
           .expect(200)
+      })
+
+      test.each(errorCases)("should receive expected error code in response to %s message", async (desc: string, request: Bundle, response: string, responseMatcher: string, statusCode: string) => {
+        const regex = new RegExp(responseMatcher)
+        const isMatch = regex.test(response)
+        expect(isMatch).toBe(true)
+
+        const requestStr = LosslessJson.stringify(request)
+        const requestJson = JSON.parse(requestStr)
+
+        const interaction = {
+          state: null,
+          uponReceiving: `a request to convert ${desc} message`,
+          withRequest: {
+            headers: {
+              "Content-Type": "application/fhir+json; fhirVersion=4.0"
+            },
+            method: "POST",
+            path: apiPath,
+            body: requestJson
+          },
+          willRespondWith: {
+            headers: {
+              "Content-Type": "application/json"
+            },
+            body: Matchers.term({ generate: response, matcher: responseMatcher }),
+            status: parseInt(statusCode)
+          }
+        }
+        await provider.addInteraction(interaction)
+        await client()
+          .post(apiPath)
+          .set('Content-Type', 'application/fhir+json; fhirVersion=4.0')
+          .send(requestJson)
+          .expect(parseInt(statusCode))
       })
     })
   }
