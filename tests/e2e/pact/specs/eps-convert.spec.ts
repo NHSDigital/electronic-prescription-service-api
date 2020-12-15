92fb956b9bba669acf41d5ae80f39d4b768a7668
harryhmc <coulterharry@gmail.com>
2020-12-15 09:28:50 +0000

    AEA-909 linting

--- tests/e2e/pact/specs/eps-convert.spec.ts
+++ tests/e2e/pact/specs/eps-convert.spec.ts
@@ -1,4 +1,4 @@
-import {InteractionObject, Matchers} from "@pact-foundation/pact"
+import {Matchers} from "@pact-foundation/pact"
 import * as jestpact from "jest-pact"
 import supertest from "supertest"
 import * as TestResources from "../resources/test-resources"
