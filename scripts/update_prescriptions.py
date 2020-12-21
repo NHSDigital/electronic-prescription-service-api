c5008634820696ec8aac3d24d210f7af1822e02b
Matt Jarvis <matt.jarvis@gmail.com>
2020-12-21 16:43:30 +0000

    Allow Authorization header in CORS preflight check.

--- scripts/update_prescriptions.py
+++ scripts/update_prescriptions.py
@@ -10,103 +10,111 @@ import requests
 examples_root_dir = "../models/examples/"
 
 
-def shortPrescID():
+def generate_short_form_id():
     """Create R2 (short format) Prescription ID
     Build the prescription ID and add the required checkdigit.
     Checkdigit is selected from the PRESCRIPTION_CHECKDIGIT_VALUES constant
     """
-    _PRESC_CHECKDIGIT_VALUES = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ+'
-    hexString = str(uuid.uuid1()).replace('-', '').upper()
-    prescriptionID = hexString[:6] + '-Z' + hexString[6:11] + '-' + hexString[12:17]
-    prscID = prescriptionID.replace('-', '')
-    prscIDLength = len(prscID)
-    runningTotal = 0
-    for stringPosition in range(prscIDLength):
-        runningTotal = runningTotal + int(prscID[stringPosition], 36) * (2 ** (prscIDLength - stringPosition))
-    checkValue = (38 - runningTotal % 37) % 37
-    checkValue = _PRESC_CHECKDIGIT_VALUES[checkValue]
-    prescriptionID += checkValue
-    return prescriptionID
+    _PRESCRIPTION_CHECKDIGIT_VALUES = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ+'
+    hex_string = str(uuid.uuid1()).replace('-', '').upper()
+    prescription_id = f'{hex_string[:6]}-Z{hex_string[6:11]}-{hex_string[12:17]}'
+    prescription_id_digits = prescription_id.replace('-', '')
+    prescription_id_digits_length = len(prescription_id_digits)
+    running_total = 0
+    for string_position in range(prescription_id_digits_length):
+        running_total = running_total\
+                       + int(prescription_id_digits[string_position], 36)\
+                       * (2 ** (prescription_id_digits_length - string_position))
+    check_value = (38 - running_total % 37) % 37
+    check_value = _PRESCRIPTION_CHECKDIGIT_VALUES[check_value]
+    prescription_id += check_value
+    return prescription_id
 
 
-def loadPrepareExamples():
+def load_prepare_examples():
     for filename in glob.iglob(examples_root_dir + '**/*Prepare-Request*.json', recursive=True):
         yield filename
 
 
-def replaceIdsAndAuthoredOn(exampleJson, prescription_id, short_prescription_id, authored_on):
-    for entry in exampleJson['entry']:
+def replace_ids_and_timestamps(bundle_json, prescription_id, short_prescription_id, authored_on):
+    for entry in bundle_json['entry']:
         resource = entry["resource"]
-        if (resource["resourceType"] == "MedicationRequest"):
+        if resource["resourceType"] == "MedicationRequest":
             resource["groupIdentifier"]["value"] = short_prescription_id
+            for extension in resource["groupIdentifier"]["extension"]:
+                if extension["url"] == "https://fhir.nhs.uk/R4/StructureDefinition/Extension-PrescriptionId":
+                    extension["valueIdentifier"]["value"] = prescription_id
             resource["authoredOn"] = authored_on
+        if resource["resourceType"] == "Provenance":
+            for signature in resource["signature"]:
+                signature["when"] = authored_on
 
 
-def updatePrepareExamples(prepare, prescription_id, short_prescription_id, authored_on):
+def update_prepare_examples(prepare, prescription_id, short_prescription_id, authored_on):
     with open(prepare) as f:
-        prepareRequest = json.load(f)
-    replaceIdsAndAuthoredOn(prepareRequest, prescription_id, short_prescription_id, authored_on)
+        prepare_request_json = json.load(f)
+    replace_ids_and_timestamps(prepare_request_json, prescription_id, short_prescription_id, authored_on)
     with open(prepare, 'w') as f:
-        json.dump(prepareRequest, f, indent=2)
+        json.dump(prepare_request_json, f, indent=2)
 
-    prepareResponseJson = requests.post(
+    prepare_response_json = requests.post(
         'http://localhost:9000/$prepare',
-        data=json.dumps(prepareRequest),
+        data=json.dumps(prepare_request_json),
         headers={'Content-Type': 'application/json'}).json()
 
-    dir = os.path.dirname(prepare)
+    example_dir = os.path.dirname(prepare)
     file = os.path.basename(prepare)
     filename_parts = file.split('-')
     number = filename_parts[0]
     status_code_and_ext = filename_parts[4] if len(filename_parts) == 5 else filename_parts[3]
 
-    prepareResponse = dir + '/' + number + '-Prepare-Response-' + status_code_and_ext
+    prepare_response = f'{example_dir}/{number}-Prepare-Response-{status_code_and_ext}'
 
-    with open(prepareResponse, 'w') as f:
-        json.dump(prepareResponseJson, f, indent=2)
+    with open(prepare_response, 'w') as f:
+        json.dump(prepare_response_json, f, indent=2)
 
 
-def updateProcessExamples(prepare, prescription_id, short_prescription_id, authored_on):
-    dir = os.path.dirname(prepare)
+def update_process_examples(prepare, prescription_id, short_prescription_id, authored_on):
+    example_dir = os.path.dirname(prepare)
     file = os.path.basename(prepare)
     filename_parts = file.split('-')
     number = filename_parts[0]
     status_code_and_ext = filename_parts[4] if len(filename_parts) == 5 else filename_parts[3]
 
-    for process in glob.iglob(dir + '/' + number + '-Process-Request-*-' + status_code_and_ext):
+    for process in glob.iglob(f'{example_dir}/{number}-Process-Request-*-{status_code_and_ext}'):
         with open(process) as f:
-            processJson = json.load(f)
-        replaceIdsAndAuthoredOn(processJson, prescription_id, short_prescription_id, authored_on)
+            process_request_json = json.load(f)
+        replace_ids_and_timestamps(process_request_json, prescription_id, short_prescription_id, authored_on)
         with open(process, 'w') as f:
-            json.dump(processJson, f, indent=2)
+            json.dump(process_request_json, f, indent=2)
 
-        dir = os.path.dirname(process)
+        example_dir = os.path.dirname(process)
         file = os.path.basename(process)
         filename_parts = file.split('-')
         number = filename_parts[0]
         operation = filename_parts[3]
         status_code_and_ext = filename_parts[4] if len(filename_parts) == 5 else filename_parts[3]
         status_code_and_xml_ext = status_code_and_ext.replace("json", "xml")
-        convertResponse = dir + '/' + number + '-Convert-Response-' + operation + '-' + status_code_and_xml_ext
+        convert_response = f'{example_dir}/{number}-Convert-Response-{operation}-{status_code_and_xml_ext}'
 
-        convertResponseXml = requests.post(
+        convert_response_xml = requests.post(
             'http://localhost:9000/$convert',
-            data=json.dumps(processJson),
+            data=json.dumps(process_request_json),
             headers={'Content-Type': 'application/json'}).text
 
-        with open(convertResponse, 'w') as f:
-            f.write(convertResponseXml)
+        with open(convert_response, 'w') as f:
+            f.write(convert_response_xml)
 
 
-def updateExamples():
+def update_examples():
     authored_on = datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S+00:00')
 
-    for prepare in loadPrepareExamples():
+    for prepare in load_prepare_examples():
         prescription_id = str(uuid.uuid4())
-        short_prescription_id = shortPrescID()
+        short_prescription_id = generate_short_form_id()
 
-        updatePrepareExamples(prepare, prescription_id, short_prescription_id, authored_on)
-        updateProcessExamples(prepare, prescription_id, short_prescription_id, authored_on)
+        update_prepare_examples(prepare, prescription_id, short_prescription_id, authored_on)
+        update_process_examples(prepare, prescription_id, short_prescription_id, authored_on)
 
 
-updateExamples()
+update_examples()
