bf21a949ff632aea0c86130549fc6b28a3a0074d
Richard <768747+ri-ch@users.noreply.github.com>
2020-12-22 15:05:53 +0000

    Include validator in status check. Make build wait for correct endpoint

--- coordinator/src/routes/health/get-status.ts
+++ coordinator/src/routes/health/get-status.ts
@@ -1,11 +1,22 @@
 import Hapi from "@hapi/hapi"
+import Axios from "axios"
 
 export default [
   {
     method: "GET",
     path: "/_status",
-    handler: (request: Hapi.Request, h: Hapi.ResponseToolkit): Hapi.ResponseObject => {
-      return h.response("Coordinator is alive")
+    handler: async (request: Hapi.Request, h: Hapi.ResponseToolkit): Promise<Hapi.ResponseObject> => {
+      const response = await Axios.get<string>("http://localhost:9001/_status", {timeout: 2})
+
+      if (response.status != 200 || response.data != "Validator is alive") {
+        request.logger.warn("Did not get positive response from validator status check")
+        return h.response().code(400)
+      }
+
+      return h.response({
+        message: "Coordinator is alive",
+        commitId: process.env.COMMIT_ID
+      })
     }
   }
 ]
