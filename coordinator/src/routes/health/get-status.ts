02b80331a73c8b89cbac93d6734566af00b00e90
Richard <768747+ri-ch@users.noreply.github.com>
2020-12-23 21:54:48 +0000

    Different status

--- coordinator/src/routes/health/get-status.ts
+++ coordinator/src/routes/health/get-status.ts
@@ -7,20 +7,23 @@ export default [
     method: "GET",
     path: "/_status",
     handler: async (request: Hapi.Request, h: Hapi.ResponseToolkit): Promise<Hapi.ResponseObject> => {
+      let validator = false
 
       try {
         const response = await axios.get<string>(`${VALIDATOR_HOST}/_status`, {timeout: 2})
 
-        if (response.status != 200 || response.data != "Validator is alive") {
+        if (response.status == 200 && response.data != "Validator is alive") {
+          validator = true;
+        } else {
           request.logger.warn("Did not get positive response from validator status check")
-          return h.response().code(400)
         }
       } catch (err) {
         request.logger.error(`Got error when making request for validator status: ${err}`)
-        return h.response().code(400)
       }
 
       return h.response({
+        coordinator: true,
+        validator,
         message: "Coordinator is alive",
         commitId: process.env.COMMIT_ID
       })
