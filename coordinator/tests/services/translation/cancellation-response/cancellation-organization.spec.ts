ea09d9130efa4f66bdfc635e7db0d863df1b3d7e
conner-jowitt <55279861+conner-jowitt@users.noreply.github.com>
2020-12-23 15:26:47 +0000

    add dispense translations to a cancellation response (#297)
    * return hl7 message with fhir response
    
    * hard code sending to int spine instead of viet07
    
    * tests written for practitioner role and organisation for performer translations. New resources added to bundle
    
    * moved comments
    
    * add dispense request reference in MedicationRequest resource and add tests around new functionality
    
    * remove connection to int and only return FHIR object
    
    * add organization display name in MedicationRequest.dispenseRequest
    
    * update sandbox pact to reflect new sandbox return type
    
    * changed medicationRequest dispense request to reference PractitionerRole instead of Practitioner
    
    * remove identifier block in sandbox process pact test for now
    
    * comment out body check
    
    Co-authored-by: Alex McCormick <amccmick1@googlemail.com>
    Co-authored-by: Dan Jones <dan.jones@infinityworks.com>
    Co-authored-by: dan-jones-iw <55280310+dan-jones-iw@users.noreply.github.com>
    Co-authored-by: Matt Jarvis <61870561+mfjarvis@users.noreply.github.com>

--- coordinator/tests/services/translation/cancellation-response/cancellation-organization.spec.ts
+++ coordinator/tests/services/translation/cancellation-response/cancellation-organization.spec.ts
@@ -1,48 +1,68 @@
 import * as TestResources from "../../../resources/test-resources"
 import {getIdentifierValueForSystem} from "../../../../src/services/translation/common"
-import {readXml} from "../../../../src/services/serialisation/xml"
 import {createOrganization} from "../../../../src/services/translation/cancellation/cancellation-organization"
-import {SPINE_CANCELLATION_ERROR_RESPONSE_REGEX} from "../../../../src/services/translation/spine-response"
+import {getCancellationResponse} from "./test-helpers"
+import * as fhir from "../../../../src/models/fhir/fhir-resources"
+import * as hl7 from "../../../../src/models/hl7-v3/hl7-v3-people-places"
 
-describe("createOrganization", () => {
-  const actualError = TestResources.spineResponses.cancellationError
-  const preParsedMsg = SPINE_CANCELLATION_ERROR_RESPONSE_REGEX.exec(actualError.response.body)[0]
-  const parsedMsg = readXml(preParsedMsg)
-  const actEvent = parsedMsg["hl7:PORX_IN050101UK31"]["hl7:ControlActEvent"]
-  const cancellationResponse = actEvent["hl7:subject"].CancellationResponse
-  const authorOrganization = createOrganization(cancellationResponse.author.AgentPerson.representedOrganization)
+const cancellationResponse = getCancellationResponse(TestResources.spineResponses.cancellationError)
+const cancellationDispensedResponse = getCancellationResponse(TestResources.spineResponses.cancellationDispensedError)
 
-  test("has an identifier block with the correct value", () => {
-    expect(authorOrganization.identifier).not.toBeUndefined()
-    const identifierValue = getIdentifierValueForSystem(
-      authorOrganization.identifier,
-      "https://fhir.nhs.uk/Id/ods-organization-code",
-      "Organization.identifier"
-    )
-    expect(identifierValue).toBe("RBA")
+const authorRepresentedOrganization = cancellationResponse.author.AgentPerson.representedOrganization
+const performerRepresentedOrganization = cancellationDispensedResponse.performer.AgentPerson.representedOrganization
+
+const authorOrganization = createOrganization(authorRepresentedOrganization)
+const performerOrganization = createOrganization(performerRepresentedOrganization)
+
+const cases = [
+  ["authorOrganization", authorOrganization, authorRepresentedOrganization],
+  ["performerOrganization", performerOrganization, performerRepresentedOrganization]
+]
+
+describe.each(cases)(
+  "createOrganization",
+  (organizationName: string, fhirOrganization: fhir.Organization, hl7Organization: hl7.Organization) => {
+
+    test(
+      "%p has an identifier block with the correct value",
+      () => {
+        expect(fhirOrganization.identifier).not.toBeUndefined()
+        const identifierValue = getIdentifierValueForSystem(
+          fhirOrganization.identifier,
+          "https://fhir.nhs.uk/Id/ods-organization-code",
+          "Organization.identifier"
+        )
+        expect(identifierValue).toBe(hl7Organization.id._attributes.extension)
+      })
+
+    test(
+      "%p has a type block with correct coding values",
+      () => {
+        expect(fhirOrganization.type).not.toBeUndefined()
+        expect(fhirOrganization.type[0].coding[0].code).toBe("RO197")
+        expect(fhirOrganization.type[0].coding[0].display).toBeTruthy()
+      })
+
+    test(
+      "%p has correct name value",
+      () => {
+        expect(fhirOrganization.name).toBe(hl7Organization.name._text)
+      })
+
+    test(
+      "%p has correct telecom value",
+      () => {
+        expect(fhirOrganization.telecom[0].system).toBe("phone")
+        expect(fhirOrganization.telecom[0].value).toBe(hl7Organization.telecom._attributes.value.split(":")[1])
+        expect(fhirOrganization.telecom[0].use).toBe("work")
+      })
+
+    test(
+      "%p has correct address value",
+      () => {
+        expect(fhirOrganization.address[0].postalCode).toBe(hl7Organization.addr.postalCode._text)
+        fhirOrganization.address[0].line.forEach(
+          (line, index) => expect(line).toBe(hl7Organization.addr.streetAddressLine[index]._text)
+        )
+      })
   })
-
-  test("has a type block with correct coding values", () => {
-    expect(authorOrganization.type).not.toBeUndefined()
-    expect(authorOrganization.type[0].coding[0].code).toBe("RO197")
-    expect(authorOrganization.type[0].coding[0].display).toBeTruthy()
-  })
-
-  test("has correct name value", () => {
-    expect(authorOrganization.name).toBe("TAUNTON AND SOMERSET NHS FOUNDATION TRUST")
-  })
-
-  test("has correct telecom value", () => {
-    expect(authorOrganization.telecom[0].system).toBe("phone")
-    expect(authorOrganization.telecom[0].value).toBe("01823333444")
-    expect(authorOrganization.telecom[0].use).toBe("work")
-  })
-
-  test("has correct address value", () => {
-    expect(authorOrganization.address[0].postalCode).toBe("TA1 5DA")
-    expect(authorOrganization.address[0].line.length).toBe(3)
-    expect(authorOrganization.address[0].line[0]).toBe("MUSGROVE PARK HOSPITAL")
-    expect(authorOrganization.address[0].line[1]).toBe("PARKFIELD DRIVE")
-    expect(authorOrganization.address[0].line[2]).toBe("TAUNTON")
-  })
-})
