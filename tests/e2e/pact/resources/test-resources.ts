8c7379ed79ddbde0ac2685700ea4487f004020bb
Alex McCormick <alex.mccormick@infinityworks.com>
2021-01-04 21:30:10 +0000

    Fix status code/status text refactor

--- tests/e2e/pact/resources/test-resources.ts
+++ tests/e2e/pact/resources/test-resources.ts
@@ -3,6 +3,6 @@ import {convertExamples} from "../services/convert-example-fetcher"
 import {prepareExamples} from "../services/prepare-example-fetcher"
 
 export const convertCases = convertExamples.filter(e => e.isSuccess).map(spec => [spec.description, spec.request, spec.response, spec.responseMatcher, spec.statusCode])
-export const convertErrorCases = convertExamples.filter(e => !e.isSuccess).map(spec => [spec.description, spec.request, spec.response, spec.responseMatcher, spec.statusCode])
+export const convertErrorCases = convertExamples.filter(e => !e.isSuccess).map(spec => [spec.description, spec.request, spec.response, spec.statusCode])
 export const prepareCases = prepareExamples.filter(e => e.isSuccess).map(spec => [spec.description, spec.request, spec.response, spec.statusCode])
 export const processCases = processExamples.filter(e => e.isSuccess).map(spec => [spec.description, spec.request, spec.prepareResponse, spec.convertResponse, spec.statusCode])
