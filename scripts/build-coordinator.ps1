e3440801ef03fadd947a0c5e0ac1ece1a81cca48
Alex McCormick <alex.mccormick@infinityworks.com>
2021-01-05 08:35:14 +0000

    Update build-coordinator windows script

--- scripts/build-coordinator.ps1
+++ scripts/build-coordinator.ps1
@@ -2,4 +2,3 @@ npm run --prefix=coordinator/ build
 cp coordinator/package.json coordinator/dist/
 mkdir -Force coordinator/dist/resources
 cp coordinator/src/resources/ebxml_request.mustache coordinator/dist/resources/
-cp coordinator/src/resources/message_display.mustache coordinator/dist/resources/
