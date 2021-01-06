1f2953956d21399420310802919e7239a43d9fdd
Matt Jarvis <matt.jarvis@gmail.com>
2021-01-06 12:45:29 +0000

    AEA-965: Add tests to verify the signatures attached to our convert responses.

---
+++ coordinator/tests/verify-prescription-signatures.spec.ts
@@ -0,0 +1,71 @@
+import {ElementCompact, xml2js} from "xml-js"
+import {writeXmlStringCanonicalized} from "../src/services/serialisation/xml"
+import * as crypto from "crypto"
+import {readFileSync} from "fs"
+import * as path from "path"
+import {SendMessagePayload} from "../src/models/hl7-v3/hl7-v3-datatypes-core"
+import {ParentPrescriptionRoot} from "../src/models/hl7-v3/hl7-v3-prescriptions"
+import {createParametersDigest} from "../src/services/translation"
+import {convertFragmentsToHashableFormat, extractFragments} from "../src/services/translation/prescription/signature"
+import {toArray} from "../src/services/translation/common"
+import {specification} from "./resources/test-resources"
+
+const verify = crypto.createVerify("RSA-SHA1")
+
+function extractDigestFromSignatureRoot(signatureRoot: ElementCompact) {
+  const signature = signatureRoot.Signature
+  const signedInfo = signature.SignedInfo
+  signedInfo._attributes = {
+    xmlns: signature._attributes.xmlns
+  }
+  return writeXmlStringCanonicalized({SignedInfo: signedInfo})
+}
+
+function verifySignatureValid(signatureRoot: ElementCompact) {
+  const digest = extractDigestFromSignatureRoot(signatureRoot)
+  const signature = signatureRoot.Signature
+  const signatureValue = signature.SignatureValue._text
+  const certificateValue = signature.KeyInfo.X509Data.X509Certificate._text
+  const certificateValueWithStuff = `-----BEGIN CERTIFICATE-----\n${certificateValue}\n-----END CERTIFICATE-----`
+  verify.update(digest)
+  return verify.verify(certificateValueWithStuff, signatureValue, "base64")
+}
+
+function verifySignatureValidAndMatchesPrescription(prescriptionRoot: ElementCompact) {
+  const sendMessagePayload = prescriptionRoot.PORX_IN020101SM31 as SendMessagePayload<ParentPrescriptionRoot>
+  const parentPrescription = sendMessagePayload.ControlActEvent.subject.ParentPrescription
+  const pertinentPrescription = parentPrescription.pertinentInformation1.pertinentPrescription
+  const signatureRoot = pertinentPrescription.author.signatureText as ElementCompact
+  const digestFromSignature = extractDigestFromSignatureRoot(signatureRoot)
+
+  pertinentPrescription.pertinentInformation2 = toArray(pertinentPrescription.pertinentInformation2)
+  const fragments = extractFragments(parentPrescription)
+  const fragmentsToBeHashed = convertFragmentsToHashableFormat(fragments)
+  const digestFromPrescriptionBase64 = createParametersDigest(fragmentsToBeHashed)
+  const digestFromPrescription = Buffer.from(digestFromPrescriptionBase64, "base64").toString("utf-8")
+
+  if (digestFromPrescription !== digestFromSignature) {
+    return false
+  }
+
+  return verifySignatureValid(signatureRoot)
+}
+
+test.skip("verify prescription signature for specific prescription", () => {
+  //eslint-disable-next-line max-len
+  const prescriptionPath = "../../models/examples/secondary-care/community/acute/nominated-pharmacy/clinical-practitioner/1-Convert-Response-Send-200_OK.xml";
+  const prescriptionStr = readFileSync(path.join(__dirname, prescriptionPath), "utf-8")
+  const prescriptionRoot = xml2js(prescriptionStr, {compact: true})
+  const result = verifySignatureValidAndMatchesPrescription(prescriptionRoot)
+  expect(result).toBeTruthy()
+})
+
+const cases = specification.map(examplePrescription => [
+  examplePrescription.description,
+  examplePrescription.hl7V3Message
+])
+
+test.skip.each(cases)("verify prescription signature for %s", (desc: string, hl7V3Message: ElementCompact) => {
+  const result = verifySignatureValidAndMatchesPrescription(hl7V3Message)
+  expect(result).toBeTruthy()
+})
