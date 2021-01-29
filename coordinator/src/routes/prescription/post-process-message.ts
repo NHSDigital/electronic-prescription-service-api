8d9a770b813282d783ba979c7c7d2e527d275938
Dan Jones <dan.jones@infinityworks.com>
2021-01-29 16:26:25 +0000

    remove dispense specific endpoint and include release endpoint

--- coordinator/src/routes/prescription/post-process-message.ts
+++ coordinator/src/routes/prescription/post-process-message.ts
@@ -3,6 +3,17 @@ import * as fhir from "../../models/fhir/fhir-resources"
 import {requestHandler} from "../../services/handlers"
 import Hapi from "@hapi/hapi"
 import {createHash, handleResponse, validatingHandler} from "../util"
+import {createOperationOutcomeIssue} from "../../services/translation/spine-response"
+import {getMessageHeader} from "../../services/translation/common/getResourcesOfType"
+
+const sandboxDispenseNotificationResponse: fhir.OperationOutcome = {
+  resourceType: "OperationOutcome",
+  issue: [createOperationOutcomeIssue(200)]
+}
+
+function isDispenseMessage(bundle: fhir.Bundle) {
+  return getMessageHeader(bundle).eventCoding.code === "prescription-dispense"
+}
 
 export default [
   /*
@@ -10,9 +21,12 @@ export default [
     */
   {
     method: "POST",
-    path: "/prescribe/$process-message",
+    path: "$process-message",
     handler: validatingHandler(
       async (bundle: fhir.Bundle, request: Hapi.Request, responseToolkit: Hapi.ResponseToolkit) => {
+        if (isDispenseMessage(bundle)) {
+          return responseToolkit.response(sandboxDispenseNotificationResponse).code(200)
+        }
         request.logger.info("Building Spine request")
         const spineRequest = translator.convertFhirMessageToSpineRequest(bundle)
         request.log("audit", {"incomingMessageHash": createHash(JSON.stringify(bundle))})
