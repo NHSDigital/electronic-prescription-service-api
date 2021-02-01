56810dc5315706e0b9b51c8fac336b70166d17f3
Conner Jowitt <conner.jowitt@infinityworks.com>
2021-02-01 16:38:03 +0000

    pass X-Request-ID header to pact tests sandbox

--- tests/e2e/pact/specs/sandbox/eps-sandbox.process.spec.ts
+++ tests/e2e/pact/specs/sandbox/eps-sandbox.process.spec.ts
@@ -5,6 +5,7 @@ import * as TestResources from "../../resources/test-resources"
 import {Bundle} from "../../models/fhir/fhir-resources"
 import * as LosslessJson from "lossless-json"
 import {processExamples} from "../../services/process-example-fetcher"
+import * as uuid from "uuid"
 
 jestpact.pactWith(
   {
@@ -24,12 +25,14 @@ jestpact.pactWith(
       test.each(TestResources.processCases)("should be able to process %s", async (desc: string, message: Bundle) => {
         const apiPath = "/$process-message"
         const messageStr = LosslessJson.stringify(message)
+        const requestId = uuid.v4().toString().toLowerCase()
         const interaction: InteractionObject = {
           state: "is not authenticated",
           uponReceiving: `a request to process ${desc} message to Spine`,
           withRequest: {
             headers: {
-              "Content-Type": "application/fhir+json; fhirVersion=4.0"
+              "Content-Type": "application/fhir+json; fhirVersion=4.0",
+              "X-Request-ID": requestId
             },
             method: "POST",
             path: "/$process-message",
@@ -49,6 +52,7 @@ jestpact.pactWith(
         await client()
           .post(apiPath)
           .set('Content-Type', 'application/fhir+json; fhirVersion=4.0')
+          .set('X-Request-ID', requestId)
           .send(messageStr)
           .expect(200)
       })
@@ -58,6 +62,7 @@ jestpact.pactWith(
 
         const apiPath = "/$process-message"
         const messageStr = LosslessJson.stringify(testCase.request)
+        const requestId = uuid.v4().toString().toLowerCase()
 
         const interaction: InteractionObject = {
           state: "is not authenticated",
@@ -65,7 +70,8 @@ jestpact.pactWith(
           withRequest: {
             headers: {
               "Content-Type": "application/fhir+json; fhirVersion=4.0",
-              "Accept": "application/fhir+json"
+              "Accept": "application/fhir+json",
+              "X-Request-ID": requestId
             },
             method: "POST",
             path: "/$process-message",
@@ -80,6 +86,7 @@ jestpact.pactWith(
           .post(apiPath)
           .set('Content-Type', 'application/fhir+json; fhirVersion=4.0')
           .set('Accept', 'application/fhir+json')
+          .set('X-Request-ID', requestId)
           .send(messageStr)
           .expect(200)
       })
