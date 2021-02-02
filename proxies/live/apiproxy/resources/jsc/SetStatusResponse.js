5ee718d337f9178b9f589d7d9abd9b6ac08e4122
Matthew Clark <matthew.clark21@nhs.net>
2021-02-02 15:16:53 +0000

    AMB-468 Changed SetStatus response to SetStatusResponse.js

--- proxies/live/apiproxy/resources/jsc/SetStatusResponse.js
+++ proxies/live/apiproxy/resources/jsc/SetStatusResponse.js
@@ -1,4 +1,54 @@
-response = JSON.parse(context.getVariable("response.content"))
-health = response.validator && response.coordinator
-status = health ? "pass" : "failure"
-context.setVariable("health.total", status)
+// These values are rendered by pipeline's template step
+var version = "{{ DEPLOYED_VERSION }}"
+var releaseId = "{{ RELEASE_RELEASEID }}"
+var commitId = "{{ SOURCE_COMMIT_ID }}"
+
+var apiproxy_revision = context.getVariable('apiproxy.revision');
+
+var healthcheck_service_response_code = context.getVariable('healthcheckResponse.status.code');
+var healthcheck_service_response = context.getVariable('healthcheckResponse.content');
+var healthcheck_service_request_url = context.getVariable('healthcheckRequest.url');
+
+var healthcheck_service_request_has_failed = context.getVariable("servicecallout.ServiceCallout.CallHealthcheckEndpoint.failed");
+
+var healthcheck_service_status = "fail";
+
+if(healthcheck_service_response_code/ 100 == 2){
+  healthcheck_service_status = "pass";
+}
+
+timeout = "false";
+
+if(healthcheck_service_response_code === null && healthcheck_service_request_has_failed){
+  timeout = "true";
+}
+
+var healthcheck_service = {
+  "healthcheckService:status" : [
+    {
+      "status": healthcheck_service_status,
+      "timeout" : timeout,
+      "responseCode" : healthcheck_service_response_code,
+      "outcome": healthcheck_service_response,
+      "links" : {"self": healthcheck_service_request_url}
+    }]
+};
+
+var apigee_status = "pass";
+
+if(healthcheck_service_status != "pass"){
+  apigee_status = "fail";
+}
+
+var response = {
+  "status" : apigee_status,
+  "version" : version ,
+  "revision" : apiproxy_revision,
+  "releaseId" : releaseId,
+  "commitId": commitId,
+  "checks" : healthcheck_service
+};
+
+context.setVariable("status.response", JSON.stringify(response));
+context.setVariable("response.content", JSON.stringify(response));
+context.setVariable("response.header.Content-Type", "application/json");
