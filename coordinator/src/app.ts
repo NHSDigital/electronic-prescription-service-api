89999d7ec7c7f2dac4c265d588c97e77cf921722
Conner Jowitt <conner.jowitt@infinityworks.com>
2021-02-02 16:59:05 +0000

    Revert app.ts to master version

--- coordinator/src/app.ts
+++ coordinator/src/app.ts
@@ -1,4 +1,4 @@
-import * as Boom from "@hapi/boom"
+import {Boom} from "@hapi/boom"
 import Hapi from "@hapi/hapi"
 import routes from "./routes"
 import {FhirMessageProcessingError, toOperationOutcome} from "./models/errors/processing-errors"
@@ -9,7 +9,7 @@ const preResponse = function (request: Hapi.Request, responseToolkit: Hapi.Respo
   if (response instanceof FhirMessageProcessingError) {
     request.log("info", response)
     return responseToolkit.response(toOperationOutcome(response)).code(400)
-  } else if (response instanceof Boom.Boom) {
+  } else if (response instanceof Boom) {
     request.log("error", response)
   }
   return responseToolkit.continue
@@ -23,12 +23,6 @@ const init = async () => {
       cors: true, // Won't run as Apigee hosted target without this
       payload: {
         parse: false
-      },
-      validate: {
-        failAction: handleValidationError,
-        options: {
-          abortEarly: false
-        }
       }
     }
   })
@@ -48,30 +42,6 @@ const init = async () => {
 
   await server.start()
   server.log("info", `Server running on ${server.info.uri}`)
-}
-
-export interface ValidationError extends Boom.Boom {
-  output: Boom.Output & {
-    payload: Boom.Payload & {
-      validation: {
-        source: string
-        keys: Array<string>
-      }
-    }
-  }
-}
-
-export const handleValidationError = (request: Hapi.Request, _: Hapi.ResponseToolkit, err?: Error): Boom.Boom => {
-  if (!err) {
-    request.log("error", "handleValidationError called without an Error")
-    return Boom.internal()
-  }
-  // hapi deliberately doesn't return the full error to the client for security reasons,
-  // log the full details to aid investigation, and return just the source and message
-  request.log("error", err)
-  const source = (err as ValidationError)?.output?.payload?.validation?.source
-  const message = source ? `Invalid request ${source}: ${err.message}` : err.message
-  return Boom.badRequest(message)
 }
 
 process.on("unhandledRejection", (err) => {
