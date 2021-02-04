e5a6d5e4f0e453fd9a0eebab25e72038c25a18cb
conner-jowitt <55279861+conner-jowitt@users.noreply.github.com>
2021-02-04 15:55:03 +0000

    Aea 550 add X-Request-ID header (#341)
    * X-Request-Header mandatory, changed mapping of bundle.identifier
    
    * Put X-Request-ID into EBXML wrapper, request header no longer required on prepare endpoint
    
    * Update tests
    
    * Update models to pass smoke tests
    
    * Update cancellation to use bundle.identifier instead of bundle.id for prescription wrapper ID
    
    * pass X-Request-ID header to pact tests
    
    * more verbose errors for required headers, fixing broken smoke tests
    
    * generate new uuid for each pact test
    
    * pass X-Request-ID header to pact tests sandbox
    
    * why does hapi lower case headers?
    
    * Copy request header flows from PDS
    
    * Remove validation from $process as it's in proxy now, look for NHSD instead of X-Request-ID
    
    * Add X-Request_ID header to all smoke tests
    
    * Make X-Request-ID header mandatory
    
    * Add extra steps from PDS proxy, pass request header to spine
    
    * Check to see if the response to pact tests includes X-Request-ID header
    
    * Remove broken step from proxy
    
    * Add steps to copy headers in sandbox
    
    * Make sandbox proxy match live proxy more closely
    
    * Add missing error
    
    * Fixing broken things hopefully
    
    * Add more steps to sandbox flow
    
    * More status check to be first
    
    * Revert app.ts to master version
    
    * change error message on missing or malformed x-request-id header
    
    * Fix error in missing header
    
    * Fix dispense smoke tests
    
    * merge master and fix conflicts
    
    * stop hiding raised faults
    
    * change generating uuids, centralise message ID
    
    * Tidy smoke tests
    
    * rename xRequestId variable to requestId
    
    * wrap messageId in SpineRequest
    
    * Added X-Request-ID header to spec, updated error  severity to be fatal
    
    * Fix tests
    
    * Add X-Correlation-ID header to smoke tests, fixed formatting on smoke tests, made sandbox tests more closely match live tests

--- tests/e2e/pact/specs/live/eps.process.spec.ts
+++ tests/e2e/pact/specs/live/eps.process.spec.ts
@@ -1,10 +1,11 @@
-import { InteractionObject } from "@pact-foundation/pact"
+import {InteractionObject} from "@pact-foundation/pact"
 import * as jestpact from "jest-pact"
 import supertest from "supertest"
 import * as TestResources from "../../resources/test-resources"
-import { Bundle } from "../../models/fhir/fhir-resources"
+import {Bundle} from "../../models/fhir/fhir-resources"
 import * as LosslessJson from "lossless-json"
-import { createUnauthorisedInteraction } from "./eps-auth"
+import {createUnauthorisedInteraction} from "./eps-auth"
+import * as uuid from "uuid"
 import {pactOptions} from "../../resources/common"
 
 jestpact.pactWith(
@@ -22,35 +23,44 @@ jestpact.pactWith(
       test(authenticationTestDescription, async () => {
         const apiPath = "/$process-message"
         const interaction: InteractionObject = createUnauthorisedInteraction(authenticationTestDescription, apiPath)
+        const requestId = uuid.v4()
+        const correlationId = uuid.v4()
         await provider.addInteraction(interaction)
         await client()
           .post(apiPath)
           .set('Content-Type', 'application/fhir+json; fhirVersion=4.0')
+          .set('X-Request-ID', requestId)
+          .set('X-Correlation-ID', correlationId)
           .send({})
           .expect(401)
       })
     })
 
     describe("process-message e2e tests", () => {
-      test.each(TestResources.processCases)("should be able to process %s", async (desc: string, message: Bundle) => {
+      test.each(TestResources.processCases)("should be able to process a %s message", async (desc: string, request: Bundle) => {
         const apiPath = "/$process-message"
-        const bundleStr = LosslessJson.stringify(message)
-        const bundle = JSON.parse(bundleStr) as Bundle
+        const requestStr = LosslessJson.stringify(request)
+        const requestId = uuid.v4()
+        const correlationId = uuid.v4()
 
         const interaction: InteractionObject = {
           state: "is authenticated",
           uponReceiving: `a request to process ${desc} message to Spine`,
           withRequest: {
             headers: {
-              "Content-Type": "application/fhir+json; fhirVersion=4.0"
+              "Content-Type": "application/fhir+json; fhirVersion=4.0",
+              "X-Request-ID": requestId,
+              "X-Correlation-ID": correlationId
             },
             method: "POST",
-            path: "/$process-message",
-            body: bundle
+            path: apiPath,
+            body: JSON.parse(requestStr)
           },
           willRespondWith: {
             headers: {
-              "Content-Type": "application/json"
+              "Content-Type": "application/json",
+              "X-Request-ID": requestId,
+              "X-Correlation-ID": correlationId
             },
             body: {
               resourceType: "OperationOutcome",
@@ -76,7 +86,9 @@ jestpact.pactWith(
         await client()
           .post(apiPath)
           .set('Content-Type', 'application/fhir+json; fhirVersion=4.0')
-          .send(bundleStr)
+          .set('X-Request-ID', requestId)
+          .set('X-Correlation-ID', correlationId)
+          .send(requestStr)
           .expect(400)
       })
     })
