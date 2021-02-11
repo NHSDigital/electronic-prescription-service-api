36d2744e39593baab003e92f8f0c7882a232a7dc
Alex McCormick <amccmick1@googlemail.com>
2021-02-11 14:17:49 +0000

    Test coverage of update-prescriptions for all value replacements

--- scripts/update_prescriptions.py
+++ scripts/update_prescriptions.py
@@ -15,13 +15,13 @@ import uuid
 from datetime import date, datetime, timedelta
 from docopt import docopt
 
-examples_root_dir = "../models/examples/"
+examples_root_dir = f"..{os.path.sep}models{os.path.sep}examples{os.path.sep}"
 api_prefix_url = "FHIR/R4"
 
 
 def generate_short_form_id(organisationCode):
     """Create R2 (short format) Prescription ID
-    Build the prescription ID and add the required checkdigit.
+    Build the prescription ID around the organisation code and add the required checkdigit.
     Checkdigit is selected from the PRESCRIPTION_CHECKDIGIT_VALUES constant
     """
     _PRESCRIPTION_CHECKDIGIT_VALUES = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ+'
@@ -63,66 +63,72 @@ def find_prepare_request_paths(examples_root_dir):
 
 
 def load_prepare_request(prepare_request_path):
-    with open(prepare_request_path) as f:
+    return load_json(prepare_request_path)
+
+
+def load_process_request(process_request_path):
+    return load_json(process_request_path)
+
+
+def load_json(path):
+    with open(path) as f:
         return json.load(f)
 
 
 def save_prepare_request(prepare_request_path, prepare_request_json):
-    with open(prepare_request_path, 'w') as f:
-        json.dump(prepare_request_json, f, indent=2)
+    save_json(prepare_request_path, prepare_request_json)
 
 
-def create_short_form_id(prepare_request_json):
+def save_prepare_response(prepare_response_path, prepare_response_json):
+    save_json(prepare_response_path, prepare_response_json)
+
+
+def save_process_request(process_request_path, process_request_json):
+    save_json(process_request_path, process_request_json)
+
+
+def save_convert_response(convert_response_path, convert_response_xml):
+    save_xml(convert_response_path, convert_response_xml)
+
+
+def save_json(path, body):
+    with open(path, 'w') as f:
+        json.dump(body, f, indent=2)
+
+
+def save_xml(path, body):
+    with open(path, 'w') as f:
+        f.write(body)
+
+
+def get_organisation_code(prepare_request_json):
     for entry in prepare_request_json['entry']:
         resource = entry["resource"]
         if resource["resourceType"] == "HealthcareService":
             for identifier in resource["identifier"]:
-                organisationCode = identifier["value"]
-                return generate_short_form_id(organisationCode)
+                return identifier["value"]
 
 
-def update_prepare_examples(api_base_url, prepare_request_path, prepare_request_json, prescription_id, authored_on):
-    short_prescription_id = create_short_form_id(prepare_request_json)
-    update_prescription(prepare_request_json, prescription_id, short_prescription_id, authored_on, None)
-    save_prepare_request(prepare_request_path, prepare_request_json)
-
-    prepare_response_json = requests.post(
+def get_prepare_response_from_a_sandbox_api(api_base_url, prepare_request_json):
+    return requests.post(
         f'{api_base_url}/{api_prefix_url}/$prepare',
         data=json.dumps(prepare_request_json),
         headers={'Content-Type': 'application/json', 'X-Request-ID': str(uuid.uuid4())}
     ).json()
 
-    prepare_response_path = derive_prepare_response_path(prepare_request_path)
-    with open(prepare_response_path, 'w') as f:
-        json.dump(prepare_response_json, f, indent=2)
 
+def get_convert_response_from_a_sandbox_api(api_base_url, process_request_json):
+    return requests.post(
+        f'{api_base_url}/{api_prefix_url}/$convert',
+        data=json.dumps(process_request_json),
+        headers={'Content-Type': 'application/json', 'X-Request-ID': str(uuid.uuid4())}
+    ).text
+
+
+def get_signature_timestamp_from_prepare_response(prepare_response_json):
     for parameter in prepare_response_json["parameter"]:
         if parameter["name"] == "timestamp":
-            return short_prescription_id, parameter["valueString"]
-
-
-def update_process_examples(
-    api_base_url, prepare_request_path, prescription_id, short_prescription_id, authored_on, signature_time
-):
-    process_request_path_pattern = derive_process_request_path_pattern(prepare_request_path)
-    for process_request_path in glob.iglob(process_request_path_pattern):
-        with open(process_request_path) as f:
-            process_request_json = json.load(f)
-        update_prescription(
-            process_request_json, prescription_id, short_prescription_id, authored_on, signature_time
-        )
-        with open(process_request_path, 'w') as f:
-            json.dump(process_request_json, f, indent=2)
-
-        convert_response_xml = requests.post(
-            f'{api_base_url}/{api_prefix_url}/$convert',
-            data=json.dumps(process_request_json),
-            headers={'Content-Type': 'application/json', 'X-Request-ID': str(uuid.uuid4())}
-        ).text
-
-        convert_response_path = derive_convert_response_path(process_request_path)
-        with open(convert_response_path, 'w') as f:
-            f.write(convert_response_xml)
+            return parameter["valueString"]
 
 
 def derive_prepare_response_path(prepare_request_path):
@@ -154,14 +160,38 @@ def derive_convert_response_path(process_request_path):
     return f'{example_dir}/{number}-Convert-Response-{operation}-{status_code_and_xml_ext}'
 
 
+def update_prepare_examples(api_base_url, prepare_request_path, prepare_request_json, prescription_id, authored_on):
+    organisation_code = get_organisation_code(prepare_request_json)
+    short_prescription_id = generate_short_form_id(organisation_code)
+    update_prescription(prepare_request_json, prescription_id, short_prescription_id, authored_on, None)
+    save_prepare_request(prepare_request_path, prepare_request_json)
+    prepare_response_json = get_prepare_response_from_a_sandbox_api(api_base_url, prepare_request_json)
+    prepare_response_path = derive_prepare_response_path(prepare_request_path)
+    save_prepare_response(prepare_response_path, prepare_response_json)
+    signature_time = get_signature_timestamp_from_prepare_response(prepare_response_json)
+    return short_prescription_id, signature_time
+
+
+def update_process_examples(
+    api_base_url, prepare_request_path, prescription_id, short_prescription_id, authored_on, signature_time
+):
+    process_request_path_pattern = derive_process_request_path_pattern(prepare_request_path)
+    for process_request_path in glob.iglob(process_request_path_pattern):
+        process_request_json = load_process_request(process_request_path)
+        update_prescription(
+            process_request_json, prescription_id, short_prescription_id, authored_on, signature_time
+        )
+        save_process_request(process_request_path, process_request_json)
+        convert_response_xml = get_convert_response_from_a_sandbox_api(api_base_url, process_request_json)
+        convert_response_path = derive_convert_response_path(process_request_path)
+        save_convert_response(convert_response_path, convert_response_xml)
+
+
 def update_examples(api_base_url):
     authored_on = datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S+00:00')
-
     for prepare_request_path in find_prepare_request_paths(examples_root_dir):
         prescription_id = str(uuid.uuid4())
-
         prepare_request_json = load_prepare_request(prepare_request_path)
-
         short_prescription_id, signature_time = update_prepare_examples(
             api_base_url, prepare_request_path, prepare_request_json, prescription_id, authored_on
         )
@@ -180,13 +210,11 @@ if __name__ == "__main__":
 
 
 # Tests
-
 test_examples_root_dir = f".{os.path.sep}models{os.path.sep}examples{os.path.sep}"
 
 
-# repeat-dispensing example to cover validity_period test in addition to base tests
-
 @pytest.fixture
+# ensure example is repeat-dispensing to cover validity_period test in addition to other test
 def test_bundle_json():
     process_request_path_root = f'{test_examples_root_dir}{os.path.sep}**{os.path.sep}'
     process_request_sub_path = f'repeat-dispensing{os.path.sep}**{os.path.sep}'
@@ -196,6 +224,17 @@ def test_bundle_json():
     with open(process_request_path) as f:
         process_request_json = json.load(f)
     return process_request_json
+
+
+@pytest.fixture
+def test_prepare_response_json():
+    prepare_response_path_root = f'{test_examples_root_dir}{os.path.sep}**{os.path.sep}'
+    prepare_response_file = '1-Prepare-Response-200_OK.json'
+    prepare_response_path_pattern = f'{prepare_response_path_root}{prepare_response_file}'
+    prepare_response_path = next(glob.iglob(prepare_response_path_pattern, recursive=True))
+    with open(prepare_response_path) as f:
+        prepare_response_json = json.load(f)
+    return prepare_response_json
 
 
 def test_generate_short_form_id__contains_org_code_in_middle():
@@ -270,11 +309,21 @@ def test_update_prescription__sets_validity_period_for_4_weeks_from_today(test_b
     assert validityEnd == (date.today() + timedelta(weeks=4)).isoformat()
 
 
-def test_create_short_form_id__creates_short_form_id_with_org_code(test_bundle_json):
-    short_form_id = create_short_form_id(test_bundle_json)
+def test_get_organisation_code__gets_org_code_from_bundle(test_bundle_json):
+    organisationCode = get_organisation_code(test_bundle_json)
     for entry in test_bundle_json['entry']:
         resource = entry["resource"]
         if resource["resourceType"] == "HealthcareService":
             for identifier in resource["identifier"]:
-                organisationCode = identifier["value"]
-    assert organisationCode in short_form_id
+                actualOrganisationCode = identifier["value"]
+    assert organisationCode == actualOrganisationCode
+
+
+def test_get_signature_timestamp_from_prepare_response(test_prepare_response_json):
+    print(test_prepare_response_json)
+    signature_time = "newValue"
+    for parameter in test_prepare_response_json["parameter"]:
+        if parameter["name"] == "timestamp":
+            parameter["valueString"] = signature_time
+    signature_time_updated = get_signature_timestamp_from_prepare_response(test_prepare_response_json)
+    assert signature_time == signature_time_updated
