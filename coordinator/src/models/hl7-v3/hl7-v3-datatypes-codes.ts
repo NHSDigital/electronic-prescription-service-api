ed48283928a0419169a52d5aaf5ee622522a5428
Conner Jowitt <conner.jowitt@infinityworks.com>
2021-02-12 12:13:53 +0000

    update interaction identifier

--- coordinator/src/models/hl7-v3/hl7-v3-datatypes-codes.ts
+++ coordinator/src/models/hl7-v3/hl7-v3-datatypes-codes.ts
@@ -226,7 +226,7 @@ export class Hl7InteractionIdentifier extends Identifier<"2.16.840.1.113883.2.1.
 
     static PARENT_PRESCRIPTION_URGENT = new Hl7InteractionIdentifier("PORX_IN020101SM31")
     static CANCEL_REQUEST = new Hl7InteractionIdentifier("PORX_IN030101SM32")
-    static NOMINATED_PRESCRIPTION_RELEASE_REQUEST = new Hl7InteractionIdentifier("PORX_IN030101SM32")
+    static NOMINATED_PRESCRIPTION_RELEASE_REQUEST = new Hl7InteractionIdentifier("PORX_IN060102UK30")
 }
 
 export class AccreditedSystemIdentifier extends Identifier<"1.2.826.0.1285.0.2.0.107"> {
