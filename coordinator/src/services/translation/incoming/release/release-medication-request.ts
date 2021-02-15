91bf8731260087429107108e38865d968111a319
Matt Jarvis <matt.jarvis@gmail.com>
2021-02-15 17:29:47 +0000

    AEA-1037: WIP: Add some more fields. Pass in less of the message to each method to make testing easier.

--- coordinator/src/services/translation/incoming/release/release-medication-request.ts
+++ coordinator/src/services/translation/incoming/release/release-medication-request.ts
@@ -1,5 +1,13 @@
 import * as fhir from "../../../../models/fhir/fhir-resources"
-import {LineItem, Prescription} from "../../../../models/hl7-v3/hl7-v3-prescriptions"
+import {
+  DaysSupply,
+  DispensingSitePreference,
+  DosageInstructions,
+  LineItem,
+  LineItemQuantity, Performer,
+  Prescription,
+  PrescriptionTreatmentType, PrescriptionType
+} from "../../../../models/hl7-v3/hl7-v3-prescriptions"
 import * as uuid from "uuid"
 import {
   createGroupIdentifier,
@@ -10,8 +18,9 @@ import {createCodeableConcept, createReference} from "../fhir-base-types"
 import * as codes from "../../../../models/hl7-v3/hl7-v3-datatypes-codes"
 import {PrescriptionTreatmentTypeCode, SnomedCode} from "../../../../models/hl7-v3/hl7-v3-datatypes-codes"
 import {CourseOfTherapyTypeCode} from "../../prescription/course-of-therapy-type"
-import {Interval, IntervalUnanchored, Timestamp} from "../../../../models/hl7-v3/hl7-v3-datatypes-core"
+import {Interval, IntervalUnanchored, NumericValue, Timestamp} from "../../../../models/hl7-v3/hl7-v3-datatypes-core"
 import {convertHL7V3DateToIsoDateString} from "../../common"
+import {Organization} from "../../../../models/hl7-v3/hl7-v3-people-places";
 
 export function createMedicationRequest(
   prescription: Prescription,
@@ -25,9 +34,9 @@ export function createMedicationRequest(
     id: uuid.v4(),
     extension: [
       //TODO - repeat information
-      createResponsiblePractitionerExtension(responsiblePartyId)
+      createResponsiblePractitionerExtension(responsiblePartyId),
       //TODO - endorsements
-      //TODO - prescription type
+      createPrescriptionTypeExtension(prescription.pertinentInformation4.pertinentPrescriptionType)
       //TODO - controlled drugs
     ],
     identifier: [
@@ -42,29 +51,47 @@ export function createMedicationRequest(
     authoredOn: undefined, //TODO - how do we populate this?
     requester: createReference(requesterId),
     groupIdentifier: createGroupIdentifierFromPrescriptionIds(prescription.id),
-    courseOfTherapyType: createCourseOfTherapyType(prescription, lineItem),
+    courseOfTherapyType: createCourseOfTherapyType(
+      prescription.pertinentInformation5.pertinentPrescriptionTreatmentType,
+      lineItem.repeatNumber
+    ),
     dosageInstruction: [
-      createDosage(lineItem)
+      createDosage(lineItem.pertinentInformation2.pertinentDosageInstructions)
     ],
-    dispenseRequest: createDispenseRequest(prescription, lineItem),
+    dispenseRequest: createDispenseRequest(
+      prescription.pertinentInformation1.pertinentDispensingSitePreference,
+      lineItem.component.lineItemQuantity,
+      prescription.component1.daysSupply,
+      prescription.performer
+    ),
     substitution: createSubstitution()
   }
 }
 
-function isRepeatDispensing(prescription: Prescription) {
-  const prescriptionTreatmentTypeCode = prescription.pertinentInformation5.pertinentPrescriptionTreatmentType.value
-  return prescriptionTreatmentTypeCode._attributes.code
-    === PrescriptionTreatmentTypeCode.CONTINUOUS_REPEAT_DISPENSING._attributes.code
+function createPrescriptionTypeExtension(pertinentPrescriptionType: PrescriptionType): fhir.CodingExtension {
+  return {
+    url: "https://fhir.nhs.uk/StructureDefinition/Extension-DM-PrescriptionType",
+    valueCoding: {
+      system: "https://fhir.nhs.uk/CodeSystem/prescription-type",
+      code: pertinentPrescriptionType.value._attributes.code,
+      display: pertinentPrescriptionType.value._attributes.displayName
+    }
+  }
 }
 
-function createCourseOfTherapyType(prescription: Prescription, lineItem: LineItem) {
-  if (isRepeatDispensing(prescription)) {
+function createCourseOfTherapyType(
+  prescriptionTreatmentType: PrescriptionTreatmentType,
+  lineItemRepeatNumber: Interval<NumericValue>
+) {
+  const isRepeatDispensing = prescriptionTreatmentType.value._attributes.code
+    === PrescriptionTreatmentTypeCode.CONTINUOUS_REPEAT_DISPENSING._attributes.code
+  if (isRepeatDispensing) {
     return createCodeableConcept(
       "https://fhir.nhs.uk/CodeSystem/medicationrequest-course-of-therapy",
       CourseOfTherapyTypeCode.CONTINUOUS_REPEAT_DISPENSING,
       "Continuous long term (repeat dispensing)"
     )
-  } else if (lineItem.repeatNumber) {
+  } else if (lineItemRepeatNumber) {
     return createCodeableConcept(
       "http://terminology.hl7.org/CodeSystem/medicationrequest-course-of-therapy",
       CourseOfTherapyTypeCode.CONTINUOUS,
@@ -83,15 +110,15 @@ function createSnomedCodeableConcept(code: SnomedCode) {
   return createCodeableConcept("http://snomed.info/sct", code._attributes.code, code._attributes.displayName)
 }
 
-function createDosage(lineItem: LineItem): fhir.Dosage {
+function createDosage(dosageInstructions: DosageInstructions): fhir.Dosage {
   //TODO - additional instructions
   return {
-    text: lineItem.pertinentInformation2.pertinentDosageInstructions.value._text
+    text: dosageInstructions.value._text
   }
 }
 
-function createDispenseRequestQuantity(lineItem: LineItem): fhir.SimpleQuantity {
-  const lineItemQuantityTranslation = lineItem.component.lineItemQuantity.quantity.translation
+function createDispenseRequestQuantity(lineItemQuantity: LineItemQuantity): fhir.SimpleQuantity {
+  const lineItemQuantityTranslation = lineItemQuantity.quantity.translation
   return {
     value: lineItemQuantityTranslation._attributes.value,
     unit: lineItemQuantityTranslation._attributes.displayName,
@@ -116,28 +143,43 @@ function createExpectedSupplyDuration(expectedUseTime: IntervalUnanchored): fhir
   }
 }
 
-function createDispenseRequest(prescription: Prescription, lineItem: LineItem): fhir.MedicationRequestDispenseRequest {
+function createDispenseRequest(
+  dispensingSitePreference: DispensingSitePreference,
+  lineItemQuantity: LineItemQuantity,
+  daysSupply: DaysSupply,
+  performer: Performer
+): fhir.MedicationRequestDispenseRequest {
   const dispenseRequest: fhir.MedicationRequestDispenseRequest = {
     extension: [
-      createPerformerSiteTypeExtension(prescription)
+      createPerformerSiteTypeExtension(dispensingSitePreference)
     ],
-    quantity: createDispenseRequestQuantity(lineItem)
+    quantity: createDispenseRequestQuantity(lineItemQuantity)
   }
-  if (isRepeatDispensing(prescription)) {
-    const daysSupply = prescription.component1.daysSupply
+  if (daysSupply) {
     dispenseRequest.validityPeriod = createValidityPeriod(daysSupply.effectiveTime)
     dispenseRequest.expectedSupplyDuration = createExpectedSupplyDuration(daysSupply.expectedUseTime)
   }
-  //TODO - performer
+  if (performer) {
+    dispenseRequest.performer = createPerformer(performer.AgentOrgSDS.agentOrganizationSDS)
+  }
   return dispenseRequest
 }
 
-function createPerformerSiteTypeExtension(prescription: Prescription): fhir.CodingExtension {
+function createPerformerSiteTypeExtension(dispensingSitePreference: DispensingSitePreference): fhir.CodingExtension {
   return {
     url: "https://fhir.nhs.uk/StructureDefinition/Extension-performerSiteType",
     valueCoding: {
       code: "https://fhir.nhs.uk/CodeSystem/dispensing-site-preference",
-      display: prescription.pertinentInformation1.pertinentDispensingSitePreference.value._attributes.code
+      display: dispensingSitePreference.value._attributes.code
+    }
+  }
+}
+
+function createPerformer(performerOrganization: Organization): fhir.Performer {
+  return {
+    identifier: {
+      system: "https://fhir.nhs.uk/Id/ods-organization-code",
+      value: performerOrganization.id._attributes.extension
     }
   }
 }
