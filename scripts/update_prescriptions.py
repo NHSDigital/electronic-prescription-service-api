8e9ca623ad4e531eb852f4d7fe1aa7fd035034c6
Alex McCormick <amccmick1@googlemail.com>
2021-02-18 12:11:11 +0000

    Update-prescriptions ensure bundle id is the same for prepare, process and convert group

--- scripts/update_prescriptions.py
+++ scripts/update_prescriptions.py
@@ -40,8 +40,8 @@ def generate_short_form_id(organisationCode):
     return prescription_id
 
 
-def update_prescription(bundle_json, prescription_id, short_prescription_id, authored_on, signature_time):
-    bundle_json["identifier"]["value"] = str(uuid.uuid4())
+def update_prescription(bundle_json, bundle_id, prescription_id, short_prescription_id, authored_on, signature_time):
+    bundle_json["identifier"]["value"] = bundle_id
     for entry in bundle_json['entry']:
         resource = entry["resource"]
         if resource["resourceType"] == "Provenance":
@@ -181,11 +181,11 @@ def derive_convert_response_path(process_request_path):
     return f'{example_dir}/{number}-Convert-Response-{operation}-{status_code_and_xml_ext}'
 
 
-def update_prepare_examples(api_base_url, prepare_request_path, prepare_request_json, prescription_id, authored_on):
+def update_prepare_examples(api_base_url, prepare_request_path, prepare_request_json, bundle_id, prescription_id, authored_on):
     try:
         organisation_code = get_organisation_code(prepare_request_json)
         short_prescription_id = generate_short_form_id(organisation_code)
-        update_prescription(prepare_request_json, prescription_id, short_prescription_id, authored_on, None)
+        update_prescription(prepare_request_json, bundle_id, prescription_id, short_prescription_id, authored_on, None)
         save_prepare_request(prepare_request_path, prepare_request_json)
         prepare_response_json = get_prepare_response_from_a_sandbox_api(api_base_url, prepare_request_json)
         prepare_response_path = derive_prepare_response_path(prepare_request_path)
@@ -198,14 +198,14 @@ def update_prepare_examples(api_base_url, prepare_request_path, prepare_request_
 
 
 def update_process_examples(
-    api_base_url, prepare_request_path, prescription_id, short_prescription_id, authored_on, signature_time
+    api_base_url, prepare_request_path, bundle_id, prescription_id, short_prescription_id, authored_on, signature_time
 ):
     try:
         process_request_path_pattern = derive_process_request_path_pattern(prepare_request_path)
         for process_request_path in glob.iglob(process_request_path_pattern):
             process_request_json = load_process_request(process_request_path)
             update_prescription(
-                process_request_json, prescription_id, short_prescription_id, authored_on, signature_time
+                process_request_json, bundle_id, prescription_id, short_prescription_id, authored_on, signature_time
             )
             save_process_request(process_request_path, process_request_json)
             convert_response_xml = get_convert_response_from_a_sandbox_api(api_base_url, process_request_json)
@@ -220,13 +220,15 @@ def update_examples(api_base_url):
     authored_on = datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S+00:00')
     for prepare_request_path in find_prepare_request_paths(examples_root_dir):
         prescription_id = str(uuid.uuid4())
+        bundle_id = str(uuid.uuid4())
         prepare_request_json = load_prepare_request(prepare_request_path)
         short_prescription_id, signature_time = update_prepare_examples(
-            api_base_url, prepare_request_path, prepare_request_json, prescription_id, authored_on
+            api_base_url, prepare_request_path, prepare_request_json, bundle_id, prescription_id,
+            authored_on
         )
         update_process_examples(
-            api_base_url, prepare_request_path, prescription_id, short_prescription_id, authored_on,
-            signature_time
+            api_base_url, prepare_request_path, bundle_id, prescription_id, short_prescription_id,
+            authored_on, signature_time
         )
 
 
