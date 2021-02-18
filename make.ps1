dde548440c27b9ea5a7f3630d8c4c472fff4dbfa
Alex McCormick <amccmick1@googlemail.com>
2021-02-18 19:42:54 +0000

    Undo accidental deletion of variables in run-smoke-tests

--- make.ps1
+++ make.ps1
@@ -93,7 +93,7 @@ function run-smoke-tests() {
     }
     $env:PACT_PROVIDER="nhsd-apim-eps$provider_suffix"
     $env:SERVICE_BASE_PATH="electronic-prescriptions$pr_prefix$pr"
-    $env:PACT_TAG="$env"	
+    $env:PACT_TAG="$env"
     $env:PACT_VERSION="$env:USERNAME".replace(' ','')
     $env:APIGEE_ACCESS_TOKEN="$token"
     $env:PACT_PROVIDER_URL="https://$env.api.service.nhs.uk/$env:SERVICE_BASE_PATH"
