7426d6a0757767fcb23f8b8b921d74348197658e
Conner Jowitt <conner.jowitt@infinityworks.com>
2021-03-12 14:53:06 +0000

    typo

--- tests/e2e/pact/services/task-example-fetcher.ts
+++ tests/e2e/pact/services/task-example-fetcher.ts
@@ -2,5 +2,5 @@ import {exampleFiles} from "./example-files-fetcher"
 import {ReleaseCase} from "../models/cases/release-case"
 
 export const taskExamples = exampleFiles
-  .filter(exampleFile => exampleFile.isRequest && exampleFile.endpoint === "Task")
+  .filter(exampleFile => exampleFile.isRequest && exampleFile.endpoint === "task")
   .map(exampleFile => new ReleaseCase(exampleFile, null))
