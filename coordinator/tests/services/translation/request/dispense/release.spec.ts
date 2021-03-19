a55075473110f6e93f0d82d8a822ae07421d7cbe
Matt Jarvis <matt.jarvis@gmail.com>
2021-03-19 14:57:23 +0000

    APM-1486: Fix release request test.

--- coordinator/tests/services/translation/request/dispense/release.spec.ts
+++ coordinator/tests/services/translation/request/dispense/release.spec.ts
@@ -10,21 +10,22 @@ jest.mock("../../../../../src/services/translation/request/agent-unattended", ()
   createAuthorForUnattendedAccess: jest.fn()
 }))
 
-describe("translateReleaseRequest", async () => {
-  const mockAuthorResponse = new hl7V3.Author()
-  mockAuthorResponse.AgentPerson = new hl7V3.AgentPerson()
-  const mockAuthorFunction = createAuthorForUnattendedAccess as jest.Mock
-  mockAuthorFunction.mockReturnValueOnce(Promise.resolve(mockAuthorResponse))
-  const parameters = new fhir.Parameters([{
-    "name": "owner",
-    "valueIdentifier": {
-      "system": "https://fhir.nhs.uk/Id/ods-organization-code",
-      "value": "FTX40"
-    }
-  }])
-  const translatedRelease = await translateReleaseRequest(parameters, logger)
-
+describe("translateReleaseRequest", () => {
   test("translated release contains author details from ODS", async () => {
+    const mockAuthorResponse = new hl7V3.Author()
+    mockAuthorResponse.AgentPerson = new hl7V3.AgentPerson()
+    const mockAuthorFunction = createAuthorForUnattendedAccess as jest.Mock
+    mockAuthorFunction.mockReturnValueOnce(Promise.resolve(mockAuthorResponse))
+    const parameters = new fhir.Parameters([{
+      "name": "owner",
+      "valueIdentifier": {
+        "system": "https://fhir.nhs.uk/Id/ods-organization-code",
+        "value": "FTX40"
+      }
+    }])
+
+    const translatedRelease = await translateReleaseRequest(parameters, logger)
+
     expect(mockAuthorFunction).toHaveBeenCalledWith("FTX40", logger)
     expect(translatedRelease.NominatedPrescriptionReleaseRequest.author).toEqual(mockAuthorResponse)
   })
