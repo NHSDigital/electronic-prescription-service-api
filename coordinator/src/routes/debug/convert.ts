20528e4f0d023e8f20f5214b73798894fa943203
dan-jones-iw <55280310+dan-jones-iw@users.noreply.github.com>
2021-03-19 11:05:06 +0000

    make use of wrapper functions to handle user auth and external FHIR validator response (#402)
    * make use of wrapper functions to handle user auth and external FHIR validator response
    
    * typo

--- coordinator/src/routes/debug/convert.ts
+++ coordinator/src/routes/debug/convert.ts
@@ -1,8 +1,8 @@
 import * as translator from "../../services/translation/request"
 import Hapi from "@hapi/hapi"
 import {
-  BASE_PATH, CONTENT_TYPE_FHIR, CONTENT_TYPE_XML,
-  getFhirValidatorErrors, getPayload, isBundle, isParameters, isTask
+  BASE_PATH, CONTENT_TYPE_FHIR, CONTENT_TYPE_XML, externalValidator,
+  getPayload, isBundle, isParameters, isTask
 } from "../util"
 import * as fhir from "../../models/fhir"
 import * as bundleValidator from "../../services/validation/bundle-validator"
@@ -16,50 +16,47 @@ export default [
   {
     method: "POST",
     path: `${BASE_PATH}/$convert`,
-    handler: async (request: Hapi.Request, responseToolkit: Hapi.ResponseToolkit): Promise<Hapi.ResponseObject> => {
-      const fhirValidatorResponse = await getFhirValidatorErrors(request)
-      if (fhirValidatorResponse) {
-        return responseToolkit.response(fhirValidatorResponse).code(400).type(CONTENT_TYPE_FHIR)
-      }
+    handler: externalValidator(
+      async (request: Hapi.Request, responseToolkit: Hapi.ResponseToolkit) => {
+        const payload = getPayload(request) as fhir.Resource
+        const requestId = request.headers["nhsd-request-id"].toUpperCase()
+        if (isBundle(payload)) {
+          const issues = bundleValidator.verifyBundle(payload)
+          if (issues.length) {
+            return responseToolkit.response(fhir.createOperationOutcome(issues)).code(400).type(CONTENT_TYPE_FHIR)
+          }
 
-      const payload = getPayload(request) as fhir.Resource
-      const requestId = request.headers["nhsd-request-id"].toUpperCase()
-      if (isBundle(payload)) {
-        const issues = bundleValidator.verifyBundle(payload)
-        if (issues.length) {
-          return responseToolkit.response(fhir.createOperationOutcome(issues)).code(400).type(CONTENT_TYPE_FHIR)
+          request.logger.info("Building HL7V3 message from Bundle")
+          const spineRequest = await translator.convertBundleToSpineRequest(payload, requestId, request.logger)
+          return responseToolkit.response(spineRequest.message).code(200).type(CONTENT_TYPE_XML)
         }
 
-        request.logger.info("Building HL7V3 message from Bundle")
-        const spineRequest = await translator.convertBundleToSpineRequest(payload, requestId, request.logger)
-        return responseToolkit.response(spineRequest.message).code(200).type(CONTENT_TYPE_XML)
-      }
+        if (isParameters(payload)) {
+          const issues = parametersValidator.verifyParameters(payload)
+          if (issues.length) {
+            return responseToolkit.response(fhir.createOperationOutcome(issues)).code(400).type(CONTENT_TYPE_FHIR)
+          }
 
-      if (isParameters(payload)) {
-        const issues = parametersValidator.verifyParameters(payload)
-        if (issues.length) {
-          return responseToolkit.response(fhir.createOperationOutcome(issues)).code(400).type(CONTENT_TYPE_FHIR)
+          request.logger.info("Building HL7V3 message from Parameters")
+          const spineRequest = await translator.convertParametersToSpineRequest(payload, requestId, request.logger)
+          return responseToolkit.response(spineRequest.message).code(200).type(CONTENT_TYPE_XML)
         }
 
-        request.logger.info("Building HL7V3 message from Parameters")
-        const spineRequest = await translator.convertParametersToSpineRequest(payload, requestId, request.logger)
-        return responseToolkit.response(spineRequest.message).code(200).type(CONTENT_TYPE_XML)
-      }
+        if (isTask(payload)) {
+          const issues = taskValidator.verifyTask(payload)
+          if (issues.length) {
+            return responseToolkit.response(fhir.createOperationOutcome(issues)).code(400).type(CONTENT_TYPE_FHIR)
+          }
 
-      if (isTask(payload)) {
-        const issues = taskValidator.verifyTask(payload)
-        if (issues.length) {
-          return responseToolkit.response(fhir.createOperationOutcome(issues)).code(400).type(CONTENT_TYPE_FHIR)
+          request.logger.info("Building HL7V3 message from Task")
+          const spineRequest = await translator.convertTaskToSpineRequest(payload, requestId, request.logger)
+          return responseToolkit.response(spineRequest.message).code(200).type(CONTENT_TYPE_XML)
         }
 
-        request.logger.info("Building HL7V3 message from Task")
-        const spineRequest = await translator.convertTaskToSpineRequest(payload, requestId, request.logger)
-        return responseToolkit.response(spineRequest.message).code(200).type(CONTENT_TYPE_XML)
+        return responseToolkit.response(unsupportedResponse).code(400).type(CONTENT_TYPE_FHIR)
       }
-
-      return responseToolkit.response(unsupportedResponse).code(400).type(CONTENT_TYPE_FHIR)
-    }
-  }
+    )
+  } as Hapi.ServerRoute
 ]
 
 const unsupportedResponse: fhir.OperationOutcome = {
