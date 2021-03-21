a2f6c143b91ca15d32ed0ef63780ea80cd50569f
Alex McCormick <amccmick1@googlemail.com>
2021-03-21 04:06:48 +0000

    Fix smoke-tests jest module @models reference

--- tests/e2e/pact/jest.config-cd.js
+++ tests/e2e/pact/jest.config-cd.js
@@ -2,6 +2,6 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   moduleNameMapper: {
-    "@models": "<rootDir>../../../models/library"
+    "@models": "<rootDir>./models/library"
   }
 }
