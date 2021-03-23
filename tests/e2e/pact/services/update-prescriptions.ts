e055c70c4c825e5a7997fd2694551b36acf7fd93
Alex McCormick <amccmick1@googlemail.com>
2021-03-23 22:50:40 +0000

    Update all prescriptions wih new ids, only save updated files for successful cases

--- tests/e2e/pact/services/update-prescriptions.ts
+++ tests/e2e/pact/services/update-prescriptions.ts
@@ -27,7 +27,7 @@ export async function updatePrescriptions(): Promise<void> {
     console.warn("No private key / x509 certifcate found, signing has been skipped")
   }
   
-  fetcher.prescriptionOrderExamples.filter(e => e.isSuccess).forEach(async(processCase) => {
+  fetcher.prescriptionOrderExamples.forEach(async(processCase) => {
     const prepareBundle = processCase.prepareRequest
     const processBundle = processCase.request
     const firstGroupIdentifier = getResourcesOfType.getMedicationRequests(prepareBundle)[0].groupIdentifier
@@ -47,16 +47,18 @@ export async function updatePrescriptions(): Promise<void> {
     setTestPatientIfProd(prepareBundle)
     setTestPatientIfProd(processBundle)
     signPrescriptionFn(processCase)
-    if (processCase.prepareRequestFile) {
-      saveFhirExample(processCase.prepareRequestFile.path, prepareBundle)
-    }
-    saveFhirExample(processCase.requestFile.path, processBundle)
-    if (processCase.convertResponseFile) {
-      saveHl7Example(processCase.convertResponseFile.path, (await convertBundleToSpineRequest(processBundle, "", pino())).message)
+    if (processCase.isSuccess) {
+      if (processCase.prepareRequestFile) {
+        saveFhirExample(processCase.prepareRequestFile.path, prepareBundle)
+      }
+      saveFhirExample(processCase.requestFile.path, processBundle)
+      if (processCase.convertResponseFile) {
+        saveHl7Example(processCase.convertResponseFile.path, (await convertBundleToSpineRequest(processBundle, "", pino())).message)
+      }
     }
   })
 
-  fetcher.prescriptionOrderUpdateExamples.filter(e => e.isSuccess).forEach(async (processCase) => {
+  fetcher.prescriptionOrderUpdateExamples.forEach(async (processCase) => {
     const bundle = processCase.request
     const firstGroupIdentifier = getResourcesOfType.getMedicationRequests(bundle)[0].groupIdentifier
 
@@ -70,9 +72,11 @@ export async function updatePrescriptions(): Promise<void> {
 
     setPrescriptionIds(bundle, newBundleIdentifier, newShortFormId, newLongFormId)
     setTestPatientIfProd(bundle)
-    saveFhirExample(processCase.requestFile.path, bundle)
-    if (processCase.convertResponseFile) {
-      saveHl7Example(processCase.convertResponseFile.path, (await convertBundleToSpineRequest(bundle, "", pino())).message)
+    if (processCase.isSuccess) {
+      saveFhirExample(processCase.requestFile.path, bundle)
+      if (processCase.convertResponseFile) {
+        saveHl7Example(processCase.convertResponseFile.path, (await convertBundleToSpineRequest(bundle, "", pino())).message)
+      }
     }
   })
 }
