5bc3eef37964544ee90c1f047145155affa199b9
Alex McCormick <amccmick1@googlemail.com>
2021-03-26 18:52:29 +0000

    AEA-1160 Fix update_prescriptions + allow skipping broker for smoke-tests (#411)

--- tests/e2e/pact/broker/verify.ts
+++ tests/e2e/pact/broker/verify.ts
@@ -1,4 +1,4 @@
-import { VerifierV3 } from "@pact-foundation/pact"
+import {VerifierV3, VerifierV3Options} from "@pact-foundation/pact"
 import {
   getPreparePactGroups,
   getProcessSendPactGroups,
@@ -9,19 +9,17 @@ import {
   getProcessDispensePactGroups,
   getTaskPactGroups
 } from "../resources/common"
+import path from "path"
 
 let token: string
 
 /* eslint-disable  @typescript-eslint/no-explicit-any */
 async function verify(endpoint: string, pactGroupName: string): Promise<any> {
+    const useBroker = process.env.PACT_USE_BROKER !== "false"
     const providerVersion = process.env.PACT_TAG
       ? `${process.env.PACT_VERSION} (${process.env.PACT_TAG})`
       : process.env.PACT_VERSION
-    const verifier =  new VerifierV3({
-      publishVerificationResult: true,
-      pactBrokerUrl: process.env.PACT_BROKER_URL,
-      pactBrokerUsername: process.env.PACT_BROKER_BASIC_AUTH_USERNAME,
-      pactBrokerPassword: process.env.PACT_BROKER_BASIC_AUTH_PASSWORD,
+    let verifierOptions: VerifierV3Options = {
       consumerVersionTag: process.env.PACT_VERSION,
       provider: `${process.env.PACT_PROVIDER}+${endpoint}${pactGroupName ? "-" + pactGroupName : ""}+${process.env.PACT_VERSION}`,
       providerVersion: providerVersion,
@@ -42,7 +40,33 @@ async function verify(endpoint: string, pactGroupName: string): Promise<any> {
         req.headers["Authorization"] = `Bearer ${token}`
         return req
       }
-    })
+    }
+
+    if (useBroker) {
+      verifierOptions = {
+        ...verifierOptions,
+        publishVerificationResult: true,
+        // use the below if you want to try a new broker without
+        // impacting other deploys until merged in
+        // then switch over variables in ADO
+        // pactBrokerUrl: process.env.PACT_BROKER_NEXT_URL,
+        // pactBrokerToken: process.env.PACT_BROKER_NEXT_TOKEN,
+        pactBrokerUrl: process.env.PACT_BROKER_URL,
+        pactBrokerUsername: process.env.PACT_BROKER_BASIC_AUTH_USERNAME,
+        pactBrokerPassword: process.env.PACT_BROKER_BASIC_AUTH_PASSWORD,
+      }
+    }
+    else {
+      verifierOptions = {
+        ...verifierOptions,
+        pactUrls: [
+          // eslint-disable-next-line max-len
+          `${path.join(__dirname, "../pact/pacts")}/nhsd-apim-eps-test-client+${process.env.PACT_VERSION}-${process.env.PACT_PROVIDER}+${endpoint}${pactGroupName ? "-" + pactGroupName : ""}+${process.env.PACT_VERSION}.json`
+        ]
+      }
+    }
+
+    const verifier =  new VerifierV3(verifierOptions)
     return await verifier.verifyProvider()
 }
 
