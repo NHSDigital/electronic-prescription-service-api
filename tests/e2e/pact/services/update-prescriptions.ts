b27ab981200a71cf4ba4007c9857014bbd31d134
Alex McCormick <amccmick1@googlemail.com>
2021-04-13 18:43:51 +0100

    Revert clone

--- tests/e2e/pact/services/update-prescriptions.ts
+++ tests/e2e/pact/services/update-prescriptions.ts
@@ -6,7 +6,6 @@ import {
 } from "@coordinator"
 import * as crypto from "crypto"
 import fs from "fs"
-import * as LosslessJson from "lossless-json"
 
 const privateKeyPath = process.env.SIGNING_PRIVATE_KEY_PATH
 const x509CertificatePath = process.env.SIGNING_CERT_PATH
@@ -27,8 +26,8 @@ export async function updatePrescriptions(): Promise<void> {
   }
 
   fetcher.prescriptionOrderExamples.filter(e => e.isSuccess).forEach(async(processCase) => {
-    const prepareBundle = clone(processCase.prepareRequest)
-    const processBundle = clone(processCase.request)
+    const prepareBundle = processCase.prepareRequest
+    const processBundle = processCase.request
     const firstGroupIdentifier = getResourcesOfType.getMedicationRequests(processBundle)[0].groupIdentifier
 
     const newBundleIdentifier = uuid.v4()
@@ -47,7 +46,7 @@ export async function updatePrescriptions(): Promise<void> {
   })
 
   fetcher.prescriptionOrderUpdateExamples.filter(e => e.isSuccess).forEach(async (processCase) => {
-    const bundle = clone(processCase.request)
+    const bundle = processCase.request
     const firstGroupIdentifier = getResourcesOfType.getMedicationRequests(bundle)[0].groupIdentifier
 
     const newBundleIdentifier = uuid.v4()
@@ -179,8 +178,4 @@ function getNhsNumberIdentifier(fhirPatient: fhir.Patient) {
   return fhirPatient
     .identifier
     .filter(identifier => identifier.system === "https://fhir.nhs.uk/Id/nhs-number")[0]
-}
-
-function clone<T>(input: T): T {
-  return LosslessJson.parse(LosslessJson.stringify(input))
 }
