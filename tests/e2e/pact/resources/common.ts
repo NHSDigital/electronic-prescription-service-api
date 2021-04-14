2035e7fc17745d0b36486f9ed36cd2b259db3d67
Alex McCormick <amccmick1@googlemail.com>
2021-04-14 10:05:17 +0100

    Cleanup smoke tests (#427)
    * Upgrade pact - remove pact groups, ensure pacts are per endpoint{+operation} remove comments
    
    * Setup ordering for smoke-test dispense interactions, skip verify live proxy interactions
    
    * Skip signing to debug smoke-test failures
    
    * Update lockfile through WSl
    
    * Remove update prescriptions to debug pipeline
    
    * Add update-prescriptions back
    
    * Clone all process-send bundles before replacing ids and signing
    
    * Revert "Clone all process-send bundles before replacing ids and signing"
    
    This reverts commit ff41ad4b14d28cc747add6b74f80fea4f5e9abee.
    
    * Ensure update-prescriptions runs before test-resources are loaded
    
    * Sync specfication version to matcher version
    
    * Updates from PR feedback
    
    * Updates from PR feedback
    
    * Updates from PR feedback
    
    * Process-message send - clone examples before replacing ids and signatures to avoid race conditions with other tests
    
    * Remove outdated comments
    
    * Revert clone
    
    Co-authored-by: Alex McCormick <alex.mccormick@infinityworks.com>

--- tests/e2e/pact/resources/common.ts
+++ tests/e2e/pact/resources/common.ts
@@ -4,126 +4,19 @@ import {fhir} from "@models"
 export const basePath = "/FHIR/R4"
 
 export type ApiMode = "live" | "sandbox"
-export type ApiEndpoint = "prepare" | "process" | "convert" | "release" | "task"
-export type ApiOperation = "send" | "dispense" | "cancel"
-
-// to use groups the group added must match a subfolder under
-// examples with path separator replaced by space
-// or set pactGroups = [""] to run all together
-export const pactGroups = [
-  "secondary-care community acute",
-  "secondary-care community repeat-dispensing",
-  "secondary-care homecare",
-  "primary-care"
-] as const
-
-export const dispensePactGroups = [
-  "secondary-care homecare"
-] as const
-
-export const cancelPactGroups = [
-  "secondary-care community acute"
-] as const
-
-export const releasePactGroups = [
-  ""
-]
-
-export const taskPactGroups = [
-  ""
-]
-
-export const failurePactGroups = [
-  "failures"
-] as const
-
-export const miscPactGroups = [
-  "accept_headers"
-] as const
-
-export const allPactGroups = [...pactGroups, ...cancelPactGroups, ...failurePactGroups, ...miscPactGroups]
-export type AllPactGroups = typeof pactGroups[number] | typeof dispensePactGroups[number] | typeof cancelPactGroups[number] | typeof failurePactGroups[number] | typeof miscPactGroups[number]
-
-export class PactGroupCases {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/explicit-module-boundary-types
-  constructor(name: AllPactGroups, cases: any) {
-    this.name = name
-    this.cases = cases
-  }
-  name: AllPactGroups
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  cases: any
-}
+export type ApiEndpoint = "prepare" | "process" | "convert" | "task"
+export type ApiOperation = "send" | "cancel" | "dispense" | "release" | "return" | "withdraw"
 
 // used to add type-safety for adding a new pact
-export function pactOptions(mode: ApiMode, endpoint: ApiEndpoint, group?: AllPactGroups, operation?: ApiOperation): JestPactOptions
+export function pactOptions(mode: ApiMode, endpoint: ApiEndpoint, operation?: ApiOperation): JestPactOptions
 {
   const sandbox = mode === "sandbox"
-  const groupName = group ? convertPactDescriptionToPactName(group) : ""
-  const operationName = operation === "send" ? "" : operation
   return {
-    spec: 3,
+    spec: 2,
     consumer: `nhsd-apim-eps-test-client+${process.env.PACT_VERSION}`,
-    provider: `nhsd-apim-eps${sandbox ? "-sandbox" : ""}+${endpoint}${groupName ? "-" + groupName : ""}${operationName ? "-" + operationName : ""}+${process.env.PACT_VERSION}`,
+    provider: `nhsd-apim-eps${sandbox ? "-sandbox" : ""}+${endpoint}${operation ? "-" + operation : ""}+${process.env.PACT_VERSION}`,
     pactfileWriteMode: "merge"
   }
-}
-
-// get pact groups for verification
-export const pactGroupNames = convertPactDescriptionsToPactNames(pactGroups)
-const releasePactGroupNames = convertPactDescriptionsToPactNames(releasePactGroups)
-const dispensePactGroupNames = convertPactDescriptionsToPactNames(dispensePactGroups)
-const cancelPactGroupNames = convertPactDescriptionsToPactNames(cancelPactGroups)
-const taskPactGroupNames = convertPactDescriptionsToPactNames(taskPactGroups)
-
-// convert pact group name from description search string format to single string
-// matching the published pact's name
-function convertPactDescriptionsToPactNames(descriptions: readonly string[]) {
- return descriptions.map(g => convertPactDescriptionToPactName(g))
-}
-
-function convertPactDescriptionToPactName(pactDescription: string): string {
-  return pactDescription.replace(/-/g, "").replace(/\s/g, "-")
-}
-
-const isSandbox = process.env.APIGEE_ENVIRONMENT?.includes("sandbox")
-
-export function getConvertPactGroups(): string[] {
-  return [...pactGroupNames, ...failurePactGroups]
-}
-
-export function getPreparePactGroups(): string[] {
-  return isSandbox
-    ? pactGroupNames
-    : [...pactGroupNames, ...failurePactGroups]
-}
-
-export function getProcessSendPactGroups(): string[] {
-  return isSandbox
-    ? [...pactGroupNames, ...miscPactGroups]
-    : [...pactGroupNames, ...failurePactGroups, ...miscPactGroups]
-}
-
-export function getProcessDispensePactGroups(): string[] {
-  return isSandbox
-    ? dispensePactGroupNames
-    : [] // todo: verify dispense for live proxy once this is available
-}
-
-export function getProcessCancelPactGroups(): string[] {
-  return cancelPactGroupNames
-}
-
-export function getReleasePactGroups(): string[] {
-  return isSandbox
-    ? releasePactGroupNames
-    : [] // todo: verify release for live proxy once this is available
-}
-
-export function getTaskPactGroups(): string[] {
-  return isSandbox
-    ? taskPactGroupNames
-    : [] // todo: verify release for live proxy once this is available
 }
 
 // helper functions
