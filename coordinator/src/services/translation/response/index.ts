f4e6d72a27c301b0a4aa0df2aa69daba69ff7ab6
Matt Jarvis <matt.jarvis@gmail.com>
2021-05-07 15:51:06 +0100

    AEA-1218: Update examples so each release request is for a separate organisation. Run update prescriptions script again.

--- coordinator/src/services/translation/response/index.ts
+++ coordinator/src/services/translation/response/index.ts
@@ -38,7 +38,5 @@ export function translateToFhir<T>(
     }
   }
   logger.error("Unhandled Spine response")
-  //TODO - remove logging to prevent leaking pii to logs
-  logger.error(JSON.stringify(hl7Message.body))
   return SpineResponseHandler.createServerErrorResponse()
 }
