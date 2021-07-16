ea5b6c559d31ed66c6acb7cb60c24923a0eae11c
Matt Jarvis <matt.jarvis@gmail.com>
2021-07-16 15:17:18 +0100

    AEA-1532: Add test that the signature is preserved by FHIR -> HL7 V3 -> FHIR translations.

--- coordinator/tests/services/translation/response/provenance.spec.ts
+++ coordinator/tests/services/translation/response/provenance.spec.ts
@@ -1,33 +1,40 @@
 import * as helpers from "../../../resources/test-helpers"
 import * as TestResources from "../../../resources/test-resources"
 import {fhir} from "@models"
-import * as common from "../../../../src/services/translation/common/getResourcesOfType"
 import {convertAuthor} from "../../../../src/services/translation/request/practitioner"
 import {convertSignatureTextToProvenance} from "../../../../src/services/translation/response/provenance"
-
-let bundle: fhir.Bundle
-let firstMedicationRequest: fhir.MedicationRequest
+import {getMedicationRequests, getProvenances} from "../../../../src/services/translation/common/getResourcesOfType"
+import {readXml, writeXmlStringCanonicalized} from "../../../../src/services/serialisation/xml"
 
 describe("Provenance", () => {
-  beforeEach(() => {
-    bundle = helpers.clone(TestResources.examplePrescription1.fhirMessageSigned)
-    firstMedicationRequest = common.getMedicationRequests(bundle)[0]
+  const bundle = helpers.clone(TestResources.examplePrescription1.fhirMessageSigned)
+  const firstMedicationRequest = getMedicationRequests(bundle)[0]
+  const provenanceBefore = getProvenances(bundle)[0]
+  const author = convertAuthor(bundle, firstMedicationRequest)
+  const resourceIds = bundle.entry.map(entry => entry.resource.id)
+  const provenanceAfter = convertSignatureTextToProvenance(author, "testAuthorId", resourceIds)
+
+  test("expected fields are populated", () => {
+    expect(provenanceAfter.target).toBeDefined()
+    expect(provenanceAfter.agent).toBeDefined()
+    expect(provenanceAfter.signature).toHaveLength(1)
+    expect(provenanceAfter.signature[0].data).toBeDefined()
+    expect(provenanceAfter.signature[0].who).toBeDefined()
+    expect(provenanceAfter.signature[0].when).toBeDefined()
+    expect(provenanceAfter.signature[0].type).toBeDefined()
+    expect(provenanceAfter.recorded).toBeDefined()
   })
 
-  test("v3 to FHIR provenance creation", () => {
-    const author = convertAuthor(bundle, firstMedicationRequest)
-
-    const resourceIds = bundle.entry.map(entry => entry.resource.id)
-
-    const provenance = convertSignatureTextToProvenance(author, "testAuthorId", resourceIds)
-
-    expect(provenance.target).toBeDefined()
-    expect(provenance.agent).toBeDefined()
-    expect(provenance.signature).toHaveLength(1)
-    expect(provenance.signature[0].data).toBeDefined()
-    expect(provenance.signature[0].who).toBeDefined()
-    expect(provenance.signature[0].when).toBeDefined()
-    expect(provenance.signature[0].type).toBeDefined()
-    expect(provenance.recorded).toBeDefined()
+  test("signature data matches once canonicalized", () => {
+    const canonicalizedSignatureBefore = getCanonicalizedSignature(provenanceBefore)
+    const canonicalizedSignatureAfter = getCanonicalizedSignature(provenanceAfter)
+    expect(canonicalizedSignatureAfter).toEqual(canonicalizedSignatureBefore)
   })
 })
+
+function getCanonicalizedSignature(provenance: fhir.Provenance) {
+  const signatureData = provenance.signature[0].data
+  const decodedSignatureData = Buffer.from(signatureData, "base64").toString("utf-8")
+  const deserializedXml = readXml(decodedSignatureData)
+  return writeXmlStringCanonicalized(deserializedXml)
+}
