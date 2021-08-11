ca414d5dfcf292f462d09bcf9c301ddcc05fdd4f
Matt Jarvis <61870561+mfjarvis@users.noreply.github.com>
2021-08-11 15:05:46 +0100

    AEA-1555: Simplify the code used to determine the system for an identifier.

--- coordinator/src/services/translation/response/identifiers.ts
+++ coordinator/src/services/translation/response/identifiers.ts
@@ -1,56 +1,41 @@
 import {fhir} from "@models"
 
-const SPURIOUS_CODE_MATCHER = /^G[67][0-9]{6}$/
-const NMC_CODE_MATCHER = /^[0-9]{2}[A-Z][0-9]{4}[A-Z]$/
-const GMP_CODE_MATCHER = /^G[01234589][0-9]{6}$/
-const GMC_CODE_MATCHER = /^C[0-9]{7}$/
-const GPHC_CODE_MATCHER = /^[0-9]{7}$/
-const HCPC_CODE_MATCHER = /^[A-Z]{2}[0-9]{6}$/
-const DIN_CODE_MATCHER = /^[0-9]{6}$/
-
-export function looksLikeSpuriousCode(userId: string): boolean {
-  return SPURIOUS_CODE_MATCHER.test(userId)
-}
-export function looksLikeNmcCode(userId: string): boolean {
-  return NMC_CODE_MATCHER.test(userId)
-}
-export function looksLikeGmpCode(userId: string): boolean {
-  return GMP_CODE_MATCHER.test(userId)
-}
-export function looksLikeGmcCode(userId: string): boolean {
-  return GMC_CODE_MATCHER.test(userId)
-}
-export function looksLikeGphcCode(userId: string): boolean {
-  return GPHC_CODE_MATCHER.test(userId)
-}
-export function looksLikeHcpcCode(userId: string): boolean {
-  return HCPC_CODE_MATCHER.test(userId)
-}
-export function looksLikeDinCode(userId: string): boolean {
-  return DIN_CODE_MATCHER.test(userId)
-}
+const IDENTIFIER_MATCHERS = [
+  {
+    system: "https://fhir.hl7.org.uk/Id/nhsbsa-spurious-code",
+    matcher: /^G[67][0-9]{6}$/
+  },
+  {
+    system: "https://fhir.hl7.org.uk/Id/nmc-number",
+    matcher: /^[0-9]{2}[A-Z][0-9]{4}[A-Z]$/
+  },
+  {
+    system: "https://fhir.hl7.org.uk/Id/gmp-number",
+    matcher: /^G[01234589][0-9]{6}$/
+  },
+  {
+    system: "https://fhir.hl7.org.uk/Id/gmc-number",
+    matcher: /^C[0-9]{7}$/
+  },
+  {
+    system: "https://fhir.hl7.org.uk/Id/gphc-number",
+    matcher: /^[0-9]{7}$/
+  },
+  {
+    system: "https://fhir.hl7.org.uk/Id/hcpc-number",
+    matcher: /^[A-Z]{2}[0-9]{6}$/
+  },
+  {
+    system: "https://fhir.hl7.org.uk/Id/din-number",
+    matcher: /^[0-9]{6}$/
+  }
+]
 
 export function createPractitionerOrRoleIdentifier(userId: string): fhir.Identifier {
-  if (looksLikeSpuriousCode(userId)) {
-    return fhir.createIdentifier("https://fhir.hl7.org.uk/Id/nhsbsa-spurious-code", userId)
-  }
-  if (looksLikeNmcCode(userId)) {
-    return fhir.createIdentifier("https://fhir.hl7.org.uk/Id/nmc-number", userId)
-  }
-  if (looksLikeGmpCode(userId)) {
-    return fhir.createIdentifier("https://fhir.hl7.org.uk/Id/gmp-number", userId)
-  }
-  if (looksLikeGmcCode(userId)) {
-    return fhir.createIdentifier("https://fhir.hl7.org.uk/Id/gmc-number", userId)
-  }
-  if (looksLikeGphcCode(userId)) {
-    return fhir.createIdentifier("https://fhir.hl7.org.uk/Id/gphc-number", userId)
-  }
-  if (looksLikeHcpcCode(userId)) {
-    return fhir.createIdentifier("https://fhir.hl7.org.uk/Id/hcpc-number", userId)
-  }
-  if (looksLikeDinCode(userId)) {
-    return fhir.createIdentifier("https://fhir.hl7.org.uk/Id/din-number", userId)
+  for (const {system, matcher} of IDENTIFIER_MATCHERS) {
+    if (matcher.test(userId)) {
+      return fhir.createIdentifier(system, userId)
+    }
   }
   return fhir.createIdentifier("https://fhir.hl7.org.uk/Id/professional-code", userId)
 }
