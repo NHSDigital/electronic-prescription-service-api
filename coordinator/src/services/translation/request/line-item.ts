b21a9c6848cda97d1b64cbf00d1a1e65ae5df14b
Matt Jarvis <61870561+mfjarvis@users.noreply.github.com>
2021-08-23 15:19:59 +0100

    AEA-1630: Fix the order of pertinent information within a line item. Simplify line item pertinent information translations.

--- coordinator/src/services/translation/request/line-item.ts
+++ coordinator/src/services/translation/request/line-item.ts
@@ -38,28 +38,26 @@ function convertDosageInstructions(dosages: Array<fhir.Dosage>, logger: pino.Log
 }
 
 export function convertPrescriptionEndorsements(
-  medicationRequest: fhir.MedicationRequest,
-  lineItem: hl7V3.LineItem
-): void {
+  medicationRequest: fhir.MedicationRequest
+): Array<hl7V3.LineItemPertinentInformation3> {
   const endorsementExtensions = medicationRequest.extension?.filter(
     extension => extension.url === "https://fhir.nhs.uk/StructureDefinition/Extension-DM-PrescriptionEndorsement"
   ) as Array<fhir.CodeableConceptExtension>
-
-  if (endorsementExtensions?.length) {
-    lineItem.pertinentInformation3 = endorsementExtensions.map(endorsementExtension => {
-      const endorsementCoding = getCodeableConceptCodingForSystem(
-        [endorsementExtension.valueCodeableConcept],
-        "https://fhir.nhs.uk/CodeSystem/medicationrequest-endorsement",
-        // eslint-disable-next-line max-len
-        "MedicationRequest.extension(https://fhir.nhs.uk/StructureDefinition/Extension-DM-PrescriptionEndorsement).valueCodeableConcept"
-      )
-      const prescriptionEndorsementValue = new hl7V3.PrescriptionEndorsementCode(endorsementCoding.code)
-      const prescriptionEndorsement = new hl7V3.PrescriptionEndorsement(prescriptionEndorsementValue)
-      return new hl7V3.LineItemPertinentInformation3(prescriptionEndorsement)
-    })
-  } else {
-    delete lineItem.pertinentInformation3
+  if (!endorsementExtensions?.length) {
+    return null
   }
+
+  return endorsementExtensions.map(endorsementExtension => {
+    const endorsementCoding = getCodeableConceptCodingForSystem(
+      [endorsementExtension.valueCodeableConcept],
+      "https://fhir.nhs.uk/CodeSystem/medicationrequest-endorsement",
+      // eslint-disable-next-line max-len
+      "MedicationRequest.extension(https://fhir.nhs.uk/StructureDefinition/Extension-DM-PrescriptionEndorsement).valueCodeableConcept"
+    )
+    const prescriptionEndorsementValue = new hl7V3.PrescriptionEndorsementCode(endorsementCoding.code)
+    const prescriptionEndorsement = new hl7V3.PrescriptionEndorsement(prescriptionEndorsementValue)
+    return new hl7V3.LineItemPertinentInformation3(prescriptionEndorsement)
+  })
 }
 
 function convertAdditionalInstructions(
@@ -67,36 +65,70 @@ function convertAdditionalInstructions(
   medicationListText: Array<hl7V3.Text>,
   patientInfoText: Array<hl7V3.Text>
 ) {
+  const controlledDrugWordsWithPrefix = getControlledDrugWordsWithPrefix(medicationRequest)
+
+  const noteText = onlyElementOrNull(medicationRequest.note, "MedicationRequest.note")?.text
+
+  const additionalInstructionsValueStr = assembleAdditionalInstructionsValueFromParts(
+    medicationListText,
+    patientInfoText,
+    controlledDrugWordsWithPrefix,
+    noteText
+  )
+  if (!additionalInstructionsValueStr) {
+    return null
+  }
+
+  const additionalInstructions = new hl7V3.AdditionalInstructions(additionalInstructionsValueStr)
+  return new hl7V3.LineItemPertinentInformation1(additionalInstructions)
+}
+
+function getControlledDrugWordsWithPrefix(medicationRequest: fhir.MedicationRequest) {
   const controlledDrugExtension = getExtensionForUrlOrNull(
     medicationRequest.extension,
     "https://fhir.nhs.uk/StructureDefinition/Extension-DM-ControlledDrug",
     "MedicationRequest.extension"
   ) as fhir.ControlledDrugExtension
-  const controlledDrugWordsExtension = controlledDrugExtension ? getExtensionForUrlOrNull(
+  if (!controlledDrugExtension) {
+    return null
+  }
+
+  const controlledDrugWordsExtension = getExtensionForUrlOrNull(
     controlledDrugExtension.extension,
     "quantityWords",
-    "MedicationRequest.extension.extension"
-  ) as fhir.StringExtension : null
-  const controlledDrugWords = controlledDrugWordsExtension?.valueString
-  const controlledDrugWordsWithPrefix = controlledDrugWords ? `CD: ${controlledDrugWords}` : ""
+    "MedicationRequest.extension(\"https://fhir.nhs.uk/StructureDefinition/Extension-DM-ControlledDrug\").extension"
+  ) as fhir.StringExtension
+  if (!controlledDrugWordsExtension) {
+    return null
+  }
 
-  const noteText = onlyElementOrNull(
-    medicationRequest.note,
-    "MedicationRequest.note"
-  )?.text
+  const controlledDrugWords = controlledDrugWordsExtension.valueString
+  if (!controlledDrugWords) {
+    return null
+  }
 
+  return `CD: ${controlledDrugWords}`
+}
+
+function assembleAdditionalInstructionsValueFromParts(
+  medicationListText: Array<hl7V3.Text>,
+  patientInfoText: Array<hl7V3.Text>,
+  controlledDrugWordsWithPrefix: string,
+  noteText: string
+) {
   const additionalInstructionsValueObj: ElementCompact = {}
+
   if (medicationListText?.length) {
     additionalInstructionsValueObj.medication = medicationListText
   }
+
   if (patientInfoText?.length) {
     additionalInstructionsValueObj.patientInfo = patientInfoText
   }
-  additionalInstructionsValueObj._text = [controlledDrugWordsWithPrefix, noteText].filter(isTruthy).join("\n")
-  const additionalInstructionsValueStr = js2xml(additionalInstructionsValueObj, {compact: true})
 
-  const additionalInstructions = new hl7V3.AdditionalInstructions(additionalInstructionsValueStr)
-  return new hl7V3.LineItemPertinentInformation1(additionalInstructions)
+  additionalInstructionsValueObj._text = [controlledDrugWordsWithPrefix, noteText].filter(isTruthy).join("\n")
+
+  return js2xml(additionalInstructionsValueObj, {compact: true})
 }
 
 export function convertMedicationRequestToLineItem(
@@ -122,15 +154,15 @@ export function convertMedicationRequestToLineItem(
 
   lineItem.product = convertProduct(medicationCoding)
   lineItem.component = convertLineItemComponent(medicationRequest.dispenseRequest.quantity)
-  convertPrescriptionEndorsements(medicationRequest, lineItem)
 
-  const pertinentInformation1 = convertAdditionalInstructions(
-    medicationRequest,
-    medicationListText,
-    patientInfoText
-  )
-  if (pertinentInformation1.pertinentAdditionalInstructions.value._text) {
+  const pertinentInformation1 = convertAdditionalInstructions(medicationRequest, medicationListText, patientInfoText)
+  if (pertinentInformation1) {
     lineItem.pertinentInformation1 = pertinentInformation1
+  }
+
+  const pertinentInformation3 = convertPrescriptionEndorsements(medicationRequest)
+  if (pertinentInformation3?.length) {
+    lineItem.pertinentInformation3 = pertinentInformation3
   }
 
   lineItem.pertinentInformation2 = convertDosageInstructions(medicationRequest.dosageInstruction, logger)
