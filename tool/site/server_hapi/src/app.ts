5be3abdab87e50bc55f8f720aa651aa2559b915b
Matt Jarvis <61870561+mfjarvis@users.noreply.github.com>
2021-10-13 09:36:26 +0100

    AEA-1752: Add hapi server to EPSAT (#596)
    * Add javascript server alongside python server to enable migration
    
    * Encapsulate container ci/cd files
    
    * Add eslint dev dep to epsat javascript server
    
    * Mock lint step for epsat
    
    * Remove duplicate lint from eps/epsat
    
    * Add Hapi, add healthcheck and status routes, refactor server build to be consistent with coordinator
    
    * Update author and license for client npm package
    
    * Enforce pipeline validation rules for copying docker artifacts
    
    * Add executable permissions to site docker-entrypoint
    
    * Passthrough a _status request from flask to hapi
    
    * Update hapi host to avoid crossover between localhost in local mode vs in ecs
    
    * Split hapi server into its own ecs container and rename servers
    
    * Update hapi dockerfile name in ecs config
    
    * Add executable permissions to all eps-api-tool-hapi runtime files
    
    * Move eps-api-tool-hapi from port 9001 to 9002 to avoid collision
    
    * Revert "Move eps-api-tool-hapi from port 9001 to 9002 to avoid collision"
    
    This reverts commit f6ee6f058a1a0615a79e0b628e07170b712b68d7.
    
    * Bind flask to port 9000 to avoid collision
    
    * Attempt to fix port collision from eps-api-tool and eps-api-tool-hapi
    
    * Move eps-api-tool-hapi to port 9001 now ecs issue is resolved
    
    * Move eps-api-tool-hapi to port 9001 now ecs is resolved
    
    * Remove internal healthcheck port from eps-api-tool-hapi to avoid address and port collision
    
    * Set commit id in eps-api-tool-hapi, remove from eps-api-tool
    
    * Attempt to bind eps-api-tool-hapi to localhost
    
    * Revert localhost binding for eps-api-tool-hapi
    
    * AEA-1752: If port 9001 is bound, log the process responsible instead of starting the hapi server.
    
    * AEA-1752: Make the condition less specific.
    
    * AEA-1752: Remove the condition entirely.
    
    * AEA-1752: Add more logging.
    
    * AEA-1752: Elevate.
    
    * AEA-1752: Remove sudo from commands as it's not installed.
    
    * AEA-1752: Change healthcheck internal port.
    
    * AEA-1752: Fix hapi passthrough URL.
    
    * AEA-1752: Remove debug logging. Remove unused healthcheck.
    
    * AEA-1752: Fix internal-qa config.
    
    * Passthrough healthcheck from flask to hapi
    
    * Re-implement linting for tool
    
    * Remove dummy echo from lint
    
    * Undo specifying tsconfig file which is already a default for webpack
    
    Co-authored-by: Alex McCormick <amccmick1@googlemail.com>

---
+++ tool/site/server_hapi/src/app.ts
@@ -0,0 +1,38 @@
+import Hapi from "@hapi/hapi"
+import routes from "./routes"
+import HapiPino from "hapi-pino"
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 9001,
+    host: "0.0.0.0",
+    routes: {
+      cors: true, // Won't run as Apigee hosted target without this
+      payload: {
+        parse: false
+      }
+    }
+  })
+
+  server.route(routes)
+
+  await server.register({
+    plugin: HapiPino,
+    options: {
+      // Dont pretty print to avoid spamming logs
+      prettyPrint: false,
+      // Redact Authorization headers, see https://getpino.io/#/docs/redaction
+      redact: ["req.headers.authorization"]
+    }
+  })
+
+  await server.start()
+  server.log("info", `Server running on ${server.info.uri}`)
+}
+
+process.on("unhandledRejection", err => {
+  console.log(err)
+  process.exit(1)
+})
+
+init()
