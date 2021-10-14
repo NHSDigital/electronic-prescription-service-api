1cf07490d0c17a402b96f0ddd6db202255107e51
Alex McCormick <amccmick1@googlemail.com>
2021-10-14 22:18:36 +0100

    Add flask session encryption back to config, remove server side session from flask

--- tool/site/server_flask/app.py
+++ tool/site/server_flask/app.py
@@ -2,8 +2,6 @@ from cryptography.fernet import Fernet
 from flask import Flask
 from flask_compress import Compress
 import config
-from flask_session import Session
-import redis
 
 fernet = Fernet(config.SESSION_TOKEN_ENCRYPTION_KEY)
 compress = Compress()
@@ -12,9 +10,10 @@ compress = Compress()
 def create_app():
     app = Flask(__name__, static_url_path=config.STATIC_URL)
     compress.init_app(app)
-    app.config.from_mapping(JSON_SORT_KEYS=False)
-    Session(app)
-
+    app.config.from_mapping(
+        SECRET_KEY=config.SESSION_TOKEN_ENCRYPTION_KEY,
+        JSON_SORT_KEYS=False
+    )
     return app
 
 
