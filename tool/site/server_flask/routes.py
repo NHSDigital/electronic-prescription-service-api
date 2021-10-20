2a51323ade0c08743340ee50098afcfe7806b7ff
Alex McCormick <amccmick1@googlemail.com>
2021-10-20 19:48:24 +0100

    Revert "Remove special cases for setting session cookie, all requests should set this after login"
    This reverts commit 35ff0566e24cd4771dfced2858728707a938f8e6.

--- tool/site/server_flask/routes.py
+++ tool/site/server_flask/routes.py
@@ -30,7 +30,8 @@ from cookies import (
     get_auth_method_from_cookie,
     set_auth_method_cookie,
     set_skip_signature_page_cookie,
-    set_session_cookie
+    set_session_cookie,
+    get_hapi_session_cookie_value
 )
 from helpers import (
     pr_redirect_required,
@@ -154,7 +155,8 @@ def get_load():
 def download():
     zFile = io.BytesIO()
     access_token = get_access_token()
-    state = hapi_passthrough.get_prescription_ids()
+    hapi_session_cookie_value = get_hapi_session_cookie_value()
+    state = hapi_passthrough.get_prescription_ids(hapi_session_cookie_value)
     short_prescription_ids = state["prescriptionIds"]
     with zipfile.ZipFile(zFile, 'w') as zip_file:
         for index, short_prescription_id in enumerate(short_prescription_ids):
@@ -201,7 +203,8 @@ def get_edit():
         return flask.redirect(f"{config.BASE_URL}change-auth")
     response_json = hapi_passthrough.get_prescription(short_prescription_id)
     response = app.make_response(response_json)
-    state = hapi_passthrough.get_prescription_ids()
+    hapi_session_cookie_value = get_hapi_session_cookie_value()
+    state = hapi_passthrough.get_prescription_ids(hapi_session_cookie_value)
     short_prescription_ids = state["prescriptionIds"]
     short_prescription_id = state["prescriptionId"]
     update_pagination(response, short_prescription_ids, short_prescription_id)
@@ -214,10 +217,15 @@ def post_edit():
     request_bundles = flask.request.json
     response_json = hapi_passthrough.post_edit(request_bundles)
     response = app.make_response(response_json)
-    state = hapi_passthrough.get_prescription_ids()
+    hapi_session_cookie_value = get_hapi_session_cookie_value()
+    state = hapi_passthrough.get_prescription_ids(hapi_session_cookie_value)
     short_prescription_ids = state["prescriptionIds"]
     short_prescription_id = state["prescriptionId"]
     update_pagination(response, short_prescription_ids, short_prescription_id)
+    # when in local mode, we might not have session cookie at this point
+    # as we've skipped login, so ensure it is set here
+    if get_hapi_session_cookie_value() is None:
+        set_session_cookie(response, hapi_session_cookie_value)
     return response
 
 
