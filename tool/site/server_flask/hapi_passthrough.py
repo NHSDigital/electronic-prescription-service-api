99af93d3c453a97d00567b94cd161332da2c6e9c
Alex McCormick <amccmick1@googlemail.com>
2021-10-21 12:13:16 +0100

    Tidy up hapi_passthrough by reusing code

--- tool/site/server_flask/hapi_passthrough.py
+++ tool/site/server_flask/hapi_passthrough.py
@@ -3,80 +3,20 @@ import os
 import httpx
 from cookies import get_hapi_session_cookie_value
 
-
 HAPI_URL = os.environ["HAPI_URL"]
-STATUS_URL = "/_status"
-HEALTHCHECK_URL = "/_healthcheck"
-EDIT_URL = "/prescribe/edit"
-SIGN_URL = "/prescribe/sign"
-SEND_URL = "/prescribe/send"
-AUTH_URL = "/login"
 
 
 def get_status():
-    return httpx.get(
-        f"{HAPI_URL}{STATUS_URL}",
-        verify=False,
-    ).json()
+    return httpx.get(f"{HAPI_URL}/_status", verify=False).json()
 
 
 def get_healthcheck():
-    return httpx.get(
-        f"{HAPI_URL}{HEALTHCHECK_URL}",
-        verify=False,
-    ).json()
-
-
-def get_prescription(prescription_id):
-    session_cookie_value = get_hapi_session_cookie_value()
-    return httpx.get(
-        f"{HAPI_URL}{EDIT_URL}?prescription_id={prescription_id}",
-        verify=False,
-        cookies={
-            "session": session_cookie_value
-        }
-    ).json()
-
-
-def post_edit(body):
-    session_cookie_value = get_hapi_session_cookie_value()
-    return httpx.post(
-        f"{HAPI_URL}{EDIT_URL}",
-        json=body,
-        verify=False,
-        cookies={
-            "session": session_cookie_value
-        }
-    ).json()
-
-
-def post_sign():
-    session_cookie_value = get_hapi_session_cookie_value()
-    return httpx.post(
-        f"{HAPI_URL}{SIGN_URL}",
-        json={},
-        verify=False,
-        cookies={
-            "session": session_cookie_value
-        }
-    ).json()
-
-
-def post_send(request):
-    session_cookie_value = get_hapi_session_cookie_value()
-    return httpx.post(
-        f"{HAPI_URL}{SEND_URL}",
-        json=request,
-        verify=False,
-        cookies={
-            "session": session_cookie_value
-        }
-    ).json()
+    return httpx.get(f"{HAPI_URL}/_healthcheck", verify=False).json()
 
 
 def post_login(auth_method, access_token):
     response =  httpx.post(
-        f"{HAPI_URL}{AUTH_URL}",
+        f"{HAPI_URL}/login",
         json={
             "auth_method": auth_method,
             "access_token": access_token
@@ -87,14 +27,34 @@ def post_login(auth_method, access_token):
     return session_cookie_value, response.json()
 
 
+def get_prescription(prescription_id):
+    return make_get_request(f"{HAPI_URL}/prescribe/edit?prescription_id={prescription_id}")
+
+
+def post_edit(body):
+    return make_post_request(f"{HAPI_URL}/prescribe/edit", body)
+
+
+def post_sign():
+    return make_post_request(f"{HAPI_URL}/prescribe/sign", {})
+
+
+def post_send(body):
+    return make_post_request(f"{HAPI_URL}/prescribe/send", body)
+
+
 def get_prescription_ids():
-    return make_get_request_raw(f"{HAPI_URL}/prescriptionIds")
+    return make_get_request(f"{HAPI_URL}/prescriptionIds")
 
 
 # Helpers
 
-def make_get_request_raw(url):
+def make_get_request(url):
     return httpx.get(url, verify=False, cookies=get_cookies()).json()
+
+
+def make_post_request(url, body):
+    return httpx.get(url, json=body, verify=False, cookies=get_cookies()).json()
 
 
 def get_cookies():
