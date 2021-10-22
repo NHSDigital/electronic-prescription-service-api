305740aa7d89f70c9479b8717225bffe4f5413ab
Alex McCormick <amccmick1@googlemail.com>
2021-10-22 03:41:29 +0100

    Debug send

--- tool/site/server_hapi/src/services/communication/live-eps-client.ts
+++ tool/site/server_hapi/src/services/communication/live-eps-client.ts
@@ -16,13 +16,15 @@ export class LiveEpsClient implements EpsClient {
 
   async makeSendRequest(body: Bundle): Promise<EpsSendReponse> {
     const requestId = uuid.v4()
-    const rawResponseHeaders = {
-      "x-raw-response": "true"
-    }
+    // todo: investigate potential eps spike arrest issue preventing
+    // 2 requests in quick succession
+    // const rawResponseHeaders = {
+    //   "x-raw-response": "true"
+    // }
     const response = await this.makeApiCall("$process-message", body, requestId)
     const statusCode = response.status
     const fhirResponse = await response.data as OperationOutcome
-    const spineResponse = await (await this.makeApiCall("$process-message", body, requestId, rawResponseHeaders)).data as string
+    const spineResponse = "" /*await (await this.makeApiCall("$process-message", body, requestId, rawResponseHeaders)).data as string*/
     return {statusCode, fhirResponse, spineResponse}
   }
 
