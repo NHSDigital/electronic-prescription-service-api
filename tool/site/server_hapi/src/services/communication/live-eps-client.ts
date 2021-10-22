cf0bb58a1190a4181ef0d28b4488e6299a2370e0
Alex McCormick <amccmick1@googlemail.com>
2021-10-22 10:50:32 +0100

    Debug send

--- tool/site/server_hapi/src/services/communication/live-eps-client.ts
+++ tool/site/server_hapi/src/services/communication/live-eps-client.ts
@@ -11,28 +11,26 @@ export class LiveEpsClient implements EpsClient {
   }
 
   async makePrepareRequest(body: Bundle): Promise<Parameters> {
-    return await (await this.makeApiCall("$prepare", body)).data as Parameters
+    return await (await this.makeApiCall<Parameters>("$prepare", body)).data
   }
 
   async makeSendRequest(body: Bundle): Promise<EpsSendReponse> {
     const requestId = uuid.v4()
-    // todo: investigate potential eps spike arrest issue preventing
-    // 2 requests in quick succession
-    // const rawResponseHeaders = {
-    //   "x-raw-response": "true"
-    // }
-    const response = await this.makeApiCall("$process-message", body, requestId)
+    const rawResponseHeaders = {
+      "x-raw-response": "true"
+    }
+    const response = await this.makeApiCall<OperationOutcome>("$process-message", body, requestId)
     const statusCode = response.status
-    const fhirResponse = await response.data as OperationOutcome
-    const spineResponse = "" /*await (await this.makeApiCall("$process-message", body, requestId, rawResponseHeaders)).data as string*/
+    const fhirResponse = await response.data
+    const spineResponse = await (await this.makeApiCall<string>("$process-message", body, requestId, rawResponseHeaders)).data
     return {statusCode, fhirResponse, spineResponse}
   }
 
   async makeConvertRequest(body: unknown): Promise<string> {
-    return await (await this.makeApiCall("$convert", body)).data as string
+    return await (await this.makeApiCall<string>("$convert", body)).data
   }
 
-  private async makeApiCall(endpoint: string, body?: unknown, requestId?: string, additionalHeaders?: AxiosRequestHeaders): Promise<AxiosResponse> {
+  private async makeApiCall<T>(endpoint: string, body?: unknown, requestId?: string, additionalHeaders?: AxiosRequestHeaders): Promise<AxiosResponse<T>> {
     const url = `https://${process.env.APIGEE_DOMAIN_NAME}/electronic-prescriptions/FHIR/R4/${endpoint}`
     let headers: AxiosRequestHeaders = {
       "Authorization": `Bearer ${this.accessToken}`,
