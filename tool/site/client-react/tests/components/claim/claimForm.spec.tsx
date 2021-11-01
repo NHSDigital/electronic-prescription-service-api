145290ea858dcfe96cc3d0ffc935e1a74c2ac85d
Matt Jarvis <61870561+mfjarvis@users.noreply.github.com>
2021-11-01 01:37:21 +0000

    AEA-1778: Add more tests for claim form.

--- tool/site/client-react/tests/components/claim/claimForm.spec.tsx
+++ tool/site/client-react/tests/components/claim/claimForm.spec.tsx
@@ -1,5 +1,7 @@
 import {staticProductInfo} from "./props"
-import {render} from "@testing-library/react"
+import {render, waitFor, waitForElementToBeRemoved} from "@testing-library/react"
+import {screen} from "@testing-library/dom"
+import userEvent from "@testing-library/user-event"
 import pretty from "pretty"
 import * as React from "react"
 import ClaimForm from "../../../src/components/claim/claimForm"
@@ -10,7 +12,47 @@ test("Renders correctly", async () => {
   expect(pretty(container.innerHTML)).toMatchSnapshot()
 })
 
+test("Form has no endorsement fields initially", async () => {
+  render(<ClaimForm products={[staticProductInfo]} sendClaim={jest.fn}/>)
+
+  expect(screen.queryAllByLabelText(/Endorsement \d+ Type/)).toHaveLength(0)
+  expect(screen.queryAllByLabelText(/Endorsement \d+ Supporting Information/)).toHaveLength(0)
+  expect(screen.queryAllByText("Remove Endorsement")).toHaveLength(0)
+})
+
+test("Clicking Add Endorsement button adds one set of endorsement fields", async () => {
+  render(<ClaimForm products={[staticProductInfo]} sendClaim={jest.fn}/>)
+
+  userEvent.click(screen.getByText("Add Endorsement"))
+
+  await expect(screen.findByLabelText("Endorsement 1 Type")).resolves.toBeTruthy()
+  expect(screen.getByLabelText("Endorsement 1 Supporting Information")).toBeTruthy()
+  expect(screen.queryByLabelText("Endorsement 2 Type")).toBeFalsy()
+  expect(screen.queryByLabelText("Endorsement 2 Supporting Information")).toBeFalsy()
+  expect(screen.queryAllByText("Remove Endorsement")).toHaveLength(1)
+})
+
+test("Clicking Add Endorsement button twice adds two sets of endorsement fields", async () => {
+  render(<ClaimForm products={[staticProductInfo]} sendClaim={jest.fn}/>)
+
+  userEvent.dblClick(screen.getByText("Add Endorsement"))
+
+  await expect(screen.findByLabelText("Endorsement 2 Type")).resolves.toBeTruthy()
+  expect(screen.getByLabelText("Endorsement 2 Supporting Information")).toBeTruthy()
+  expect(screen.queryAllByText("Remove Endorsement")).toHaveLength(2)
+})
+
+test("Clicking Remove Endorsement button removes one set of endorsement fields", async () => {
+  render(<ClaimForm products={[staticProductInfo]} sendClaim={jest.fn}/>)
+  userEvent.dblClick(screen.getByText("Add Endorsement"))
+  await expect(screen.findByLabelText("Endorsement 2 Type")).resolves.toBeTruthy()
+
+  userEvent.click(screen.getAllByText("Remove Endorsement")[0])
+
+  await waitFor(() => expect(screen.queryByLabelText("Endorsement 2 Type")).toBeFalsy())
+  expect(screen.queryByLabelText("Endorsement 2 Supporting Information")).toBeFalsy()
+  expect(screen.queryAllByText("Remove Endorsement")).toHaveLength(1)
+})
+
 //TODO - test submit
 //TODO - test reset
-//TODO - test add endorsement
-//TODO - test remove endorsement
