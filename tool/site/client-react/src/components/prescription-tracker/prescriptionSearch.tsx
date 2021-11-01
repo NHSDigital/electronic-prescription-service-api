ab7d460d8802197eed239d2ff5ee479e10c8cd3f
Alex McCormick <amccmick1@googlemail.com>
2021-11-01 15:21:46 +0000

    Add types for prescription search query and results

--- tool/site/client-react/src/components/prescription-tracker/prescriptionSearch.tsx
+++ tool/site/client-react/src/components/prescription-tracker/prescriptionSearch.tsx
@@ -2,22 +2,38 @@ import * as React from "react"
 import {useState} from "react"
 import {Button, Details, Input, Label} from "nhsuk-react-components"
 import Pre from "../pre"
+import {Bundle} from "fhir/r4"
 
 interface PrescriptionSearchProps {
   baseUrl: string
   prescriptionId?: string
 }
 
+interface PrescriptionSearchCriteria {
+  prescriptionId: string
+}
+
+interface PrescriptionSearchResults {
+  searchset: Bundle
+  count: number
+  pluralSuffix: string
+}
+
 const PrescriptionSearch: React.FC<PrescriptionSearchProps> = ({
   baseUrl,
   prescriptionId
 }) => {
-  const [searchCriteria, setSearchCriteria] = useState({prescriptionId: prescriptionId ?? ""})
-  const [searchResults, setSearchResults] = useState(null)
+  const [searchCriteria, setSearchCriteria] = useState<PrescriptionSearchCriteria>({prescriptionId: prescriptionId ?? ""})
+  const [searchResults, setSearchResults] = useState<PrescriptionSearchResults>(null)
 
   async function handleSearch() {
     const response = await fetch(`${baseUrl}tracker?prescription_id=${searchCriteria.prescriptionId}`)
-    const results = await response.json()
+    const searchset = await response.json()
+    const results: PrescriptionSearchResults = {
+      ...searchset,
+      count: searchset.total,
+      pluralSuffix: searchResults.count > 1 || searchResults.count === 0 ? "s": ""
+    }
     setSearchResults(results)
   }
 
@@ -29,27 +45,27 @@ const PrescriptionSearch: React.FC<PrescriptionSearchProps> = ({
     <>
       {!searchResults
         ? <div>
-          <Label isPageHeading>Search for a Prescription</Label>
-          <Input
-            label="Prescription ID"
-            hint="Use the short form here, e.g. E3E6FA-A83008-41F09Y"
-            width={30}
-            value={searchCriteria.prescriptionId}
-            onChange={event => setSearchCriteria({prescriptionId: event.currentTarget.value})}
-          />
-          <Button onClick={handleSearch}>Search</Button>
-          <Button secondary href={baseUrl}>Back</Button>
-        </div>
+            <Label isPageHeading>Search for a Prescription</Label>
+            <Input
+              label="Prescription ID"
+              hint="Use the short form here, e.g. E3E6FA-A83008-41F09Y"
+              width={30}
+              value={searchCriteria.prescriptionId}
+              onChange={event => setSearchCriteria({prescriptionId: event.currentTarget.value})}
+            />
+            <Button onClick={handleSearch}>Search</Button>
+            <Button secondary href={baseUrl}>Back</Button>
+          </div>
         : <div>
-          <Label isPageHeading>Search Results</Label>
-          <Details expander>
-            <Details.Summary>Details</Details.Summary>
-            <Details.Text>
-              <Pre>{JSON.stringify(searchResults, null, 2)}</Pre>
-            </Details.Text>
-          </Details>
-          <Button secondary onClick={handleReset}>Back</Button>
-        </div>
+            <Label isPageHeading>Found {searchResults.count} Prescription{searchResults.pluralSuffix}</Label>
+            <Details expander>
+              <Details.Summary>Details</Details.Summary>
+              <Details.Text>
+                <Pre>{JSON.stringify(searchResults.searchset, null, 2)}</Pre>
+              </Details.Text>
+            </Details>
+            <Button secondary onClick={handleReset}>Back</Button>
+          </div>
       }
     </>
   )
