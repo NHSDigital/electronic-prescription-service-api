fce272484575546ff138c9ad98afb635ec253e58
Alex McCormick <amccmick1@googlemail.com>
2021-11-04 17:30:54 +0000

    Add pharmacy to tracker ui

--- tool/site/client-react/src/components/prescription-tracker/prescriptionDetails.tsx
+++ tool/site/client-react/src/components/prescription-tracker/prescriptionDetails.tsx
@@ -11,16 +11,26 @@ export interface PrescriptionDetailProps {
   type: string
   patientNhsNumber: string
   creationDate: string
+  pharmacy?: {key: string, value: string},
   status: string
 }
 
 export function createPrescriptionDetailProps(task: Task): PrescriptionDetailProps {
+  const status = task.businessStatus.coding[0].display
+  const pharmacyOdsCode = task.owner.identifier.value
+  const pharmacyTitle = status === "To Be Dispensed" ? "Nominated Pharmacy ODS Code" : "Assigned Pharmacy ODS Code"
   return {
     id: task.focus.identifier.value,
     type: getCourseOfTherapyTypeExtension(task.extension).valueCoding.display,
     patientNhsNumber: formatNhsNumber(task.for.identifier.value),
     creationDate: formatDate(task.authoredOn),
-    status: task.businessStatus.coding[0].display
+    pharmacy: pharmacyOdsCode
+      ? {
+        key: pharmacyTitle,
+        value: pharmacyOdsCode
+      }
+      : undefined,
+    status
   }
 }
 
@@ -39,6 +49,13 @@ export const PrescriptionDetails: React.FC<PrescriptionProps> = ({prescription})
         <SummaryList.Key>NHS Number</SummaryList.Key>
         <SummaryList.Value>{prescription.patientNhsNumber}</SummaryList.Value>
       </SummaryList.Row>
+      {prescription.pharamcy
+        ? <SummaryList.Row>
+          <SummaryList.Key>{prescription.pharamcy.key}</SummaryList.Key>
+          <SummaryList.Value>{prescription.pharamcy.value}</SummaryList.Value>
+        </SummaryList.Row>
+        : ""
+      }
       <SummaryList.Row>
         <SummaryList.Key>Created On</SummaryList.Key>
         <SummaryList.Value>{prescription.creationDate}</SummaryList.Value>
