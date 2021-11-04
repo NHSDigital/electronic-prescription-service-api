2d21e343daa56680629836c79f18b708ee056a5e
Matt Jarvis <61870561+mfjarvis@users.noreply.github.com>
2021-11-04 16:36:54 +0000

    AEA-1730: Fix formatting for quantities with a value of 0.

--- tool/site/client-react/src/formatters/quantity.ts
+++ tool/site/client-react/src/formatters/quantity.ts
@@ -1,5 +1,5 @@
 import * as fhir from "fhir/r4"
 
 export function formatQuantity(quantity: fhir.Quantity): string {
-  return [quantity.value, quantity.unit].filter(Boolean).join(" ")
+  return [quantity.value, quantity.unit].join(" ")
 }
