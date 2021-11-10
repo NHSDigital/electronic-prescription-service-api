0ed25bcd97863feff7e2db5788cc5a323eae0313
Matt Jarvis <61870561+mfjarvis@users.noreply.github.com>
2021-11-10 16:56:46 +0000

    AEA-1822: Query Task by patient identifier. (#635)
    * AEA-1822: Query Task by patient identifier.
    
    * AEA-1822: Fix query parameter validation. Add missing status codes to translation.
    
    * AEA-1822: Fix broken mapping for line item status. Fix test. Simplify query validation. Return multiple errors.
    
    * AEA-1822: Move stuff around. Make the types nicer.
    
    * AEA-1822: Move tests, fix tests, add tests.
    
    * AEA-1822: Add first test for summary response translation.
    
    * AEA-1822: Consolidate Spine model exports.
    
    * AEA-1822: Fix default scope in sandbox.
    
    * AEA-1822: Fix filtering and add tests.
    
    * AEA-1822: Use example fetcher.
    
    * AEA-1822: Update Bundle total when filtering.
    
    * AEA-1822: Review changes.

--- coordinator/src/routes/tracker/task.ts
+++ coordinator/src/routes/tracker/task.ts
@@ -1,13 +1,41 @@
 import Hapi from "@hapi/hapi"
-import {fhir, validationErrors} from "@models"
+import {fhir} from "@models"
 import {BASE_PATH, ContentTypes} from "../util"
 import {getStatusCode} from "../../utils/status-code"
 import {trackerClient} from "../../services/communication/tracker"
-import {convertSpineResponseToFhir} from "../../services/communication/tracker/translation"
-import {RequestHeaders} from "../../utils/headers"
+import {convertSpineTrackerResponseToFhir} from "../../services/translation/response/tracker/translation"
+import {getScope, RequestHeaders} from "../../utils/headers"
 import * as LosslessJson from "lossless-json"
+import {isBundle, isTask} from "../../utils/type-guards"
+import {validateQueryParameters} from "../../services/validation/query-validator"
 
-const VALID_QUERY_PARAMS = ["identifier", "focus:identifier"]
+export enum QueryParam {
+  IDENTIFIER = "identifier",
+  FOCUS_IDENTIFIER = "focus:identifier",
+  PATIENT_IDENTIFIER = "patient:identifier"
+}
+
+export type ValidQuery = Partial<Record<QueryParam, string>>
+
+interface QueryParamProperties {
+  system: string
+  getTaskField: (task: fhir.Task) => string
+}
+
+export const queryParamMetadata = new Map<QueryParam, QueryParamProperties>([
+  [QueryParam.FOCUS_IDENTIFIER, {
+    system: "https://fhir.nhs.uk/Id/prescription-order-number",
+    getTaskField: task => task.focus.identifier.value
+  }],
+  [QueryParam.IDENTIFIER, {
+    system: "https://fhir.nhs.uk/Id/prescription-order-number",
+    getTaskField: task => task.focus.identifier.value
+  }],
+  [QueryParam.PATIENT_IDENTIFIER, {
+    system: "https://fhir.nhs.uk/Id/nhs-number",
+    getTaskField: task => task.for.identifier.value
+  }]
+])
 
 export default [{
   method: "GET",
@@ -15,47 +43,73 @@ export default [{
   handler: async (
     request: Hapi.Request, responseToolkit: Hapi.ResponseToolkit
   ): Promise<Hapi.Lifecycle.ReturnValue> => {
-    const queryParams = request.query
-
-    const issues = validateQueryParameters(queryParams)
+    const query = request.query
+    const scope = getScope(request.headers)
+    const issues = validateQueryParameters(query, scope)
     if (issues.length) {
       const response = fhir.createOperationOutcome(issues)
       const statusCode = getStatusCode(issues)
       return responseToolkit.response(response).code(statusCode).type(ContentTypes.FHIR)
-    } else {
-      const validatedParams = queryParams as { [key: string]: string }
-      const prescriptionIdentifier = validatedParams["focus:identifier"] || validatedParams["identifier"]
-
-      const spineResponse = await trackerClient.getPrescription(prescriptionIdentifier, request.headers, request.logger)
-      if (request.headers[RequestHeaders.RAW_RESPONSE]) {
-        return responseToolkit
-          .response(JSON.stringify(spineResponse))
-          .code(200)
-          .type(ContentTypes.JSON)
-      } else {
-        return responseToolkit
-          .response(LosslessJson.stringify(convertSpineResponseToFhir(spineResponse)))
-          .code(200)
-          .type(ContentTypes.FHIR)
-      }
     }
+
+    const validQuery = query as ValidQuery
+    const spineResponse = await makeSpineRequest(validQuery, request)
+
+    if (request.headers[RequestHeaders.RAW_RESPONSE]) {
+      return responseToolkit
+        .response(JSON.stringify(spineResponse))
+        .code(200)
+        .type(ContentTypes.JSON)
+    }
+
+    const result = convertSpineTrackerResponseToFhir(spineResponse)
+    if (isBundle(result)) {
+      filterBundleEntries(result, validQuery)
+    }
+    return responseToolkit
+      .response(LosslessJson.stringify(result))
+      .code(200)
+      .type(ContentTypes.FHIR)
   }
 }]
 
-export const validateQueryParameters = (queryParams: Hapi.RequestQuery): Array<fhir.OperationOutcomeIssue> => {
-  const validQueryParamsFound = VALID_QUERY_PARAMS.filter(param => queryParams[param])
-  if (validQueryParamsFound.length === 0) {
-    return [validationErrors.createMissingQueryParameterIssue(VALID_QUERY_PARAMS)]
+async function makeSpineRequest(validQuery: ValidQuery, request: Hapi.Request) {
+  const prescriptionIdentifier = getValue(validQuery, QueryParam.FOCUS_IDENTIFIER)
+    || getValue(validQuery, QueryParam.IDENTIFIER)
+  if (prescriptionIdentifier) {
+    return await trackerClient.getPrescriptionById(prescriptionIdentifier, request.headers, request.logger)
   }
 
-  const duplicatedParams = validQueryParamsFound.some(param => Array.isArray(queryParams[param]))
-  if (duplicatedParams) {
-    return [validationErrors.invalidQueryParameterCombinationIssue]
+  const patientIdentifier = getValue(validQuery, QueryParam.PATIENT_IDENTIFIER)
+  if (patientIdentifier) {
+    return await trackerClient.getPrescriptionsByPatientId(patientIdentifier, request.headers, request.logger)
   }
-
-  if (validQueryParamsFound.includes("identifier") && validQueryParamsFound.includes("focus:identifier")) {
-    return [validationErrors.invalidQueryParameterCombinationIssue]
-  }
-  return []
 }
 
+export function getValue(query: ValidQuery, param: QueryParam): string {
+  const rawValue = query[param]
+  const pipeIndex = rawValue?.indexOf("|") || -1
+  if (pipeIndex !== -1) {
+    return rawValue.substring(pipeIndex + 1)
+  }
+  return rawValue
+}
+
+export function filterBundleEntries(bundle: fhir.Bundle, queryParams: ValidQuery): void {
+  const filteredEntries = bundle.entry.filter(entry =>
+    isTask(entry.resource)
+    && matchesQuery(entry.resource, queryParams)
+  )
+  bundle.entry = filteredEntries
+  bundle.total = filteredEntries.length
+}
+
+export function matchesQuery(task: fhir.Task, queryParams: ValidQuery): boolean {
+  for (const [queryParam, metadata] of queryParamMetadata) {
+    const queryParamValue = getValue(queryParams, queryParam)
+    if (queryParamValue && metadata.getTaskField(task) !== queryParamValue) {
+      return false
+    }
+  }
+  return true
+}
