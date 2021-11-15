311be984f148ca50ad4d4e3069d43ba749d0b2c7
Alex McCormick <amccmick1@googlemail.com>
2021-11-15 12:11:43 +0000

    AEA-1837 Update test-pack resource creation to match new validation rules

--- tool/site/client/parsers/read/medication-request-parser.ts
+++ tool/site/client/parsers/read/medication-request-parser.ts
@@ -2,7 +2,7 @@ import {MedicationRequest} from "../../models"
 import * as fhirExtension from "../../models/extension"
 
 export function getMedicationRequestNumberOfRepeatsAllowed(medicationRequest: MedicationRequest): string | number {
-  const repeatInformation = medicationRequest.extension.find(e =>
+  const repeatInformation = medicationRequest.dispenseRequest?.extension.find(e =>
     e.url === "https://fhir.hl7.org.uk/StructureDefinition/Extension-UKCore-MedicationRepeatInformation"
   )
   const numberOfRepeatPrescriptionsAllowedExtension =
