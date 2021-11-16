aee466e2f34ebae27b93dca2faf7dfcae8c333df
Matt Jarvis <61870561+mfjarvis@users.noreply.github.com>
2021-11-16 14:41:50 +0000

    AEA-1822: Add sorting to prescription summary table.

--- tool/site/client-react/src/formatters/dates.ts
+++ tool/site/client-react/src/formatters/dates.ts
@@ -1,7 +1,11 @@
 import * as moment from "moment"
 
 export function formatDate(date: string): string {
-  return moment.utc(date).format("DD-MMM-YYYY")
+  return formatMomentAsDate(moment.utc(date))
+}
+
+export function formatMomentAsDate(date: moment.Moment): string {
+  return date.format("DD-MMM-YYYY")
 }
 
 export function formatCurrentDate(): string {
---
+++ tool/site/client-react/tests/components/sorter.spec.tsx
@@ -0,0 +1,84 @@
+import React from "react"
+import {Table} from "nhsuk-react-components"
+import {Sorter, useSorter} from "../../src/components/sorter"
+import {render, screen, waitFor} from "@testing-library/react"
+import pretty from "pretty"
+import userEvent from "@testing-library/user-event"
+
+interface TestItem {
+  a: string
+  b: Date
+  c: number
+}
+
+const TestTable: React.FC<Sorter<TestItem>> = ({sortedItems, sortBy, getIcon}) => (
+  <Table>
+    <Table.Head>
+      <Table.Row>
+        <Table.Cell onClick={() => sortBy("a")}>{getIcon("a")}String</Table.Cell>
+        <Table.Cell onClick={() => sortBy("b")}>{getIcon("b")}Date</Table.Cell>
+        <Table.Cell onClick={() => sortBy("c")}>{getIcon("c")}Number</Table.Cell>
+      </Table.Row>
+    </Table.Head>
+    <Table.Body>
+      {sortedItems.map(item => (
+        <Table.Row key={item.a}>
+          <Table.Cell>{item.a}</Table.Cell>
+          <Table.Cell>{item.b.toISOString()}</Table.Cell>
+          <Table.Cell>{item.c}</Table.Cell>
+        </Table.Row>
+      ))}
+    </Table.Body>
+  </Table>
+)
+
+const item1 = {a: "testing", b: new Date("2021-06-05T09:30:00Z"), c: 42}
+const item2 = {a: "also testing", b: new Date("2021-03-11T12:57:00Z"), c: 420}
+const item3 = {a: "still testing", b: new Date("2021-07-28T04:05:00Z"), c: 4200}
+const items: Array<TestItem> = [item1, item2, item3]
+
+test("Table renders without initial sort config", () => {
+  const TestSortableTable: React.FC = () => {
+    const {sortedItems, sortBy, getIcon} = useSorter(items)
+    return <TestTable sortedItems={sortedItems} sortBy={sortBy} getIcon={getIcon}/>
+  }
+  const {container} = render(<TestSortableTable/>)
+  expect(pretty(container.innerHTML)).toMatchSnapshot()
+})
+
+test("Table renders with initial sort config", () => {
+  const TestSortableTable: React.FC = () => {
+    const {sortedItems, sortBy, getIcon} = useSorter(items, {key: "c", ascending: false})
+    return <TestTable sortedItems={sortedItems} sortBy={sortBy} getIcon={getIcon}/>
+  }
+  const {container} = render(<TestSortableTable/>)
+  expect(pretty(container.innerHTML)).toMatchSnapshot()
+})
+
+test("Clicking a column header which is not the current sort key changes the sort key", async () => {
+  let sortedItemsRef: Array<TestItem>
+  const TestSortableTable: React.FC = () => {
+    const {sortedItems, sortBy, getIcon} = useSorter(items, {key: "c", ascending: false})
+    sortedItemsRef = sortedItems
+    return <TestTable sortedItems={sortedItems} sortBy={sortBy} getIcon={getIcon}/>
+  }
+  const {container} = render(<TestSortableTable/>)
+  userEvent.click(await screen.findByText("Date"))
+  await waitFor(() => expect(sortedItemsRef).toEqual([item2, item1, item3]))
+  expect(pretty(container.innerHTML)).toMatchSnapshot()
+})
+
+test("Clicking a column header which is the current sort key toggles the sort direction", async () => {
+  let sortedItemsRef: Array<TestItem>
+  const TestSortableTable: React.FC = () => {
+    const {sortedItems, sortBy, getIcon} = useSorter(items, {key: "c", ascending: false})
+    sortedItemsRef = sortedItems
+    return <TestTable sortedItems={sortedItems} sortBy={sortBy} getIcon={getIcon}/>
+  }
+  const {container} = render(<TestSortableTable/>)
+  userEvent.click(await screen.findByText("Date"))
+  await waitFor(() => expect(sortedItemsRef).toEqual([item2, item1, item3]))
+  userEvent.click(await screen.findByText("Date"))
+  await waitFor(() => expect(sortedItemsRef).toEqual([item3, item1, item2]))
+  expect(pretty(container.innerHTML)).toMatchSnapshot()
+})
