f6c4159577843ac56d0470b8a9844c3b510886b0
Alex McCormick <amccmick1@googlemail.com>
2022-01-20 11:37:55 +0000

    Split up my prescriptions page into components

--- tool/site/client-react/src/pages/myPrescriptionsPage.tsx
+++ tool/site/client-react/src/pages/myPrescriptionsPage.tsx
@@ -1,10 +1,10 @@
 import * as React from "react"
 import {useContext} from "react"
-import {Label, Table} from "nhsuk-react-components"
+import {Label} from "nhsuk-react-components"
 import {AppContext} from "../index"
 import LongRunningTask from "../components/longRunningTask"
 import {axiosInstance} from "../requests/axiosInstance"
-import PrescriptionActions from "../components/prescriptionActions"
+import MyPrescriptions, {Prescriptions} from "../components/my-prescriptions/myPrescriptions"
 
 const MyPrescriptionsPage: React.FC = () => {
   const {baseUrl} = useContext(AppContext)
@@ -24,18 +24,7 @@ const MyPrescriptionsPage: React.FC = () => {
         return (
           <>
             <Label isPageHeading>My Prescriptions</Label>
-            <PrescriptionGroupTable
-              name="Sent Prescriptions"
-              description="Prescriptions ready to release"
-              prescriptions={prescriptions.sentPrescriptions}
-              actions={{view: true, release: true}}
-            />
-            <PrescriptionGroupTable
-              name="Released Prescriptions"
-              description="Prescriptions ready to dispense"
-              prescriptions={prescriptions.releasedPrescriptions}
-              actions={{view: true, dispense: true}}
-            />
+            <MyPrescriptions {...prescriptions} />
           </>
         )
       }}
@@ -43,65 +32,8 @@ const MyPrescriptionsPage: React.FC = () => {
   )
 }
 
-interface Prescriptions {
-  any: Boolean
-  sentPrescriptions: Array<PrescriptionSummary>
-  releasedPrescriptions: Array<PrescriptionSummary>
-}
-
-interface PrescriptionSummary {
-  id: string
-}
-
 async function retrievePrescriptions(baseUrl: string): Promise<Prescriptions> {
   return await (await axiosInstance.get<Prescriptions>(`${baseUrl}prescriptions`)).data
-}
-
-interface PrescriptionGroupTableProps {
-  name: string
-  description: string
-  prescriptions: Array<{id: string}>
-  actions: PrescriptionActionProps
-}
-
-interface PrescriptionActionProps {
-  view?: boolean
-  release?: boolean
-  dispense?: boolean
-  claim?: boolean
-}
-
-export const PrescriptionGroupTable: React.FC<PrescriptionGroupTableProps> = ({
-  name,
-  description,
-  prescriptions,
-  actions
-}) => {
-  if (!prescriptions.length) {
-    return null
-  }
-  return (
-    <Table.Panel heading={name}>
-      <Table caption={description}>
-        <Table.Head>
-          <Table.Row>
-            <Table.Cell>ID</Table.Cell>
-            <Table.Cell>Actions</Table.Cell>
-          </Table.Row>
-        </Table.Head>
-        <Table.Body>
-          {prescriptions.map(prescription =>
-            <Table.Row>
-              <Table.Cell>{prescription.id}</Table.Cell>
-              <Table.Cell>
-                <PrescriptionActions prescriptionId={prescription.id} {...actions}/>
-              </Table.Cell>
-            </Table.Row>
-          )}
-        </Table.Body>
-      </Table>
-    </Table.Panel>
-  )
 }
 
 export default MyPrescriptionsPage
