7e7b8fb85ecb7226bd7b52827a7b420bc1e6be4f
Alex McCormick <amccmick1@googlemail.com>
2022-01-20 19:42:00 +0000

    Remove redundant args for rendering old client

--- tool/site/server_flask/client.py
+++ tool/site/server_flask/client.py
@@ -1,15 +1,12 @@
 import flask
 import config
 
-def render_rivets_client(sign_response=None, send_response=None, release_response=None):
+def render_rivets_client():
     return flask.render_template(
         "client.html",
         environment=config.ENVIRONMENT,
         public_apigee_url=config.PUBLIC_APIGEE_URL,
-        base_url=config.BASE_URL,
-        sign_response=sign_response,
-        send_response=send_response,
-        release_response=release_response,
+        base_url=config.BASE_URL
     )
 
 
