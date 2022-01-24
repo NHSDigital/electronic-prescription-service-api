9afdbb3fc3c5cf0a33682ff9076fe095686654bf
callumtipper-IW <callum.tipper@infinityworks.com>
2022-01-24 15:48:45 +0000

    updated react tests and snapshots

--- tool/site/client-react/tests/components/dispense/props.ts
+++ tool/site/client-react/tests/components/dispense/props.ts
@@ -5,13 +5,15 @@ export const staticLineItemInfoArray: Array<StaticLineItemInfo> = [
   {
     id: "599f341b-f94a-4157-92a6-7883feb6b499",
     name: "Salbutamol 100micrograms/dose inhaler CFC free",
-    quantity: "200 dose",
+    quantityValue: 200,
+    quantityUnit: "dose",
     priorStatusCode: LineItemStatus.WITH_DISPENSER
   },
   {
     id: "85ad7441-845b-4ec4-b836-f503ee33b805",
     name: "Paracetamol 500mg soluble tablets",
-    quantity: "100 tablet",
+    quantityValue: 100,
+    quantityUnit: "tablet",
     priorStatusCode: LineItemStatus.NOT_DISPENSED,
     priorNonDispensingReasonCode: "0011"
   }]
