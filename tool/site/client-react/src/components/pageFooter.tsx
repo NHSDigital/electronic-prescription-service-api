2d2ac827f738d2ab4a591e852070b3c45dd1b935
Alex McCormick <amccmick1@googlemail.com>
2022-01-27 14:06:57 +0000

    Add software versions to epsat (#717)
    * Show hardcoded software versions in footer
    
    * Add hooks to populate software versions and lint
    
    * Fix callout to get software versions from status endpoint
    
    * Move software version status checks from _status to _healthcheck
    
    * Use apigee domain name instead of public url for backend calls to get versions
    
    * Render software versions in deployed and local version
    
    * Render software versions in deployed and local version
    
    * Rollback validator submodule upgrade
    
    Co-authored-by: Callum Tipper <77498779+callumtipper-IW@users.noreply.github.com>

---
+++ tool/site/client-react/src/components/pageFooter.tsx
@@ -0,0 +1,56 @@
+import * as React from "react"
+import {useContext, useEffect, useState} from "react"
+import {Footer} from "nhsuk-react-components"
+import {AppContext} from "../index"
+import {axiosInstance} from "../requests/axiosInstance"
+
+interface Status {
+  checks: Checks
+}
+
+interface Checks {
+  eps: Array<Check>
+  "signing-service": Array<Check>
+  validator: Array<Check>
+}
+
+interface Check {
+  version: string
+}
+
+interface SoftwareVersions {
+  eps: string
+  signingService: string
+  validator: string
+}
+
+export const PageFooter: React.FC = () => {
+  const {baseUrl} = useContext(AppContext)
+
+  const [softwareVersions, setSoftwareVersions] = useState<SoftwareVersions>()
+
+  useEffect(() => {
+    if (!softwareVersions) {
+      (async() => {
+        const statusResult = await (await axiosInstance.get<Status>(`${baseUrl}_healthcheck`)).data
+        setSoftwareVersions({
+          eps: statusResult.checks.eps[0].version,
+          signingService: statusResult.checks["signing-service"][0].version,
+          validator: statusResult.checks.validator[0].version})
+      })()
+    }
+  }, [baseUrl, softwareVersions])
+
+  return (
+    <Footer>
+      {softwareVersions &&
+        <Footer.List>
+          <Footer.ListItem>EPS: {softwareVersions.eps}</Footer.ListItem>
+          <Footer.ListItem>Signing-Service: {softwareVersions.signingService}</Footer.ListItem>
+          <Footer.ListItem>Validator: {softwareVersions.validator}</Footer.ListItem>
+        </Footer.List>
+      }
+      <Footer.Copyright>&copy; Crown copyright</Footer.Copyright>
+    </Footer>
+  )
+}
