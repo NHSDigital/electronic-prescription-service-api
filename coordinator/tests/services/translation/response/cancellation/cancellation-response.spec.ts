bcccbaee8242e85a4e1063efbce75e30b1db24c3
Alex McCormick <amccmick1@googlemail.com>
2022-01-28 12:33:21 +0000

    AEA-1978 Handle Cancel/Release response care setting translations (#704)
    (cherry picked from commit ee859ca27f39c53cc64efd8e7ee64d88e6f16e34)

--- coordinator/tests/services/translation/response/cancellation/cancellation-response.spec.ts
+++ coordinator/tests/services/translation/response/cancellation/cancellation-response.spec.ts
@@ -4,7 +4,6 @@ import {
   translateSpineCancelResponseIntoOperationOutcome
 } from "../../../../../src/services/translation/response/cancellation/cancellation-response"
 import {
-  getHealthcareServices,
   getLocations,
   getMedicationRequests,
   getMessageHeader,
@@ -69,10 +68,6 @@ describe("bundle entries", () => {
     expect(getPractitioners(fhirBundle)).toHaveLength(2)
   })
 
-  test("entries contains two HealthcareServices", () => {
-    expect(getHealthcareServices(fhirBundle)).toHaveLength(2)
-  })
-
   test("entries contains two Locations", () => {
     expect(getLocations(fhirBundle)).toHaveLength(2)
   })
@@ -110,12 +105,6 @@ describe("bundle entries", () => {
     expect(postcodes).toContain("PR26 7QN")
   })
 
-  test("performer field in hl7 message adds performer healthcareService", () => {
-    const healthcareServices = getHealthcareServices(performerFhirBundle)
-    const healthcareServiceNames = healthcareServices.map(healthcareService => healthcareService.name)
-    expect(healthcareServiceNames).toContain("CRx PM Chetna2 EPS")
-  })
-
   test("performer field in hl7 message adds dispense reference to MedicationRequest", () => {
     expect(getMedicationRequests(performerFhirBundle)[0].dispenseRequest).toBeDefined()
   })
@@ -127,7 +116,6 @@ describe("bundle entries", () => {
     const translatedDispenseBundle = translateSpineCancelResponseIntoBundle(dispenseError)
     expect(getPractitioners(translatedDispenseBundle)).toHaveLength(1)
     expect(getPractitionerRoles(translatedDispenseBundle)).toHaveLength(1)
-    expect(getHealthcareServices(translatedDispenseBundle)).toHaveLength(1)
     expect(getLocations(translatedDispenseBundle)).toHaveLength(1)
   })
 })
