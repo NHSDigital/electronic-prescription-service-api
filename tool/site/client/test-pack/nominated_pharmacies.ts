5308199efc1591e8a5dcfb03472c7f11302fa96a
Alex McCormick <amccmick1@googlemail.com>
2022-01-28 17:34:35 +0000

    Remove old client from epsat (#719)
    (cherry picked from commit 91f100da4b2e821afce136f1984996d65f1cbc70)

--- tool/site/client/test-pack/nominated_pharmacies.ts
+++
@@ -1,5 +0,0 @@
-import {StringKeyedObject} from "./helpers"
-
-export function createNominatedPharmacies(rows: Array<StringKeyedObject>): Array<string> {
-  return rows.map(row => row["ODS Code"])
-}
