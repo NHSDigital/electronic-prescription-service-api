96781eb168a552989be1dd7c162913f26b947f0c
Dan Jones <dan.jones@infinityworks.com>
2022-01-31 11:44:54 +0000

    Revert "Migrate epsat load page to react client (#718)"
    This reverts commit dd25b5b5a3408e4f07d09b564218461b48c8e4f6.

--- tool/site/client-react/src/fhir/helpers.ts
+++ tool/site/client-react/src/fhir/helpers.ts
@@ -1,15 +1,11 @@
 import * as fhir from "fhir/r4"
+import {MedicationRequest} from "fhir/r4"
 import {
-  getLongFormIdExtension,
   getUkCoreNumberOfRepeatsIssuedExtension,
   RepeatInformationExtension
 } from "./customExtensions"
 import * as uuid from "uuid"
 import {COURSE_OF_THERAPY_TYPE_CODES} from "./reference-data/valueSets"
-import {getMedicationRequestResources} from "./bundleResourceFinder"
-import {generateShortFormIdFromExisting} from "./generatePrescriptionIds"
-import {convertMomentToISODate} from "../formatters/dates"
-import * as moment from "moment"
 
 export function getMedicationRequestLineItemId(medicationRequest: fhir.MedicationRequest): string {
   return medicationRequest.identifier[0].value
@@ -42,11 +38,11 @@ export function createIdentifier(): fhir.Identifier {
   }
 }
 
-export function requiresDispensingRepeatInformationExtension(medicationRequest: fhir.MedicationRequest): boolean {
+export function requiresDispensingRepeatInformationExtension(medicationRequest: MedicationRequest): boolean {
   return medicationRequest.courseOfTherapyType.coding[0].code !== COURSE_OF_THERAPY_TYPE_CODES.ACUTE
 }
 
-export function createDispensingRepeatInformationExtension(medicationRequest: fhir.MedicationRequest): RepeatInformationExtension {
+export function createDispensingRepeatInformationExtension(medicationRequest: MedicationRequest): RepeatInformationExtension {
   const [repeatsIssued, repeatsAllowed] = getRepeatsIssuedAndAllowed(medicationRequest)
   return {
     url: "https://fhir.nhs.uk/StructureDefinition/Extension-EPS-RepeatInformation",
@@ -63,45 +59,11 @@ export function createDispensingRepeatInformationExtension(medicationRequest: fh
   }
 }
 
-export function getRepeatsIssuedAndAllowed(medicationRequest: fhir.MedicationRequest): [number, number] {
+export function getRepeatsIssuedAndAllowed(medicationRequest: MedicationRequest): [number, number] {
   const ukCoreRepeatsIssuedExtension = getUkCoreNumberOfRepeatsIssuedExtension(medicationRequest.extension)
   const numberOfRepeatPrescriptionsIssued = ukCoreRepeatsIssuedExtension
     ? ukCoreRepeatsIssuedExtension.valueUnsignedInt
     : 1
   const numberOfRepeatPrescriptionsAllowed = (medicationRequest.dispenseRequest?.numberOfRepeatsAllowed || 0) + 1
   return [numberOfRepeatPrescriptionsIssued, numberOfRepeatPrescriptionsAllowed]
-}
-
-export function updateBundleIds(bundle: fhir.Bundle): void {
-  const firstGroupIdentifier = getMedicationRequestResources(bundle)[0].groupIdentifier
-  const originalShortFormId = firstGroupIdentifier.value
-
-  const newShortFormId = generateShortFormIdFromExisting(originalShortFormId)
-  const newLongFormId = uuid.v4()
-
-  bundle.identifier.value = uuid.v4()
-  getMedicationRequestResources(bundle).forEach(medicationRequest => {
-    medicationRequest.identifier[0].value = uuid.v4()
-    const groupIdentifier = medicationRequest.groupIdentifier
-    groupIdentifier.value = newShortFormId
-    getLongFormIdExtension(groupIdentifier.extension).valueIdentifier.value = newLongFormId
-  })
-}
-
-export function updateValidityPeriodIfRepeatDispensing(bundle: fhir.Bundle): void {
-  if (isRepeatDispensing(bundle)) {
-    const start = convertMomentToISODate(moment.utc())
-    const end = convertMomentToISODate(moment.utc().add(1, "month"))
-    getMedicationRequestResources(bundle).forEach(request => {
-      const validityPeriod = request.dispenseRequest.validityPeriod
-      validityPeriod.start = start
-      validityPeriod.end = end
-    })
-  }
-}
-
-export function isRepeatDispensing(bundle: fhir.Bundle): boolean {
-  return getMedicationRequestResources(bundle)
-    .flatMap(medicationRequest => medicationRequest.courseOfTherapyType.coding)
-    .some(coding => coding.code === "continuous-repeat-dispensing")
 }
