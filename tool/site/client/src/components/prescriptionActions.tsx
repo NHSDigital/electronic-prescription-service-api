0bc8db6554c53934952ba3906b4b5fa9d933565a
Alex McCormick <amccmick1@googlemail.com>
2022-02-03 11:35:38 +0000

    Encode uri for prescription actions to handle + character in prescription_id query parameter

--- tool/site/client/src/components/prescriptionActions.tsx
+++ tool/site/client/src/components/prescriptionActions.tsx
@@ -28,37 +28,37 @@ const PrescriptionActions: React.FC<PrescriptionActionsProps> = ({
   return (
     <>
       {cancel && (
-        <ActionLink href={`${baseUrl}prescribe/cancel?prescription_id=${prescriptionId}`}>
+        <ActionLink href={encodeURI(`${baseUrl}prescribe/cancel?prescription_id=${prescriptionId}`)}>
           Cancel prescription
         </ActionLink>
       )}
       {release && (
-        <ActionLink href={`${baseUrl}dispense/release?prescription_id=${prescriptionId}`}>
+        <ActionLink href={encodeURI(`${baseUrl}dispense/release?prescription_id=${prescriptionId}`)}>
           Release prescription
         </ActionLink>
       )}
       {releaseReturn && (
-        <ActionLink href={`${baseUrl}dispense/return?prescription_id=${prescriptionId}`}>
+        <ActionLink href={encodeURI(`${baseUrl}dispense/return?prescription_id=${prescriptionId}`)}>
           Return prescription
         </ActionLink>
       )}
       {dispense && (
-        <ActionLink href={`${baseUrl}dispense/dispense?prescription_id=${prescriptionId}`}>
+        <ActionLink href={encodeURI(`${baseUrl}dispense/dispense?prescription_id=${prescriptionId}`)}>
           Dispense prescription
         </ActionLink>
       )}
       {withdraw && (
-        <ActionLink href={`${baseUrl}dispense/withdraw?prescription_id=${prescriptionId}`}>
+        <ActionLink href={encodeURI(`${baseUrl}dispense/withdraw?prescription_id=${prescriptionId}`)}>
           Withdraw prescription
         </ActionLink>
       )}
       {claim && (
-        <ActionLink href={`${baseUrl}dispense/claim?prescription_id=${prescriptionId}`}>
+        <ActionLink href={encodeURI(`${baseUrl}dispense/claim?prescription_id=${prescriptionId}`)}>
           Claim for prescription
         </ActionLink>
       )}
       {view && (
-        <ActionLink href={`${baseUrl}search?prescription_id=${prescriptionId}`}>
+        <ActionLink href={encodeURI(`${baseUrl}search?prescription_id=${prescriptionId}`)}>
           View prescription
         </ActionLink>
       )}
