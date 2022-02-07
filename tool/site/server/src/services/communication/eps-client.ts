07d0755968f242e87a5d6669f5b9feb15868721d
Alex McCormick <amccmick1@googlemail.com>
2022-02-07 03:49:47 +0000

    Cleanup sandbox and live eps clients

--- tool/site/server/src/services/communication/eps-client.ts
+++ tool/site/server/src/services/communication/eps-client.ts
@@ -1,33 +1,115 @@
+import * as uuid from "uuid"
 import {SandboxEpsClient} from "./sandbox-eps-client"
 import {LiveEpsClient} from "./live-eps-client"
 import {Bundle, Claim, FhirResource, OperationOutcome, Parameters, Task} from "fhir/r4"
 import {isLocal} from "../environment"
+import {URLSearchParams} from "url"
+import axios, {AxiosRequestHeaders, AxiosResponse} from "axios"
+import {CONFIG} from "../../config"
 
-export interface EpsClient {
-  makeGetTrackerRequest(query: Record<string, string>): Promise<Bundle | OperationOutcome>
-  makePrepareRequest(body: Bundle): Promise<Parameters | OperationOutcome>
-  makeSendRequest(body: Bundle): Promise<EpsResponse<OperationOutcome>>
-  makeReleaseRequest(body: Parameters): Promise<EpsResponse<Bundle | OperationOutcome>>
-  makeReturnRequest(body: Task): Promise<EpsResponse<OperationOutcome>>
-  makeWithdrawRequest(body: Task): Promise<EpsResponse<OperationOutcome>>
-  makeClaimRequest(body: Claim): Promise<EpsResponse<OperationOutcome>>
-  makeValidateRequest(body: FhirResource): Promise<EpsResponse<OperationOutcome>>
-  makeConvertRequest(body: FhirResource): Promise<string>
-}
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-export function getEpsClient(accessToken: string): EpsClient {
-  return isLocal()
-    ? new SandboxEpsClient()
-    : new LiveEpsClient(accessToken)
-}
-
-export interface EpsResponse<T> {
+interface EpsResponse<T> {
   statusCode: number,
   fhirResponse: T
   spineResponse?: string
 }
 
-export function asString(response: string | OperationOutcome): string {
-  return typeof response === "string" ? response : JSON.stringify(response, null, 2)
+export class EpsClient {
+  async makeGetTrackerRequest(query: Record<string, string | Array<string>>): Promise<Bundle | OperationOutcome> {
+    const urlSearchParams = new URLSearchParams()
+    Object.keys(query).forEach(key => {
+      const valueOrValues = query[key]
+      if (typeof valueOrValues === "string") {
+        urlSearchParams.append(key, valueOrValues)
+      } else {
+        valueOrValues.forEach(value => urlSearchParams.append(key, value))
+      }
+    })
+    return (await this.makeApiCall<Bundle | OperationOutcome>("Task", undefined, urlSearchParams)).data
+  }
+
+  async makePrepareRequest(body: Bundle): Promise<Parameters | OperationOutcome> {
+    return (await this.makeApiCall<Parameters | OperationOutcome>("$prepare", body)).data
+  }
+
+  async makeSendRequest(body: Bundle): Promise<EpsResponse<OperationOutcome>> {
+    return await this.getEpsResponse("$process-message", body)
+  }
+
+  async makeReleaseRequest(body: Parameters): Promise<EpsResponse<Bundle | OperationOutcome>> {
+    return await this.getEpsResponse("Task/$release", body)
+  }
+
+  async makeReturnRequest(body: Task): Promise<EpsResponse<OperationOutcome>> {
+    return await this.getEpsResponse("Task", body)
+  }
+
+  async makeWithdrawRequest(body: Task): Promise<EpsResponse<OperationOutcome>> {
+    return await this.getEpsResponse("Task", body)
+  }
+
+  async makeClaimRequest(body: Claim): Promise<EpsResponse<OperationOutcome>> {
+    return await this.getEpsResponse("Claim", body)
+  }
+
+  async makeValidateRequest(body: FhirResource): Promise<EpsResponse<OperationOutcome>> {
+    const requestId = uuid.v4()
+    const response = await this.makeApiCall<OperationOutcome>("$validate", body, undefined, requestId)
+    const statusCode = response.status
+    const fhirResponse = response.data
+    return {statusCode, fhirResponse}
+  }
+
+  async makeConvertRequest(body: FhirResource): Promise<string> {
+    const response = (await this.makeApiCall<string | OperationOutcome>("$convert", body)).data
+    return typeof response === "string" ? response : JSON.stringify(response, null, 2)
+  }
+
+  private async getEpsResponse<T>(endpoint: string, body: FhirResource) {
+    const requestId = uuid.v4()
+    const response = await this.makeApiCall<T>(endpoint, body, undefined, requestId)
+    const statusCode = response.status
+    const fhirResponse = response.data
+    const spineResponse = (await this.makeApiCall<string | OperationOutcome>(endpoint, body, undefined, requestId, {"x-raw-response": "true"})).data
+    return {statusCode, fhirResponse, spineResponse: this.asString(spineResponse)}
+  }
+
+  private async makeApiCall<T>(
+    path: string,
+    body?: unknown,
+    params?: URLSearchParams,
+    requestId?: string,
+    additionalHeaders?: AxiosRequestHeaders
+  ): Promise<AxiosResponse<T>> {
+    const basePath = `${CONFIG.basePath}`.replace("eps-api-tool", "electronic-prescriptions")
+    const url = `${CONFIG.privateApigeeUrl}/${basePath}/FHIR/R4/${path}`
+    const headers: AxiosRequestHeaders = this.getHeaders(requestId)
+    if (additionalHeaders) {
+      Object.assign(headers, additionalHeaders)
+    }
+
+    return axios.request({
+      url,
+      method: body ? "POST" : "GET",
+      headers,
+      data: body,
+      params
+    })
+  }
+
+  protected getHeaders(requestId: string | undefined): AxiosRequestHeaders {
+    return {
+      "x-request-id": requestId ?? uuid.v4(),
+      "x-correlation-id": uuid.v4()
+    }
+  }
+
+  private asString(response: string | OperationOutcome): string {
+    return typeof response === "string" ? response : JSON.stringify(response, null, 2)
+  }
+}
+
+export function getEpsClient(accessToken: string): EpsClient {
+  return isLocal()
+    ? new SandboxEpsClient()
+    : new LiveEpsClient(accessToken)
 }
