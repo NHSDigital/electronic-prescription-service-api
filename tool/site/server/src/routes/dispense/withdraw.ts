b905362949d1363e28a0701027d7289e01e5283b
Jack Robinson <jack.robinson@infinityworks.com>
2022-02-08 16:58:05 +0000

    withdraw removes dispense notification from session storage

--- tool/site/server/src/routes/dispense/withdraw.ts
+++ tool/site/server/src/routes/dispense/withdraw.ts
@@ -1,6 +1,12 @@
 import Hapi from "@hapi/hapi"
-import {getSessionValue} from "../../services/session"
-import {Task} from "fhir/r4"
+import {
+  appendToSessionValue,
+  getSessionValue,
+  removeFromSessionValue,
+  setSessionValue,
+  clearSessionValue
+} from "../../services/session"
+import {Bundle, Task} from "fhir/r4"
 import {getEpsClient} from "../../services/communication/eps-client"
 
 export default [
@@ -14,6 +20,22 @@ export default [
       const withdrawResponse = await epsClient.makeWithdrawRequest(withdrawRequest)
       const withdrawRequestHl7 = await epsClient.makeConvertRequest(withdrawRequest)
       const success = withdrawResponse.statusCode === 200
+      if (success) {
+        const prescriptionId = withdrawRequest.groupIdentifier?.value
+        const dispenseNotificationRequestKey = `dispense_notification_requests_${prescriptionId}`
+
+        const dispenseNotificationRequests = getSessionValue(dispenseNotificationRequestKey, request) as Bundle[]
+        const newDispenseNotifications = dispenseNotificationRequests.slice(0, -1)
+
+        if (newDispenseNotifications.length === 0) {
+          clearSessionValue(dispenseNotificationRequestKey, request)
+          appendToSessionValue("released_prescription_ids", [prescriptionId], request)
+          removeFromSessionValue("dispensed_prescription_ids", prescriptionId, request)
+        } else {
+          setSessionValue(dispenseNotificationRequestKey, newDispenseNotifications, request)
+        }
+      }
+
       return responseToolkit.response({
         success,
         request_xml: withdrawRequestHl7,
