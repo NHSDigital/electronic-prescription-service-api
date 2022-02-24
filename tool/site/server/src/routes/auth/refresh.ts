60c19611385fb2ce6171b0871fc0488c4814fb9f
Alex McCormick <amccmick1@googlemail.com>
2022-02-24 17:44:22 +0000

    Fix refresh route

--- tool/site/server/src/routes/auth/refresh.ts
+++ tool/site/server/src/routes/auth/refresh.ts
@@ -3,16 +3,14 @@ import {refreshToken} from "../../oauthUtils"
 import {getSessionValue, setSessionValue} from "../../services/session"
 import {getUtcEpochSeconds} from "../util"
 
-export default [
-  {
-    method: "POST",
-    path: "/auth/refresh",
-    handler: async (request: Hapi.Request, h: Hapi.ResponseToolkit): Promise<Hapi.ResponseObject> => {
-      const oauthData = getSessionValue("oauth_data", request)
-      const token = await refreshToken(oauthData)
-      setSessionValue("oauth_data", token.data, request)
-      setSessionValue("access_token", token.accessToken, request)
-      return h.response({success: true, lastTokenFetched: getUtcEpochSeconds()}).code(200)
-    }
+export default {
+  method: "POST",
+  path: "/auth/refresh",
+  handler: async (request: Hapi.Request, h: Hapi.ResponseToolkit): Promise<Hapi.ResponseObject> => {
+    const oauthData = getSessionValue("oauth_data", request)
+    const token = await refreshToken(oauthData)
+    setSessionValue("oauth_data", token.data, request)
+    setSessionValue("access_token", token.accessToken, request)
+    return h.response({success: true, lastTokenFetched: getUtcEpochSeconds()}).code(200)
   }
-]
+}
