129d9a9d0fe72793a9495d43e67b6fe24853d3d7
Alex McCormick <amccmick1@googlemail.com>
2022-02-28 22:19:50 +0000

    Test refresh token flow

--- tool/site/server/src/routes/auth/refresh.ts
+++ tool/site/server/src/routes/auth/refresh.ts
@@ -8,27 +8,18 @@ export default {
   method: "POST",
   path: "/auth/refresh",
   handler: async (request: Hapi.Request, h: Hapi.ResponseToolkit): Promise<Hapi.ResponseObject> => {
-    const lastTokenRefresh = getSessionValue("last_token_refresh", request)
-    let nextRefreshTime = getSessionValue("next_refresh_time", request)
-
-    if (lastTokenRefresh <= nextRefreshTime) {
-      return h.response({nextRefreshTime}).code(200)
-    }
-
     const oauthData = getSessionValue("oauth_data", request)
     const token = await refreshToken(oauthData)
     if (token.expired()) {
       return h.response({redirectUri: `${CONFIG.baseUrl}logout`}).code(400)
     }
-
     const tokenRefreshTime = getUtcEpochSeconds()
     const timeTillRefresh = 599
-    nextRefreshTime = tokenRefreshTime + timeTillRefresh - 10
+    const nextRefreshTime = tokenRefreshTime + timeTillRefresh - 10
     setSessionValue("oauth_data", token.data, request)
     setSessionValue("access_token", token.accessToken, request)
     setSessionValue("last_token_refresh", tokenRefreshTime, request)
     setSessionValue("next_refresh_time", nextRefreshTime, request)
-
     return h.response({nextRefreshTime}).code(200)
   }
 }
