2b1aad8042e1213d69c6f7cb4d8ae6bc7fd5fe68
Alex McCormick <amccmick1@googlemail.com>
2022-03-01 18:07:16 +0000

    Remove whitespace

--- coordinator/src/routes/prescribe/prepare.ts
+++ coordinator/src/routes/prescribe/prepare.ts
@@ -34,10 +34,8 @@ export default [
         request.logger.info("Encoding HL7V3 signature fragments")
 
         const response = translator.createProvenanceForFhirBundle(bundle, request.logger)
-
         request.log("audit", {"incomingMessageHash": createHash(JSON.stringify(bundle))})
         request.log("audit", {"PrepareEndpointResponse": response})
-
         return responseToolkit.response(response).code(200).type(ContentTypes.FHIR)
       }
     )
