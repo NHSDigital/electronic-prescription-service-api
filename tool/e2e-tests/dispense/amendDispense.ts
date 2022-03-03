c36ad94e8a09f84451a9fc5a6b9e0e55bb5edac7
callumtipper-IW <callum.tipper@infinityworks.com>
2022-03-03 17:08:46 +0000

    AEA-2095: Dispense amend selenium 2

---
+++ tool/e2e-tests/dispense/amendDispense.ts
@@ -0,0 +1,19 @@
+import {driver} from "../all.test"
+import {
+  sendPrescriptionUserJourney,
+  releasePrescriptionUserJourney,
+  dispensePrescriptionUserJourney,
+  amendDispenseUserJourney
+} from "../helpers"
+import {searchForPrescriptionUserJourney} from "../tracker/searchPrescription"
+
+describe("firefox", () => {
+  test("can amend a dispense", async () => {
+    const prescriptionId = await sendPrescriptionUserJourney(driver)
+    expect(prescriptionId).toBeTruthy()
+    await releasePrescriptionUserJourney(driver)
+    await dispensePrescriptionUserJourney(driver)
+    await searchForPrescriptionUserJourney(driver, prescriptionId)
+    await amendDispenseUserJourney(driver)
+  })
+})
--- tool/e2e-tests/helpers.ts
+++ tool/e2e-tests/helpers.ts
@@ -28,7 +28,11 @@ import {
   backButton,
   configButton,
   configLink,
-  configPageTitle
+  configPageTitle,
+  DispenseExpanderAction,
+  AmendDispenseAction,
+  itemAmendNotDispensedStatus,
+  amendDispensePageTitle
 } from "./locators"
 
 export const LOCAL_MODE = Boolean(process.env.LOCAL_MODE)
@@ -93,6 +97,23 @@ export async function dispensePrescriptionUserJourney(
   await driver.findElement(dispenseButton).click()
 
   finaliseWebAction(driver, "DISPENSING PRESCRIPTION...")
+
+  await checkApiResult(driver)
+}
+
+export async function amendDispenseUserJourney(
+  driver: ThenableWebDriver
+): Promise<void> {
+  await driver.findElement(DispenseExpanderAction).click()
+  await driver.findElement(AmendDispenseAction).click()
+
+  await driver.wait(until.elementsLocated(amendDispensePageTitle), fiveTimesDefaultWaitTimeout)
+
+  await (await driver.findElements(itemAmendNotDispensedStatus)).forEach(element => element.click())
+
+  await driver.findElement(dispenseButton).click()
+
+  finaliseWebAction(driver, "AMENDING DISPENSE...")
 
   await checkApiResult(driver)
 }
