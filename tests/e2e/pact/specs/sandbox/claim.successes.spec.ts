bcbf94256c0d4cee22a6f90bc1c2e564d3850117
Dan Jones <dan.jones@infinityworks.com>
2022-03-08 11:23:08 +0000

    add sandbox amend pact

--- tests/e2e/pact/specs/sandbox/claim.successes.spec.ts
+++ tests/e2e/pact/specs/sandbox/claim.successes.spec.ts
@@ -60,3 +60,57 @@ jestpact.pactWith(
       )
     })
   })
+
+jestpact.pactWith(
+  pactOptions("sandbox", "claim", "amend"),
+  /* eslint-disable  @typescript-eslint/no-explicit-any */
+  async (provider: any) => {
+    const client = () => {
+      const url = `${provider.mockService.baseUrl}`
+      return supertest(url)
+    }
+
+    describe("process-message claim sandbox e2e tests", () => {
+      test.each(TestResources.claimAmendCases)(
+        "should be able to claim %s",
+        async (desc: string, message: fhir.Claim) => {
+          const apiPath = `${basePath}/Claim`
+          const claimStr = LosslessJson.stringify(message)
+          const claim = JSON.parse(claimStr) as fhir.Claim
+
+          const requestId = uuid.v4()
+          const correlationId = uuid.v4()
+
+          const interaction: InteractionObject = {
+            state: "is authenticated",
+            uponReceiving: `a request to process ${desc} message to Spine`,
+            withRequest: {
+              headers: {
+                "Content-Type": "application/fhir+json; fhirVersion=4.0",
+                "X-Request-ID": requestId,
+                "X-Correlation-ID": correlationId
+              },
+              method: "POST",
+              path: apiPath,
+              body: claim
+            },
+            willRespondWith: {
+              headers: {
+                "Content-Type": "application/json"
+              },
+              //TODO - Verify response body for claims
+              status: 200
+            }
+          }
+          await provider.addInteraction(interaction)
+          await client()
+            .post(apiPath)
+            .set("Content-Type", "application/fhir+json; fhirVersion=4.0")
+            .set("X-Request-ID", requestId)
+            .set("X-Correlation-ID", correlationId)
+            .send(claimStr)
+            .expect(200)
+        }
+      )
+    })
+  })
