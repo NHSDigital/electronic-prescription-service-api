2d97373b9df651671634463451840f08291b4a26
Jack Robinson <jack.robinson@infinityworks.com>
2022-03-16 14:25:00 +0000

    split up telecom logic

--- coordinator/src/services/translation/request/agent-unattended.ts
+++ coordinator/src/services/translation/request/agent-unattended.ts
@@ -111,9 +111,11 @@ export async function createAgentPerson(
   agentPerson.id = new hl7V3.SdsRoleProfileIdentifier(sdsRoleProfileId)
   agentPerson.code = new hl7V3.SdsJobRoleCode(sdsJobRoleCode)
 
-  // TODO: remove this check when user provides the telecom value and error if not present
-  if (fhirTelecom || representedOrganisation.telecom?._attributes.value) {
-    agentPerson.telecom = [convertTelecom(fhirTelecom ?? organization.telecom[0], "Organisation.telecom")]
+  // TODO: remove this check when user provides the telecom value in the message and error if not present
+  if (fhirTelecom) {
+    agentPerson.telecom = [convertTelecom(fhirTelecom, "")]
+  } else if (representedOrganisation.telecom?._attributes.value) {
+    agentPerson.telecom = [convertTelecom(organization.telecom[0], "Organisation.telecom")]
   } else {
     agentPerson.telecom = [new hl7V3.Telecom()]
   }
