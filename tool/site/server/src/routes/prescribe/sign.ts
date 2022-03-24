3ff76381a6af769f26292f6e2bbfe199acdca640
Alex McCormick <amccmick1@googlemail.com>
2022-03-24 16:57:23 +0000

    Only bulk sign successful prepare responses

--- tool/site/server/src/routes/prescribe/sign.ts
+++ tool/site/server/src/routes/prescribe/sign.ts
@@ -2,6 +2,7 @@ import Hapi from "@hapi/hapi"
 import {getSigningClient} from "../../services/communication/signing-client"
 import {getEpsClient} from "../../services/communication/eps-client"
 import {getSessionValue, setSessionValue} from "../../services/session"
+import * as fhir from "fhir/r4"
 
 export default [
   {
@@ -12,14 +13,23 @@ export default [
       const epsClient = getEpsClient(accessToken, request)
       const signingClient = getSigningClient(request, accessToken)
       const prescriptionIds = getSessionValue("prescription_ids", request)
+      const successfulPreparePrescriptionIds = []
       for (const id of prescriptionIds) {
         const prepareRequest = getSessionValue(`prepare_request_${id}`, request)
         const prepareResponse = await epsClient.makePrepareRequest(prepareRequest)
         setSessionValue(`prepare_response_${id}`, prepareResponse, request)
+        if (!prepareResponseIsError(prepareResponse)) {
+          successfulPreparePrescriptionIds.push(id)
+        }
       }
-      const prepareResponses = prescriptionIds.map((id: string) => getSessionValue(`prepare_response_${id}`, request))
+      const prepareResponses = successfulPreparePrescriptionIds.map((id: string) => getSessionValue(`prepare_response_${id}`, request))
       const response = await signingClient.uploadSignatureRequest(prepareResponses)
       return responseToolkit.response(response).code(200)
     }
   }
 ]
+
+function prepareResponseIsError(prepareResponse: fhir.Parameters | fhir.OperationOutcome)
+: prepareResponse is fhir.OperationOutcome {
+  return !!(prepareResponse as fhir.OperationOutcome).issue?.length
+}
