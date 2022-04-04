e5c94977ea96e6d989dd45039b2c454c8340d435
Dan Jones <dan.jones@infinityworks.com>
2022-04-04 14:50:26 +0100

    remove claim amend pacts

--- tests/e2e/pact/broker/verify.ts
+++ tests/e2e/pact/broker/verify.ts
@@ -193,7 +193,7 @@ function isGroupIdentifier(parameter: fhir.Parameter): boolean {
     .then(verifyDispenseAmend)
     .then(verifyWithdraw)
     .then(verifyClaim)
-    .then(verifyClaimAmend)
+    // .then(verifyClaimAmend)
     .then(verifyMetadata)
     .then(verifyTracker)
 })()
