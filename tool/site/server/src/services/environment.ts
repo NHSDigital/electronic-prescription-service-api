71a0fd87ef16605fbc429ab0f6434bcde1ba209b
Alex McCormick <amccmick1@googlemail.com>
2022-04-06 15:46:17 +0100

    AEA-1571 Add sandbox EPSAT environment with singular dose to text placeholder page (#805)

--- tool/site/server/src/services/environment.ts
+++ tool/site/server/src/services/environment.ts
@@ -1,9 +1,10 @@
-export type Environment = "internal-dev" | "internal-dev-sandbox" | "internal-qa" | "int"
+export type Environment = "internal-dev" | "internal-dev-sandbox" | "internal-qa" | "int" | "sandbox"
 
 export const internalDev = "internal-dev"
 export const internalDevSandbox = "internal-dev-sandbox"
 export const int = "int"
-export const qa = "internal-qa" 
+export const sandbox = "sandbox"
+export const qa = "internal-qa"
 
 export function isLocal(environment: Environment): boolean {
   return environment === internalDevSandbox
@@ -15,6 +16,10 @@ export function isDev(environment: Environment): boolean {
 
 export function isInt(environment: Environment): boolean {
   return environment === int
+}
+
+export function isSandbox(environment: Environment): boolean {
+  return environment === sandbox
 }
 
 export function isQa(environment: Environment): boolean {
