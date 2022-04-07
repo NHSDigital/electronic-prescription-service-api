b5d467060797d7f4d124424deb6e2c24867a138e
Alex McCormick <amccmick1@googlemail.com>
2022-04-07 09:46:03 +0100

    Start to remove old test-packs

--- tool/e2e-tests/prescribe/sendPrescriptionsFromTestPack.ts
+++ tool/e2e-tests/prescribe/sendPrescriptionsFromTestPack.ts
@@ -1,13 +1,9 @@
 import {driver} from "../all.test"
 import {sendBulkPrescriptionUserJourney} from "../helpers"
-import {loadTestPack1Examples, loadTestPack2Examples} from "../test-packs/test-packs"
+import {loadTestPack1Examples} from "../test-packs/test-packs"
 
 describe("firefox", () => {
   test("can send prescriptions from test pack 1", async () => {
     await sendBulkPrescriptionUserJourney(driver, loadTestPack1Examples, 5)
-  })
-
-  test("can send prescriptions from test pack 2", async () => {
-    await sendBulkPrescriptionUserJourney(driver, loadTestPack2Examples, 30)
   })
 })
