8941fefeec8d880b80c2fe68dfdccda89a446d5b
callumtipper-IW <callum.tipper@infinityworks.com>
2022-04-26 15:43:16 +0100

    AEA-1847: Changing doseAndRate to handle an array of elements

--- tool/site/client/src/fhir/typeGuards.ts
+++ tool/site/client/src/fhir/typeGuards.ts
@@ -1,4 +1,13 @@
 import * as fhir from "fhir/r4"
+import {
+  Dose,
+  DoseRange,
+  DoseSimpleQuantity,
+  Rate,
+  RateRange,
+  RateRatio,
+  RateSimpleQuantity
+} from "../../../../../models/fhir"
 
 export function isOperationOutcome(resource: fhir.FhirResource): resource is fhir.OperationOutcome {
   return resource.resourceType === "OperationOutcome"
@@ -14,6 +23,26 @@ export function isClaim(resource: fhir.FhirResource): resource is fhir.Claim {
 
 export function isTask(resource: fhir.FhirResource): resource is fhir.Task {
   return resource.resourceType === "Task"
+}
+
+export function isDoseSimpleQuantity(element: Dose): element is DoseSimpleQuantity {
+  return "doseQuantity" in element
+}
+
+export function isDoseRange(element: Dose): element is DoseRange {
+  return "doseRange" in element
+}
+
+export function isRateSimpleQuantity(element: Rate): element is RateSimpleQuantity {
+  return "rateQuantity" in element
+}
+
+export function isRateRange(element: Rate): element is RateRange {
+  return "rateRange" in element
+}
+
+export function isRateRatio(element: Rate): element is RateRatio {
+  return "rateRatio" in element
 }
 
 export function getArrayTypeGuard<T>(
