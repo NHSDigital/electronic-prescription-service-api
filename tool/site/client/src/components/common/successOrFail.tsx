0a7e2c6223ac43a823e0aa0152db42005707c680
mukhtar-abdul-ghani <mukhtar.abdul.ghani@infinityworks.com>
2022-04-28 16:34:17 +0100

    wrote units tests for successOrFail component

--- tool/site/client/src/components/common/successOrFail.tsx
+++ tool/site/client/src/components/common/successOrFail.tsx
@@ -1,7 +1,7 @@
 import React from "react"
 import {CrossIcon, TickIcon} from "nhsuk-react-components"
 
-const SuccessOrFail : React.FC<{condition:boolean}> = ({
+const SuccessOrFail: React.FC<{condition: boolean}> = ({
   condition
 }) => {
   return condition ? <TickIcon /> : <CrossIcon />
