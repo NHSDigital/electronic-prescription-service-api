a118623c5d9d051b85eb920ad7f6f4b8b1f4d358
TomSmithCoding <tom_smith1337@hotmail.co.uk>
2022-04-28 10:58:55 +0100

    INT changes again

--- tool/site/server/src/oauthUtils.ts
+++ tool/site/server/src/oauthUtils.ts
@@ -31,14 +31,14 @@ function createOAuthClient(): ClientOAuth2 {
 
 function createCIS2OAuthClient(): ClientOAuth2 {
   return new ClientOAuth2({
-    clientId: CONFIG.clientId,
-    clientSecret: CONFIG.clientSecret,
+    clientId: "128936811467.apps.national",
+    clientSecret: "1f88b870-9980-482a-95f4-395aaa01d699",
     redirectUri: getRegisteredCallbackUrl("callback"),
     accessTokenUri: `${CONFIG.privateApigeeUrl}/oauth2/token`,
     authorizationUri: `https://am.nhsdev.auth-ptl.cis2.spineservices.nhs.uk:443/openam/oauth2/realms/root/realms/oidc/authorize`,
     body: {
-      client_id: CONFIG.clientId,
-      client_secret: CONFIG.clientSecret
+      client_id: "128936811467.apps.national",
+      client_secret: "1f88b870-9980-482a-95f4-395aaa01d699"
     }
   })
 }
