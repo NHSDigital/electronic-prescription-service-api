d9aa906e467d7ef1ce309799c24f9c4a29e711ca
TomSmithCoding <tom_smith1337@hotmail.co.uk>
2022-04-29 10:42:18 +0100

    Linting issues + jest config fix

--- tool/site/client/jest.config.js
+++ tool/site/client/jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
   transform: {
     "^.+\\.tsx?$": "ts-jest"
   },
-  testRegex: "(/__tests__/.*|(\\.|/)(test|specx))\\.tsx?$",
+  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
   testEnvironment: "jsdom"
 }
