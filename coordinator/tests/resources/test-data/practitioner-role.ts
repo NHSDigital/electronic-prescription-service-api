1c0b710fc7b9db79d28109f5ade69499dc76bf9b
Dan Jones <dan.jones@infinityworks.com>
2022-06-17 17:25:46 +0100

    fix tests

--- coordinator/tests/resources/test-data/practitioner-role.ts
+++ coordinator/tests/resources/test-data/practitioner-role.ts
@@ -12,8 +12,8 @@ export const practitionerRole: fhir.PractitionerRole = {
   ],
   practitioner: {
     identifier: {
-      system: "https://fhir.nhs.uk/Id/sds-user-id",
-      value: "3415870201"
+      system: "https://fhir.hl7.org.uk/Id/gphc-number",
+      value: "7654321"
     },
     display: "Jackie Clark"
   },
