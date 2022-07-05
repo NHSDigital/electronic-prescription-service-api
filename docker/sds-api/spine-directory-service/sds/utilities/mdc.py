9e7745ac8719a5c408469fab54ead8d7d879bc3e
mukhtar-abdul-ghani <mukhtar.abdul.ghani@infinityworks.com>
2022-07-05 15:19:06 +0100

    removed sds container by deleting the docker folder

--- docker/sds-api/spine-directory-service/sds/utilities/mdc.py
+++
@@ -1,4 +0,0 @@
-import contextvars
-
-correlation_id: contextvars.ContextVar[str] = contextvars.ContextVar('correlation_id', default='')
-trace_id: contextvars.ContextVar[str] = contextvars.ContextVar('trace_id', default='')
