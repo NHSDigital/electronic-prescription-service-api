64f6e950345e72acaee3672cce23acf1a0716b63
Alex McCormick <amccmick1@googlemail.com>
2022-07-07 18:27:23 +0100

    Ensure both prescription type test-packs run in tests

--- tool/e2e-tests/prescribe/sendPrescriptionsFromTestPack.ts
+++ tool/e2e-tests/prescribe/sendPrescriptionsFromTestPack.ts
@@ -1,21 +1,21 @@
 import {driver} from "../live.test"
 import {sendBulkPrescriptionUserJourney} from "../helpers"
-import {loadClinicalTestPack1Examples, loadPrescriptionTypeTestPack1Examples, loadPrescriptionTypeTestPack2Examples, loadSupplierTestPack1Examples} from "../test-packs/test-packs"
+import * as testPacks from "../test-packs/test-packs"
 
 describe("firefox", () => {
   test("can send prescriptions from clinical test pack 1", async () => {
-    await sendBulkPrescriptionUserJourney(driver, loadClinicalTestPack1Examples, 25)
+    await sendBulkPrescriptionUserJourney(driver, testPacks.loadClinicalTestPack1Examples, 25)
   })
 
   test("can send prescriptions from supplier test pack 1", async () => {
-    await sendBulkPrescriptionUserJourney(driver, loadSupplierTestPack1Examples, 25)
+    await sendBulkPrescriptionUserJourney(driver, testPacks.loadSupplierTestPack1Examples, 25)
   })
 
   test("can send prescriptions from prescription types test pack 1", async () => {
-    await sendBulkPrescriptionUserJourney(driver, loadPrescriptionTypeTestPack1Examples, 25)
+    await sendBulkPrescriptionUserJourney(driver, testPacks.loadPrescriptionTypeTestPack1Examples, 25)
   })
 
-  test("can send prescriptions from prescription types test pack 1", async () => {
-    await sendBulkPrescriptionUserJourney(driver, loadPrescriptionTypeTestPack2Examples, 25)
+  test("can send prescriptions from prescription types test pack 2", async () => {
+    await sendBulkPrescriptionUserJourney(driver, testPacks.loadPrescriptionTypeTestPack2Examples, 25)
   })
 })
