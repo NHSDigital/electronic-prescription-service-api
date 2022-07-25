a8db0d04c270215a888656daefd661385a13a0e3
Stefano Frazzetto <stefano.frazzetto@infinityworks.com>
2022-07-25 13:20:41 +0100

    Remove document_key from interface

--- models/spine/tracker.ts
+++ models/spine/tracker.ts
@@ -6,7 +6,6 @@ export interface GenericTrackerRequest {
 export interface TrackerRequest extends GenericTrackerRequest {
   prescription_id: string
   repeat_number: string
-  document_key: string
 }
 
 export interface GetPrescriptionDocumentRequest extends GenericTrackerRequest {
