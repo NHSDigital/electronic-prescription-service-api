4d4a7ba5614b19f4ea04aca2439c67adf40e0711
Alex McCormick <amccmick1@googlemail.com>
2022-07-26 21:00:16 +0100

    Return FHIR prescription from tracker

--- coordinator/src/routes/tracker/tracker.ts
+++ coordinator/src/routes/tracker/tracker.ts
@@ -1,10 +1,10 @@
 import Hapi from "@hapi/hapi"
-import {spine} from "@models"
+import {hl7V3, fhir, spine} from "@models"
 import {extractHl7v3PrescriptionFromMessage} from "../../services/communication/tracker/tracker-response-parser"
 import {spineClient} from "../../services/communication/spine-client"
-import {writeXmlStringPretty} from "../../services/serialisation/xml"
 import {BASE_PATH, ContentTypes, getPayload} from "../util"
 import {getRequestId} from "../../utils/headers"
+import {createInnerBundle} from "../../services/translation/response/release/release-response"
 
 export default [{
   method: "POST",
@@ -15,14 +15,34 @@ export default [{
     const trackerRequest = getPayload(request) as spine.GetPrescriptionMetadataRequest
     trackerRequest.message_id = getRequestId(request.headers)
     request.logger.info(`Tracker - Received request:\n${JSON.stringify(trackerRequest)}`)
-    const response = await spineClient.track(trackerRequest, request.logger)
-    request.logger.info(`Tracker - Received response:\n${response.body}`)
-    const hl7v3Prescription = extractHl7v3PrescriptionFromMessage(response.body, request.logger)
-    const xmlPrescription = hl7v3Prescription ? writeXmlStringPretty(hl7v3Prescription) : ""
+    const trackerResponse = await spineClient.track(trackerRequest, request.logger)
+    request.logger.info(`Tracker - Received response:\n${trackerResponse.body}`)
+    const hl7v3Prescription = extractHl7v3PrescriptionFromMessage(trackerResponse.body, request.logger)
+    
+    const response = hl7v3Prescription
+      ? createFhirPrescriptionResponse(hl7v3Prescription)
+      : createErrorResponse()
 
     return responseToolkit
-      .response(xmlPrescription)
-      .code(response.statusCode)
-      .type(ContentTypes.XML)
+      .response(response)
+      .code(trackerResponse.statusCode)
+      .type(ContentTypes.FHIR)
   }
 }]
+
+function createFhirPrescriptionResponse(hl7v3Prescription: hl7V3.ParentPrescription) {
+  return createInnerBundle(hl7v3Prescription, "")
+}
+
+function createErrorResponse() {
+  return fhir.createOperationOutcome([
+    fhir.createOperationOutcomeIssue(
+      fhir.IssueCodes.NOT_FOUND,
+      "error",
+      fhir.createCodeableConcept(
+        "https://fhir.nhs.uk/CodeSystem/Spine-ErrorOrWarningCode",
+        "RESOURCE_NOT_FOUND",
+        "Resource not found"
+      ))
+  ])
+}
