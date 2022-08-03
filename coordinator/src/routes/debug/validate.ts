6fbdf5c6eba8f113662256144471d81b01af6a72
Alex McCormick <amccmick1@googlemail.com>
2022-08-03 18:29:51 +0100

    Overriding types in hapi.request for logging purposes test

--- coordinator/src/routes/debug/validate.ts
+++ coordinator/src/routes/debug/validate.ts
@@ -1,5 +1,13 @@
 import {fhir} from "@models"
-import {BASE_PATH, ContentTypes, externalValidator} from "../util"
+import {BASE_PATH, ContentTypes} from "../util"
+import Hapi from "@hapi/hapi"
+import {BaseLogger} from "pino"
+
+interface EpsRequest extends Modify<Hapi.Request, {
+  logger: BaseLogger
+}>{}
+
+type Modify<T, R> = Omit<T, keyof R> & R;
 
 export default [
   /*
@@ -8,7 +16,9 @@ export default [
   {
     method: "POST",
     path: `${BASE_PATH}/$validate`,
-    handler: externalValidator(async (request, responseToolkit) => {
+    handler: async (request: EpsRequest, responseToolkit: Hapi.ResponseToolkit): Promise<Hapi.ResponseObject> => {
+      request.logger.info("Validate test logger in route!!!")
+      logInFunction(request.logger)
       const successfulResponse: fhir.OperationOutcome = {
         resourceType: "OperationOutcome",
         issue: [{
@@ -17,6 +27,10 @@ export default [
         }]
       }
       return responseToolkit.response(successfulResponse).code(200).type(ContentTypes.FHIR)
-    })
+    }
   }
 ]
+
+function logInFunction(logger: BaseLogger) {
+  logger.info("Validate test logger in route!!!")
+}
