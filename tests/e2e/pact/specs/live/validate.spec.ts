0ab02d3061bdb0431d1c2d4821998b84ace5d8ec
Alex McCormick <amccmick1@googlemail.com>
2022-08-04 03:19:24 +0100

    Add validate live/sandbox smoke-tests

---
+++ tests/e2e/pact/specs/live/validate.spec.ts
@@ -0,0 +1,24 @@
+import {
+  createInteraction,
+  CreatePactOptions,
+  pactOptions,
+  successfulOperationOutcome
+} from "../../resources/common"
+import {Pact} from "@pact-foundation/pact"
+import {fetcher} from "@models"
+
+test("validate e2e tests", async () => {
+  const testCase = fetcher.convertExamples[0]
+
+  const options = new CreatePactOptions("live", "validate")
+  const provider = new Pact(pactOptions(options))
+  await provider.setup()
+  
+  const interaction = createInteraction(
+    options,
+    testCase.request,
+    successfulOperationOutcome)
+    
+  await provider.addInteraction(interaction)
+  await provider.writePact()
+})
