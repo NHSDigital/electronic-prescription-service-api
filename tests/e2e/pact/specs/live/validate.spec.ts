a545e695606ea823deb06ce3dd7d69ff195261e3
Alex McCormick <amccmick1@googlemail.com>
2022-08-04 03:19:54 +0100

    Lint

--- tests/e2e/pact/specs/live/validate.spec.ts
+++ tests/e2e/pact/specs/live/validate.spec.ts
@@ -13,12 +13,12 @@ test("validate e2e tests", async () => {
   const options = new CreatePactOptions("live", "validate")
   const provider = new Pact(pactOptions(options))
   await provider.setup()
-  
+
   const interaction = createInteraction(
     options,
     testCase.request,
     successfulOperationOutcome)
-    
+
   await provider.addInteraction(interaction)
   await provider.writePact()
 })
--- tests/e2e/pact/specs/sandbox/validate.disabled-spec.ts
+++ tests/e2e/pact/specs/sandbox/validate.disabled-spec.ts
@@ -1,11 +1,16 @@
-import {createInteraction, CreatePactOptions, pactOptions, successfulOperationOutcome} from "../../resources/common"
+import {
+  createInteraction,
+  CreatePactOptions,
+  pactOptions,
+  successfulOperationOutcome
+} from "../../resources/common"
 import {fetcher} from "@models"
 import {Pact} from "@pact-foundation/pact"
 
 describe("validate e2e tests", () => {
   test("validate endpoint should return 200 on success", async () => {
     const testCase = fetcher.convertExamples[0]
-    
+
     const options = new CreatePactOptions("sandbox", "validate")
     const provider = new Pact(pactOptions(options))
     await provider.setup()
