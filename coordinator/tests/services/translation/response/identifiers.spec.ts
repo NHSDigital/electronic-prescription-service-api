1940621c55973b41c246d5087935cfc2b15423c9
Dan Jones <dan.jones@infinityworks.com>
2022-08-12 16:37:27 +0100

    test fixes

--- coordinator/tests/services/translation/response/identifiers.spec.ts
+++ coordinator/tests/services/translation/response/identifiers.spec.ts
@@ -1,19 +1,34 @@
 import {createPractitionerOrRoleIdentifier} from "../../../../src/services/translation/response/identifiers"
+import {hl7V3} from "@models"
 
 describe("createPractitionerOrRoleIdentifier", () => {
+  test("1.54 gets mapped to professional code", () => {
+    const testValue = "612345"
+    const testAuthorId: hl7V3.PrescriptionAuthorId = {
+      _attributes: {
+        root: "1.2.826.0.1285.0.2.1.54",
+        extension: testValue
+      }
+    }
+
+    const result = createPractitionerOrRoleIdentifier(testAuthorId)
+    expect(result.system).toEqual("https://fhir.hl7.org.uk/Id/professional-code")
+    expect(result.value).toEqual(testValue)
+  })
+
   const cases = [
-    ["612345", "https://fhir.hl7.org.uk/Id/nhsbsa-spurious-code"],
-    ["12A3456B", "https://fhir.hl7.org.uk/Id/nmc-number"],
-    ["G1234567", "https://fhir.hl7.org.uk/Id/gmp-number"],
-    ["C1234567", "https://fhir.hl7.org.uk/Id/gmc-number"],
-    ["1234567", "https://fhir.hl7.org.uk/Id/gphc-number"],
-    ["AB123456", "https://fhir.hl7.org.uk/Id/hcpc-number"],
-    ["123456", "https://fhir.hl7.org.uk/Id/din-number"],
-    ["12345678", "https://fhir.hl7.org.uk/Id/professional-code"]
+    ["https://fhir.hl7.org.uk/Id/nhsbsa-spurious-code", new hl7V3.SdsUniqueIdentifier("612345")],
+    ["https://fhir.hl7.org.uk/Id/nmc-number", new hl7V3.SdsUniqueIdentifier("12A3456B")],
+    ["https://fhir.hl7.org.uk/Id/gmp-number", new hl7V3.SdsUniqueIdentifier("G1234567")],
+    ["https://fhir.hl7.org.uk/Id/gmc-number", new hl7V3.SdsUniqueIdentifier("C1234567")],
+    ["https://fhir.hl7.org.uk/Id/gphc-number", new hl7V3.SdsUniqueIdentifier("1234567")],
+    ["https://fhir.hl7.org.uk/Id/hcpc-number", new hl7V3.SdsUniqueIdentifier("AB123456")],
+    ["https://fhir.hl7.org.uk/Id/din-number", new hl7V3.SdsUniqueIdentifier("123456")],
+    ["https://fhir.hl7.org.uk/Id/professional-code", new hl7V3.SdsUniqueIdentifier("12345678")]
   ]
-  test.each(cases)("identifies %s as %s", (input: string, system: string) => {
+  test.each(cases)("identifies SdsUserId correctly as %s", (system: string, input: hl7V3.SdsUniqueIdentifier) => {
     const result = createPractitionerOrRoleIdentifier(input)
     expect(result.system).toEqual(system)
-    expect(result.value).toEqual(input)
+    expect(result.value).toEqual(input._attributes.extension)
   })
 })
