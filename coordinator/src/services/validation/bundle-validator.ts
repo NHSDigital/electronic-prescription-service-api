d1ea6f6cfa7469714764772afa0f1e2569d41313
TomSmithCoding <tom_smith1337@hotmail.co.uk>
2022-09-13 16:40:09 +0100

    Updated unknown to professional code

--- coordinator/src/services/validation/bundle-validator.ts
+++ coordinator/src/services/validation/bundle-validator.ts
@@ -231,7 +231,7 @@ export function verifyPrescriptionBundle(bundle: fhir.Bundle): Array<fhir.Operat
       errors.createInvalidIdentifierIssue(
         "Practitioner",
         "GMP",
-        "GMC|NMC|GPhC|HCPC|unknown"
+        "GMC|NMC|GPhC|HCPC|professional-code"
       )
     )
   }
