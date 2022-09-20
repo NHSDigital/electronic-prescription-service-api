91c298b09e9ef50da28e4d289c8c100027988eea
andy mather <anyd-mather@live.co.uk>
2022-09-20 17:35:37 +0100

    send  with non ascii

--- tool/e2e-tests/prescribe/sendPrescription.ts
+++ tool/e2e-tests/prescribe/sendPrescription.ts
@@ -1,10 +1,26 @@
-import {driver} from "../live.test"
-import {checkMyPrescriptions, sendPrescriptionUserJourney} from "../helpers"
+import { driver } from "../live.test"
+import { checkMyPrescriptions, sendPrescriptionUserJourney } from "../helpers"
+import { loadNonASCIIDosageInstructionsFHIRMessage, loadNonASCIINoteToDispenseFHIRMessage, loadNonASCIIPatientAdditionalInstructionsFHIRMessage } from "../test-packs/test-packs"
 
 describe("firefox", () => {
   test("can send prescription", async () => {
     const prescriptionId = await sendPrescriptionUserJourney(driver)
     expect(prescriptionId).toBeTruthy()
     await checkMyPrescriptions(driver, "Sent Prescriptions", prescriptionId)
+  })
+})
+
+describe('Can send perscriptions from FHIR message upload', () => {
+
+  test("from Non-ASCII Dosage Instructions.json", async () => {
+    await sendPrescriptionUserJourney(driver, loadNonASCIIDosageInstructionsFHIRMessage)
+  })
+
+  test("from Non-ASCII Note to dispenser.json", async () => {
+    await sendPrescriptionUserJourney(driver, loadNonASCIINoteToDispenseFHIRMessage)
+  })
+
+  test("from Non-ASCII Patient additional Instructions.json", async () => {
+    await sendPrescriptionUserJourney(driver, loadNonASCIIPatientAdditionalInstructionsFHIRMessage)
   })
 })
