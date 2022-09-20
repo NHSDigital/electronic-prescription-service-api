7d48b660ae705f4c15a3c4337a176d7b3e690eba
andy mather <anyd-mather@live.co.uk>
2022-09-20 17:05:55 +0100

    can claim with non ASCII characters

--- tool/e2e-tests/dispense/claimPrescription.ts
+++ tool/e2e-tests/dispense/claimPrescription.ts
@@ -1,4 +1,4 @@
-import {driver} from "../live.test"
+import { driver } from "../live.test"
 import {
   sendPrescriptionUserJourney,
   releasePrescriptionUserJourney,
@@ -6,10 +6,43 @@ import {
   checkMyPrescriptions,
   claimPrescriptionUserJourney
 } from "../helpers"
+import {
+  loadNonASCIIDosageInstructionsFHIRMessage,
+  loadNonASCIINoteToDispenseFHIRMessage,
+  loadNonASCIIPatientAdditionalInstructionsFHIRMessage
+} from "../test-packs/test-packs"
 
 describe("firefox", () => {
   test("can claim prescription", async () => {
     const prescriptionId = await sendPrescriptionUserJourney(driver)
+    expect(prescriptionId).toBeTruthy()
+    await releasePrescriptionUserJourney(driver)
+    await dispensePrescriptionUserJourney(driver)
+    await claimPrescriptionUserJourney(driver)
+    await checkMyPrescriptions(driver, "Claimed Prescriptions", prescriptionId)
+  })
+
+  test("can claim prescription which has none ASCII chars in dosage Instructions ", async () => {
+    const prescriptionId = await sendPrescriptionUserJourney(driver, loadNonASCIIDosageInstructionsFHIRMessage)
+    expect(prescriptionId).toBeTruthy()
+    await releasePrescriptionUserJourney(driver)
+    await dispensePrescriptionUserJourney(driver)
+    await claimPrescriptionUserJourney(driver)
+    await checkMyPrescriptions(driver, "Claimed Prescriptions", prescriptionId)
+  })
+
+  test("can claim prescription which has none ASCII chars in note to dispense ", async () => {
+    const prescriptionId = await sendPrescriptionUserJourney(driver, loadNonASCIINoteToDispenseFHIRMessage)
+    expect(prescriptionId).toBeTruthy()
+    await releasePrescriptionUserJourney(driver)
+    await dispensePrescriptionUserJourney(driver)
+    await claimPrescriptionUserJourney(driver)
+    await checkMyPrescriptions(driver, "Claimed Prescriptions", prescriptionId)
+  })
+
+
+  test("can claim prescription which has none ASCII chars in additional instructions", async () => {
+    const prescriptionId = await sendPrescriptionUserJourney(driver, loadNonASCIIPatientAdditionalInstructionsFHIRMessage)
     expect(prescriptionId).toBeTruthy()
     await releasePrescriptionUserJourney(driver)
     await dispensePrescriptionUserJourney(driver)
