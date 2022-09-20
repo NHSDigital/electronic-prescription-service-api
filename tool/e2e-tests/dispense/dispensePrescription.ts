e057e0e7ae3e2d3f52544405b179e1aa9bded19e
andy mather <anyd-mather@live.co.uk>
2022-09-20 17:33:51 +0100

    release with non ascii

--- tool/e2e-tests/dispense/dispensePrescription.ts
+++ tool/e2e-tests/dispense/dispensePrescription.ts
@@ -1,4 +1,4 @@
-import {driver} from "../live.test"
+import { driver } from "../live.test"
 import {
   sendPrescriptionUserJourney,
   releasePrescriptionUserJourney,
@@ -6,9 +6,43 @@ import {
   checkMyPrescriptions
 } from "../helpers"
 
+import {
+  loadNonASCIIDosageInstructionsFHIRMessage,
+  loadNonASCIINoteToDispenseFHIRMessage,
+  loadNonASCIIPatientAdditionalInstructionsFHIRMessage
+} from "../test-packs/test-packs"
+
+
+
 describe("firefox", () => {
   test("can dispense prescription", async () => {
     const prescriptionId = await sendPrescriptionUserJourney(driver)
+    expect(prescriptionId).toBeTruthy()
+    await releasePrescriptionUserJourney(driver)
+    await dispensePrescriptionUserJourney(driver)
+    await checkMyPrescriptions(driver, "Dispensed Prescriptions", prescriptionId)
+  })
+
+
+  test("can dispense prescription which has none ASCII chars in dosage Instructions ", async () => {
+    const prescriptionId = await sendPrescriptionUserJourney(driver, loadNonASCIIDosageInstructionsFHIRMessage)
+    expect(prescriptionId).toBeTruthy()
+    await releasePrescriptionUserJourney(driver)
+    await dispensePrescriptionUserJourney(driver)
+    await checkMyPrescriptions(driver, "Dispensed Prescriptions", prescriptionId)
+  })
+
+  test("can dispense prescription which has none ASCII chars in note to dispense ", async () => {
+    const prescriptionId = await sendPrescriptionUserJourney(driver, loadNonASCIINoteToDispenseFHIRMessage)
+    expect(prescriptionId).toBeTruthy()
+    await releasePrescriptionUserJourney(driver)
+    await dispensePrescriptionUserJourney(driver)
+    await checkMyPrescriptions(driver, "Dispensed Prescriptions", prescriptionId)
+  })
+
+
+  test("can dispense prescription which has none ASCII chars in additional instructions", async () => {
+    const prescriptionId = await sendPrescriptionUserJourney(driver, loadNonASCIIPatientAdditionalInstructionsFHIRMessage)
     expect(prescriptionId).toBeTruthy()
     await releasePrescriptionUserJourney(driver)
     await dispensePrescriptionUserJourney(driver)
