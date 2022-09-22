63ef2e16ec788dfc27a88094b4161a97c26c9241
andymather24 <andy-mather@live.co.uk>
2022-09-22 10:41:46 +0100

    import only required methods

--- tool/e2e-tests/dispense/releasePrescription.ts
+++ tool/e2e-tests/dispense/releasePrescription.ts
@@ -4,7 +4,15 @@ import {
   releasePrescriptionUserJourney,
   checkMyPrescriptions
 } from "../helpers"
-import * as testPacks from "../test-packs/test-packs"
+
+import {
+  loadNonASCIIDosageInstructionsFHIRMessage,
+  loadNonASCIINoteToDispenseFHIRMessage,
+  loadNonASCIIPatientAdditionalInstructionsFHIRMessage,
+  loadXMLTagDosageInstructionsFHIRMessage,
+  loadXMLTagNotesToDispenseFHIRMessage,
+  loadXMLTagPatientAdditionalInstructionsFHIRMessage
+} from "../test-packs/test-packs"
 
 describe("firefox", () => {
   test("can release prescription", async () => {
@@ -16,42 +24,42 @@ describe("firefox", () => {
   })
 
   test("can release prescription which has non-ASCII chars in dosage Instructions", async () => {
-    const prescriptionId = await sendPrescriptionUserJourney(driver, testPacks.loadNonASCIIDosageInstructionsFHIRMessage)
+    const prescriptionId = await sendPrescriptionUserJourney(driver, loadNonASCIIDosageInstructionsFHIRMessage)
     expect(prescriptionId).toBeTruthy()
     await releasePrescriptionUserJourney(driver)
     await checkMyPrescriptions(driver, "Released Prescriptions", prescriptionId)
   })
 
   test("can release prescription which has non-ASCII chars in note to dispense", async () => {
-    const prescriptionId = await sendPrescriptionUserJourney(driver, testPacks.loadNonASCIINoteToDispenseFHIRMessage)
+    const prescriptionId = await sendPrescriptionUserJourney(driver, loadNonASCIINoteToDispenseFHIRMessage)
     expect(prescriptionId).toBeTruthy()
     await releasePrescriptionUserJourney(driver)
     await checkMyPrescriptions(driver, "Released Prescriptions", prescriptionId)
   })
 
   test("can release prescription which has non-ASCII chars in additional instructions", async () => {
-    const prescriptionId = await sendPrescriptionUserJourney(driver, testPacks.loadNonASCIIPatientAdditionalInstructionsFHIRMessage)
+    const prescriptionId = await sendPrescriptionUserJourney(driver, loadNonASCIIPatientAdditionalInstructionsFHIRMessage)
     expect(prescriptionId).toBeTruthy()
     await releasePrescriptionUserJourney(driver)
     await checkMyPrescriptions(driver, "Released Prescriptions", prescriptionId)
   })
 
   test("can release Patient additional Instructions contains XML tag", async () => {
-    const prescriptionId = await sendPrescriptionUserJourney(driver, testPacks.loadXMLTagPatientAdditionalInstructionsFHIRMessage)
+    const prescriptionId = await sendPrescriptionUserJourney(driver, loadXMLTagPatientAdditionalInstructionsFHIRMessage)
     expect(prescriptionId).toBeTruthy()
     await releasePrescriptionUserJourney(driver)
     await checkMyPrescriptions(driver, "Released Prescriptions", prescriptionId)
   })
 
   test("can release Dosage Instructions contains XML tag", async () => {
-    const prescriptionId = await sendPrescriptionUserJourney(driver, testPacks.loadXMLTagDosageInstructionsFHIRMessage)
+    const prescriptionId = await sendPrescriptionUserJourney(driver, loadXMLTagDosageInstructionsFHIRMessage)
     expect(prescriptionId).toBeTruthy()
     await releasePrescriptionUserJourney(driver)
     await checkMyPrescriptions(driver, "Released Prescriptions", prescriptionId)
   })
 
   test("can release Note to dispenser contains XML tag", async () => {
-    const prescriptionId = await sendPrescriptionUserJourney(driver, testPacks.loadXMLTagNotesToDispenseFHIRMessage)
+    const prescriptionId = await sendPrescriptionUserJourney(driver, loadXMLTagNotesToDispenseFHIRMessage)
     expect(prescriptionId).toBeTruthy()
     await releasePrescriptionUserJourney(driver)
     await checkMyPrescriptions(driver, "Released Prescriptions", prescriptionId)
