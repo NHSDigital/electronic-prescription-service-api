ad7df82d5889a80b8ac62209a86a6bc671c64749
Adeel Shahzad <a.shahzad@accenture.com>
2022-10-19 20:20:17 +0100

    limit the result count test

--- tool/e2e-tests/prescribe/sendPrescriptionsFromTestPack.ts
+++ tool/e2e-tests/prescribe/sendPrescriptionsFromTestPack.ts
@@ -6,5 +6,21 @@ describe("firefox", () => {
   test("can send prescriptions from clinical full prescriber test pack", async () => {
     await sendBulkPrescriptionUserJourney(driver, fileInfoFactory.getClinicalFullPrescriberTestPackInfo(), 10)
   })
+
+  test("can send prescriptions from supplier test pack 1", async () => {
+    await sendBulkPrescriptionUserJourney(driver, fileInfoFactory.getSupplierTestPackInfo(), 10)
+  })
+
+  test("can send prescriptions from prescription types test pack", async () => {
+    await sendBulkPrescriptionUserJourney(driver, fileInfoFactory.getPrescriptionTypeTestPackInfo(), 10)
+  })
+
+  test("can send prescriptions from prescription types with invalid types test pack", async () => {
+    await sendBulkPrescriptionUserJourney(driver, fileInfoFactory.getPrescriptionTypesWithInvalidTypesTestPackInfo(), 10)
+  })
+
+  test("can send prescriptions from post dated prescription test pack", async () => {
+    await sendBulkPrescriptionUserJourney(driver, fileInfoFactory.getPostDatedPrescriptionTestPackInfo(), 2)
+  })
 })
 
