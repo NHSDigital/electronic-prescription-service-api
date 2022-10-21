e75025c485f3e4e391f1e3bddf45b677198c1406
Stefano Frazzetto <stefano.frazzetto@infinityworks.com>
2022-10-21 09:52:45 +0100

    Add FHIR path reader for GP (ODS Code)

--- models/common/fhir-path-builder.ts
+++ models/common/fhir-path-builder.ts
@@ -20,45 +20,63 @@ class AbstractPathBuilder {
 }
 
 class BundlePathBuilder extends AbstractPathBuilder {
-    constructor(path: string) {
-      super(path)
-    }
+  constructor(path: string) {
+    super(path)
+  }
 
-    patient(): PatientPathBuilder {
-      return new PatientPathBuilder(`${this.path}.ofType(Patient).first()`)
-    }
-    medicationRequest(): MedicationRequestPathBuilder {
-      return new MedicationRequestPathBuilder(`${this.path}.ofType(MedicationRequest).first()`)
-    }
-    medicationRequests(): MedicationRequestPathBuilder {
-      return new MedicationRequestPathBuilder(`${this.path}.ofType(MedicationRequest)`)
-    }
+  patient(): PatientPathBuilder {
+    return new PatientPathBuilder(`${this.path}.ofType(Patient).first()`)
+  }
+
+  medicationRequest(): MedicationRequestPathBuilder {
+    return new MedicationRequestPathBuilder(`${this.path}.ofType(MedicationRequest).first()`)
+  }
+
+  medicationRequests(): MedicationRequestPathBuilder {
+    return new MedicationRequestPathBuilder(`${this.path}.ofType(MedicationRequest)`)
+  }
 }
 
 class MedicationRequestPathBuilder extends AbstractPathBuilder {
-    constructor(path: string) {
-      super(path)
-    }
+  constructor(path: string) {
+    super(path)
+  }
 
-    prescriptionId(): string {
-      // eslint-disable-next-line max-len
-      return `${this.path}.groupIdentifier.extension.where(url = 'https://fhir.nhs.uk/StructureDefinition/Extension-DM-PrescriptionId').first().valueIdentifier.value`
-    }
-    prescriptionShortFormId(): string {
-      return `${this.path}.groupIdentifier.value`
-    }
-    repeatsIssued(): string {
-      // eslint-disable-next-line max-len
-      return `${this.path}.extension.where(url = 'https://fhir.hl7.org.uk/StructureDefinition/Extension-UKCore-MedicationRepeatInformation').extension.where(url = 'numberOfPrescriptionsIssued').valueUnsignedInt`
-    }
+  prescriptionId(): string {
+    // eslint-disable-next-line max-len
+    return `${this.path}.groupIdentifier.extension.where(url = 'https://fhir.nhs.uk/StructureDefinition/Extension-DM-PrescriptionId').first().valueIdentifier.value`
+  }
+
+  prescriptionShortFormId(): string {
+    return `${this.path}.groupIdentifier.value`
+  }
+
+  repeatsIssued(): string {
+    // eslint-disable-next-line max-len
+    return `${this.path}.extension.where(url = 'https://fhir.hl7.org.uk/StructureDefinition/Extension-UKCore-MedicationRepeatInformation').extension.where(url = 'numberOfPrescriptionsIssued').valueUnsignedInt`
+  }
 }
 
 class PatientPathBuilder extends AbstractPathBuilder {
-    constructor(path: string) {
-      super(path)
-    }
+  constructor(path: string) {
+    super(path)
+  }
 
-    nhsNumber(): string {
-      return `${this.path}.identifier.where(system = 'https://fhir.nhs.uk/Id/nhs-number').value`
-    }
+  nhsNumber(): string {
+    return `${this.path}.identifier.where(system = 'https://fhir.nhs.uk/Id/nhs-number').value`
+  }
+
+  generalPractitioner(): GeneralPractitionerPathBuilder {
+    return new GeneralPractitionerPathBuilder(`${this.path}.generalPractitioner`)
+  }
+}
+
+class GeneralPractitionerPathBuilder extends AbstractPathBuilder {
+  constructor(path: string) {
+    super(path)
+  }
+
+  odsOrganizationCode(): string {
+    return `${this.path}.identifier.where(system = 'https://fhir.nhs.uk/Id/ods-organization-code').value`
+  }
 }
