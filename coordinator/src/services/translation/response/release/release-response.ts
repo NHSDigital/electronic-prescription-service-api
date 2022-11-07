91e2be7fb8f3667c6e288f1474eaaeee23df2d9a
Matthew Popat <matthew.popat1@nhs.net>
2022-11-07 10:29:54 +0000

    AEA-2749 Added initial structure of responses for signature verification on release

--- coordinator/src/services/translation/response/release/release-response.ts
+++ coordinator/src/services/translation/response/release/release-response.ts
@@ -1,32 +1,42 @@
-import {fhir, hl7V3} from "@models"
+import { fhir, hl7V3 } from "@models"
 import * as uuid from "uuid"
-import {toArray} from "../../common"
-import {convertHL7V3DateTimeToIsoDateTimeString} from "../../common/dateTime"
-import {createBundle} from "../../common/response-bundles"
-import {convertResourceToBundleEntry} from "../common"
+import { toArray } from "../../common"
+import { convertHL7V3DateTimeToIsoDateTimeString } from "../../common/dateTime"
+import { createBundle } from "../../common/response-bundles"
+import { convertResourceToBundleEntry } from "../common"
+import { verifySignature } from "../../../verification/signature-verification"
 
 const SUPPORTED_MESSAGE_TYPE = "PORX_MT122003UK32"
 
-export function createOuterBundle(releaseResponse: hl7V3.PrescriptionReleaseResponse): fhir.Bundle {
+export function createOuterBundle(releaseResponse: hl7V3.PrescriptionReleaseResponse): fhir.Parameters {
   const releaseRequestId = releaseResponse.inFulfillmentOf.priorDownloadRequestRef.id._attributes.root
   const parentPrescriptions = toArray(releaseResponse.component)
     .filter(component => component.templateId._attributes.extension === SUPPORTED_MESSAGE_TYPE)
-    .map(component => component.ParentPrescription)
+    .map(component => ({ prescription: component.ParentPrescription, errors: verifySignature(component.ParentPrescription) }))
+  const passedPrescriptions = parentPrescriptions.filter(prescriptionResult => prescriptionResult.errors.length == 0)
   return {
-    resourceType: "Bundle",
-    id: uuid.v4(),
-    meta: {
-      lastUpdated: convertHL7V3DateTimeToIsoDateTimeString(releaseResponse.effectiveTime)
-    },
-    identifier: {
-      system: "https://tools.ietf.org/html/rfc4122",
-      value: releaseResponse.id._attributes.root.toLowerCase()
-    },
-    type: "searchset",
-    total: parentPrescriptions.length,
-    entry: parentPrescriptions
-      .map(parentPrescription => createInnerBundle(parentPrescription, releaseRequestId))
-      .map(convertResourceToBundleEntry)
+    resourceType: "Parameters",
+    parameter: [
+      <fhir.ResourceParameter<fhir.Bundle>>{
+        name: "passedPrescriptions",
+        resource: {
+          resourceType: "Bundle",
+          id: uuid.v4(),
+          meta: {
+            lastUpdated: convertHL7V3DateTimeToIsoDateTimeString(releaseResponse.effectiveTime)
+          },
+          identifier: {
+            system: "https://tools.ietf.org/html/rfc4122",
+            value: releaseResponse.id._attributes.root.toLowerCase()
+          },
+          type: "collection",
+          total: passedPrescriptions.length,
+          entry: passedPrescriptions
+            .map(prescriptionResult => createInnerBundle(prescriptionResult.prescription, releaseRequestId))
+            .map(convertResourceToBundleEntry)
+        }
+      }
+    ]
   }
 }
 
