572168ca3b926a8094febd717b4cbde73a95c071
Stefano Frazzetto <stefano.frazzetto@infinityworks.com>
2022-11-28 13:53:34 +0000

    Try creating prescription for tracker test in CI

--- tests/e2e/pact/specs/live/tracker.spec.ts
+++ tests/e2e/pact/specs/live/tracker.spec.ts
@@ -1,18 +1,81 @@
-import {createInteraction, CreatePactOptions, pactOptions} from "../../resources/common"
+import {
+  createInteraction,
+  CreatePactOptions,
+  pactOptions,
+  successfulOperationOutcome
+} from "../../resources/common"
 import {Pact} from "@pact-foundation/pact"
+import {pino} from "pino"
+import {fhir, fetcher} from "@models"
+import * as TestResources from "../../resources/test-resources"
+import {updatePrescriptions} from "../../services/update-prescriptions"
+import {generateTestOutputFile} from "../../services/genereate-test-output-file"
 
-test("prescription tracker e2e test", async () => {
-  const createPactOptions = new CreatePactOptions("live", "tracker")
-  const provider = new Pact(pactOptions(createPactOptions))
-  await provider.setup()
-  const interaction = createInteraction(
-    createPactOptions
-  )
-  interaction.withRequest.query = {
-    "prescription_id": "EB8B1F-A83008-42DC8L",
-    "repeat_number": "1"
+const logger = pino()
+
+const updateTestPrescriptions = async (): Promise<void> => {
+  if (process.env.UPDATE_PRESCRIPTIONS !== "false") {
+    await updatePrescriptions(
+      fetcher.prescriptionOrderExamples.filter(e => e.isSuccess),
+      [],
+      [],
+      [],
+      [],
+      [],
+      logger
+    )
   }
+  generateTestOutputFile()
+}
+
+const sendTestPrescription = async (): Promise<string> => {
+  const testCase = TestResources.processOrderCases[0]
+  const caseDesc: string = testCase[0]
+  const bundle: fhir.Bundle = testCase[1]
+
+  const options = new CreatePactOptions("live", "process", "send")
+  const provider = new Pact(pactOptions(options))
+  await provider.setup()
+
+  const firstMedicationRequest = bundle.entry.map(e => e.resource)
+    .find(r => r.resourceType === "MedicationRequest") as fhir.MedicationRequest
+  const prescriptionId = firstMedicationRequest.groupIdentifier.value
+
+  const interaction = createInteraction(
+    options,
+    bundle,
+    successfulOperationOutcome,
+    `a request to process prescription: ${prescriptionId} - ${caseDesc} message to Spine`
+  )
+
   await provider.addInteraction(interaction)
   await provider.writePact()
   await provider.finalize()
+
+  return prescriptionId
+}
+
+describe("prescription tracker e2e test", async () => {
+  let prescriptionId: string
+
+  beforeAll(async () => {
+    updateTestPrescriptions()
+    prescriptionId = await sendTestPrescription()
+  })
+
+  test("is able to retrieve prescription from Spine", async () => {
+    const createPactOptions = new CreatePactOptions("live", "tracker")
+    const provider = new Pact(pactOptions(createPactOptions))
+    await provider.setup()
+    const interaction = createInteraction(
+      createPactOptions
+    )
+    interaction.withRequest.query = {
+      "prescription_id": prescriptionId,
+      "repeat_number": "1"
+    }
+    await provider.addInteraction(interaction)
+    await provider.writePact()
+    await provider.finalize()
+  })
 })
