caf37017a9748508953692e46e1576ab2bbc44c4
Stefano Frazzetto <stefano.frazzetto@infinityworks.com>
2022-11-28 15:49:56 +0000

    Address Jest warning

--- tests/e2e/pact/specs/live/tracker.spec.ts
+++ tests/e2e/pact/specs/live/tracker.spec.ts
@@ -28,25 +28,33 @@ const updateTestPrescriptions = async (): Promise<void> => {
   generateTestOutputFile()
 }
 
+const getTestCase = () => {
+  const testCase = TestResources.processOrderCases[0]
+  return {
+    caseDesc: testCase[0],
+    bundle: testCase[1]
+  }
+}
+
+const getPrescriptionShortFormId = (bundle: fhir.Bundle) => {
+  const firstMedicationRequest = bundle.entry.map(e => e.resource)
+    .find(r => r.resourceType === "MedicationRequest") as fhir.MedicationRequest
+  return firstMedicationRequest.groupIdentifier.value
+}
+
 describe("prescription tracker e2e test", async () => {
-  let prescriptionId: string
 
   beforeAll(async () => {
     updateTestPrescriptions()
   })
 
   test("is able to create a test prescription", async () => {
-    const testCase = TestResources.processOrderCases[0]
-    const caseDesc: string = testCase[0]
-    const bundle: fhir.Bundle = testCase[1]
-
     const options = new CreatePactOptions("live", "process", "send")
     const provider = new Pact(pactOptions(options))
     await provider.setup()
 
-    const firstMedicationRequest = bundle.entry.map(e => e.resource)
-      .find(r => r.resourceType === "MedicationRequest") as fhir.MedicationRequest
-    prescriptionId = firstMedicationRequest.groupIdentifier.value
+    const {caseDesc, bundle} = getTestCase()
+    const prescriptionId = getPrescriptionShortFormId(bundle)
 
     const interaction = createInteraction(
       options,
@@ -67,6 +75,10 @@ describe("prescription tracker e2e test", async () => {
     const interaction = createInteraction(
       createPactOptions
     )
+
+    const {bundle} = getTestCase()
+    const prescriptionId = getPrescriptionShortFormId(bundle)
+
     interaction.withRequest.query = {
       "prescription_id": prescriptionId,
       "repeat_number": "1"
