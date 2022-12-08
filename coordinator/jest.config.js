5bae20d81fe9971a59dad51c11bd7e56d9c3ce4b
Stefano Frazzetto <stefano.frazzetto@infinityworks.com>
2022-12-08 17:34:31 +0000

    Update axios to latest version

--- coordinator/jest.config.js
+++ coordinator/jest.config.js
@@ -12,6 +12,13 @@ module.exports = {
     "jest-expect-message"
   ],
   moduleNameMapper: {
-    "@models": "<rootDir>../models"
+    "@models": "<rootDir>../models",
+
+    // Because axios is now published with both ESM and CommonJS exports, and
+    // because Jest runs code in Node, we have to force it to import the
+    // module using the CommonJS import, to avoid the following error:
+    // "Jest encountered an unexpected token [...] import axios from './lib/axios.js';"
+    // Source: https://github.com/axios/axios/issues/5101#issuecomment-1276572468
+    "^axios$": require.resolve("axios"),
   }
 }
