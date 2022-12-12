f3638c6e645b3cabdfd51b100dc6ecf6679481e7
Stefano Frazzetto <stefano.frazzetto@infinityworks.com>
2022-12-12 19:31:19 +0000

    Update paths to examples on CI

--- packages/models/examples/fetchers/example-files-fetcher.ts
+++ packages/models/examples/fetchers/example-files-fetcher.ts
@@ -24,11 +24,17 @@ function walk(dir: string) {
   }
 }
 
-// Hierarchy is different on CI, as top level directories for components are on the same level
-const examplesRootPath = process.env.NODE_ENV === "production" ? "../../../examples" : "../../../../examples"
+const getExamplesDirectory = (examplesSubDirectory: string): string[] => {
+  const isRunningOnAzureCI = process.env.TF_BUILD
+  console.log("Checking whether we are currently on a CI build - TF_BUILD:", isRunningOnAzureCI)
+  // The directories 'models' and 'examples' are on the same level on CI, so we need to adjust the path
+  const examplesRootPath = isRunningOnAzureCI ? "../../../examples" : "../../../../examples"
+  return walk(path.join(__dirname, examplesRootPath, examplesSubDirectory))
+}
 
-const primaryCareFilePaths: Array<string> = walk(path.join(__dirname, examplesRootPath, "primary-care"))
-const secondaryCareFilePaths: Array<string> = walk(path.join(__dirname, examplesRootPath, "secondary-care"))
-const errorFilePaths: Array<string> = walk(path.join(__dirname, examplesRootPath, "errors"))
+const primaryCareFilePaths: Array<string> = getExamplesDirectory("primary-care")
+const secondaryCareFilePaths: Array<string> = getExamplesDirectory("secondary-care")
+const errorFilePaths: Array<string> = getExamplesDirectory("errors")
+
 const allExampleFilePaths = [...primaryCareFilePaths, ...secondaryCareFilePaths, ...errorFilePaths]
 export const exampleFiles: Array<ExampleFile> = allExampleFilePaths.map(examplePath => new ExampleFile(examplePath))
