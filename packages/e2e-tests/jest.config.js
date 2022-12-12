9faabbcea6b47aabedd74c850523194362e5eb46
Stefano Frazzetto <stefano.frazzetto@infinityworks.com>
2022-12-12 12:25:41 +0000

    Update jest paths mapping in Pact tests

--- packages/e2e-tests/jest.config.js
+++ packages/e2e-tests/jest.config.js
@@ -1,15 +1,9 @@
-const fs = require("fs")
-const path = require("path")
-
-const modelsPath = "../models"
-const coordinatorPath = "../coordinator" 
-
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   setupFiles: ["./jest/setEnvVars.js"],
   moduleNameMapper: {
-    "@models": `<rootDir>${modelsPath}`,
-    "@coordinator": `<rootDir>${coordinatorPath}`
+    "@models": `<rootDir>../models`,
+    "@coordinator": `<rootDir>../coordinator`
   }
 }
