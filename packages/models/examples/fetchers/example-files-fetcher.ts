83ca405da483ae8a923409c3c80f06ef7ed65c2d
Stefano Frazzetto <9978588+StefanoFrazzetto@users.noreply.github.com>
2022-12-13 16:48:46 +0000

    AEA-2644 Project structure improvements (#1047)
    * Move components to packages directory
    
    - coordinator
    - epsat
    - models
    - OAS
    - e2e tests
    
    * Update paths in scripts
    
    * Fix paths in Makefile
    
    * Fix ECS container path
    
    * Fix paths for Pact tests
    
    * Update jest paths mapping in Pact tests
    
    * Update jest paths mapping in Pact tests - 2nd attempt
    
    * Make examples path dynamic for CI
    
    * Update Pact tests to spec 3
    
    Hopefully this will avoid the warning:
    "WARN ThreadId(02) pact_models::pact: Note: Existing pact is an older specification version (V2), and will be upgraded" from showing again, and it will stop making the build agent process to suddenly stop running.
    
    * Update paths to examples on CI
    
    * Fix paths to top level examples directory
    
    * Copy sources to dist/e2e-tests/src for make release
    
    Before refactoring the components hierarchy, when 'make release' was run, relative imports within the example files fetchers resolved to the same absolute paths, when running tests from /coordinator and /dist/pact.
    
    Moving the components to /packages created an additional level in the hierarchy, so the imports in the source had to be modified to account for it (i.e. '../../../../examples'), but the structure created by 'make release' was left unchanged, meaning that fetchers were now trying to get the example files from outside the root.
    
    The fix in this case was to add an additional nesting level within the /dist/e2e-tests directory, resulting in the level of directory nesting for both cases, i.e.
    
    /examples
    /packages/models/examples/fetchers
    
    /dist/e2e-tests/examples
    /dist/e2e-tests/src/models/examples/fetchers
    
    * Revert "Update Pact tests to spec 3"
    
    This reverts commit 87b398947fcaffcee1f213971c7c13287adcce6a.

---
+++ packages/models/examples/fetchers/example-files-fetcher.ts
@@ -0,0 +1,37 @@
+import fs from "fs"
+import path from "path"
+import {ExampleFile} from "../example-file"
+
+function walk(dir: string) {
+  let results = Array<string>()
+  try {
+    const list = fs.readdirSync(dir)
+    list.forEach(function(file) {
+      file = dir + "/" + file
+      const stat = fs.statSync(file)
+      if (stat && stat.isDirectory()) {
+        results = results.concat(walk(file))
+      } else {
+        const fileExt = path.parse(file).ext
+        if (fileExt.includes("json") || fileExt.includes("xml")) {
+          results.push(file)
+        }
+      }
+    })
+    return results
+  } catch {
+    return []
+  }
+}
+
+const getExamplesFromDirectory = (examplesSubDirectory: string): string[] => {
+  const examplesRootPath = "../../../../examples"
+  return walk(path.join(__dirname, examplesRootPath, examplesSubDirectory))
+}
+
+const primaryCareFilePaths: Array<string> = getExamplesFromDirectory("primary-care")
+const secondaryCareFilePaths: Array<string> = getExamplesFromDirectory("secondary-care")
+const errorFilePaths: Array<string> = getExamplesFromDirectory("errors")
+
+const allExampleFilePaths = [...primaryCareFilePaths, ...secondaryCareFilePaths, ...errorFilePaths]
+export const exampleFiles: Array<ExampleFile> = allExampleFilePaths.map(examplePath => new ExampleFile(examplePath))
