b276ce472cfc0f03ac74f833ff59951bd11f108a
andymather24 <andy-mather@live.co.uk>
2023-01-05 11:18:55 +0000

    fix tests

--- packages/coordinator/tests/Factory/dispenseProposalReturnPayload.spec.ts
+++ packages/coordinator/tests/Factory/dispenseProposalReturnPayload.spec.ts
@@ -1,28 +1,25 @@
 import {DispenseReturnPayloadFactory} from "../../src/services/translation/request/return/payload/return-payload-factory"
-import {Author, DispenseProposalReturn, DispenseProposalReturnPertinentInformation1, DispenseProposalReturnPertinentInformation3, DispenseProposalReturnReversalOf, DispenseProposalReturnRoot, GlobalIdentifier, Hl7InteractionIdentifier, PrescriptionId, PrescriptionReleaseResponseRef, ReturnReason, ReturnReasonCode, Timestamp} from "../../../models/hl7-v3"
-jest.mock("../../../models/hl7-v3")
+import {Hl7InteractionIdentifier, ReturnReasonCode, SendMessagePayload} from "../../../models/hl7-v3"
+import {validTestHeaders}  from "../resources/test-resources"
+import {DispenseProposalReturnFactory} from "../../src/services/translation/request/return/return-factory"
+import {getExamplePrescriptionReleaseResponse} from "../services/translation/response/release/release-response.spec"
+
 describe("createPayload", () => {
   const returnPayloadFactory = new DispenseReturnPayloadFactory()
-  const dispenseProposalReturnMock = new DispenseProposalReturn(
-    new GlobalIdentifier("1"),
-    new Timestamp("1"),
-    new Author(),
-    new DispenseProposalReturnPertinentInformation1(new PrescriptionId("1")),
-    new DispenseProposalReturnPertinentInformation3(new ReturnReason(new ReturnReasonCode("1", "1"))),
-    new DispenseProposalReturnReversalOf(new PrescriptionReleaseResponseRef("1"))
+  const releaseResponse = getExamplePrescriptionReleaseResponse("release_success.xml")
+  const dispenseProposalReturns = new DispenseProposalReturnFactory().create(
+    releaseResponse, 
+    new ReturnReasonCode("0005", "Invalid Digital Signature")
     )
-  test("should return content which is instance of DispenseProposalReturnRoot", () => {
 
-    const result = returnPayloadFactory.createPayload(new DispenseProposalReturnRoot(dispenseProposalReturnMock))
-    expect(result.content).toBeInstanceOf(DispenseProposalReturnRoot)
+  test("should return instance of SendMessagePayload", () => {
+    const result = returnPayloadFactory.createPayload(dispenseProposalReturns, validTestHeaders)
+    expect(result).toBeInstanceOf(SendMessagePayload)
 
   })
 
   test("should return interactionId of value DISPENSE_PROPOSAL_RETURN ", () => {
-    const returnPayloadFactory = new DispenseReturnPayloadFactory()
-
-
-    const result = returnPayloadFactory.createPayload(new DispenseProposalReturnRoot(dispenseProposalReturnMock))
+    const result = returnPayloadFactory.createPayload(dispenseProposalReturns, validTestHeaders)
     expect(result.interactionId).toEqual(Hl7InteractionIdentifier.DISPENSE_PROPOSAL_RETURN)
 
   })
