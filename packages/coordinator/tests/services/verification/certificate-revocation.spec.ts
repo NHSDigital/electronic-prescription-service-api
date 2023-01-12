7b623bd319f3f7fd752f9fe24b168a75a69cf984
Stefano Frazzetto <stefano.frazzetto@infinityworks.com>
2023-01-12 18:08:15 +0000

    Add sanity check for mock certs resources

--- packages/coordinator/tests/services/verification/certificate-revocation.spec.ts
+++ packages/coordinator/tests/services/verification/certificate-revocation.spec.ts
@@ -1,8 +1,11 @@
 import pino from "pino"
 import {CertificateRevocationList} from "pkijs"
-import * as TestResources from "../../resources/test-resources"
-import {isSignatureCertificateValid} from "../../../src/services/verification/certificate-revocation"
+import * as TestPrescriptions from "../../resources/test-resources"
+import * as TestCertificates from "../../resources/certificates/test-resources"
 import {getRevocationList} from "../../../src/services/verification/certificate-revocation/utils"
+import {getRevokedCertReasonCode} from "../../../src/services/verification/certificate-revocation/utils"
+import {isSignatureCertificateValid} from "../../../src/services/verification/certificate-revocation"
+import {CRLReasonCode} from "../../../src/services/verification/certificate-revocation/crl-reason-code"
 
 const logger = pino()
 
@@ -16,24 +19,38 @@ jest.mock("../../../src/services/verification/certificate-revocation/utils", () 
   }
 })
 
+describe("Mock data is read correctly...", () => {
+  describe("CRL", () => {
+    const list: CertificateRevocationList = TestCertificates.revocationList
+
+    test("contains 3 revoked certs", async () => {
+      expect(list.revokedCertificates.length).toBeGreaterThanOrEqual(3)
+
+      const revocationReasons = list.revokedCertificates.map((cert) => getRevokedCertReasonCode(cert))
+      expect(revocationReasons).toContain(CRLReasonCode.CACompromise)
+      expect(revocationReasons).toContain(CRLReasonCode.KeyCompromise)
+      expect(revocationReasons).toContain(CRLReasonCode.CessationOfOperation)
+    })
+  })
+})
+
 describe("isSignatureCertificateValid...", () => {
   test("returns true if certificate has not been revoked", async () => {
-    const validSignature = TestResources.parentPrescriptions.validSignature.ParentPrescription
+    const validSignature = TestPrescriptions.parentPrescriptions.validSignature.ParentPrescription
     const revoked = await isSignatureCertificateValid(validSignature, logger)
     expect(revoked).toEqual(true)
   })
 
   test("returns false if certificate has been revoked", async () => {
-    const invalidSignature = TestResources.parentPrescriptions.invalidSignature.ParentPrescription
+    const invalidSignature = TestPrescriptions.parentPrescriptions.invalidSignature.ParentPrescription
     const revoked = await isSignatureCertificateValid(invalidSignature, logger)
     expect(revoked).toEqual(false)
   })
 })
 
 describe("getRevocationList...", () => {
-  let list: CertificateRevocationList
   test("returns a CRL containing one or more certificates", async () => {
-    list = await getRevocationList("http://crl.nhs.uk/int/1d/crlc2.crl")
+    const list: CertificateRevocationList = await getRevocationList("http://crl.nhs.uk/int/1d/crlc2.crl")
     expect(list.revokedCertificates.length).toBeGreaterThan(0)
   })
 })
