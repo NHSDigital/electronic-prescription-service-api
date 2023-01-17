0ab1e4e49fb8a5b06c98f10b8769093c6b8d362a
Stefano Frazzetto <stefano.frazzetto@infinityworks.com>
2023-01-17 13:20:28 +0000

    Add missing types

--- packages/coordinator/tests/resources/certificates/test-resources.ts
+++ packages/coordinator/tests/resources/certificates/test-resources.ts
@@ -7,13 +7,14 @@ import * as fs from "fs"
 import {fromBER} from "asn1js"
 import {Certificate, CertificateRevocationList} from "pkijs"
 
-const readFile = (filename: string) => {
+
+const readFile = (filename: string): string => {
   const filePath = path.join(__dirname, `./${filename}`)
   return fs.readFileSync(filePath, "utf-8")
 }
 
 // Source https://gist.github.com/adisbladis/c84e533e591b1737fedd26658021fef2
-const decodeCertificate = (contents: string) => {
+const decodeCertificate = (contents: string): Certificate => {
   const b64 = contents.replace(/(-----(BEGIN|END) CERTIFICATE-----|[\n\r])/g, "")
   const der = Buffer.from(b64, "base64")
   const ber = new Uint8Array(der).buffer
@@ -21,7 +22,7 @@ const decodeCertificate = (contents: string) => {
   return new Certificate({schema: asn1.result})
 }
 
-const decodeCrl = (contents: string) => {
+const decodeCrl = (contents: string): CertificateRevocationList => {
   const b64 = contents.replace(/(-----(BEGIN|END) X509 CRL-----|[\n\r])/g, "")
   const der = Buffer.from(b64, "base64")
   const ber = new Uint8Array(der).buffer
@@ -29,13 +30,15 @@ const decodeCrl = (contents: string) => {
   return new CertificateRevocationList({schema: asn1crl.result})
 }
 
-export const validCertificates = {
+type MockCertificates = { [key: string]: Certificate }
+
+const validCertificates: MockCertificates = {
   certificate: decodeCertificate(
     readFile("certs/validSmartcard.pem")
   )
 }
 
-export const revokedCertificates = {
+const revokedCertificates: MockCertificates = {
   cessationOfOperation: decodeCertificate(
     readFile("certs/cessationOfOperation.pem")
   ),
@@ -47,6 +50,13 @@ export const revokedCertificates = {
   )
 }
 
-export const revocationList = decodeCrl(
+const revocationList: CertificateRevocationList = decodeCrl(
   readFile("crl/ca.crl")
 )
+
+export type {MockCertificates}
+export {
+  validCertificates,
+  revokedCertificates,
+  revocationList
+}
