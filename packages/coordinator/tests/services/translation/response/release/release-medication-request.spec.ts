636482fcd7ad8cbd5ef32b8c7209838bd4f8a110
Phil Gee <phil.gee@airelogic.com>
2023-01-17 15:07:48 +0000

    Merge fixes.

--- packages/coordinator/tests/services/translation/response/release/release-medication-request.spec.ts
+++ packages/coordinator/tests/services/translation/response/release/release-medication-request.spec.ts
@@ -173,7 +173,7 @@ describe("extension", () => {
         },
         {
           url: "numberOfPrescriptionsIssued",
-          valueUnsignedInt: new LosslessNumber("1")
+          valueInteger: new LosslessNumber("1")
         }
       ]
     }
@@ -217,7 +217,7 @@ describe("extension", () => {
       extension: [
         {
           url: "numberOfPrescriptionsIssued",
-          valueUnsignedInt: new LosslessNumber("1")
+          valueInteger: new LosslessNumber("1")
         }
       ]
     }
@@ -676,7 +676,7 @@ describe("createMedicationRequest", () => {
 
     it("should have dispenseRequest numberOfRepeatsAllowed", () => {
       const actualNumberOfRepeatsAllowed = result.dispenseRequest.numberOfRepeatsAllowed
-      const expected = new LosslessNumber(6)
+      const expected = new LosslessNumber("6")
       expect(actualNumberOfRepeatsAllowed).toEqual(expected)
     })
 
@@ -691,7 +691,7 @@ describe("createMedicationRequest", () => {
         const numberOfRepeatsAllowedExtension = extension.extension.find(e => e.url === "numberOfRepeatsAllowed")
         const expectedExtensions = {
           "url": "numberOfRepeatsAllowed",
-          "valueInteger": new LosslessNumber(6)
+          "valueInteger": new LosslessNumber("6")
         }
         expect(numberOfRepeatsAllowedExtension).toEqual(expectedExtensions)
 
@@ -701,7 +701,7 @@ describe("createMedicationRequest", () => {
         const numberOfRepeatsIssuedExtension = extension.extension.find(e => e.url === "numberOfRepeatsIssued")
         const expectedExtensions = {
           "url": "numberOfRepeatsIssued",
-          "valueInteger": new LosslessNumber(1)
+          "valueInteger": new LosslessNumber("1")
         }
         expect(numberOfRepeatsIssuedExtension).toEqual(expectedExtensions)
 
@@ -711,7 +711,7 @@ describe("createMedicationRequest", () => {
         const numberOfRepeatsIssuedExtension = extension.extension.find(e => e.url === "numberOfPrescriptionsIssued")
         const expectedExtensions = {
           "url": "numberOfPrescriptionsIssued",
-          "valueInteger": new LosslessNumber(1)
+          "valueInteger": new LosslessNumber("1")
         }
         expect(numberOfRepeatsIssuedExtension).toEqual(expectedExtensions)
 
