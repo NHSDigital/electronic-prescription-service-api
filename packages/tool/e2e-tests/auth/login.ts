e575fb08d0691b6dcc762158e054d06f278e05f9
Anthony Brown <anthony.brown8@nhs.net>
2023-01-25 10:31:29 +0000

    make everything work

--- packages/tool/e2e-tests/auth/login.ts
+++ packages/tool/e2e-tests/auth/login.ts
@@ -1,11 +1,9 @@
 import {driver} from "../live.test"
 import {loginUnattendedAccess, loginViaSimulatedAuthSmartcardUser} from "../helpers"
-import assert from "node:assert/strict"
 
 describe("firefox", () => {
   test("can login to attended session", async () => {
     await loginViaSimulatedAuthSmartcardUser(driver)
-    assert.fail("testing failure")
   })
 
   test("can login to unattended session", async () => {
