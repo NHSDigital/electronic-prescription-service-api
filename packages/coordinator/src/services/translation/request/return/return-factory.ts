66a0a2c6aedd54305cfd375624d445a1760a74af
Stefano Frazzetto <stefano.frazzetto@infinityworks.com>
2023-01-30 17:12:22 +0000

    Remove duplicated interface function

--- packages/coordinator/src/services/translation/request/return/return-factory.ts
+++ packages/coordinator/src/services/translation/request/return/return-factory.ts
@@ -14,12 +14,10 @@ import {
 } from "../../../../../../models/hl7-v3"
 
 type response = PrescriptionReleaseResponse
-
 type ReturnProposal = DispenseProposalReturnRoot
 
 export interface ReturnFactory {
   create(response: response, returnReasonCode: ReturnReasonCode): ReturnProposal
-  create(response: PrescriptionReleaseResponse, returnReasonCode: ReturnReasonCode): DispenseProposalReturnRoot
 }
 
 export class DispenseProposalReturnFactory implements ReturnFactory {
