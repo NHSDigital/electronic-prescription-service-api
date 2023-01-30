d0eff9354cef3128104e47cbf154b5ab443de999
TomSmithCoding <tom_smith1337@hotmail.co.uk>
2023-01-30 00:45:11 +0000

    Updated EPSAT

--- packages/tool/site/client/src/pages/withdrawPage.tsx
+++ packages/tool/site/client/src/pages/withdrawPage.tsx
@@ -228,10 +228,6 @@ function createWithdraw(withdrawFormValues: WithdrawFormValues, dispenseNotifica
         url: "https://fhir.nhs.uk/StructureDefinition/Extension-EPS-RepeatInformation",
         extension: [
           {
-            url: "numberOfRepeatsAllowed",
-            valueInteger: 3
-          },
-          {
             url: "numberOfRepeatsIssued",
             valueInteger: 2
           }
