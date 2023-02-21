f17588f4a2399f56ce4c716ed2f0a0435b74ace7
Stefano Frazzetto <stefano.frazzetto@infinityworks.com>
2023-02-21 10:36:31 +0000

    Update axios to latest in EPSAT client

--- packages/tool/site/client/jest.config.js
+++ packages/tool/site/client/jest.config.js
@@ -5,5 +5,13 @@ module.exports = {
   },
   testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
-  testEnvironment: "jsdom"
+  testEnvironment: "jsdom",
+  moduleNameMapper: {
+    // Because axios is now published with both ESM and CommonJS exports, and
+    // because Jest runs code in Node, we have to force it to import the
+    // module using the CommonJS import, to avoid the following error:
+    // "Jest encountered an unexpected token [...] import axios from './lib/axios.js';"
+    // Source: https://github.com/axios/axios/issues/5101#issuecomment-1276572468
+    "^axios$": require.resolve("axios"),
+  }
 }
