7538a157e9efedc093242ce930d5f973ceb0868c
Jack Spagnoli <jack.spagnoli1@nhs.net>
2023-02-24 10:02:47 +0000

    adds NaN test and refactors tests

--- packages/tool/site/client/tests/unit_tests/services/test-packs/medicationRequest.spec.tsx
+++ packages/tool/site/client/tests/unit_tests/services/test-packs/medicationRequest.spec.tsx
@@ -3,82 +3,53 @@ import {PrescriptionRow} from "../../../../src/services/test-packs/xls"
 
 describe("getMedicationQuantity", () => {
   test("Correctly parses quantity with integer value", () => {
-    const input: PrescriptionRow = {
-      testId: "1",
-      prescriptionTreatmentTypeCode: "acute",
-      prescriptionTypeCode: "0101",
-      prescriptionTypeDescription:
-        "Primary Care Prescriber - Medical Prescriber",
-      medicationName: "Aciclovir 3% eye ointment",
-      medicationSnomed: "330284003",
-      medicationQuantity: "4",
-      medicationUnitOfMeasureName: "gram",
-      medicationUnitOfMeasureCode: "258682000",
-      dosageInstructions: "As directed",
-      repeatsAllowed: 0,
-      issueDurationInDays: "28",
-      dispenserNotes: [],
-      nominatedPharmacy: "D7K8P",
-      nominatedPharmacyType: "P1",
-      endorsements: undefined,
-      controlledDrugSchedule: undefined,
-      controlledDrugQuantity: undefined,
-      additionalInstructions: undefined
+    let input = {
+      ...template_input
     }
+    input.medicationQuantity = "4"
 
     expect(getMedicationQuantity(input).value).toEqual(4)
   })
   test("Correctly parses quantity with decimal value", () => {
-    const input: PrescriptionRow = {
-      testId: "1",
-      prescriptionTreatmentTypeCode: "acute",
-      prescriptionTypeCode: "0101",
-      prescriptionTypeDescription:
-        "Primary Care Prescriber - Medical Prescriber",
-      medicationName: "Aciclovir 3% eye ointment",
-      medicationSnomed: "330284003",
-      medicationQuantity: "4.5",
-      medicationUnitOfMeasureName: "gram",
-      medicationUnitOfMeasureCode: "258682000",
-      dosageInstructions: "As directed",
-      repeatsAllowed: 0,
-      issueDurationInDays: "28",
-      dispenserNotes: [],
-      nominatedPharmacy: "D7K8P",
-      nominatedPharmacyType: "P1",
-      endorsements: undefined,
-      controlledDrugSchedule: undefined,
-      controlledDrugQuantity: undefined,
-      additionalInstructions: undefined
+    let input = {
+      ...template_input
     }
+    input.medicationQuantity = "4.5"
 
     expect(getMedicationQuantity(input).value).toEqual(4.5)
   })
 
   test("Handles string", () => {
-    const input: PrescriptionRow = {
-      testId: "1",
-      prescriptionTreatmentTypeCode: "acute",
-      prescriptionTypeCode: "0101",
-      prescriptionTypeDescription:
-        "Primary Care Prescriber - Medical Prescriber",
-      medicationName: "Aciclovir 3% eye ointment",
-      medicationSnomed: "330284003",
-      medicationQuantity: "hello",
-      medicationUnitOfMeasureName: "gram",
-      medicationUnitOfMeasureCode: "258682000",
-      dosageInstructions: "As directed",
-      repeatsAllowed: 0,
-      issueDurationInDays: "28",
-      dispenserNotes: [],
-      nominatedPharmacy: "D7K8P",
-      nominatedPharmacyType: "P1",
-      endorsements: undefined,
-      controlledDrugSchedule: undefined,
-      controlledDrugQuantity: undefined,
-      additionalInstructions: undefined
+    let input = {
+      ...template_input
     }
+    input.medicationQuantity = "hello"
 
-    expect(getMedicationQuantity(input).value).toEqual(4.5)
+    const call = () => getMedicationQuantity(input).value
+
+    expect(call).toThrow(new Error(`NaN encountered when parsing line item quantity: hello`))
   })
 })
+
+const template_input: PrescriptionRow = {
+  testId: "1",
+  prescriptionTreatmentTypeCode: "acute",
+  prescriptionTypeCode: "0101",
+  prescriptionTypeDescription:
+    "Primary Care Prescriber - Medical Prescriber",
+  medicationName: "Aciclovir 3% eye ointment",
+  medicationSnomed: "330284003",
+  medicationQuantity: "",
+  medicationUnitOfMeasureName: "gram",
+  medicationUnitOfMeasureCode: "258682000",
+  dosageInstructions: "As directed",
+  repeatsAllowed: 0,
+  issueDurationInDays: "28",
+  dispenserNotes: [],
+  nominatedPharmacy: "D7K8P",
+  nominatedPharmacyType: "P1",
+  endorsements: undefined,
+  controlledDrugSchedule: undefined,
+  controlledDrugQuantity: undefined,
+  additionalInstructions: undefined
+}
