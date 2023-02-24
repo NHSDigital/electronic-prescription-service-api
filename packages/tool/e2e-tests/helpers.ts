1c171a6065983ecacf1985e5dccc9bb60f2937aa
Jack Spagnoli <jack.spagnoli1@nhs.net>
2023-02-24 10:15:34 +0000

    updates imports, comment

--- packages/tool/e2e-tests/helpers.ts
+++ packages/tool/e2e-tests/helpers.ts
@@ -44,7 +44,7 @@ import {
   cancelButton,
   dispenseByFormRadio,
   dispenseWithBodyRadio,
-  dispenseBodyField
+  dispenseBodyField,
   logoutNavLink,
   logoutPageTitle
 } from "./locators"
@@ -169,7 +169,7 @@ export async function dispensePrescriptionWithFormUserJourney(
   await checkApiResult(driver)
 }
 
-//Currently only works with the default Primary Care Paracetamol/Salbutamol prescription.
+//dispenseBody currently only works with the default Primary Care Paracetamol/Salbutamol prescription.
 //getPrescriptionItemIds should be scalable
 export async function dispensePrescriptionWithBodyUserJourney(
   driver: ThenableWebDriver
