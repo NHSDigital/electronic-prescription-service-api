23e59c02217aeeb1318307d8ccaebe8f80735318
Jack Spagnoli <jack.spagnoli1@nhs.net>
2023-02-24 14:08:38 +0000

    adds function to generate dispense body for test

--- packages/tool/e2e-tests/utils/dispenseBody.ts
+++ packages/tool/e2e-tests/utils/dispenseBody.ts
@@ -1,5 +1,16 @@
+import * as uuid from "uuid"
+
 export function createDispenseBody(prescriptionId: string, lineItemIds: string[]): string{
   console.log(prescriptionId, lineItemIds)
 
-  return ""
+  let body = require("./templateBodies/templateDispenseBody.json")
+  body.identifier.value = uuid.v4()
+
+  body.entry[1].contained[1].identifier.value = lineItemIds[0];
+  body.entry[2].contained[1].identifier.value = lineItemIds[1];
+
+  body.entry[1].contained[1].groupIdentifier.value = prescriptionId;
+  body.entry[2].contained[1].groupIdentifier.value = prescriptionId;
+
+  return JSON.stringify(body);
 }
