cdcdfe2775149ecfe19d3dca7aadd5836ae43066
Jack Spagnoli <jack.spagnoli1@nhs.net>
2023-03-03 11:59:54 +0000

    nests templateBody to remove default element

--- packages/tool/e2e-tests/utils/dispenseBody.ts
+++ packages/tool/e2e-tests/utils/dispenseBody.ts
@@ -2,7 +2,7 @@ import * as uuid from "uuid"
 import * as templateBody from "./templateBodies/templateDispenseBody.json"
 
 export function createDispenseBody(prescriptionId: string, lineItemIds: string[]): string {
-  const body = {...templateBody}
+  const body = {...templateBody.templateBody}
 
   body.identifier.value = uuid.v4()
 
