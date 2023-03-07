4d57d7ad647d8bbacedf9dba19dc924d6b6291ef
Jack Spagnoli <jack.spagnoli1@nhs.net>
2023-03-07 15:52:59 +0000

    removes array index from key

--- packages/tool/site/client/src/components/common/newLineFormatter.tsx
+++ packages/tool/site/client/src/components/common/newLineFormatter.tsx
@@ -1,8 +1,9 @@
 import React, {Fragment} from "react"
+import hash from "object-hash"
 
 export function newLineFormatter(lines: Array<string>): Array<JSX.Element> {
   return lines.map((line, index) => (
-    <Fragment key={index}>
+    <Fragment key={hash.sha1(`${index}${line}`)}>
       {index > 0 && <br/>}
       {line}
     </Fragment>
