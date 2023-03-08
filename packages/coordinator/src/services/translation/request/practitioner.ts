ecec5e25145abcf0a6955e1849f9edffdfe3310a
Matthew Popat <matthew.popat1@nhs.net>
2023-03-08 16:56:56 +0000

    Lint fix

--- packages/coordinator/src/services/translation/request/practitioner.ts
+++ packages/coordinator/src/services/translation/request/practitioner.ts
@@ -2,7 +2,6 @@ import {convertName, convertTelecom} from "./demographics"
 import {
   getCodeableConceptCodingForSystem,
   getExtensionForUrlOrNull,
-  getIdentifierValueForSystem,
   getIdentifierValueOrNullForSystem,
   identifyMessageType,
   onlyElement,
