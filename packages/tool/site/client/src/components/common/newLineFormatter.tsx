dcf4adddebc57f68b888c594b39e7a941469d186
Jack Spagnoli <jack.spagnoli1@nhs.net>
2023-03-08 12:08:09 +0000

    updates hash

--- packages/tool/site/client/src/components/common/newLineFormatter.tsx
+++ packages/tool/site/client/src/components/common/newLineFormatter.tsx
@@ -3,7 +3,7 @@ import React, {Fragment} from "react"
 
 export function newLineFormatter(lines: Array<string>): Array<JSX.Element> {
   return lines.map((line, index) => (
-    <Fragment key={SHA1(`${line}`)}>
+    <Fragment key={SHA1(line).toString()}>
       {index > 0 && <br/>}
       {line}
     </Fragment>
