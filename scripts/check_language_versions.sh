102b44f25e2da01476b50dbe591358b1c56d6520
Anthony Brown <anthony.brown8@nhs.net>
2023-03-17 10:46:04 +0000

    fix linting on shell script

--- scripts/check_language_versions.sh
+++ scripts/check_language_versions.sh
@@ -13,19 +13,19 @@ EPSAT_RELEASE_PIPELINE_FILE='packages/tool/azure/azure-release-template.yml'
 DEVCONTAINER_DOCKERFILE='.devcontainer/Dockerfile'
 
 
-TOOL_VERSIONS_NODE_VERSION=`grep nodejs ${ROOT_DIR}/${TOOL_VERSION_FILE} | awk '{ print $NF }'`
-API_DOCKER_NODE_BASE_VERSION=`grep "FROM node.* AS base" ${ROOT_DIR}/${API_DOCKER_FILE} |cut -d : -f2 |cut -d- -f1`
-API_DOCKER_NODE_BUILD_VERSION=`grep "FROM node.* AS build" ${ROOT_DIR}/${API_DOCKER_FILE} |cut -d : -f2 |cut -d- -f1`
-EPSAT_DOCKER_NODE_BASE_VERSION=`grep "FROM node.* AS base" ${ROOT_DIR}/${EPSAT_DOCKER_FILE} |cut -d : -f2 |cut -d- -f1`
-EPSAT_DOCKER_NODE_BUILD_CLIENT_VERSION=`grep "FROM node.* AS build-client" ${ROOT_DIR}/${EPSAT_DOCKER_FILE} |cut -d : -f2 |cut -d- -f1`
-EPSAT_DOCKER_NODE_BUILD_SERVER_VERSION=`grep "FROM node.* AS build-server" ${ROOT_DIR}/${EPSAT_DOCKER_FILE} |cut -d : -f2 |cut -d- -f1`
-API_BUILD_PIPELINE_NODE_VERSION=`awk '/NodeTool@0/{x=NR+3;next}(NR==x){print}' ${ROOT_DIR}/${API_BUILD_PIPELINE_FILE} | awk '{ print $NF }'  | tr -d '"'`
-API_RELEASE_PIPELINE_NODE_VERSION=`awk '/NodeTool@0/{x=NR+3;next}(NR==x){print}' ${ROOT_DIR}/${API_RELEASE_PIPELINE_FILE} | awk '{ print $NF }'  | tr -d '"'`
-EPSAT_BUILD_PIPELINE_NODE_VERSION=`awk '/NodeTool@0/{x=NR+3;next}(NR==x){print}' ${ROOT_DIR}/${EPSAT_BUILD_PIPELINE_FILE} | awk '{ print $NF }'  | tr -d '"'`
-EPSAT_RELEASE_PIPELINE_NODE_VERSION=`awk '/NodeTool@0/{x=NR+3;next}(NR==x){print}' ${ROOT_DIR}/${EPSAT_RELEASE_PIPELINE_FILE} | awk '{ print $NF }'  | tr -d '"'`
+TOOL_VERSIONS_NODE_VERSION=$(grep nodejs "${ROOT_DIR}/${TOOL_VERSION_FILE}" | awk '{ print $NF }')
+API_DOCKER_NODE_BASE_VERSION=$(grep "FROM node.* AS base" "${ROOT_DIR}/${API_DOCKER_FILE}" |cut -d : -f2 |cut -d- -f1)
+API_DOCKER_NODE_BUILD_VERSION=$(grep "FROM node.* AS build" "${ROOT_DIR}/${API_DOCKER_FILE}" |cut -d : -f2 |cut -d- -f1)
+EPSAT_DOCKER_NODE_BASE_VERSION=$(grep "FROM node.* AS base" "${ROOT_DIR}/${EPSAT_DOCKER_FILE}" |cut -d : -f2 |cut -d- -f1)
+EPSAT_DOCKER_NODE_BUILD_CLIENT_VERSION=$(grep "FROM node.* AS build-client" "${ROOT_DIR}/${EPSAT_DOCKER_FILE}" |cut -d : -f2 |cut -d- -f1)
+EPSAT_DOCKER_NODE_BUILD_SERVER_VERSION=$(grep "FROM node.* AS build-server" "${ROOT_DIR}/${EPSAT_DOCKER_FILE}" |cut -d : -f2 |cut -d- -f1)
+API_BUILD_PIPELINE_NODE_VERSION=$(awk '/NodeTool@0/{x=NR+3;next}(NR==x){print}' "${ROOT_DIR}/${API_BUILD_PIPELINE_FILE}" | awk '{ print $NF }'  | tr -d '"')
+API_RELEASE_PIPELINE_NODE_VERSION=$(awk '/NodeTool@0/{x=NR+3;next}(NR==x){print}' "${ROOT_DIR}/${API_RELEASE_PIPELINE_FILE}" | awk '{ print $NF }'  | tr -d '"')
+EPSAT_BUILD_PIPELINE_NODE_VERSION=$(awk '/NodeTool@0/{x=NR+3;next}(NR==x){print}' "${ROOT_DIR}/${EPSAT_BUILD_PIPELINE_FILE}" | awk '{ print $NF }'  | tr -d '"')
+EPSAT_RELEASE_PIPELINE_NODE_VERSION=$(awk '/NodeTool@0/{x=NR+3;next}(NR==x){print}' "${ROOT_DIR}/${EPSAT_RELEASE_PIPELINE_FILE}" | awk '{ print $NF }'  | tr -d '"')
 
-DEVCONTAINER_VALIDATOR_VERSION=`grep "ARG VALIDATOR_VERSION_TAG" ${ROOT_DIR}/${DEVCONTAINER_DOCKERFILE} |cut -d = -f 2`
-API_BUILD_PIPELINE_VALIDATOR_VERSION=`awk '/validation-service-fhir-r4/{x=NR+3;next}(NR==x){print}' ${ROOT_DIR}/${API_BUILD_PIPELINE_FILE} | awk '{ print $NF }'`
+DEVCONTAINER_VALIDATOR_VERSION=$(grep "ARG VALIDATOR_VERSION_TAG" "${ROOT_DIR}/${DEVCONTAINER_DOCKERFILE}" |cut -d = -f 2)
+API_BUILD_PIPELINE_VALIDATOR_VERSION=$(awk '/validation-service-fhir-r4/{x=NR+3;next}(NR==x){print}' "${ROOT_DIR}/${API_BUILD_PIPELINE_FILE}" | awk '{ print $NF }')
 
 FAILED_CHECK=0
 
@@ -88,9 +88,9 @@ if [[ ${FAILED_CHECK} == 1 ]]; then
     echo "EPSAT_DOCKER_NODE_BUILD_CLIENT_VERSION: ${EPSAT_DOCKER_NODE_BUILD_CLIENT_VERSION}"
     echo "EPSAT_DOCKER_NODE_BUILD_SERVER_VERSION: ${EPSAT_DOCKER_NODE_BUILD_SERVER_VERSION}"
     echo "API_BUILD_PIPELINE_NODE_VERSION: ${API_BUILD_PIPELINE_NODE_VERSION}"
-    echo "API_RELEAEE_PIPELINE_NODE_VERSION: ${API_RELEAESE_PIPELINE_NODE_VERSION}"
+    echo "API_RELEASE_PIPELINE_NODE_VERSION: ${API_RELEASE_PIPELINE_NODE_VERSION}"
     echo "EPSAT_BUILD_PIPELINE_NODE_VERSION: ${EPSAT_BUILD_PIPELINE_NODE_VERSION}"
-    echo "EPSAT_RELEASE_PIPELINE_NODE_VERSION: ${EPSAT_RELEAESE_PIPELINE_NODE_VERSION}"
+    echo "EPSAT_RELEASE_PIPELINE_NODE_VERSION: ${EPSAT_RELEASE_PIPELINE_NODE_VERSION}"
     echo "DEVCONTAINER_VALIDATOR_VERSION: ${DEVCONTAINER_VALIDATOR_VERSION}"
     echo "API_BUILD_PIPELINE_VALIDATOR_VERSION: ${API_BUILD_PIPELINE_VALIDATOR_VERSION}"
 
