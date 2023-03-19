89d131463913f5c99ac33ec8b088b275ee5253ae
Anthony Brown <anthony.brown8@nhs.net>
2023-03-19 09:30:29 +0000

    fix tests

--- packages/coordinator/tests/logging/helpers.ts
+++ packages/coordinator/tests/logging/helpers.ts
@@ -10,7 +10,7 @@ const testIfValidPayload = (payload?: fhir.Resource): jest.It => {
 }
 
 const configureLogging = async (server: Hapi.Server): Promise<void> => {
-  await HapiPino.register(server, {
+  return await HapiPino.register(server, {
     // Redact Authorization headers, see https://getpino.io/#/docs/redaction
     redact: ["req.headers.authorization"],
     wrapSerializers: false
@@ -31,7 +31,8 @@ type ServerRequest = {
 const getPostRequestValidHeaders = (
   url: string,
   headers: Hapi.Utils.Dictionary<string>,
-  payload: fhir.Resource): ServerRequest => {
+  payload: fhir.Resource
+): ServerRequest => {
   return {
     method: "POST",
     url: url,
@@ -42,14 +43,8 @@ const getPostRequestValidHeaders = (
 
 const getPayloadIdentifiersFromLogs = (logs: Array<Hapi.RequestLog>): Array<PayloadIdentifiers> => {
   return logs
-    .filter(log => isPayloadIdentifiersLog(log.data))
-    .map(log => (log.data as PayloadIdentifiersLog).payloadIdentifiers)
+    .filter((log) => isPayloadIdentifiersLog(log.data))
+    .map((log) => (log.data as PayloadIdentifiersLog).payloadIdentifiers)
 }
 
-export {
-  configureLogging,
-  hasAuditTag,
-  getPostRequestValidHeaders,
-  getPayloadIdentifiersFromLogs,
-  testIfValidPayload
-}
+export {configureLogging, hasAuditTag, getPostRequestValidHeaders, getPayloadIdentifiersFromLogs, testIfValidPayload}
