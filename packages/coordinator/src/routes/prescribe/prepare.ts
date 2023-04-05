5a15ae541079168174c2825b64ba401b1192d40b
Anthony Brown <anthony.brown8@nhs.net>
2023-04-05 09:26:03 +0000

    log verify bundle issues

--- packages/coordinator/src/routes/prescribe/prepare.ts
+++ packages/coordinator/src/routes/prescribe/prepare.ts
@@ -19,27 +19,26 @@ export default [
   {
     method: "POST",
     path: `${BASE_PATH}/$prepare`,
-    handler: externalValidator(
-      async (request: Hapi.Request, responseToolkit: Hapi.ResponseToolkit) => {
-        const bundle = getPayload(request) as fhir.Bundle
-        const scope = getScope(request.headers)
-        const accessTokenSDSUserID = getSdsUserUniqueId(request.headers)
-        const accessTokenSDSRoleID = getSdsRoleProfileId(request.headers)
-        const issues = bundleValidator.verifyBundle(bundle, scope, accessTokenSDSUserID, accessTokenSDSRoleID)
-        if (issues.length) {
-          const response = fhir.createOperationOutcome(issues)
-          const statusCode = getStatusCode(issues)
-          return responseToolkit.response(response).code(statusCode).type(ContentTypes.FHIR)
-        }
-
-        request.logger.info("Encoding HL7V3 signature fragments")
-
-        const response = translator.convertFhirMessageToSignedInfoMessage(bundle, request.logger)
-        request.log("audit", {"incomingMessageHash": createHash(JSON.stringify(bundle))})
-        request.log("audit", {"PrepareEndpointResponse": response})
-
-        return responseToolkit.response(response).code(200).type(ContentTypes.FHIR)
+    handler: externalValidator(async (request: Hapi.Request, responseToolkit: Hapi.ResponseToolkit) => {
+      const bundle = getPayload(request) as fhir.Bundle
+      const scope = getScope(request.headers)
+      const accessTokenSDSUserID = getSdsUserUniqueId(request.headers)
+      const accessTokenSDSRoleID = getSdsRoleProfileId(request.headers)
+      const issues = bundleValidator.verifyBundle(bundle, scope, accessTokenSDSUserID, accessTokenSDSRoleID)
+      if (issues.length) {
+        request.log("warning", {verifyBundleIssues: issues})
+        const response = fhir.createOperationOutcome(issues)
+        const statusCode = getStatusCode(issues)
+        return responseToolkit.response(response).code(statusCode).type(ContentTypes.FHIR)
       }
-    )
+
+      request.logger.info("Encoding HL7V3 signature fragments")
+
+      const response = translator.convertFhirMessageToSignedInfoMessage(bundle, request.logger)
+      request.log("audit", {incomingMessageHash: createHash(JSON.stringify(bundle))})
+      request.log("audit", {PrepareEndpointResponse: response})
+
+      return responseToolkit.response(response).code(200).type(ContentTypes.FHIR)
+    })
   }
 ]
