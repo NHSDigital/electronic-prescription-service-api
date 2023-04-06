ef32afaa1a39ef4ce93b0d1c618d34a12fd78da1
Jack Spagnoli <jack.spagnoli1@nhs.net>
2023-04-06 08:27:40 +0000

    hardcodes conversion table and adds reference. Restructures into separate directory

--- packages/coordinator/src/services/translation/response/practitioner-role/index.ts
+++ packages/coordinator/src/services/translation/response/practitioner-role/index.ts
@@ -3,16 +3,17 @@ import {
   convertTelecom,
   generateResourceId,
   humanNameArrayToString
-} from "./common"
-import {toArray} from "../common"
+} from "../common"
+import {toArray} from "../../common"
 import {fhir, hl7V3} from "@models"
-import {createPractitionerOrRoleIdentifier} from "./identifiers"
-import {createPractitionerIdentifier} from "./practitioner"
-import {getOrganizationCodeIdentifier} from "./organization"
+import {createPractitionerOrRoleIdentifier} from "../identifiers"
+import {createPractitionerIdentifier} from "../practitioner"
+import {getOrganizationCodeIdentifier} from "../organization"
+import roleNames from "./role-names"
 
 export function createPractitionerRole(
   hl7AgentPerson: hl7V3.AgentPerson,
-  practitionerId: string,
+  practitionerId: string
 ): fhir.PractitionerRole {
   return {
     resourceType: "PractitionerRole",
@@ -24,9 +25,7 @@ export function createPractitionerRole(
   }
 }
 
-export function createRefactoredPractitionerRole(
-  hl7AgentPerson: hl7V3.AgentPerson
-): fhir.PractitionerRole {
+export function createRefactoredPractitionerRole(hl7AgentPerson: hl7V3.AgentPerson): fhir.PractitionerRole {
   const representedOrganization = hl7AgentPerson.representedOrganization
 
   const practitionerName = humanNameArrayToString(convertName(hl7AgentPerson.agentPerson.name))
@@ -40,9 +39,7 @@ export function createRefactoredPractitionerRole(
     id: generateResourceId(),
     identifier: createPractitionerRoleIdentifiers(hl7AgentPerson),
     practitioner: fhir.createIdentifierReference(practitionerIdentifier, practitionerName),
-    healthcareService: [
-      fhir.createIdentifierReference(healthcareServiceIdentifier, healthcareServiceName)
-    ],
+    healthcareService: [fhir.createIdentifierReference(healthcareServiceIdentifier, healthcareServiceName)],
     code: createJobRoleNameCode(hl7AgentPerson.code._attributes.code),
     telecom: toArray(hl7AgentPerson.telecom)[0]?._attributes ? convertTelecom(hl7AgentPerson.telecom) : undefined
   }
@@ -62,9 +59,7 @@ export function createRefactoredPractitionerRole(
 
 function createPractitionerRoleIdentifiers(hl7AgentPerson: hl7V3.AgentPerson) {
   const roleId = hl7AgentPerson.id._attributes.extension
-  const identifiers = [
-    fhir.createIdentifier("https://fhir.nhs.uk/Id/sds-role-profile-id", roleId)
-  ]
+  const identifiers = [fhir.createIdentifier("https://fhir.nhs.uk/Id/sds-role-profile-id", roleId)]
 
   const userId = hl7AgentPerson.agentPerson.id._attributes.extension
   const extraIdentifier = createPractitionerOrRoleIdentifier(userId)
@@ -77,12 +72,21 @@ function createPractitionerRoleIdentifiers(hl7AgentPerson: hl7V3.AgentPerson) {
 
 function createJobRoleNameCode(practitionerCode: string) {
   const jobRole = practitionerCode.includes(":") === true ? "Code" : "Name"
-  return [{
-    coding: [{
-      system: `https://fhir.hl7.org.uk/CodeSystem/UKCore-SDSJobRole${jobRole}`,
-      code: practitionerCode,
-      //TODO - remove once profile has been relaxed
-      display: practitionerCode
-    }]
-  }]
+  const jobRoleCode = practitionerCode.split(":").pop()
+  return [
+    {
+      coding: [
+        {
+          system: `https://fhir.hl7.org.uk/CodeSystem/UKCore-SDSJobRole${jobRole}`,
+          code: practitionerCode,
+          //TODO - remove once profile has been relaxed
+          display: getDisplayName(jobRoleCode)
+        }
+      ]
+    }
+  ]
+}
+
+function getDisplayName(jobRoleCode: string): string {
+  return roleNames.find(({code}) => code === jobRoleCode)?.display
 }
