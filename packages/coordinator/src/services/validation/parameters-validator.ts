4bcafe794d67eedd7ceb931012af279084122680
TomSmithCoding <tom_smith1337@hotmail.co.uk>
2023-04-24 12:05:34 +0100

    Agent parameter now optional depending on if group identifier present

--- packages/coordinator/src/services/validation/parameters-validator.ts
+++ packages/coordinator/src/services/validation/parameters-validator.ts
@@ -2,9 +2,9 @@ import {fhir, validationErrors as errors} from "@models"
 import {validatePermittedAttendedDispenseMessage, validatePermittedUnattendedDispenseMessage} from "./scope-validator"
 import {
   getIdentifierParameterOrNullByName,
-  getAgentParameter,
   getOwnerParameterOrNull,
-  getIdentifierValueForSystem
+  getIdentifierValueForSystem,
+  getAgentParameterOrNull
 } from "../translation/common"
 import {isReference} from "../../utils/type-guards"
 
@@ -35,45 +35,51 @@ export function verifyParameters(
     )
   }
 
-  const agentParameter = getAgentParameter(parameters)
-  const practitionerRole = agentParameter.resource
-  const {practitioner, telecom} = practitionerRole
-  if (practitioner && isReference(practitioner)) {
-    incorrectValueErrors.push(
-      errors.fieldIsReferenceButShouldNotBe('Parameters.parameter("agent").resource.practitioner')
-    )
-  }
-  if (!telecom?.length) {
-    incorrectValueErrors.push(
-      errors.missingRequiredField('Parameters.parameter("agent").resource.telecom')
-    )
+  const agentParameter = getAgentParameterOrNull(parameters)
+  if (prescriptionIdParameter && !agentParameter) {
+    errors.missingRequiredParameter("agent")
   }
 
-  if (practitioner && !isReference(practitioner)) {
-    const bodySDSUserID = getIdentifierValueForSystem(
-      [practitioner.identifier],
-      "https://fhir.nhs.uk/Id/sds-user-id",
-      'parameters.parameter("PractitionerRole").practitioner.identifier'
-    )
-    if (bodySDSUserID !== accessTokenSDSUserID) {
-      console.warn(
-        // eslint-disable-next-line max-len
-        `SDS Unique User ID does not match between access token and message body. Access Token: ${accessTokenSDSRoleID} Body: ${bodySDSUserID}.`
+  if (agentParameter) {
+    const practitionerRole = agentParameter.resource
+    const {practitioner, telecom} = practitionerRole
+    if (practitioner && isReference(practitioner)) {
+      incorrectValueErrors.push(
+        errors.fieldIsReferenceButShouldNotBe('Parameters.parameter("agent").resource.practitioner')
       )
     }
-  }
-
-  if (practitionerRole.identifier) {
-    const bodySDSRoleID = getIdentifierValueForSystem(
-      practitionerRole.identifier,
-      "https://fhir.nhs.uk/Id/sds-role-profile-id",
-      'parameters.parameter("PractitionerRole").identifier'
-    )
-    if (bodySDSRoleID !== accessTokenSDSRoleID) {
-      console.warn(
-        // eslint-disable-next-line max-len
-        `SDS Role ID does not match between access token and message body. Access Token: ${accessTokenSDSRoleID} Body: ${bodySDSRoleID}.`
+    if (!telecom?.length) {
+      incorrectValueErrors.push(
+        errors.missingRequiredField('Parameters.parameter("agent").resource.telecom')
       )
+    }
+
+    if (practitioner && !isReference(practitioner)) {
+      const bodySDSUserID = getIdentifierValueForSystem(
+        [practitioner.identifier],
+        "https://fhir.nhs.uk/Id/sds-user-id",
+        'parameters.parameter("PractitionerRole").practitioner.identifier'
+      )
+      if (bodySDSUserID !== accessTokenSDSUserID) {
+        console.warn(
+          // eslint-disable-next-line max-len
+          `SDS Unique User ID does not match between access token and message body. Access Token: ${accessTokenSDSRoleID} Body: ${bodySDSUserID}.`
+        )
+      }
+    }
+
+    if (practitionerRole.identifier) {
+      const bodySDSRoleID = getIdentifierValueForSystem(
+        practitionerRole.identifier,
+        "https://fhir.nhs.uk/Id/sds-role-profile-id",
+        'parameters.parameter("PractitionerRole").identifier'
+      )
+      if (bodySDSRoleID !== accessTokenSDSRoleID) {
+        console.warn(
+          // eslint-disable-next-line max-len
+          `SDS Role ID does not match between access token and message body. Access Token: ${accessTokenSDSRoleID} Body: ${bodySDSRoleID}.`
+        )
+      }
     }
   }
 
