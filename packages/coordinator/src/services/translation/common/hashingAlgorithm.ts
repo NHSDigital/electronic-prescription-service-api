8b2dca32be0f3c296af23e14b1e6a38e0fea6dd1
Jack Spagnoli <jack.spagnoli1@nhs.net>
2023-04-25 14:53:34 +0000

    updates digest algorithm references

--- packages/coordinator/src/services/translation/common/hashingAlgorithm.ts
+++ packages/coordinator/src/services/translation/common/hashingAlgorithm.ts
@@ -13,9 +13,9 @@ export function getPrepareHashingAlgorithmFromEnvVar(): HashingAlgorithm {
 export function getHashingAlgorithmFromSignatureRoot(signatureRoot: ElementCompact): HashingAlgorithm {
   const digestHashingAlgorithm = signatureRoot.Signature.SignedInfo.Reference.DigestMethod._attributes.Algorithm
   switch (digestHashingAlgorithm) {
-    case "http://www.w3.org/2001/04/xmldsig-more#rsa-sha256":
+    case "http://www.w3.org/2001/04/xmlenc#sha256":
       return HashingAlgorithm.SHA256
-    case "http://www.w3.org/2000/09/xmldsig#rsa-sha1":
+    case "http://www.w3.org/2000/09/xmldsig#sha1":
     default:
       return HashingAlgorithm.SHA1
   }
