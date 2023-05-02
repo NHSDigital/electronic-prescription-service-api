5955d2d2d0e062d72dcd099647969843739c57b0
Phil Gee <phil.gee@airelogic.com>
2023-05-02 12:28:26 +0100

    Moved into own file for re-use.

--- packages/coordinator/tests/services/translation/request/practitioner.spec.ts
+++ packages/coordinator/tests/services/translation/request/practitioner.spec.ts
@@ -61,40 +61,6 @@ describe("getAgentPersonTelecom", () => {
   })
 })
 
-describe("getJobRoleCodeOrName", () => {
-  test("Only JobRoleName", () => {
-    const practitionerRole = new fhir.PractitionerRole()
-    const coding = {
-      system: "https://fhir.hl7.org.uk/CodeSystem/UKCore-SDSJobRoleName",
-      code: "R8000",
-      display: "Clinical Practitioner Access Role"
-    }
-    practitionerRole.code = [{coding: [coding]}]
-    const jobRoleCode = practitioner.getJobRoleCodeOrName(practitionerRole)
-    expect(jobRoleCode).toEqual(coding)
-  })
-  test("Only JobRoleCode", () => {
-    const practitionerRole = new fhir.PractitionerRole()
-    const coding = {
-      system: "https://fhir.nhs.uk/CodeSystem/NHSDigital-SDS-JobRoleCode",
-      code: "S8000:G8000:R8000",
-      display: "Clinical Practitioner Access Role"
-    }
-    practitionerRole.code = [{coding: [coding]}]
-    const jobRoleCode = practitioner.getJobRoleCodeOrName(practitionerRole)
-    expect(jobRoleCode).toEqual(coding)
-  })
-  test("Neither JobRoleCode nor JobRoleName", () => {
-    const practitionerRole = new fhir.PractitionerRole()
-    practitionerRole.code = [{coding: []}]
-    const jobRoleCode = () => practitioner.getJobRoleCodeOrName(practitionerRole)
-    expect(jobRoleCode).toThrowError(new errors.TooFewValuesError(
-      // eslint-disable-next-line max-len
-      "Too few values submitted. Expected at least 1 element where system in [https://fhir.nhs.uk/CodeSystem/NHSDigital-SDS-JobRoleCode, https://fhir.hl7.org.uk/CodeSystem/UKCore-SDSJobRoleName]."
-    ))
-  })
-})
-
 describe("getAgentPersonPersonIdForAuthor", () => {
   const gmcCodeValue = "1234567"
 
