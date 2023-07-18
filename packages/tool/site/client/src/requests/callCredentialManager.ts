a122d44e2f6d84ad38a4eac0dcbe8ebfa1ee2420
TomSmithCoding <tom_smith1337@hotmail.co.uk>
2023-07-18 15:22:12 +0000

    Remove undefined jquery

--- packages/tool/site/client/src/requests/callCredentialManager.ts
+++ packages/tool/site/client/src/requests/callCredentialManager.ts
@@ -1,10 +1,5 @@
 import {HubResponse, start} from "./helpers"
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-(window as any).jQuery = jQuery;
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-(window as any).$ = jQuery
-
 const SIGNALR_URL = `"http://localhost:"+prService.portNumber()+"/signalr"`
 const SIGNALR_HUB_NAME = "signingHub"
 const SIGNALR_METHOD_NAME = "requestToSign"
