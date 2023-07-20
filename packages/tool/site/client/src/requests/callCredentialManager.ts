c25d1a0b97ef566c036dd53d920d563dfab9c279
TomSmithCoding <tom_smith1337@hotmail.co.uk>
2023-07-20 16:52:27 +0000

    hubconnection

--- packages/tool/site/client/src/requests/callCredentialManager.ts
+++ packages/tool/site/client/src/requests/callCredentialManager.ts
@@ -1,12 +1,12 @@
 import {HubResponse} from "./helpers"
-import $ from "../../../../../../node_modules/@types/jquery"
+import {hubConnection} from "jquery"
 
 const SIGNALR_URL = `"http://localhost:"+prService.portNumber()+"/signalr"`
 const SIGNALR_HUB_NAME = "signingHub"
 const SIGNALR_METHOD_NAME = "requestToSign"
 
 export async function sign(): Promise<HubResponse> {
-  const connection = $.hubConnection(SIGNALR_URL, {logging: true})
+  const connection = hubConnection(SIGNALR_URL, {logging: true})
   if (connection.state !== SignalR.ConnectionState.Disconnected) {
     console.log(`Unexpected SignalR connection state ${connection.state}`)
     throw new Error("SignalR connection is already active.")
