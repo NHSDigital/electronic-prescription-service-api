c1792ef68b7cab18d75d23d33c6d247125986c5a
TomSmithCoding <tom_smith1337@hotmail.co.uk>
2023-07-20 16:38:09 +0000

    jquery import

--- packages/tool/site/client/src/requests/callCredentialManager.ts
+++ packages/tool/site/client/src/requests/callCredentialManager.ts
@@ -1,9 +1,6 @@
 import {HubResponse} from "./helpers"
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-(window as any).jQuery = $;
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-(window as any).$ = $
+import $ from "jquery"
 
 const SIGNALR_URL = `"http://localhost:"+prService.portNumber()+"/signalr"`
 const SIGNALR_HUB_NAME = "signingHub"
