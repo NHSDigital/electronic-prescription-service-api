4094e6788679513bf88863a2e6be64a868b01aa6
TomSmithCoding <tom_smith1337@hotmail.co.uk>
2023-07-27 10:16:22 +0000

    JS Reformatting and url change for jquery

--- packages/tool/site/client/static/pr-service.js
+++ packages/tool/site/client/static/pr-service.js
@@ -2,13 +2,13 @@
 /* eslint-disable func-style */
 //identify IE with IE version 11 support
 function isBrowserIE() {
-  var ua = window.navigator.userAgent
-  var msie = ua.indexOf("MSIE ")
+  var ua = window.navigator.userAgent;
+  var msie = ua.indexOf("MSIE ");
   // If Internet Explorer, return version number
   if (msie > 0 || !!navigator.userAgent.match(/Trident.*rv:11\./)) {
-    return true
+    return true;
   }
-  return false
+  return false;
 }
 
 /*!
@@ -35,75 +35,75 @@ var PRService = function () {
   //This method is auto invoked when the object is created.
   getPRSPortNumber = function () {
     $.each(configuration.PRServicePortRange, function (key, value) {
-      connectPRService(value)
+      connectPRService(value);
       if (configuration.PRPortNumber !== undefined) {
-        configuration.GetPortNumberURL = configuration.GetPortNumberURL.replace("{0}", configuration.PRPortNumber)
-        return false
+        configuration.GetPortNumberURL = configuration.GetPortNumberURL.replace("{0}", configuration.PRPortNumber);
+        return false;
       }
     })
   }
 
   //Connects to NHS Port Service using the port number from the range provided.
   connectPRService = function (value) {
-    _ajax.ajaxCall(configuration.PRServiceURL.replace("{0}", value), configuration.Method, true, false, assignPRServicePortNumber, null, value)
+    _ajax.ajaxCall(configuration.PRServiceURL.replace("{0}", value), configuration.Method, true, false, assignPRServicePortNumber, null, value);
   }
 
   //NHS Port service connectivity is successful and hence the port number is kept in memory
   assignPRServicePortNumber = function (data, value) {
     if (data.statusCode === "200") {
-      configuration.PRPortNumber = value
+      configuration.PRPortNumber = value;
     }
   }
 
   //Retrieves the port number on which NHS Credential Management is listening.
   getPortNumber = function (successCallBack, errorCallBack) {
-    var callbacks = {"SuccessCallBack": successCallBack, "ErrorCallBack": errorCallBack}
+    var callbacks = {"SuccessCallBack": successCallBack, "ErrorCallBack": errorCallBack};
     if (IsObjectUndefined(configuration.PRPortNumber)) {
-      onError(null, callbacks)
-      return
+      onError(null, callbacks);
+      return;
     }
-    _ajax.ajaxCall(configuration.GetPortNumberURL.replace("{0}", configuration.PRPortNumber), configuration.Method, true, false, parseJSON, onError, callbacks)
+    _ajax.ajaxCall(configuration.GetPortNumberURL.replace("{0}", configuration.PRPortNumber), configuration.Method, true, false, parseJSON, onError, callbacks);
   }
 
   //Parses the JSON data received from the PR Service.
   parseJSON = function (data, userArgs) {
     if (data.portData !== undefined) {
       configuration.PortNumber = data.portData.portNumber
-      callMethod(userArgs.SuccessCallBack, configuration.PortNumber, null)
-      return
+      callMethod(userArgs.SuccessCallBack, configuration.PortNumber, null);
+      return;
     }
-    onError(data, userArgs)
+    onError(data, userArgs);
   }
 
   //Invoked when there is an error reported by the library.
   onError = function (data, userArgs) {
     if(!IsObjectUndefined(data) && !IsObjectUndefined(data.FaultException)) {
-      callMethod(userArgs.ErrorCallBack, null, {"Code": data.FaultException.Code, "Message": data.FaultException.Message})
+      callMethod(userArgs.ErrorCallBack, null, {"Code": data.FaultException.Code, "Message": data.FaultException.Message});
       return
     }
-    callMethod(userArgs.ErrorCallBack, null, {"Code": configuration.ErrorCode, "Message": configuration.ErrorMessage})
+    callMethod(userArgs.ErrorCallBack, null, {"Code": configuration.ErrorCode, "Message": configuration.ErrorMessage});
   }
 
   function IsObjectUndefined(data) {
-    return data === undefined
+    return data === undefined;
   }
 
   this.initialize = function (successCallBack, errorCallBack) {
-    getPortNumber(successCallBack, errorCallBack)
+    getPortNumber(successCallBack, errorCallBack);
   }
 
   //Returns the port number on which the NHS Credential Management application is currently listening for the user.
   this.portNumber = function () {
     if (configuration.PortNumber === undefined) {
-      getPortNumber(null, null)
+      getPortNumber(null, null);
     }
-    return configuration.PortNumber
+    return configuration.PortNumber;
   }
 
   //Invokes the method based on the delegations.
   callMethod = function (func, data, userArgs) {
     if (!IsObjectUndefined(func)) {
-      func(data, userArgs)
+      func(data, userArgs);
     }
   }
 
@@ -126,10 +126,10 @@ var ajax = function () {
         },
         async: isAsyncCall,
         success: function (data) {
-          callMethod(successCallBack, data, userArgs)
+          callMethod(successCallBack, data, userArgs);
         },
         error: function (data) {
-          callMethod(errorCallBack, data, userArgs)
+          callMethod(errorCallBack, data, userArgs);
         }
       })
   }
@@ -137,6 +137,6 @@ var ajax = function () {
 
 jQuery(function() {
   if (prService !== undefined) {
-    prService.initialize(window.PRSuccessCallBack, window.errorCallBack)
+    prService.initialize(window.PRSuccessCallBack, window.errorCallBack);
   }
 })
