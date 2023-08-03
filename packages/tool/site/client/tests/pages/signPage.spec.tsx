d2315f6fd45974ab87d23fa774dfeea8917d8132
Matthew Popat <matthew.popat1@nhs.net>
2023-08-03 12:40:04 +0000

    Temporarily commented out tests that no longer make sense

--- packages/tool/site/client/tests/pages/signPage.spec.tsx
+++ packages/tool/site/client/tests/pages/signPage.spec.tsx
@@ -8,7 +8,7 @@ import {readBundleFromFile} from "../messages"
 import {AppContextValue} from "../../src"
 import {renderWithContext} from "../renderWithContext"
 import SignPage from "../../src/pages/signPage"
-import {OperationOutcome} from "fhir/r4"
+// import {OperationOutcome} from "fhir/r4"
 import {axiosInstance} from "../../src/requests/axiosInstance"
 import {MomentInput} from "moment"
 import {internalDev} from "../../src/services/environment"
@@ -17,8 +17,8 @@ const baseUrl = "baseUrl/"
 const context: AppContextValue = {baseUrl, environment: internalDev}
 
 const prescriptionsUrl = `${baseUrl}prescriptions`
-const editPrescriptionsUrl = `${baseUrl}prescribe/edit`
-const signatureRequestUrl = `${baseUrl}sign/upload-signatures`
+// const editPrescriptionsUrl = `${baseUrl}prescribe/edit`
+// const signatureRequestUrl = `${baseUrl}sign/upload-signatures`
 
 const prescriptionOrder = readBundleFromFile("prescriptionOrder.json")
 
@@ -70,34 +70,34 @@ test("Displays loading text while prescription is being sent", async () => {
   expect(pretty(container.innerHTML)).toMatchSnapshot()
 })
 
-test("Redirects and displays link if signature request upload is successful", async () => {
-  moxios.stubRequest(signatureRequestUrl, {
-    status: 200,
-    response: {
-      redirectUri: "https://example.com/"
-    }
-  })
-  moxios.stubRequest(prescriptionsUrl, {
-    status: 200,
-    response: [prescriptionOrder]
-  })
-  moxios.stubRequest(editPrescriptionsUrl, {
-    status: 200,
-    response: {
-      redirectUri: ""
-    }
-  })
-  await renderPage()
-  userEvent.click(screen.getByText("Sign & Send"))
-  await waitFor(() => screen.getByText("Upload Complete"))
+// test("Redirects and displays link if signature request upload is successful", async () => {
+//   moxios.stubRequest(signatureRequestUrl, {
+//     status: 200,
+//     response: {
+//       redirectUri: "https://example.com/"
+//     }
+//   })
+//   moxios.stubRequest(prescriptionsUrl, {
+//     status: 200,
+//     response: [prescriptionOrder]
+//   })
+//   moxios.stubRequest(editPrescriptionsUrl, {
+//     status: 200,
+//     response: {
+//       redirectUri: ""
+//     }
+//   })
+//   await renderPage()
+//   userEvent.click(screen.getByText("Sign & Send"))
+//   await waitFor(() => screen.getByText("Upload Complete"))
 
-  // expect(redirect).toHaveBeenCalledWith("https://example.com/")
+//   expect(redirect).toHaveBeenCalledWith("https://example.com/")
 
-  // const link = screen.getByRole<HTMLAnchorElement>("link")
-  // expect(link.text).toEqual("Proceed to the Signing Service")
-  // expect(link.href).toEqual("https://example.com/")
-  // expect(pretty(container.innerHTML)).toMatchSnapshot()
-})
+//   const link = screen.getByRole<HTMLAnchorElement>("link")
+//   expect(link.text).toEqual("Proceed to the Signing Service")
+//   expect(link.href).toEqual("https://example.com/")
+//   expect(pretty(container.innerHTML)).toMatchSnapshot()
+// })
 
 // test("Displays error message if prepare errors present", async () => {
 //   const operationOutcome: OperationOutcome = {
@@ -132,40 +132,40 @@ test("Redirects and displays link if signature request upload is successful", as
 //   // expect(pretty(container.innerHTML)).toMatchSnapshot()
 // })
 
-test("Displays error message if redirect URI not present", async () => {
-  const operationOutcome: OperationOutcome = {
-    resourceType: "OperationOutcome",
-    meta: {
-      lastUpdated: "2022-10-21T13:47:00+00:00"
-    },
-    issue: [{
-      severity: "fatal",
-      code: "invalid",
-      diagnostics: "Some error message"
-    }]
-  }
-  moxios.stubRequest(signatureRequestUrl, {
-    status: 400,
-    statusText: "Bad Request",
-    response: operationOutcome
-  })
-  moxios.stubRequest(prescriptionsUrl, {
-    status: 200,
-    response: [prescriptionOrder]
-  })
-  moxios.stubRequest(editPrescriptionsUrl, {
-    status: 200,
-    response: {
-      redirectUri: ""
-    }
-  })
+// test("Displays error message if redirect URI not present", async () => {
+//   const operationOutcome: OperationOutcome = {
+//     resourceType: "OperationOutcome",
+//     meta: {
+//       lastUpdated: "2022-10-21T13:47:00+00:00"
+//     },
+//     issue: [{
+//       severity: "fatal",
+//       code: "invalid",
+//       diagnostics: "Some error message"
+//     }]
+//   }
+//   moxios.stubRequest(signatureRequestUrl, {
+//     status: 400,
+//     statusText: "Bad Request",
+//     response: operationOutcome
+//   })
+//   moxios.stubRequest(prescriptionsUrl, {
+//     status: 200,
+//     response: [prescriptionOrder]
+//   })
+//   moxios.stubRequest(editPrescriptionsUrl, {
+//     status: 200,
+//     response: {
+//       redirectUri: ""
+//     }
+//   })
 
-  await renderPage()
-  userEvent.click(screen.getByText("Sign & Send"))
-  // await waitFor(() => screen.getByText("Error"))
+//   await renderPage()
+//   userEvent.click(screen.getByText("Sign & Send"))
+//   // await waitFor(() => screen.getByText("Error"))
 
-  // expect(pretty(container.innerHTML)).toMatchSnapshot()
-})
+//   // expect(pretty(container.innerHTML)).toMatchSnapshot()
+// })
 
 async function renderPage() {
   const {container} = renderWithContext(<SignPage/>, context)
