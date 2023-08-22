b92a6d014ed4af0a6d5a02c4cd7a91ad59df4069
Anthony Brown <121869075+anthony-nhs@users.noreply.github.com>
2023-08-22 18:29:17 +0100

    correct date

--- packages/bdd-tests/util/helper.ts
+++ packages/bdd-tests/util/helper.ts
@@ -41,7 +41,7 @@ export async function preparePrescription(number, site, medReqNo = 1, table: Dat
 
   for (let i = 0; i < number; i++) {
     const now = new Date()
-    const later = now
+    const later = new Date()
     later.setMonth(later.getMonth() + 3)
     const authoredOn = now.toISOString()
     const validStart = `${now.getFullYear()}-${("0" + (now.getMonth() + 1)).slice(-2)}-${("0" + now.getDate()).slice(-2)}`
