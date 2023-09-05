35951be7c66c9f5baa7beb1eaaca82cc18bf0f52
MatthewPopat-NHS <matthew.popat1@nhs.net>
2023-09-05 12:15:44 +0100

    AEA-3204 Switched to new mock auth

--- packages/tool/site/client/src/pages/loginPage.tsx
+++ packages/tool/site/client/src/pages/loginPage.tsx
@@ -6,38 +6,44 @@ import ButtonList from "../components/common/buttonList"
 import {redirect} from "../browser/navigation"
 import {isInternalDev, isInternalDevSandbox, isInt, isQa} from "../services/environment"
 
-const LoginPage: React.FC <{separateAuth?:string}> = ({separateAuth}) => {
+const LoginPage: React.FC<{separateAuth?: string}> = ({separateAuth}) => {
   const {baseUrl, environment} = useContext(AppContext)
 
-  const [combinedAuthSelected, setCombinedAuthSelected] = useState(false)
+  const [mockAuthSelected, setMockAuthSelected] = useState(false)
 
   if (isInt(environment)) {
     makeLoginRequest(baseUrl, separateAuth ? "user-separate" : "user-combined")
-    return <>
-      <Label isPageHeading>Login</Label>
-      <Label>Redirecting to auth...</Label>
-    </>
+    return (
+      <>
+        <Label isPageHeading>Login</Label>
+        <Label>Redirecting to auth...</Label>
+      </>
+    )
   }
 
-  if (combinedAuthSelected) {
+  if (mockAuthSelected) {
     if (isInternalDev(environment) || isInternalDevSandbox(environment) || isQa(environment)) {
-      makeLoginRequest(baseUrl, "user-combined")
-      return <>
-        <Label isPageHeading>Login</Label>
-        <Label>Redirecting to simulated auth...</Label>
-      </>
+      makeLoginRequest(baseUrl, "user-mock")
+      return (
+        <>
+          <Label isPageHeading>Login</Label>
+          <Label>Redirecting to simulated auth...</Label>
+        </>
+      )
     }
   }
 
-  return <>
-    <Label isPageHeading>Login</Label>
-    <Label>Select access level:</Label>
-    <ButtonList>
-      <Button onClick={() => setCombinedAuthSelected(true)}>User - Combined Auth</Button>
-      <Button onClick={() => makeLoginRequest(baseUrl, "user-separate")}>User - Separate Auth</Button>
-      <Button onClick={() => makeLoginRequest(baseUrl, "system")}>System</Button>
-    </ButtonList>
-  </>
+  return (
+    <>
+      <Label isPageHeading>Login</Label>
+      {isInternalDev(environment) || isInternalDevSandbox(environment) || isQa(environment) ? <Label>When prompted enter the following User Id: 9449304130</Label> : <></>}
+      <Label>Select access level:</Label>
+      <ButtonList>
+        <Button onClick={() => setMockAuthSelected(true)}>User - Mock</Button>
+        <Button onClick={() => makeLoginRequest(baseUrl, "system")}>System</Button>
+      </ButtonList>
+    </>
+  )
 }
 
 interface AuthResponse {
@@ -45,10 +51,7 @@ interface AuthResponse {
 }
 
 const makeLoginRequest = async (baseUrl: string, authLevel: string) => {
-  const response = await axiosInstance.post<AuthResponse>(
-    `${baseUrl}login`,
-    {authLevel}
-  )
+  const response = await axiosInstance.post<AuthResponse>(`${baseUrl}login`, {authLevel})
   redirect(`${response.data.redirectUri}`)
 }
 
