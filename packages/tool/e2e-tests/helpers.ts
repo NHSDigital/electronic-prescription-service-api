e45b7acb09941d6d94d36df2e1c8b6faba8bbd96
Anthony Brown <121869075+anthony-nhs@users.noreply.github.com>
2023-09-27 12:48:16 +0000

    fix epsat login in tests

--- packages/tool/e2e-tests/helpers.ts
+++ packages/tool/e2e-tests/helpers.ts
@@ -60,7 +60,7 @@ export const FIREFOX_BINARY_PATH = process.env.FIREFOX_BINARY_PATH || "/usr/bin/
 
 export const SERVICE_BASE_PATH = process.env.SERVICE_BASE_PATH || "eps-api-tool"
 export const APIGEE_ENVIRONMENT = process.env.APIGEE_ENVIRONMENT || "internal-dev"
-export const EPSAT_HOME_URL = `https://${APIGEE_ENVIRONMENT}.api.service.nhs.uk/${SERVICE_BASE_PATH}`
+export const EPSAT_HOME_URL = `https://${APIGEE_ENVIRONMENT}.api.service.nhs.uk/${SERVICE_BASE_PATH}/`
 
 export const defaultWaitTimeout = 1500
 export const twoTimesDefaultWaitTimeout = defaultWaitTimeout * 2
