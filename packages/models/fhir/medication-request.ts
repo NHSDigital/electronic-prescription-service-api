aace6c80418eb0530bd6d4dd22fb9540d526fb26
Tom Smith <69810386+TomSmithCoding@users.noreply.github.com>
2023-10-23 10:33:21 +0100

    AEA-3379-AuthoredOn-DateTime-Issue (#1599)
    ## Summary
    
    **Remove items from this list if they are not relevant. Remove this line
    once this has been done**
    
    - Routine Change
    - :exclamation: Breaking Change
    - :robot: Operational or Infrastructure Change
    - :sparkles: New Feature
    - :warning: Potential issues that might be caused by this change
    
    ### Details
    
    Add any summary information of what is in the change. **Remove this line
    if you have nothing to add.**
    
    ## Reviews Required
    
    **Check who should review this. Remove this line once this has been
    done**
    
    - [x] Dev
    - [ ] Test
    - [ ] Tech Author
    - [ ] Product Owner
    
    ## Review Checklist
    
    :information_source: This section is to be filled in by the
    **reviewer**.
    
    - [ ] I have reviewed the changes in this PR and they fill all or part
    of the acceptance criteria of the ticket, and the code is in a mergeable
    state.
    - [ ] If there were infrastructure, operational, or build changes, I
    have made sure there is sufficient evidence that the changes will work.
    - [ ] I have ensured the jira ticket has been updated with the github
    pull request link

--- packages/models/fhir/medication-request.ts
+++ packages/models/fhir/medication-request.ts
@@ -54,7 +54,7 @@ export interface BaseMedicationRequest extends common.Resource {
   medicationCodeableConcept?: common.CodeableConcept
   medicationReference?: common.Reference<medication.Medication>
   subject: common.Reference<patient.Patient>
-  authoredOn: string
+  authoredOn?: string
   requester: common.Reference<practitionerRole.PractitionerRole>
   groupIdentifier: MedicationRequestGroupIdentifier
   note?: Array<Annotation>
