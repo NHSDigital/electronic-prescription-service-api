1d8d41e4dfc795f0de3d707927d3a89f03d64ece
natasafrgk <natasa.fragkou@airelogic.com>
2024-01-17 17:17:55 +0000

    looks for job status

--- scripts/run_regression_tests.py
+++ scripts/run_regression_tests.py
@@ -29,11 +29,11 @@ parser.add_argument(
 argument = parser.parse_args()
 
 user_credentials = argument.user.split(":")
-print(user_credentials)
+# print(user_credentials)
 authHeader = HTTPBasicAuth(user_credentials[0], user_credentials[1])
-print(f"hello Header: {authHeader}")
+# print(f"hello Header: {authHeader}")
 
-run_identifier = ''.join(random.choices(string.ascii_uppercase + string.digits, k=15))
+run_identifier = "".join(random.choices(string.ascii_uppercase + string.digits, k=15))
 # filter runs that were created after this date minus 5 minutes
 delta_time = datetime.timedelta(minutes=5)
 run_date_filter = (datetime.datetime.utcnow() - delta_time).strftime("%Y-%m-%dT%H:%M")
@@ -45,7 +45,7 @@ body = {
         "environment": argument.env,
     },
 }
-print(f"Hello body: {body}")
+# print(f"Hello body: {body}")
 
 request = requests.post(
     url="https://api.github.com/repos/NHSDigital/electronic-prescription-service-api-regression-tests/actions/workflows/regression_tests.yml/dispatches",
@@ -87,11 +87,31 @@ while workflow_id == "":
                 job = jobs[0]
                 steps = job["steps"]
                 if len(steps) >= 2:
-                    second_step = steps[
-                        1
+                    third_step = steps[
+                        2
                     ]  # if you have position the run_identifier step at 1st position
-                    if second_step["name"] == run_identifier:
+                    if third_step["name"] == run_identifier:
                         workflow_id = job["run_id"]
+
+                        if workflow_id:
+                            request = requests.get(
+                                f"https://api.github.com/repos/NHSDigital/electronic-prescription-service-api-regression-tests/actions/runs/{workflow_id}/jobs",
+                                headers={
+                                    "Accept": "application/vnd.github+json",
+                                    "X-GitHub-Api-Version": "2022-11-28",
+                                },
+                                auth=authHeader,
+                            )
+                            final_job = request.json()["jobs"][0]
+                            print(final_job)
+                            if final_job["status"] == "completed":
+                                if final_job["conclusion"] == "succeeded":
+                                    print("PASSED")
+                                if final_job["conclusion"] == "failed":
+                                    print("FAILED")
+                            else:
+                                print("sorry It's been pending")
+
                 else:
                     print("waiting for steps to be executed...")
                     time.sleep(3)
