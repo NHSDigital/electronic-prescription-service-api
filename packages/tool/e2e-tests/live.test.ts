99c991d2a653247f7405e6248749c977effe6f55
Anthony Brown <121869075+anthony-nhs@users.noreply.github.com>
2024-01-24 14:14:28 +0000

    use new headless mode for epsat tests

--- packages/tool/e2e-tests/live.test.ts
+++ packages/tool/e2e-tests/live.test.ts
@@ -88,7 +88,7 @@ function buildFirefoxOptions() {
     firefoxOptions.setBinary(FIREFOX_BINARY_PATH)
   }
   if (!LOCAL_MODE) {
-    firefoxOptions.headless()
+    firefoxOptions.addArguments("--headless=new")
   }
   return firefoxOptions
 }
