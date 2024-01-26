861d229e7a56620b22b9981807a5d330d7c52ba5
SeanSteberis <sean.steberis@airelogic.com>
2024-01-26 12:10:25 +0000

    Changed --env=DEV to --env=INTERNAL-DEV in run_tests.yml

--- scripts/generate_prescription_ids.py
+++ scripts/generate_prescription_ids.py
@@ -8,14 +8,16 @@ def short_presc_id():
     Build the prescription ID and add the required checkdigit.
     Checkdigit is selected from the PRESCRIPTION_CHECKDIGIT_VALUES constant
     """
-    _PRESC_CHECKDIGIT_VALUES = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ+'
-    hexString = str(uuid.uuid1()).replace('-', '').upper()
-    prescriptionID = hexString[:6] + '-Z' + hexString[6:11] + '-' + hexString[12:17]
-    prscID = prescriptionID.replace('-', '')
+    _PRESC_CHECKDIGIT_VALUES = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ+"
+    hexString = str(uuid.uuid1()).replace("-", "").upper()
+    prescriptionID = hexString[:6] + "-Z" + hexString[6:11] + "-" + hexString[12:17]
+    prscID = prescriptionID.replace("-", "")
     prscIDLength = len(prscID)
     runningTotal = 0
     for stringPosition in range(prscIDLength):
-        runningTotal = runningTotal + int(prscID[stringPosition], 36) * (2 ** (prscIDLength - stringPosition))
+        runningTotal = runningTotal + int(prscID[stringPosition], 36) * (
+            2 ** (prscIDLength - stringPosition)
+        )
     checkValue = (38 - runningTotal % 37) % 37
     checkValue = _PRESC_CHECKDIGIT_VALUES[checkValue]
     prescriptionID += checkValue
