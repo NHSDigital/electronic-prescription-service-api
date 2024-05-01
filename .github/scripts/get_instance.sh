a38c852e65f5f68d0fcea80cb763768134eda2ba
Jack Spagnoli <jack.spagnoli1@nhs.net>
2024-05-01 10:11:37 +0000

    updates instance names

--- .github/scripts/get_instance.sh
+++ .github/scripts/get_instance.sh
@@ -10,14 +10,15 @@ echo "Stack name: $stack_name"
 printf "\n\n------------------------------------------------------------\n\n"
 
 # Determine the proxy instance based on the provided $stack_name
-if [[ $stack_name == psu-pr-* ]]; then
+if [[ $stack_name == electronic-prescription-service-api-pr-* ]]; then
     echo "Detected PR stack name"
     # Extracting the PR ID from $stack_name
-    pr_id=$(echo "$stack_name" | cut -d'-' -f3)
-    instance=prescription-status-update-pr-$pr_id
+    pr_id=$(echo "$stack_name" | cut -d'-' -f6)
+    # Use proxygen suffix for parallel deployment. Remove when switching to proxygen
+    instance=electronic-prescription-service-api-pr-$pr_id-proxygen
 else
     echo "Detected non-PR stack name"
-    instance=prescription-status-update
+    instance=electronic-prescription-service-api
 fi
 
 printf "\n\n------------------------------------------------------------\n\n"
