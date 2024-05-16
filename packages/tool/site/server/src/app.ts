ea832dce1cdf27f04cbfed931d98c35944527414
Andrew Rae <andrew.rae5@nhs.net>
2024-05-16 14:46:52 +0000

    modified logs

--- packages/tool/site/server/src/app.ts
+++ packages/tool/site/server/src/app.ts
@@ -220,8 +220,8 @@ function addViewRoutes(server: Hapi.Server) {
         if (prRedirect) {
           // const parsedRequest = request.payload as {signatureToken: string, state?: string}
           console.log("pr redirect for path :" + path)
-          console.log("request query: ")
-          console.dir(request.query)
+          console.log("this is what request is: ")
+          console.dir(request)
           const anothertest = request.query.state ? true : false
           console.log("is there a  state? " + anothertest)
           if(request.query.state) {
