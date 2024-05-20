f0865d7647ac1bc6e86da2804cf872775ca61dc9
Anthony Brown <121869075+anthony-nhs@users.noreply.github.com>
2024-05-20 08:05:32 +0000

    fix amend

--- packages/tool/e2e-tests/utils/prescriptionIds.ts
+++ packages/tool/e2e-tests/utils/prescriptionIds.ts
@@ -1,4 +1,4 @@
-import {ThenableWebDriver, until} from "selenium-webdriver"
+import {By, ThenableWebDriver, until} from "selenium-webdriver"
 import {fiveTimesDefaultWaitTimeout, getElement} from "../helpers"
 import {
   checkFirstReleasedPrescriptionStatusButton,
@@ -13,21 +13,24 @@ export async function getPrescriptionItemIds(
 ): Promise<string[]> {
   (await getElement(driver, myPrescriptionsNavLink)).click()
 
+  // wait 10 seconds for click to register
+  await new Promise(r => setTimeout(r, 10000));
+
   await driver.wait(
     until.elementsLocated(checkFirstReleasedPrescriptionStatusButton),
     fiveTimesDefaultWaitTimeout
   );
 
   (await getElement(driver, checkFirstReleasedPrescriptionStatusButton)).click()
-
   await driver.wait(
     until.elementsLocated(dispensePrescriptionAction),
     fiveTimesDefaultWaitTimeout
   )
+  // wait 10 seconds for page to refresh
+  await new Promise(r => setTimeout(r, 10000))
 
   const idElements = await driver.findElements(prescriptionLineItemIds)
   const idPromises = idElements.map(element => element.getText());
-
   (await getElement(driver, dispensePrescriptionAction)).click()
 
   await driver.wait(
