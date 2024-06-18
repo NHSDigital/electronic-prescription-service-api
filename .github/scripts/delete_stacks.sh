8b8bbca41b96aa6949abb341b60b7ed85b5974f5
Phil Gee <originalphil@gmail.com>
2024-06-18 13:56:08 +0100

    AEA-3922 Using Ant's newer script.

--- .github/scripts/delete_stacks.sh
+++ .github/scripts/delete_stacks.sh
@@ -1,17 +1,35 @@
 #!/usr/bin/env bash
 
-delete_stacks () {
-  ACTIVE_STACKS="$1"
+# script for removing cloudformation stacks where the pull request is closed - original (with proxygen functions) can be found in the eps-prescription-status-update-api repo
+
+# the name of the repo this is running in
+REPO_NAME=electronic-prescription-service-api
+
+# regex used in jq command that parses the output from aws cloudformation list-stacks and just captures stacks we are interested in
+CAPTURE_REGEX="^fhir-prescribing-and-dispensing-pr-(\\d+)(-sandbox)?$"
+
+# regex that is used to get the pull request id from the cloud formation stack name
+# this is used in a replace command to replace the stack name so what is left is just the pull request id
+PULL_REQUEST_STACK_REGEX=fhir-prescribing-and-dispensing-pr-
+
+main() {
+  delete_cloudformation_stacks
+}
+
+delete_cloudformation_stacks() {
+  echo "checking cloudformation stacks"
+  echo
+  ACTIVE_STACKS=$(aws cloudformation list-stacks | jq -r --arg CAPTURE_REGEX "${CAPTURE_REGEX}" '.StackSummaries[] | select ( .StackStatus != "DELETE_COMPLETE" ) | select( .StackName | capture($CAPTURE_REGEX) ) | .StackName ')
+
   mapfile -t ACTIVE_STACKS_ARRAY <<< "$ACTIVE_STACKS"
+
   for i in "${ACTIVE_STACKS_ARRAY[@]}"
   do 
     echo "Checking if stack $i has open pull request"
-    PULL_REQUEST=${i//pfp-pr-/}
-    PULL_REQUEST=${PULL_REQUEST//pr-}
-    PULL_REQUEST=${PULL_REQUEST//sandbox-/}
+    PULL_REQUEST=${i//${PULL_REQUEST_STACK_REGEX}/}
     PULL_REQUEST=${PULL_REQUEST//-sandbox/}
     echo "Checking pull request id ${PULL_REQUEST}"
-    URL="https://api.github.com/repos/NHSDigital/electronic-prescription-service-api/pulls/${PULL_REQUEST}"
+    URL="https://api.github.com/repos/NHSDigital/${REPO_NAME}/pulls/${PULL_REQUEST}"
     RESPONSE=$(curl "${URL}" 2>/dev/null)
     STATE=$(echo "${RESPONSE}" | jq -r .state)
     if [ "$STATE" == "closed" ]; then
@@ -25,7 +43,4 @@ delete_stacks () {
   done
 }
 
-
-ACTIVE_STACKS=$(aws cloudformation list-stacks | jq -r '.StackSummaries[] | select ( .StackStatus != "DELETE_COMPLETE" ) | select( .StackName | capture("^fhir-prescribing-and-dispensing-pr-(\\d+)(-sandbox)?$") ) | .StackName ')
-
-delete_stacks "${ACTIVE_STACKS}"
+main
