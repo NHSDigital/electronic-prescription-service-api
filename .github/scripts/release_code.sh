f7bac85e601d61b37635fe04c00b4aac8121e7f1
Kris Szlapa <kris.szlapa1@nhs.net>
2024-08-12 18:29:18 +0000

    Remove VPC and subnet env variables from the script and Makefile

--- .github/scripts/release_code.sh
+++ .github/scripts/release_code.sh
@@ -7,7 +7,7 @@ artifact_bucket=$(aws cloudformation list-exports --output json | jq -r '.Export
 export artifact_bucket
 
 # Fetch the CloudFormation execution role
-cloud_formation_execution_role=$(aws cloudformation list-exports --output json | jq -r '.Exports[] | select(.Name == "ci-resources:CloudFormationExecutionRole") | .Value' )
+cloud_formation_execution_role=$(aws cloudformation list-exports --output json | jq -r '.Exports[] | select(.Name == "ci-resources:CloudFormationExecutionRole") | .Value')
 export cloud_formation_execution_role
 
 # Fetch trust store details
@@ -24,42 +24,9 @@ if [ -z "${ZONE_ID_EXPORT}" ]; then
   export ZONE_ID_EXPORT="NOT_SET"
 fi
 
-# Fetch the VPC and subnets
-VPC=$(aws cloudformation list-exports --output json | jq -r '.Exports[] | select(.Name == "DefaultVPCId") | .Value')
-if [ -z "${VPC}" ]; then
-  echo "Environment variable VPC not set"
-  exit 1
-fi
-export VPC
-
-SUBNET_A=$(aws cloudformation list-exports --output json | jq -r '.Exports[] | select(.Name == "vpc-resources:PrivateSubnetA") | .Value')
-if [ -z "${SUBNET_A}" ]; then
-  echo "Environment variable SUBNET_A not set"
-  exit 1
-fi
-export SUBNET_A
-
-SUBNET_B=$(aws cloudformation list-exports --output json | jq -r '.Exports[] | select(.Name == "vpc-resources:PrivateSubnetB") | .Value')
-if [ -z "${SUBNET_B}" ]; then
-  echo "Environment variable SUBNET_B not set"
-  exit 1
-fi
-export SUBNET_B
-
-SUBNET_C=$(aws cloudformation list-exports --output json | jq -r '.Exports[] | select(.Name == "vpc-resources:PrivateSubnetC") | .Value')
-if [ -z "${SUBNET_C}" ]; then
-  echo "Environment variable SUBNET_C not set"
-  exit 1
-fi
-export SUBNET_C
-
 # Debugging: Print the values for verification
 echo "DOMAIN_NAME_EXPORT: $DOMAIN_NAME_EXPORT"
 echo "ZONE_ID_EXPORT: $ZONE_ID_EXPORT"
-echo "VPC: $VPC"
-echo "SUBNET_A: $SUBNET_A"
-echo "SUBNET_B: $SUBNET_B"
-echo "SUBNET_C: $SUBNET_C"
 
 # Change directory and invoke the make command
 cd ../../.aws-sam/build || exit
