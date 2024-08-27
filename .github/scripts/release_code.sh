fa4c159809232a7947dc9c5768c2bd7e984a06f7
Kris Szlapa <kris.szlapa1@nhs.net>
2024-08-27 11:36:38 +0000

    Remove code from the script to fetch the IP address of the validator container

--- .github/scripts/release_code.sh
+++ .github/scripts/release_code.sh
@@ -1,14 +1,6 @@
 #!/usr/bin/env bash
 
-set -euo pipefail
-
 echo "$COMMIT_ID"
-
-# Ensure stack_name is set
-if [ -z "${stack_name:-}" ]; then
-  echo "Error: stack_name is not set."
-  exit 1
-fi
 
 # Fetch the artifact bucket
 artifact_bucket=$(aws cloudformation list-exports --output json | jq -r '.Exports[] | select(.Name == "account-resources:ArtifactsBucket") | .Value' | grep -o '[^:]*$')
@@ -37,40 +29,6 @@ echo "DOMAIN_NAME_EXPORT: $DOMAIN_NAME_EXPORT"
 echo "ZONE_ID_EXPORT: $ZONE_ID_EXPORT"
 echo "ECR_REPOSITORY: $ECR_REPOSITORY"
 echo "IMAGE_TAG: $IMAGE_TAG"
-
-# Fetch the ECS cluster name from CloudFormation outputs
-ECS_CLUSTER_NAME=$(aws cloudformation describe-stacks --stack-name "$stack_name" --query "Stacks[0].Outputs[?OutputKey=='ECSClusterName'].OutputValue" --output text)
-
-# List ECS services in the cluster and find the ECS service name
-ECS_SERVICE_NAME=$(aws ecs list-services --cluster "$ECS_CLUSTER_NAME" --query "serviceArns[?contains(@, 'ECSService')]" --output text | awk -F'/' '{print $2}')
-
-# Ensure we have the ECS service name
-if [ -z "$ECS_SERVICE_NAME" ]; then
-  echo "ECS service name could not be found."
-  exit 1
-fi
-
-echo "ECS Service Name: $ECS_SERVICE_NAME"
-
-# List ECS tasks in the service
-TASK_ARNS=$(aws ecs list-tasks --cluster "$ECS_CLUSTER_NAME" --service-name "$ECS_SERVICE_NAME" --query "taskArns" --output text)
-
-# Describe tasks and fetch the IP address of the validator container
-VALIDATOR_IP=""
-for TASK_ARN in $TASK_ARNS; do
-  TASK_DETAILS=$(aws ecs describe-tasks --cluster "$ECS_CLUSTER_NAME" --tasks "$TASK_ARN" --query "tasks[0].containers[?name=='${stack_name}-validator'].networkInterfaces[0].privateIpv4Address" --output text)
-  if [ -n "$TASK_DETAILS" ]; then
-    VALIDATOR_IP=$TASK_DETAILS
-    break
-  fi
-done
-
-if [ -n "$VALIDATOR_IP" ]; then
-  export VALIDATOR_IP
-  echo "Validator IP: $VALIDATOR_IP"
-else
-  echo "Validator IP not found."
-fi
 
 # Change directory and invoke the make command
 cd ../../.aws-sam/build || exit
