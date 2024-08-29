24d05b27226effef7cce43dd36ecefab4c336df4
anthony-nhs <121869075+anthony-nhs@users.noreply.github.com>
2024-08-29 13:56:01 +0100

    Fix: [AEA-4389] - delete old cname records (#2681)
    ## Summary
    
    - Routine Change
    
    ### Details
    
    - delete old CNAME records
    
    ---------
    
    Co-authored-by: Kris Szlapa <kris.szlapa1@nhs.net>

--- .github/scripts/delete_stacks.sh
+++ .github/scripts/delete_stacks.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# script for removing cloudformation stacks where the pull request is closed - original (with proxygen functions) can be found in the eps-prescription-status-update-api repo
+# generic script for removing cloudformation stacks and old CNAME records where the pull request is closed
 
 # the name of the repo this is running in
 REPO_NAME=electronic-prescription-service-api
@@ -12,8 +12,12 @@ CAPTURE_REGEX="^prescribe-dispense-pr-(\\d+)(-sandbox)?$"
 # this is used in a replace command to replace the stack name so what is left is just the pull request id
 PULL_REQUEST_STACK_REGEX=prescribe-dispense-pr-
 
+# this should be a query to get old CNAME records to delete
+CNAME_QUERY=prescribe-dispense-pr-
+
 main() {
   delete_cloudformation_stacks
+  delete_cname_records
 }
 
 delete_cloudformation_stacks() {
@@ -40,6 +44,40 @@ delete_cloudformation_stacks() {
     else
       echo "not going to delete stack $i as state is ${STATE}"
     fi
+  done
+}
+
+delete_cname_records() {
+  HOSTED_ZONE_ID=$(aws route53 list-hosted-zones-by-name --dns-name dev.eps.national.nhs.uk. | jq -r ".HostedZones[0] | .Id")
+  CNAME_RECORDS=$(aws route53 list-resource-record-sets --hosted-zone-id "${HOSTED_ZONE_ID}" \
+    --query "ResourceRecordSets[?Type == 'CNAME' && contains(Name, '${CNAME_QUERY}')]" \
+    | jq -r " .[] | .Name")
+
+  mapfile -t CNAME_RECORDS_ARRAY <<< "$CNAME_RECORDS"
+
+  for i in "${CNAME_RECORDS_ARRAY[@]}"
+  do
+    echo "Checking if CNAME record $i has open pull request"
+
+    PULL_REQUEST=$(echo "$i" | grep -Po '(?<=-pr-)\d+')
+    echo "Checking pull request id ${PULL_REQUEST}"
+    URL="https://api.github.com/repos/NHSDigital/${REPO_NAME}/pulls/${PULL_REQUEST}"
+    RESPONSE=$(curl --url "${URL}" --header "Authorization: Bearer ${GITHUB_TOKEN}" 2>/dev/null)
+    STATE=$(echo "${RESPONSE}" | jq -r .state)
+    if [ "$STATE" == "closed" ]; then
+      echo "** going to delete CNAME record $i as state is ${STATE} **"
+      record_set=$(aws route53 list-resource-record-sets --hosted-zone-id "${HOSTED_ZONE_ID}" \
+        --query "ResourceRecordSets[?Name == '$i']" --output json | jq .[0])
+
+      jq -n --argjson record_set "${record_set}" \
+          '{Changes: [{Action: "DELETE", ResourceRecordSet: $record_set}]}' > /tmp/payload.json
+
+      aws route53 change-resource-record-sets --hosted-zone-id "${HOSTED_ZONE_ID}" --change-batch file:///tmp/payload.json
+
+      echo "CNAME record $i deleted"
+      else
+        echo "not going to delete CNAME record $i as state is ${STATE} **"
+      fi
   done
 }
 
