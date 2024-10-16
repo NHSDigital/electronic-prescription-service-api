94b571a0b4fb490ec509b8931b39890f51018f9f
Kris Szlapa <kris.szlapa1@nhs.net>
2024-10-16 22:44:22 +0000

    Use hardcoded TARGET_SPINE_SERVER in SPINE_ENDPOINT

--- packages/coordinator/src/services/communication/live-spine-client.ts
+++ packages/coordinator/src/services/communication/live-spine-client.ts
@@ -6,10 +6,7 @@ import {addEbXmlWrapper} from "./ebxml-request-builder"
 import {SpineClient} from "./spine-client"
 
 const SPINE_URL_SCHEME = "https"
-const SPINE_ENDPOINT = process.env.SPINE_URL
-const SPINE_ENDPOINT_TARGET_SPINE_SERVER = process.env.TARGET_SPINE_SERVER
-console.log("SPINE_ENDPOINT:", SPINE_ENDPOINT)
-console.log("SPINE_ENDPOINT_TARGET_SPINE_SERVER:", SPINE_ENDPOINT_TARGET_SPINE_SERVER)
+const SPINE_ENDPOINT = process.env.TARGET_SPINE_SERVER
 const SPINE_PATH = "Prescription"
 const BASE_PATH = process.env.BASE_PATH
 
