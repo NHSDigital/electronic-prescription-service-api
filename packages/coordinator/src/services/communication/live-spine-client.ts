71bf669be5c8dbafed66c0550efe13e53d7252e0
Kris Szlapa <kris.szlapa1@nhs.net>
2024-10-16 15:49:06 +0000

    Debug SPINE_URL value

--- packages/coordinator/src/services/communication/live-spine-client.ts
+++ packages/coordinator/src/services/communication/live-spine-client.ts
@@ -7,7 +7,9 @@ import {SpineClient} from "./spine-client"
 
 const SPINE_URL_SCHEME = "https"
 const SPINE_ENDPOINT = process.env.SPINE_URL
-console.log("SPINE_ENDPOINT_SPINE_URL:", SPINE_ENDPOINT)
+const SPINE_ENDPOINT_TEST = process.env.SPINE_URL_TEST
+console.log("SPINE_ENDPOINT:", SPINE_ENDPOINT)
+console.log("SPINE_ENDPOINT_TEST:", SPINE_ENDPOINT_TEST)
 const SPINE_PATH = "Prescription"
 const BASE_PATH = process.env.BASE_PATH
 
