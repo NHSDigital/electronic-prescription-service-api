6fbff7d29e023320643fd1e5b0db81827d4b7b66
Kris Szlapa <kris.szlapa1@nhs.net>
2024-10-30 02:59:28 +0000

    Add a step and script to deploy fhir-prescribing API

---
+++ .github/scripts/deploy_api.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -eu pipefail
+
+echo "FHIR Prescribing specification path: ${SPEC_PATH_PRESCRIBING}"
+echo "FHIR Dispensing specification path: ${SPEC_PATH_DISPENSING}"
+echo "Specification version: ${VERSION_NUMBER}"
+echo "Stack name: ${STACK_NAME}"
+echo "AWS environment: ${AWS_ENVIRONMENT}"
+echo "Apigee environment: ${APIGEE_ENVIRONMENT}"
+echo "Proxygen private key name: ${PROXYGEN_PRIVATE_KEY_NAME}"
+echo "Proxygen KID: ${PROXYGEN_KID}"
+echo "Deploy Check Prescription Status Update: ${DEPLOY_CHECK_PRESCRIPTION_STATUS_UPDATE}"
+echo "Dry run: ${DRY_RUN}"
