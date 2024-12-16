ab852fd259a8d7cc4fb11882c305581f2570e7e0
Anthony Brown <121869075+anthony-nhs@users.noreply.github.com>
2024-12-16 14:38:43 +0000

    run sha1 tests

--- scripts/run_regression_tests.py
+++ scripts/run_regression_tests.py
@@ -55,7 +55,8 @@ def trigger_test_run(env, pr_label, product, auth_header):
             "environment": env,
             "pull_request_id": pr_label,
             "product": product,
-            "github_tag": REGRESSION_TESTS_REPO_TAG
+            "github_tag": REGRESSION_TESTS_REPO_TAG,
+            "run_sha1_tests": "true"
         },
     }
 
