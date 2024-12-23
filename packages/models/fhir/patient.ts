e04cf3c6066c6921acb6066b083cf0709a4a7b2a
Jim Wild <james.wild6@nhs.net>
2024-12-23 15:39:36 +0000

    Use a default GP practice as V81999

--- packages/models/fhir/patient.ts
+++ packages/models/fhir/patient.ts
@@ -2,6 +2,18 @@ import * as demographics from "./demographics"
 import * as practitionerRole from "./practitioner-role"
 import * as common from "./common"
 
+const GP_PRACTICE_CODE_NOT_KNOWN = "V81999"
+
+function unknownGPPractice() {
+  const unknownGP: common.IdentifierReference<practitionerRole.Organization> = {
+    identifier: {
+      system: "http://hl7.org/fhir/sid/us-npi",
+      value: GP_PRACTICE_CODE_NOT_KNOWN
+    }
+  }
+  return unknownGP
+}
+
 export class Patient extends common.Resource {
   readonly resourceType = "Patient"
   identifier?: Array<PatientIdentifier>
@@ -10,7 +22,23 @@ export class Patient extends common.Resource {
   gender?: string
   birthDate?: string
   address?: Array<demographics.Address>
-  generalPractitioner?: Array<common.IdentifierReference<practitionerRole.Organization>>
+
+  private _generalPractitioner?: Array<common.IdentifierReference<practitionerRole.Organization>>
+
+  get generalPractitioner(): Array<common.IdentifierReference<practitionerRole.Organization>> {
+    if (!this._generalPractitioner) {
+      // Provide a default value if none is set
+      const defaultGP = unknownGPPractice()
+      this._generalPractitioner = [defaultGP]
+    }
+    return this._generalPractitioner
+  }
+
+  set generalPractitioner(
+    value: Array<common.IdentifierReference<practitionerRole.Organization>> | undefined
+  ) {
+    this._generalPractitioner = value
+  }
 }
 
 export type PatientIdentifier = common.Identifier
