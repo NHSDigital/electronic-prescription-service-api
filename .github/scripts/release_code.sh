4cd9327918c9d56ff8ba35f3cd0e0f403a408463
Anthony Brown <121869075+anthony-nhs@users.noreply.github.com>
2025-02-12 11:07:40 +0000

    fix names

--- .github/scripts/release_code.sh
+++ .github/scripts/release_code.sh
@@ -1,16 +1,19 @@
 #!/usr/bin/env bash
+set -e
 
-echo "$COMMIT_ID"
 
 # Fetch the artifact bucket
+echo "Getting artifact bucket"
 artifact_bucket=$(aws cloudformation list-exports --output json | jq -r '.Exports[] | select(.Name == "account-resources:ArtifactsBucket") | .Value' | grep -o '[^:]*$')
 export artifact_bucket
 
 # Fetch the CloudFormation execution role
+echo "Getting cloudformation execution role"
 cloud_formation_execution_role=$(aws cloudformation list-exports --output json | jq -r '.Exports[] | select(.Name == "ci-resources:CloudFormationExecutionRole") | .Value')
 export cloud_formation_execution_role
 
 # Fetch trust store details
+echo "Getting truststore details"
 TRUSTSTORE_BUCKET_ARN=$(aws cloudformation describe-stacks --stack-name account-resources --query "Stacks[0].Outputs[?OutputKey=='TrustStoreBucket'].OutputValue" --output text)
 TRUSTSTORE_BUCKET_NAME=$(echo "${TRUSTSTORE_BUCKET_ARN}" | cut -d ":" -f 6)
 LATEST_TRUSTSTORE_VERSION=$(aws s3api list-object-versions --bucket "${TRUSTSTORE_BUCKET_NAME}" --prefix "${TRUSTSTORE_FILE}" --query 'Versions[?IsLatest].[VersionId]' --output text)
@@ -36,28 +39,35 @@ echo "TARGET_SPINE_SERVER: $TARGET_SPINE_SERVER"
 cd ../../.aws-sam/build || exit
 
 # fix the deployment file
-sed -i "s/DEFAULT_SHA1_ENABLED_APPLICATION_IDS/${SHA1_ENABLED_APPLICATION_IDS}/g" samconfig_package_and_deploy.toml
-sed -i "s/DEFAULT_TRUSTSTORE_BUCKET_NAME/${TRUSTSTORE_BUCKET_NAME}/g" samconfig_package_and_deploy.toml
-sed -i "s/DEFAULT_LATEST_TRUSTSTORE_VERSION/${LATEST_TRUSTSTORE_VERSION}/g" samconfig_package_and_deploy.toml
-sed -i "s/DEFAULT_TRUSTSTORE_FILE/${TRUSTSTORE_FILE}/g" samconfig_package_and_deploy.toml
-sed -i "s/default_enable_mutual_tls/${enable_mutual_tls}/g" samconfig_package_and_deploy.toml
-sed -i "s/DEFAULT_VERSION_NUMBER/${VERSION_NUMBER}/g" samconfig_package_and_deploy.toml
-sed -i "s/DEFAULT_COMMIT_ID/${COMMIT_ID}/g" samconfig_package_and_deploy.toml
-sed -i "s/DEFAULT_LOG_LEVEL/${LOG_LEVEL}/g" samconfig_package_and_deploy.toml
-sed -i "s/DEFAULT_VALIDATOR_LOG_LEVEL/${VALIDATOR_LOG_LEVEL}/g" samconfig_package_and_deploy.toml
-sed -i "s/DEFAULT_LOG_RETENTION_DAYS/${LOG_RETENTION_DAYS}/g" samconfig_package_and_deploy.toml
-sed -i "s/DEFAULT_TARGET_ENVIRONMENT/${TARGET_ENVIRONMENT}/g" samconfig_package_and_deploy.toml
-sed -i "s/DEFAULT_DOMAIN_NAME_EXPORT/${DOMAIN_NAME_EXPORT}/g" samconfig_package_and_deploy.toml
-sed -i "s/DEFAULT_ZONE_ID_EXPORT/${ZONE_ID_EXPORT}/g" samconfig_package_and_deploy.toml
-sed -i "s/DEFAULT_TARGET_SPINE_SERVER/${TARGET_SPINE_SERVER}/g" samconfig_package_and_deploy.toml
-sed -i "s/DEFAULT_DOCKER_IMAGE_TAG/${DOCKER_IMAGE_TAG}/g" samconfig_package_and_deploy.toml
-sed -i "s/DEFAULT_TO_ASID/${TO_ASID}/g" samconfig_package_and_deploy.toml
-sed -i "s/DEFAULT_TO_PARTY_KEY/${TO_PARTY_KEY}/g" samconfig_package_and_deploy.toml
-sed -i "s/DEFAULT_ENABLE_DEFAULT_ASID_PARTY_KEY/${ENABLE_DEFAULT_ASID_PARTY_KEY}/g" samconfig_package_and_deploy.toml
-sed -i "s/DEFAULT_DEFAULT_PTL_ASID/${DEFAULT_PTL_ASID}/g" samconfig_package_and_deploy.toml
-sed -i "s/DEFAULT_DEFAULT_PTL_PARTY_KEY/${DEFAULT_PTL_PARTY_KEY}/g" samconfig_package_and_deploy.toml
+
+echo "Fixing samconfig_package_and_deploy file"
+echo 
+
+sed -i "s/CHANGE_SHA1_ENABLED_APPLICATION_IDS/${SHA1_ENABLED_APPLICATION_IDS}/g" samconfig_package_and_deploy.toml
+sed -i "s/CHANGE_TRUSTSTORE_BUCKET_NAME/${TRUSTSTORE_BUCKET_NAME}/g" samconfig_package_and_deploy.toml
+sed -i "s/CHANGE_LATEST_TRUSTSTORE_VERSION/${LATEST_TRUSTSTORE_VERSION}/g" samconfig_package_and_deploy.toml
+sed -i "s/CHANGE_TRUSTSTORE_FILE/${TRUSTSTORE_FILE}/g" samconfig_package_and_deploy.toml
+sed -i "s/CHANGE_enable_mutual_tls/${enable_mutual_tls}/g" samconfig_package_and_deploy.toml
+sed -i "s/CHANGE_VERSION_NUMBER/${VERSION_NUMBER}/g" samconfig_package_and_deploy.toml
+sed -i "s/CHANGE_COMMIT_ID/${COMMIT_ID}/g" samconfig_package_and_deploy.toml
+sed -i "s/CHANGE_LOG_LEVEL/${LOG_LEVEL}/g" samconfig_package_and_deploy.toml
+sed -i "s/CHANGE_VALIDATOR_LOG_LEVEL/${VALIDATOR_LOG_LEVEL}/g" samconfig_package_and_deploy.toml
+sed -i "s/CHANGE_LOG_RETENTION_DAYS/${LOG_RETENTION_DAYS}/g" samconfig_package_and_deploy.toml
+sed -i "s/CHANGE_TARGET_ENVIRONMENT/${TARGET_ENVIRONMENT}/g" samconfig_package_and_deploy.toml
+sed -i "s/CHANGE_DOMAIN_NAME_EXPORT/${DOMAIN_NAME_EXPORT}/g" samconfig_package_and_deploy.toml
+sed -i "s/CHANGE_ZONE_ID_EXPORT/${ZONE_ID_EXPORT}/g" samconfig_package_and_deploy.toml
+sed -i "s/CHANGE_TARGET_SPINE_SERVER/${TARGET_SPINE_SERVER}/g" samconfig_package_and_deploy.toml
+sed -i "s/CHANGE_DOCKER_IMAGE_TAG/${DOCKER_IMAGE_TAG}/g" samconfig_package_and_deploy.toml
+sed -i "s/CHANGE_TO_ASID/${TO_ASID}/g" samconfig_package_and_deploy.toml
+sed -i "s/CHANGE_TO_PARTY_KEY/${TO_PARTY_KEY}/g" samconfig_package_and_deploy.toml
+sed -i "s/CHANGE_ENABLE_DEFAULT_ASID_PARTY_KEY/${ENABLE_DEFAULT_ASID_PARTY_KEY}/g" samconfig_package_and_deploy.toml
+sed -i "s/CHANGE_DEFAULT_PTL_ASID/${DEFAULT_PTL_ASID}/g" samconfig_package_and_deploy.toml
+sed -i "s/CHANGE_DEFAULT_PTL_PARTY_KEY/${DEFAULT_PTL_PARTY_KEY}/g" samconfig_package_and_deploy.toml
 
 cat samconfig_package_and_deploy.toml
 
+echo
+echo "******************************************"
 echo "Starting deployment"
+echo "******************************************"
 make sam-deploy-package
