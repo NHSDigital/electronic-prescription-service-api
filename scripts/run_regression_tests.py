78ac582a767f59d413a6f188e4fc8efd7ed2130e
Anthony Brown <121869075+anthony-nhs@users.noreply.github.com>
2025-02-13 13:11:34 +0000

    use latest test pack

--- scripts/run_regression_tests.py
+++ scripts/run_regression_tests.py
@@ -13,7 +13,7 @@ import time
 from requests.auth import HTTPBasicAuth
 
 # This should be set to a known good version of regression test repo
-REGRESSION_TESTS_REPO_TAG = "re_enable_validator"
+REGRESSION_TESTS_REPO_TAG = "v2.11.15"
 
 GITHUB_API_URL = "https://api.github.com/repos/NHSDigital/electronic-prescription-service-api-regression-tests/actions"
 GITHUB_RUN_URL = "https://github.com/NHSDigital/electronic-prescription-service-api-regression-tests/actions/runs"
