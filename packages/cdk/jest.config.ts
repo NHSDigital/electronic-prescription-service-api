03b4268644cdfda681ede72f0f36ef0b80867d6e
Anthony Brown <121869075+anthony-nhs@users.noreply.github.com>
2025-02-17 08:53:07 +0000

    add cdk nag test

--- packages/cdk/jest.config.ts
+++ packages/cdk/jest.config.ts
@@ -1,8 +1,28 @@
 import type {JestConfigWithTsJest} from "ts-jest"
-import defaultConfig from "../../jest.default.config"
 
 const jestConfig: JestConfigWithTsJest = {
-  ...defaultConfig,
+  preset: "ts-jest/presets/default-esm",
+  moduleFileExtensions: ["js", "json", "ts", "d.ts", "jsx", "tsx"],
+  moduleNameMapper: {
+    "^(\\.{1,2}/.*)\\.js$": "$1"
+  },
+  transform: {
+    "^.+\\.ts?$": [
+      "ts-jest",
+      {
+        useESM: true,
+        tsconfig: "./tsconfig.json"
+      }
+    ]
+  },
+  clearMocks: true,
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coverageProvider: "v8",
+  testMatch: ["**/tests/**/*.test.ts", "**/__tests__/**/*.test.js?(x)", "**/__tests__/**/*.test.ts?(x)"],
+  testEnvironment: "node",
+  extensionsToTreatAsEsm: [".ts", ".tsx"],
+  verbose: true,
   "rootDir": "./"
 }
 
