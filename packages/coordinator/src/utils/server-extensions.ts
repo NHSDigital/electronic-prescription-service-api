afa69929c164a3824f273a0ce9bf29aeadba912b
Anthony Brown <121869075+anthony-nhs@users.noreply.github.com>
2025-02-19 13:40:37 +0000

    unit tests for logging

--- packages/coordinator/src/utils/server-extensions.ts
+++ packages/coordinator/src/utils/server-extensions.ts
@@ -10,36 +10,56 @@ export function reformatUserErrorsToFhir(
   request: Hapi.Request, responseToolkit: Hapi.ResponseToolkit
 ): Hapi.ResponseObject | symbol {
   const response = request.response
-  const logger = request.logger
   const logPayload = isEpsHostedContainer()
   if (response instanceof processingErrors.InconsistentValuesError) {
-    logger.info({
-      payload: logPayload ? request.payload.toString() : {}
-    }, "InconsistentValuesError")
+    // we do not log response here as we are sending back a different response
+    request.log("info", {
+      msg: "InconsistentValuesError",
+      payload: getPayload(request, logPayload)
+    })
     return responseToolkit.response(
       processingErrors.toOperationOutcomeError(response)
     ).code(400).type(ContentTypes.FHIR)
   } else if (response instanceof processingErrors.FhirMessageProcessingError) {
-    logger.info({
-      payload: logPayload ? request.payload.toString() : {}
-    }, "FhirMessageProcessingError")
+    // we do not log response here as we are sending back a different response
+    request.log("info", {
+      msg: "FhirMessageProcessingError",
+      payload: getPayload(request, logPayload)
+    })
     return responseToolkit.response(
       processingErrors.toOperationOutcomeFatal(response)
     ).code(400).type(ContentTypes.FHIR)
   } else if (response instanceof Boom) {
-    logger.info({
-      payload: logPayload ? request.payload.toString() : {},
+    // we DO log response here as we are sending back the same response
+    request.log("error", {
+      msg: "Boom",
+      payload: getPayload(request, logPayload),
       response
-    }, "Boom")
+    })
   } else {
     if (response.statusCode >= 400) {
-      logger.warn({
-        payload: logPayload ? request.payload.toString() : {},
+    // we DO log response here as we are sending back the same response
+      request.log("info", {
+        msg: "ErrorOrWarningResponse",
+        payload: getPayload(request, logPayload),
         response
-      }, "ErrorOrWarningResponse")
+      })
     }
   }
   return responseToolkit.continue
+}
+
+function getPayload(request: Hapi.Request<Hapi.ReqRefDefaults>, logPayload: boolean) {
+  if (!logPayload) {
+    return {}
+  }
+  if (request.payload) {
+    if (request.payload instanceof Buffer) {
+      return request.payload.toString()
+    }
+    return request.payload
+  }
+  return {}
 }
 
 export function switchContentTypeForSmokeTest(
@@ -84,7 +104,7 @@ export const rejectInvalidProdHeaders: Hapi.Lifecycle.Method = (
         listOfInvalidHeaders
       }`
       logger.warn({
-        payload: logPayload ? request.payload.toString() : {},
+        payload: getPayload(request, logPayload),
         errorMessage
       }, "invalid headers")
       const issue = validationErrors.invalidHeaderOperationOutcome(listOfInvalidHeaders)
