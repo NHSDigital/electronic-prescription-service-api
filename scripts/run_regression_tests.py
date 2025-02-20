a127cb8fbb789ec6535f7d98023addae7e4325a5
Anthony Brown <121869075+anthony-nhs@users.noreply.github.com>
2025-02-20 14:16:07 +0000

    run regression tests

--- scripts/run_regression_tests.py
+++ scripts/run_regression_tests.py
@@ -13,7 +13,7 @@ import time
 from requests.auth import HTTPBasicAuth
 
 # This should be set to a known good version of regression test repo
-REGRESSION_TESTS_REPO_TAG = "v2.11.13"
+REGRESSION_TESTS_REPO_TAG = "metadata_tests"
 
 GITHUB_API_URL = "https://api.github.com/repos/NHSDigital/electronic-prescription-service-api-regression-tests/actions"
 GITHUB_RUN_URL = "https://github.com/NHSDigital/electronic-prescription-service-api-regression-tests/actions/runs"
