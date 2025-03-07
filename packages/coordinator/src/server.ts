9f556e1f91794f49d6f24e211d1e23a1601bf484
Anthony Brown <121869075+anthony-nhs@users.noreply.github.com>
2025-03-07 17:13:20 +0000

    tweaks

--- packages/coordinator/src/server.ts
+++ packages/coordinator/src/server.ts
@@ -71,19 +71,18 @@ const configureLogging = async (server: Hapi.Server) => {
 
 export const init = async (): Promise<void> => {
   const server = createServer({}, 9000)
+  // need to set the keep alive timeout higher than the ALB idle timeout
+  server.listener.keepAliveTimeout = 65000
   await configureLogging(server)
   await server.start()
   server.log("info", `Server running on ${server.info.uri}`)
 
   // handle shutdown gracefully
-  // note - we use console.log here rather than server.log as the server will have died
-  process.on("SIGTERM", function () {
-    const stopTimeoutInSeconds = 10
-    console.log(`stopping server with a timeout of ${stopTimeoutInSeconds}`)
-
-    server.stop({timeout: stopTimeoutInSeconds * 1000})
-      .then(function () {
-        console.log("server stopped")
-      })
+  // note - we use console.log here rather than server.log as the server may not be available
+  process.on("SIGTERM", async () => {
+    const serverStopTimeout: number = 5
+    console.log(`Gracefully stopping server with a timeout of ${serverStopTimeout} seconds`)
+    await server.stop({timeout: serverStopTimeout * 1000})
+    console.log("Server stopped")
   })
 }
