e5a866dce8266de18d0e04cdcf8183fdb1a0f868
anthony-nhs <121869075+anthony-nhs@users.noreply.github.com>
2025-03-31 10:58:46 +0100

    Fix: [AEA-4443] - handle missing gp_reg (#3330)
    ## Summary
    
    - Routine Change
    
    ### Details
    
    - handle missing generalPractitioner in fhir message as though it was
    V81999
    - log responses on 4xx as well as payload

--- packages/coordinator/tests/services/translation/request/patient.spec.ts
+++ packages/coordinator/tests/services/translation/request/patient.spec.ts
@@ -4,6 +4,9 @@ import * as TestResources from "../../../resources/test-resources"
 import {getPatient} from "../../../../src/services/translation/common/getResourcesOfType"
 import {fhir, processingErrors as errors} from "@models"
 import {UNKNOWN_GP_ODS_CODE} from "../../../../src/services/translation/common"
+import pino from "pino"
+
+const logger = pino()
 
 describe("convertPatient", () => {
   let bundle: fhir.Bundle
@@ -16,14 +19,14 @@ describe("convertPatient", () => {
 
   test("Throws TooManyValuesUserFacingError when passed multiple copies of identifier", () => {
     fhirPatient.identifier.push(fhirPatient.identifier[0])
-    expect(() => convertPatient(bundle, fhirPatient)).toThrow(errors.TooManyValuesError)
+    expect(() => convertPatient(bundle, fhirPatient, logger)).toThrow(errors.TooManyValuesError)
   })
 
   test("ID gets put in correct field", () => {
     const idValue = "exampleId"
     fhirPatient.identifier[0].value = idValue
 
-    const actual = convertPatient(bundle, fhirPatient).id._attributes.extension
+    const actual = convertPatient(bundle, fhirPatient, logger).id._attributes.extension
 
     expect(actual).toBe(idValue)
   })
@@ -31,7 +34,7 @@ describe("convertPatient", () => {
   test("If there is a patient.telecom, it gets put in the right place", () => {
     fhirPatient.telecom = [{use: "home", value: "0123456789"}]
 
-    const actual = convertPatient(bundle, fhirPatient).telecom[0]._attributes
+    const actual = convertPatient(bundle, fhirPatient, logger).telecom[0]._attributes
 
     expect(actual).toEqual({use: "HP", value: "tel:0123456789"})
   })
@@ -39,7 +42,7 @@ describe("convertPatient", () => {
   test("If there isn't a patient.telecom, leave it off", () => {
     delete fhirPatient.telecom
 
-    const actual = convertPatient(bundle, fhirPatient).telecom
+    const actual = convertPatient(bundle, fhirPatient, logger).telecom
 
     expect(actual).toEqual(undefined)
   })
@@ -47,7 +50,7 @@ describe("convertPatient", () => {
   test("If the GP has ID 'V81999' make the Id have nullFlavor 'UNK'", () => {
     fhirPatient.generalPractitioner = createGpWithIdValue(UNKNOWN_GP_ODS_CODE)
 
-    const patientsubjectOf = convertPatient(bundle, fhirPatient).patientPerson.playedProviderPatient.subjectOf
+    const patientsubjectOf = convertPatient(bundle, fhirPatient, logger).patientPerson.playedProviderPatient.subjectOf
     const actual = patientsubjectOf.patientCareProvision.responsibleParty.healthCareProvider.id._attributes
 
     expect(actual).toEqual({nullFlavor: "UNK"})
@@ -57,12 +60,33 @@ describe("convertPatient", () => {
     const idValue = "testValue"
     fhirPatient.generalPractitioner = createGpWithIdValue(idValue)
 
-    const patientsubjectOf = convertPatient(bundle, fhirPatient).patientPerson.playedProviderPatient.subjectOf
+    const patientsubjectOf = convertPatient(bundle, fhirPatient, logger).patientPerson.playedProviderPatient.subjectOf
     const actual = patientsubjectOf.patientCareProvision.responsibleParty.healthCareProvider.id._attributes
 
     expect(actual).toEqual({extension: idValue, root: "1.2.826.0.1285.0.1.10"})
   })
 
+  test("If the GP code is missing, then the Id should have nullFlavor 'UNK'", () => {
+    const newBundle = clone(TestResources.specification[0].fhirMessageUnsigned)
+    const fhirPatient = getPatient(newBundle)
+    delete fhirPatient.generalPractitioner
+
+    const patientsubjectOf = convertPatient(bundle, fhirPatient, logger).patientPerson.playedProviderPatient.subjectOf
+    const actual = patientsubjectOf.patientCareProvision.responsibleParty.healthCareProvider.id._attributes
+
+    expect(actual).toEqual({nullFlavor: "UNK"})
+  })
+
+  test("If the GP code is empty, then the Id should have nullFlavor 'UNK'", () => {
+    const newBundle = clone(TestResources.specification[0].fhirMessageUnsigned)
+    const fhirPatient = getPatient(newBundle)
+    fhirPatient.generalPractitioner = []
+
+    const patientsubjectOf = convertPatient(bundle, fhirPatient, logger).patientPerson.playedProviderPatient.subjectOf
+    const actual = patientsubjectOf.patientCareProvision.responsibleParty.healthCareProvider.id._attributes
+
+    expect(actual).toEqual({nullFlavor: "UNK"})
+  })
   function createGpWithIdValue(idValue: string) {
     return [
       {
