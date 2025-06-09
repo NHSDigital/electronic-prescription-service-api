233de850ee524f864ce7fd0ea80a29c79bf9d001
Jonno <jonathan.welch1@nhs.net>
2025-06-09 11:00:20 +0100

    changed function to change null to undefined

--- packages/coordinator/src/services/translation/response/practitioner.ts
+++ packages/coordinator/src/services/translation/response/practitioner.ts
@@ -2,20 +2,42 @@ import {convertName, generateResourceId} from "./common"
 import {fhir, hl7V3} from "@models"
 import {createPractitionerOrRoleIdentifier} from "./identifiers"
 
+// export function createPractitioner(agentPerson: hl7V3.AgentPerson): fhir.Practitioner {
+//   return {
+//     resourceType: "Practitioner",
+//     id: generateResourceId(),
+//     identifier: createPractitionerIdentifier(agentPerson.agentPerson.id._attributes.extension),
+//     name: convertName(agentPerson.agentPerson.name)
+//   }
+// }
+
 export function createPractitioner(agentPerson: hl7V3.AgentPerson): fhir.Practitioner {
-  return {
+  const identifier = createPractitionerIdentifier(agentPerson.agentPerson.id._attributes.extension);
+  const practitioner: fhir.Practitioner = {
     resourceType: "Practitioner",
     id: generateResourceId(),
-    identifier: createPractitionerIdentifier(agentPerson.agentPerson.id._attributes.extension),
     name: convertName(agentPerson.agentPerson.name)
   }
+  if (identifier) {
+    practitioner.identifier = identifier;
+  }
+  return practitioner;
 }
 
-export function createPractitionerIdentifier(userId: string): Array<fhir.Identifier> {
+
+// export function createPractitionerIdentifier(userId: string): Array<fhir.Identifier> {
+//   const identifier = createPractitionerOrRoleIdentifier(userId)
+//   if (identifier.system !== "https://fhir.hl7.org.uk/Id/nhsbsa-spurious-code") {
+//     return [identifier]
+//   }
+
+//   return null
+// }
+
+export function createPractitionerIdentifier(userId: string): Array<fhir.Identifier> | undefined {
   const identifier = createPractitionerOrRoleIdentifier(userId)
   if (identifier.system !== "https://fhir.hl7.org.uk/Id/nhsbsa-spurious-code") {
     return [identifier]
   }
-
-  return null
+  return undefined
 }
