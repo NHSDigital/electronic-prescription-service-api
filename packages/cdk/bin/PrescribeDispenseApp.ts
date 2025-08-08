511e6f94fd9d6bdb5c2bc7316c33f45c426a2e3e
anthony-nhs <121869075+anthony-nhs@users.noreply.github.com>
2025-08-08 11:56:11 +0100

    Fix: [AEA-000] - add more cloudformation tags (#3726)
    ## Summary
    
    - Routine Change
    
    ### Details
    
    - add more cloudformation tags
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- packages/cdk/bin/PrescribeDispenseApp.ts
+++ packages/cdk/bin/PrescribeDispenseApp.ts
@@ -10,14 +10,18 @@ const serviceName = app.node.tryGetContext("serviceName")
 const version = app.node.tryGetContext("VERSION_NUMBER")
 const commit = app.node.tryGetContext("commitId")
 const accountId = app.node.tryGetContext("accountId")
+const cfnDriftDetectionGroup = app.node.tryGetContext("cfnDriftDetectionGroup")
 
 // add cdk-nag to everything
 Aspects.of(app).add(new AwsSolutionsChecks({verbose: true}))
 
+Tags.of(app).add("accountId", accountId)
 Tags.of(app).add("serviceName", serviceName)
 Tags.of(app).add("version", version)
 Tags.of(app).add("commit", commit)
 Tags.of(app).add("cdkApp", "prescribe-dispense")
+Tags.of(app).add("repo", "electronic-prescription-service-clinical-prescription-tracker")
+Tags.of(app).add("cfnDriftDetectionGroup", cfnDriftDetectionGroup)
 
 new PrescribeDispenseStack(app, "prescribe-dispense", {
   env: {
