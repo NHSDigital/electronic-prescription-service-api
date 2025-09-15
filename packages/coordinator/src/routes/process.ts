45f9723c501c9c345448424e1d6e7952d0688c1c
Tim Stephenson <231503406+tstephen-nhs@users.noreply.github.com>
2025-09-15 10:22:43 +0000

    chore: [AEA-5087] - debug logging

--- packages/coordinator/src/routes/process.ts
+++ packages/coordinator/src/routes/process.ts
@@ -28,6 +28,7 @@ export default [
     method: "POST" as RouteDefMethods,
     path: `${BASE_PATH}/$process-message`,
     handler: externalValidator(async (request: Hapi.Request, responseToolkit: Hapi.ResponseToolkit) => {
+      request.log("info", `TS Processing $process-message request to ${JSON.stringify(request.url)}`)
       const bundle = await getPayload(request) as fhir.Bundle
       request.log("audit", {incomingMessageHash: createHash(JSON.stringify(bundle), HashingAlgorithm.SHA256)})
 
@@ -48,10 +49,14 @@ export default [
         return responseToolkit.response(response).code(statusCode).type(ContentTypes.FHIR)
       }
 
-      request.logger.info("Building Spine request")
+      request.logger.info("TS Building Spine request")
       const spineRequest = await translator.convertBundleToSpineRequest(bundle, request.headers, request.logger)
+      request.logger.info(`TS Request built, sending to Spine: ${JSON.stringify(spineRequest)}`)
       const spineResponse = await spineClient.send(spineRequest, getAsid(request.headers), request.logger)
-      return await handleResponse(request, spineResponse, responseToolkit)
+      request.logger.info(`TS Received Spine response: ${JSON.stringify(spineResponse)}`)
+      const resp = await handleResponse(request, spineResponse, responseToolkit)
+      request.logger.info(`TS Returning response to consumer: ${JSON.stringify(resp)}`)
+      return resp
     })
   }
 ]
