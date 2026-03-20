ed41fca906692985b37155e59274f5e85d22d873
Kieran Wilkinson <244729020+kieran-wilkinson-4@users.noreply.github.com>
2026-03-20 15:16:57 +0000

    feat: Implement sonar

--- packages/fhir-schema-generation/jest.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-export default {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  testMatch: ['**/*.test.ts'],
-  moduleNameMapper: {
-    '^(\\.{1,2}/.*)\\.js$': '$1',
-  },
-};
