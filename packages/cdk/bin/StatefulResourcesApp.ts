c9e7e7c22481584d48a94ddbc56d1cdd6e4688b1
Phil Gee <originalphil@gmail.com>
2026-04-21 09:35:29 +0000

    AEA-6449 Consistent var naming through workflow and script.

--- packages/cdk/bin/StatefulResourcesApp.ts
+++ packages/cdk/bin/StatefulResourcesApp.ts
@@ -7,7 +7,7 @@ import {addCfnGuardMetadata} from "./utils/appUtils"
 
 const app = new cdk.App()
 
-const statefulResourcesServiceName = app.node.tryGetContext("statefulResourcesServiceName")
+const stackName = app.node.tryGetContext("statefulResourcesStackName")
 const version = app.node.tryGetContext("VERSION_NUMBER")
 const commit = app.node.tryGetContext("commitId")
 const accountId = app.node.tryGetContext("accountId")
@@ -17,7 +17,7 @@ const cfnDriftDetectionGroup = app.node.tryGetContext("cfnDriftDetectionGroup")
 Aspects.of(app).add(new AwsSolutionsChecks({verbose: true}))
 
 Tags.of(app).add("accountId", accountId)
-Tags.of(app).add("statefulResourcesServiceName", statefulResourcesServiceName)
+Tags.of(app).add("stackName", stackName)
 Tags.of(app).add("version", version)
 Tags.of(app).add("commit", commit)
 Tags.of(app).add("cdkApp", "prescriptions")
@@ -29,7 +29,7 @@ const StatefulResources = new StatefulResourcesStack(app, "stateful-resources", 
     region: "eu-west-2",
     account: accountId
   },
-  stackName: statefulResourcesServiceName
+  stackName: stackName
 })
 
 addCfnGuardMetadata(StatefulResources, "Custom::S3AutoDeleteObjectsCustomResourceProvider", "Handler")
