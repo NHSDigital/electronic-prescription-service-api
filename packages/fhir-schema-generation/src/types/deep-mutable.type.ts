6acc45c1a4ffbab356d6df0acf502a1d2641c201
Kieran Wilkinson <244729020+kieran-wilkinson-4@users.noreply.github.com>
2026-04-22 11:19:27 +0000

    feat: Add new tests for filtering

--- packages/fhir-schema-generation/src/types/deep-mutable.type.ts
+++ packages/fhir-schema-generation/src/types/deep-mutable.type.ts
@@ -1,3 +1,7 @@
-export type DeepMutable<T> = {
-  -readonly [P in keyof T]: T[P] extends object ? DeepMutable<T[P]> : T[P];
-};
+export type DeepMutable<T> =
+  // If it's an array, explicitly convert it to a mutable Array
+  T extends ReadonlyArray<infer U> ? Array<DeepMutable<U>> :
+  // If it's an object, map over its properties
+  T extends object ? { -readonly [P in keyof T]: DeepMutable<T[P]> } :
+  // Otherwise, leave it as is
+  T;
